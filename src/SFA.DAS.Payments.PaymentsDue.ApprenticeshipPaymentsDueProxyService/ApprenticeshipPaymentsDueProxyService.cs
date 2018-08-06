49c7b5952788019f76515b87871d5396cfd12421
Maxim Beliy <maximbel@gmail.com>
2018-08-06 17:24:21 +0100

    PV2-54: service bus listener proxy WIP

---
+++ src/SFA.DAS.Payments.PaymentsDue.ApprenticeshipPaymentsDueProxyService/ApprenticeshipPaymentsDueProxyService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace SFA.DAS.Payments.PaymentsDue.ApprenticeshipPaymentsDueProxyService
+{
+    internal sealed class ApprenticeshipPaymentsDueProxyService : StatelessService
+    {
+        private EndpointCommunicationListener _listener;
+
+        public ApprenticeshipPaymentsDueProxyService(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new List<ServiceInstanceListener>
+            {
+                new ServiceInstanceListener(context =>
+                    (_listener = new EndpointCommunicationListener()))
+            };
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _listener.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError($"Error running actor proxy service. Error: {ex.Message}. Ex: {ex}");
+                throw;
+            }
+        }
+    }
+}
