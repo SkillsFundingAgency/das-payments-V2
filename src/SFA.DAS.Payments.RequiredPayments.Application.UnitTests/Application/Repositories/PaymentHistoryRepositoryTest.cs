f4e990e75145b343b93e95da6b42213c08576757
abroodie <abroodie@outlook.com>
2018-08-23 23:38:29 +0100

    required payments application unit tests now AutoMoqCore rather than AutoMoq

--- src/SFA.DAS.Payments.RequiredPayments.Application.UnitTests/Application/Repositories/PaymentHistoryRepositoryTest.cs
+++ src/SFA.DAS.Payments.RequiredPayments.Application.UnitTests/Application/Repositories/PaymentHistoryRepositoryTest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
-using AutoMoq;
+using AutoMoqCore;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
