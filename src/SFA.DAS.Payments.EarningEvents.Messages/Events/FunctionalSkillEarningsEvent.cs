8879d0d128f02a87e6a3d3438a2c2c30b63587a0
abroodie <abroodie@outlook.com>
2018-08-29 12:27:14 +0100

    added comments to functional skills classes

--- src/SFA.DAS.Payments.EarningEvents.Messages/Events/FunctionalSkillEarningsEvent.cs
+++ src/SFA.DAS.Payments.EarningEvents.Messages/Events/FunctionalSkillEarningsEvent.cs
@@ -3,8 +3,20 @@ using SFA.DAS.Payments.Model.Core.Incentives;
 
 namespace SFA.DAS.Payments.EarningEvents.Messages.Events
 {
+    /// <summary>
+    /// Earning events for sub aims such as Maths & English
+    /// </summary>
+    /// <seealso cref="SFA.DAS.Payments.EarningEvents.Messages.Events.EarningEvent" />
     public class FunctionalSkillEarningsEvent : EarningEvent
     {
+        /// <summary>
+        /// Gets or sets the earnings.  Earnings can be 
+        /// </summary>
+        /// <value>
+        /// The earnings.
+        /// </value>
+        /// <seealso cref="SFA.DAS.Payments.Model.Core.Incentives.FunctionalSkillEarning" />
+        /// <seealso cref="SFA.DAS.Payments.Model.Core.Incentives.FunctionalSkillType" />
         public IReadOnlyCollection<FunctionalSkillEarning> Earnings { get; set; }
     }
 }
--- src/SFA.DAS.Payments.Model.Core/Incentives/FunctionalSkillEarning.cs
+++ src/SFA.DAS.Payments.Model.Core/Incentives/FunctionalSkillEarning.cs
@@ -2,8 +2,18 @@
 
 namespace SFA.DAS.Payments.Model.Core.Incentives
 {
+    /// <summary>
+    /// Earnings for Sub Aims such as Maths & English. 
+    /// </summary>
     public class FunctionalSkillEarning
     {
+        /// <summary>
+        /// The type of earning. Can be "On Programme Maths And English" (13) or "Balancing Maths And English" (14)
+        /// </summary>
+        /// <value>
+        /// Can be "On Programme Maths And English" (13) or "Balancing Maths And English" (14)
+        /// </value>
+        /// <seealso cref="SFA.DAS.Payments.Model.Core.Incentives.FunctionalSkillType" />
         public FunctionalSkillType Type { get; set; }
         public IReadOnlyCollection<EarningPeriod> Periods { get; set; }
     }
