3ad44601cd1104ffedda86f6b656d5148fd823ad
kola-samuel <samuel.dev.dba@gmail.com>
2018-09-24 09:37:59 +0100

    Use AutoFac for Provider Payment Service

--- src/SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService.Interfaces/IProviderPaymentsService.cs
+++ src/SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService.Interfaces/IProviderPaymentsService.cs
@@ -1,32 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.ServiceFabric.Actors;
+﻿using Microsoft.ServiceFabric.Actors;
 using Microsoft.ServiceFabric.Actors.Remoting.FabricTransport;
 using Microsoft.ServiceFabric.Services.Remoting;
+using System.Threading;
+using System.Threading.Tasks;
 
-[assembly: FabricTransportActorRemotingProvider(RemotingListener = RemotingListener.V2Listener, RemotingClient = RemotingClient.V2Client)]
+[assembly: FabricTransportActorRemotingProvider(RemotingListenerVersion = RemotingListenerVersion.V2_1, RemotingClientVersion = RemotingClientVersion.V2_1)]
 namespace SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService.Interfaces
 {
-    /// <summary>
-    /// This interface defines the methods exposed by an actor.
-    /// Clients use this interface to interact with the actor that implements it.
-    /// </summary>
     public interface IProviderPaymentsService : IActor
     {
-        /// <summary>
-        /// TODO: Replace with your own actor method.
-        /// </summary>
-        /// <returns></returns>
         Task<int> GetCountAsync(CancellationToken cancellationToken);
 
-        /// <summary>
-        /// TODO: Replace with your own actor method.
-        /// </summary>
-        /// <param name="count"></param>
-        /// <returns></returns>
         Task SetCountAsync(int count, CancellationToken cancellationToken);
     }
 }
--- src/SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService/Program.cs
+++ src/SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService/Program.cs
@@ -1,30 +1,20 @@
-﻿using System;
-using System.Diagnostics;
-using System.Fabric;
+﻿using SFA.DAS.Payments.ServiceFabric.Core.Infrastructure.Ioc;
+using System;
 using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.ServiceFabric.Actors.Runtime;
 
 namespace SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService
 {
     internal static class Program
     {
-        /// <summary>
-        /// This is the entry point of the service host process.
-        /// </summary>
         private static void Main()
         {
             try
             {
-                // This line registers an Actor Service to host your actor class with the Service Fabric runtime.
-                // The contents of your ServiceManifest.xml and ApplicationManifest.xml files
-                // are automatically populated when you build this project.
-                // For more information, see https://aka.ms/servicefabricactorsplatform
-
-                ActorRuntime.RegisterActorAsync<ProviderPaymentsService>(
-                   (context, actorType) => new ActorService(context, actorType)).GetAwaiter().GetResult();
-
-                Thread.Sleep(Timeout.Infinite);
+                var builder = ServiceFabricContainerFactory.CreateBuilderForActor<ProviderPaymentsService>();
+                using (builder.Build())
+                {
+                    Thread.Sleep(Timeout.Infinite);
+                }
             }
             catch (Exception e)
             {
@@ -32,5 +22,6 @@ namespace SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService
                 throw;
             }
         }
+
     }
 }
