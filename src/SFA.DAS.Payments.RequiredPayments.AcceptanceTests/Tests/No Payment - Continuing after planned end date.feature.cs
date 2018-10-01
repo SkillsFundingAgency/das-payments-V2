16fec80140086df119c793d9f7757298acc3ba62
davehowlett <dave_howlett@hotmail.com>
2018-10-01 08:12:38 +0100

    PV2-245 - removal of learner specific info from all required payments acceptance tests

--- src/SFA.DAS.Payments.RequiredPayments.AcceptanceTests/Tests/No Payment - Continuing after planned end date.feature.cs
+++ src/SFA.DAS.Payments.RequiredPayments.AcceptanceTests/Tests/No Payment - Continuing after planned end date.feature.cs
@@ -83,214 +83,136 @@ namespace SFA.DAS.Payments.RequiredPayments.AcceptanceTests.Tests
  testRunner.And("the SFA contribution percentage is 90%", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             TechTalk.SpecFlow.Table table1 = new TechTalk.SpecFlow.Table(new string[] {
-                        "LearnRefNumber",
-                        "Ukprn",
                         "PriceEpisodeIdentifier",
                         "Period",
-                        "ULN",
                         "TransactionType",
                         "Amount"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "1",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "2",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "3",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "4",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "5",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "6",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "7",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "8",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "9",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "10",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "11",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table1.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "12",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
 #line 9
  testRunner.And("the payments due component generates the following contract type 2 payments due:", ((string)(null)), table1, "And ");
 #line hidden
             TechTalk.SpecFlow.Table table2 = new TechTalk.SpecFlow.Table(new string[] {
-                        "LearnRefNumber",
-                        "Ukprn",
                         "PriceEpisodeIdentifier",
                         "Period",
-                        "ULN",
                         "TransactionType",
                         "Amount"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "1",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "2",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "3",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "4",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "5",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "6",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "7",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "8",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "9",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "10",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "11",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
             table2.AddRow(new string[] {
-                        "learnref1",
-                        "10000",
                         "p1",
                         "12",
-                        "10000",
                         "Learning (TT1)",
                         "600"});
 #line 24
