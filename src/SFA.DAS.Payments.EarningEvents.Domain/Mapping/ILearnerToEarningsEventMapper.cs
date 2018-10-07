33cf7eb8020194ff7cd0255c04a62e76bc0fc0e0
abroodie <abroodie@outlook.com>
2018-10-07 17:24:58 +0100

    Made a start on mapping code

---
+++ src/SFA.DAS.Payments.EarningEvents.Domain/Mapping/ILearnerToEarningsEventMapper.cs
@@ -0,0 +1,11 @@
+﻿using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
+using SFA.DAS.Payments.EarningEvents.Messages.Events;
+using SFA.DAS.Payments.Model.Core;
+
+namespace SFA.DAS.Payments.EarningEvents.Domain.Mapping
+{
+    public interface ILearnerToEarningsEventMapper
+    {
+        IEarningEvent Map(FM36Learner learner);
+    }
+}
