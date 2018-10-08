1873e8121db6a6ea7a84a52cfca45d8bb42480b7
Maxim Beliy <maximbel@gmail.com>
2018-10-08 11:03:05 +0100

    PV2-173: endpoint name changed as requested

--- src/SFA.DAS.Payments.RequiredPayments.AcceptanceTests/EndpointNames.cs
+++ src/SFA.DAS.Payments.RequiredPayments.AcceptanceTests/EndpointNames.cs
@@ -2,6 +2,6 @@
 {
     public class EndpointNames
     {
-        public static string RequiredPayments = "sfa-das-payments-requiredpayments-acceptancetests";
+        public static string RequiredPayments = "sfa-das-payments-requiredpayments";
     }
 }
