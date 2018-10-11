f4dc688553641ef97f3ee101441b37fa77cb1bb3
abroodie <abroodie@outlook.com>
2018-10-11 10:23:31 +0100

    removed redundant CreateEarningPeriods method

--- src/SFA.DAS.Payments.EarningEvents.Application/Mapping/OnProgrammeEarningValueResolver.cs
+++ src/SFA.DAS.Payments.EarningEvents.Application/Mapping/OnProgrammeEarningValueResolver.cs
@@ -11,23 +11,6 @@ namespace SFA.DAS.Payments.EarningEvents.Application.Mapping
 {
     public class OnProgrammeEarningValueResolver : IValueResolver<FM36Learner, ApprenticeshipContractTypeEarningsEvent, List<OnProgrammeEarning>>
     {
-        private List<EarningPeriod> CreateEarningPeriods(string priceEpisodeIdentifier, PriceEpisodePeriodisedValues values)
-        {
-            var result = new List<EarningPeriod>();
-            result.AddPeriodValue(values.Period1, 1, priceEpisodeIdentifier);
-            result.AddPeriodValue(values.Period2, 2, priceEpisodeIdentifier);
-            result.AddPeriodValue(values.Period3, 3, priceEpisodeIdentifier);
-            result.AddPeriodValue(values.Period4, 4, priceEpisodeIdentifier);
-            result.AddPeriodValue(values.Period5, 5, priceEpisodeIdentifier);
-            result.AddPeriodValue(values.Period6, 6, priceEpisodeIdentifier);
-            result.AddPeriodValue(values.Period7, 7, priceEpisodeIdentifier);
-            result.AddPeriodValue(values.Period8, 8, priceEpisodeIdentifier);
-            result.AddPeriodValue(values.Period9, 9, priceEpisodeIdentifier);
-            result.AddPeriodValue(values.Period10, 10, priceEpisodeIdentifier);
-            result.AddPeriodValue(values.Period11, 11, priceEpisodeIdentifier);
-            result.AddPeriodValue(values.Period12, 12, priceEpisodeIdentifier);
-            return result;
-        }
 
         private static string OnProgrammeEarningTypeToFM36AttributeName(OnProgrammeEarningType onProgrammeEarningType)
         {
