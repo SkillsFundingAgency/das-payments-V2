e6c80187d9cfcf6621791155cb707458365765d1
Maxim Beliy <maximbel@gmail.com>
2018-10-16 16:22:29 +0100

    PV2-230: payments due tests for PV2-197

---
+++ src/SFA.DAS.Payments.PaymentsDue.AcceptanceTests/Data/LearnerOnCourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFA.DAS.Payments.AcceptanceTests.Core.Data;
+
+namespace SFA.DAS.Payments.PaymentsDue.AcceptanceTests.Data
+{
+    public class LearnerOnCourse : Course
+    {
+        public string LearnerId { get; set; }
+    }
+}
--- src/SFA.DAS.Payments.PaymentsDue.AcceptanceTests/Steps/LearnerSteps.cs
+++ src/SFA.DAS.Payments.PaymentsDue.AcceptanceTests/Steps/LearnerSteps.cs
@@ -1,13 +1,30 @@
-﻿using TechTalk.SpecFlow;
+﻿using SFA.DAS.Payments.AcceptanceTests.Core;
+using TechTalk.SpecFlow;
 
 namespace SFA.DAS.Payments.PaymentsDue.AcceptanceTests.Steps
 {
     [Binding]
-    public class LearnerSteps
+    public class LearnerSteps : StepsBase
     {
+        public LearnerSteps(ScenarioContext scenarioContext) : base(scenarioContext)
+        {
+        }
+
         [Given(@"a learner is undertaking a training with a training provider")]
         public void GivenALearnerIsUndertakingATrainingWithATrainingProvider()
         {
+        }
+
+        [Given(@"following learners are undertaking training with a training provider")]
+        public void GivenFollowingLearnersAreUndertakingTrainingWithATrainingProvider(Table table)
+        {
+            TestSession.Learners.Clear();
+            foreach (var row in table.Rows)
+            {
+                var learner = TestSession.GenerateLearner();
+                learner.LearnRefNumber = TestSession.LearnRefNumberGenerator.Generate(learner.Ukprn, row["LearnerId"]);
+                TestSession.Learners.Add(learner);
+            }
         }
     }
 }
