e32da42e74caf4b3f20943a1a94bc5bacc671120
davehowlett <dave_howlett@hotmail.com>
2018-10-18 09:11:22 +0100

    PV2-171 - removing unused usings

--- src/SFA.DAS.Payments.EarningEvents.AcceptanceTests/Steps/BindingBootstrapper.cs
+++ src/SFA.DAS.Payments.EarningEvents.AcceptanceTests/Steps/BindingBootstrapper.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using Autofac;
-using ESFA.DC.JobContext.Interface;
-using ESFA.DC.Queueing;
-using ESFA.DC.Queueing.Interface;
-using ESFA.DC.Serialization.Interfaces;
+﻿using Autofac;
 using NServiceBus;
 using SFA.DAS.Payments.AcceptanceTests.Core;
-using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.EarningEvents.Messages.Events;
 using SFA.DAS.Payments.EarningEvents.Messages.Internal.Commands;
 using SFA.DAS.Payments.Messages.Core;
@@ -30,19 +24,5 @@ namespace SFA.DAS.Payments.EarningEvents.AcceptanceTests.Steps
             var routing = transportConfig.Routing();
             routing.RouteToEndpoint(typeof(ProcessLearnerCommand), EndpointNames.EarningEventsService);
         }
-
-        //[BeforeTestRun(Order = 25)]
-        //public static void AddTopicSubscription()
-        //{
-        //    Builder.Register(c =>
-        //    {
-        //        var topicSubscriptionConfig = new TopicConfiguration(TestConfiguration.ServiceBusConnectionString, TestConfiguration.TopicName, TestConfiguration.SubscriptionName, 1, maximumCallbackTimeSpan: TimeSpan.FromMinutes(40));
-
-        //        return new TopicSubscriptionSevice<JobContextDto>(
-        //            topicSubscriptionConfig,
-        //            c.Resolve<IJsonSerializationService>(),
-        //            c.Resolve<IPaymentLogger>());
-        //    }).As<ITopicSubscriptionService<JobContextDto>>();
-        //}
     }
 }
