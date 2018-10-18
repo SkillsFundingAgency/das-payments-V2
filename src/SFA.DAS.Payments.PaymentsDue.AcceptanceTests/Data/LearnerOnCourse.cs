623870887b79191e683e0a27ead2c67d01016019
Maxim Beliy <maximbel@gmail.com>
2018-10-18 16:19:47 +0100

    code cleaned up

--- src/SFA.DAS.Payments.PaymentsDue.AcceptanceTests/Data/LearnerOnCourse.cs
+++ src/SFA.DAS.Payments.PaymentsDue.AcceptanceTests/Data/LearnerOnCourse.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SFA.DAS.Payments.AcceptanceTests.Core.Data;
+﻿using SFA.DAS.Payments.AcceptanceTests.Core.Data;
 
 namespace SFA.DAS.Payments.PaymentsDue.AcceptanceTests.Data
 {
