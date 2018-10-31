0f0978145a9bf69c59735aceb9713ab986064169
abroodie <abroodie@outlook.com>
2018-10-31 10:27:33 +0000

    Added scripts to manage apps in local cluster

---
+++ src/Scripts/ServiceFabric/Remove-PaymentsApplications.ps1
@@ -0,0 +1,17 @@
+﻿# Variables
+$endpoint = 'localhost:19000'
+
+#Earning Events
+.\Remove-ServiceFabricApplicationAndType.ps1 -applicationName SFA.DAS.Payments.EarningEvents.ServiceFabric
+
+#Payments Due
+.\Remove-ServiceFabricApplicationAndType.ps1 -applicationName SFA.DAS.Payments.PaymentsDue.ServiceFabric
+
+#Required Payments
+.\Remove-ServiceFabricApplicationAndType.ps1 -applicationName SFA.DAS.Payments.RequiredPayments.ServiceFabric
+
+#Funding Source
+.\Remove-ServiceFabricApplicationAndType.ps1 -applicationName SFA.DAS.Payments.FundingSource.ServiceFabric
+
+#Provider Payments
+.\Remove-ServiceFabricApplicationAndType.ps1 -applicationName SFA.DAS.Payments.ProviderPayments.ServiceFabric
---
+++ src/Scripts/ServiceFabric/Remove-ServiceFabricApplicationAndType.ps1
@@ -0,0 +1,42 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$False)]
+    [string]$endpoint = 'localhost:19000',
+
+    [Parameter(Mandatory=$True)]
+    [string]$applicationName,
+
+    [Parameter(Mandatory=$False)]
+    [string]$applicationTypeVersion = '1.0.0'
+
+)
+
+if (-not $connection){
+    write-verbose "Connection to local service fabric cluster: $endpoint"
+    Connect-ServiceFabricCluster -ConnectionEndpoint $endpoint
+}
+    
+
+$uri = [System.Uri]"fabric:/$applicationName"
+$applicationUri = $uri.ToString()
+$applicationTypeName = $applicationName+"Type"
+write-verbose "Application Name: $applicationName"
+write-verbose "Application Type Name: $applicationTypeName"
+write-verbose "Application uri: $applicationUri"
+
+$sfApp = Get-ServiceFabricApplication -ApplicationName $uri
+if ($sfApp) {
+    # Remove an application instance
+    Remove-ServiceFabricApplication -ApplicationName $uri -Force
+}
+else {
+    write-host "Application not found."
+}
+
+if ((Get-ServiceFabricApplication -ApplicationTypeName $applicationTypeName)) {
+    # Unregister the application type
+    Unregister-ServiceFabricApplicationType -ApplicationTypeName $applicationTypeName  -ApplicationTypeVersion $applicationTypeVersion -Force
+}
+else {
+    write-host "Application type not found."
+}
