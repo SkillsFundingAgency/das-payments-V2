bbc1b9a065e5333a088e7f94fc02ece8d705cad3
abroodie <abroodie@outlook.com>
2018-11-09 12:15:15 +0000

    removed redundant test methods

--- src/SFA.DAS.Payments.AcceptanceTests.Core/Extensions.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.Core/Extensions.cs
@@ -53,35 +53,7 @@ namespace SFA.DAS.Payments.AcceptanceTests.Core
 
         public static int ToMonthNumber(this string month)
         {
-            switch (month.ToLower().Substring(0, 3))
-            {
-                case "jan":
-                    return 1;
-                case "feb":
-                    return 2;
-                case "mar":
-                    return 3;
-                case "apr":
-                    return 4;
-                case "may":
-                    return 5;
-                case "jun":
-                    return 6;
-                case "jul":
-                    return 7;
-                case "aug":
-                    return 8;
-                case "sep":
-                    return 9;
-                case "oct":
-                    return 10;
-                case "nov":
-                    return 11;
-                case "dec":
-                    return 12;
-                default:
-                    throw new InvalidOperationException($"Invalid month: {month}");
-            }
+            return DateTime.ParseExact(month.ToLower().Substring(0, 3), "MMM", CultureInfo.CurrentCulture).Month;
         }
 
         public static int ToMonthPeriod(this string month)
@@ -115,18 +87,6 @@ namespace SFA.DAS.Payments.AcceptanceTests.Core
                 default:
                     throw new InvalidOperationException($"Invalid month: {month}");
             }
-        }
-
-        public static DateTime GetDateFromPeriod(string periodText)
-        {
-            var today = DateTime.Today;
-            var period = byte.Parse(periodText.ToLower().Replace("r", ""));
-            var month = today.Month;
-            if (month >= 8 && period >= 6)
-                return new DateTime(today.Year + 1, month, 1);
-            if (month <= 7 && period <= 5)
-                return new DateTime(today.Year - 1, month, 1);
-            return new DateTime(today.Year, month, 1);
         }
 
         public static int ToMonth(this byte period)
