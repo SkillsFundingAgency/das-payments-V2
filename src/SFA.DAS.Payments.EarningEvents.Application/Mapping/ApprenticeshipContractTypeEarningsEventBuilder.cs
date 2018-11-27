7945088c1ec824fda9fef7ef828ba97734d6b163
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-11-27 15:33:39 +0000

    Moved IntermediateLearningAim to its own file

--- src/SFA.DAS.Payments.EarningEvents.Application/Mapping/ApprenticeshipContractTypeEarningsEventBuilder.cs
+++ src/SFA.DAS.Payments.EarningEvents.Application/Mapping/ApprenticeshipContractTypeEarningsEventBuilder.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
-using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
 using SFA.DAS.Payments.EarningEvents.Domain.Mapping;
 using SFA.DAS.Payments.EarningEvents.Messages.Events;
 using SFA.DAS.Payments.EarningEvents.Messages.Internal.Commands;
@@ -57,51 +56,5 @@ namespace SFA.DAS.Payments.EarningEvents.Application.Mapping
 
             return results;
         }
-    }
-
-    public class IntermediateLearningAim
-    {
-        public IntermediateLearningAim(
-            ProcessLearnerCommand command, IEnumerable<PriceEpisode> priceEpisodes, LearningDelivery aim)
-        {
-            Aim = aim;
-            PriceEpisodes.AddRange(priceEpisodes);
-            Learner = command.Learner;
-            Ukprn = command.Ukprn;
-            CollectionYear = command.CollectionYear;
-            CollectionPeriod = command.CollectionPeriod;
-            IlrSubmissionDateTime = command.IlrSubmissionDateTime;
-            JobId = command.JobId;
-        }
-
-        protected IntermediateLearningAim(FM36Learner learner, LearningDelivery aim,
-            IEnumerable<PriceEpisode> priceEpisodes, long ukprn, string collectionYear,
-            int collectionPeriod, DateTime ilrSubmissionDateTime, long jobId)
-        {
-            Aim = aim;
-            Learner = learner;
-            PriceEpisodes.AddRange(priceEpisodes);
-            Ukprn = ukprn;
-            CollectionYear = collectionYear;
-            CollectionPeriod = collectionPeriod;
-            IlrSubmissionDateTime = ilrSubmissionDateTime;
-            JobId = jobId;
-        }
-
-        public IntermediateLearningAim CopyReplacingPriceEpisodes(IEnumerable<PriceEpisode> priceEpisodes)
-        {
-            var copy = new IntermediateLearningAim(Learner, Aim, priceEpisodes, Ukprn, 
-                CollectionYear, CollectionPeriod, IlrSubmissionDateTime, JobId);
-            return copy;
-        }
-
-        public LearningDelivery Aim { get; protected set; }
-        public List<PriceEpisode> PriceEpisodes { get; protected set; } = new List<PriceEpisode>();
-        public FM36Learner Learner { get; protected set; }
-        public long Ukprn { get; protected set; }
-        public string CollectionYear { get; protected set; }
-        public int CollectionPeriod { get; protected set; }
-        public DateTime IlrSubmissionDateTime { get; protected set; }
-        public long JobId { get; set; }
     }
 }
