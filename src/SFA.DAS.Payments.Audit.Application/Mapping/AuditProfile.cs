7b49ee3f961c843e6bbe527f94dea13ce0207f3d
abroodie <abroodie@outlook.com>
2018-11-29 22:58:47 +0000

    Added infrastructure for mapping from funding source event to funding source payment model

---
+++ src/SFA.DAS.Payments.Audit.Application/Mapping/AuditProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SFA.DAS.Payments.Audit.Model;
+using SFA.DAS.Payments.Messages.Core.Events;
+
+namespace SFA.DAS.Payments.Audit.Application.Mapping
+{
+    public class AuditProfile: Profile
+    {
+        public AuditProfile()
+        {
+            CreateMap<IPeriodisedPaymentEvent,PeriodisedModel>()
+        }
+    }
+}
---
+++ src/SFA.DAS.Payments.Audit.Model/FundingSourceEventModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SFA.DAS.Payments.Model.Core.Entities;
+
+namespace SFA.DAS.Payments.Audit.Model
+{
+    public class FundingSourceEventModel : PeriodisedModel
+    {
+        public Guid FundingSourceEventId { get; set; }
+        public Guid RequiredPaymentEventId { get; set; }
+        public FundingSourceType FundingSource { get; set; }
+    }
+}
