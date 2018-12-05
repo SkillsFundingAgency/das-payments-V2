4b6d4f7131cdd90f33a7f83f5d862833d43a5ffc
davehowlett <dave_howlett@hotmail.com>
2018-12-05 10:45:49 +0000

    PV2-406 - refactoring following review

--- src/SFA.DAS.Payments.AcceptanceTests.Core/StepsBase.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.Core/StepsBase.cs
@@ -71,27 +71,6 @@ namespace SFA.DAS.Payments.AcceptanceTests.Core
             Assert.Fail($"{failText}  Time: {DateTime.Now:G}.  Ukprn: {TestSession.Ukprn}. Job Id: {TestSession.JobId}");
         }
 
-        protected async Task WaitForIt(Func<Tuple<bool, string>> lookForIt, string failText)
-        {
-            var endTime = DateTime.Now.Add(Config.TimeToWait);
-            var reason = string.Empty;
-            var lastRun = false;
-
-            while (DateTime.Now < endTime || lastRun)
-            {
-                bool pass;
-                (pass, reason) = lookForIt();
-                if (pass)
-                {
-                    if (lastRun) return;
-                    lastRun = true;
-                }
-                await Task.Delay(Config.TimeToPause);
-            }
-
-            Assert.Fail($"{failText} - {reason}  Time: {DateTime.Now:G}.  Ukprn: {TestSession.Ukprn}. Job Id: {TestSession.JobId}");
-        }
-
         protected async Task WaitForIt(Func<(bool pass, string reason)> lookForIt, string failText)
         {
             var endTime = DateTime.Now.Add(Config.TimeToWait);
