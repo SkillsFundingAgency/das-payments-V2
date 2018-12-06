b66aeade2cfc7e1b5d1c179724aca1b41a752509
abroodie <abroodie@outlook.com>
2018-12-06 23:55:12 +0000

    Fixed earning event service

--- src/SFA.DAS.Payments.Audit.EarningEventsService/EarningEventsService.cs
+++ src/SFA.DAS.Payments.Audit.EarningEventsService/EarningEventsService.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Fabric;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Fabric;
 using Autofac;
 using Microsoft.ServiceFabric.Actors.Runtime;
-using Microsoft.ServiceFabric.Data.Collections;
-using Microsoft.ServiceFabric.Services.Communication.Runtime;
-using Microsoft.ServiceFabric.Services.Runtime;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
-using SFA.DAS.Payments.ServiceFabric.Core;
+using SFA.DAS.Payments.Audit.Application.PaymentsEventProcessing;
+using SFA.DAS.Payments.Audit.Application.ServiceFabric.Infrastructure;
+using SFA.DAS.Payments.Audit.Model;
 
 namespace SFA.DAS.Payments.Audit.EarningEventsService
 {
@@ -18,9 +12,9 @@ namespace SFA.DAS.Payments.Audit.EarningEventsService
     /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
     [StatePersistence(StatePersistence.Persisted)]
-    public class EarningEventsService : AuditStatefulService<PaymentsDueEventModel>
+    public class EarningEventsService : AuditStatefulService<EarningEventModel>
     {
-        public EarningEventsService(StatefulServiceContext context, IPaymentLogger logger, ILifetimeScope lifetimeScope, IPaymentsEventModelBatchService<EarningEventsEventModel> batchService) : base(context, logger, lifetimeScope, batchService)
+        public EarningEventsService(StatefulServiceContext context, IPaymentLogger logger, ILifetimeScope lifetimeScope, IPaymentsEventModelBatchService<EarningEventModel> batchService) : base(context, logger, lifetimeScope, batchService)
         {
         }
     }
