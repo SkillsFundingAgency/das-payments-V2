c96dc16e20775e1e717f7ce067579f0068932d12
abroodie <abroodie@outlook.com>
2018-12-10 13:52:10 +0000

    added new RecordNewJobCommand

---
+++ src/SFA.DAS.Payments.Monitoring.JobStatus.Messages/Commands/RecordNewJobCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SFA.DAS.Payments.Monitoring.JobStatus.Messages.Commands
+{
+    public class RecordNewJobCommand
+    {
+
+    }
+
+    public class RecordProviderEarningsReceivedCommand : RecordNewJobCommand
+    {
+        public long JobId { get; set; }
+        public DateTime IlrSubmissionTime { get; set; }
+        public long Ukprn { get; set; }
+        public string CollectionYear { get; set; }
+        public byte CollectionPeriod { get; set; }
+    }
+}
