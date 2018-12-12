d09585427cb9cf0da5902e1dfb2c6270afb1afce
abroodie <abroodie@outlook.com>
2018-12-12 13:17:59 +0000

    added app insights key to services

--- src/SFA.DAS.Payments.ProviderPayments.Application/Services/ProviderPaymentsService.cs
+++ src/SFA.DAS.Payments.ProviderPayments.Application/Services/ProviderPaymentsService.cs
@@ -8,6 +8,7 @@ using System;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
+using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
 using SFA.DAS.Payments.Audit.Application.PaymentsEventModelCache;
 using SFA.DAS.Payments.ProviderPayments.Model;
 
