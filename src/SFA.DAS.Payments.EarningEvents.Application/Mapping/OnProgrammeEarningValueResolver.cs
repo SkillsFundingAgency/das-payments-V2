6359331d63d7ee9796b4f9906b041066cb48beb4
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-01-07 11:53:37 +0000

    Moved SfaContributionPercentageExtensions to a separate file

--- src/SFA.DAS.Payments.EarningEvents.Application/Mapping/OnProgrammeEarningValueResolver.cs
+++ src/SFA.DAS.Payments.EarningEvents.Application/Mapping/OnProgrammeEarningValueResolver.cs
@@ -1,44 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using AutoMapper;
-using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
-using FastMember;
 using Microsoft.EntityFrameworkCore.Internal;
 using SFA.DAS.Payments.EarningEvents.Messages.Events;
 using SFA.DAS.Payments.Model.Core;
 using SFA.DAS.Payments.Model.Core.OnProgramme;
-using PriceEpisode = ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output.PriceEpisode;
 
 namespace SFA.DAS.Payments.EarningEvents.Application.Mapping
 {
-    public static class SfaContributionPercentageExtensions
-    {
-        private static readonly TypeAccessor PeriodAccessor = TypeAccessor.Create(typeof(PriceEpisodePeriodisedValues));
-
-        public static decimal? CalculateSfaContributionPercentage(this List<PriceEpisode> source, int period,
-            string priceEpisodeIdentifier)
-        {
-            if (priceEpisodeIdentifier == null)
-            {
-                return null;
-            }
-            var priceEpisode = source.Single(x => x.PriceEpisodeIdentifier == priceEpisodeIdentifier);
-            var value = priceEpisode.PriceEpisodePeriodisedValues
-                .Where(x => x.AttributeName == "PriceEpisodeSFAContribPct")
-                .Select(p => (decimal?)PeriodAccessor[p, "Period" + period])
-                .SingleOrDefault();
-
-            if (value == null)
-            {
-                value = priceEpisode.PriceEpisodeValues.PriceEpisodeSFAContribPct;
-            }
-
-            return value;
-        }
-    }
-
     public class OnProgrammeEarningValueResolver : IValueResolver<IntermediateLearningAim, ApprenticeshipContractTypeEarningsEvent, ReadOnlyCollection<OnProgrammeEarning>>
     {
         // ReSharper disable once InconsistentNaming
