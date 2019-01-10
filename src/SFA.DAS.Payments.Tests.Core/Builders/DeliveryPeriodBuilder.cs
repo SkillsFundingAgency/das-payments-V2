aabb083f6a0074841e8d586b267b34de93e8aa6d
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-01-10 09:58:02 +0000

    Removed CalendarPeriodBuilder

--- src/SFA.DAS.Payments.Tests.Core/Builders/DeliveryPeriodBuilder.cs
+++ src/SFA.DAS.Payments.Tests.Core/Builders/DeliveryPeriodBuilder.cs
@@ -3,8 +3,12 @@ using SFA.DAS.Payments.Model.Core;
 
 namespace SFA.DAS.Payments.Tests.Core.Builders
 {
-    public class DeliveryPeriodBuilder : CalendarPeriodBuilder<DeliveryPeriodBuilder, DeliveryPeriod>
+    public class DeliveryPeriodBuilder 
     {
+        protected DateTime Date { get; set; } = DateTime.MinValue;
+        protected int Year { get; set; } = -1;
+        protected int Month { get; set; } = -1;
+
         public DeliveryPeriod BuildLastOnProgPeriod()
         {
             var lastDayOfMonth = DateTime.DaysInMonth(Date.Year, Date.Month);
@@ -31,7 +35,7 @@ namespace SFA.DAS.Payments.Tests.Core.Builders
                 {
                     throw new ArgumentException("Please include at least a month and a year");
                 }
-                Date = new DateTime(Year, Month, Day);
+                Date = new DateTime(Year, Month, 1);
             }
 
             var instance = new DeliveryPeriod();
@@ -54,6 +58,72 @@ namespace SFA.DAS.Payments.Tests.Core.Builders
             var builtInstance = BuildInstance();
             builtInstance.Identifier = $"{builtInstance.Year}-{builtInstance.Month:D2}";
             return builtInstance;
+        }
+
+        protected DeliveryPeriodBuilderType BuilderType { get; set; }
+        protected DatePortionsIncluded IncludedPortions { get; set; }
+        
+        public virtual DeliveryPeriodBuilder WithSpecDate(string date)
+        {
+            return WithDate(date.ToDate());
+        }
+
+        public DeliveryPeriodBuilder WithDate(DateTime date)
+        {
+            if (BuilderType == DeliveryPeriodBuilderType.WithDateComponents)
+            {
+                throw new ArgumentException("Please either use a date or date components but not both");
+            }
+
+            BuilderType = DeliveryPeriodBuilderType.WithDate;
+
+            Date = date;
+            Year = date.Year;
+            Month = date.Month;
+            return this;
+        }
+
+        public DeliveryPeriodBuilder WithYear(int year)
+        {
+            if (BuilderType == DeliveryPeriodBuilderType.WithDate)
+            {
+                throw new ArgumentException("Please either use a date or date components but not both");
+            }
+
+            BuilderType = DeliveryPeriodBuilderType.WithDateComponents;
+            IncludedPortions |= DatePortionsIncluded.Year;
+            Year = year;
+
+            return this;
+        }
+
+        public DeliveryPeriodBuilder WithMonth(int month)
+        {
+            if (BuilderType == DeliveryPeriodBuilderType.WithDate)
+            {
+                throw new ArgumentException("Please either use a date or date components but not both");
+            }
+
+            BuilderType = DeliveryPeriodBuilderType.WithDateComponents;
+            IncludedPortions |= DatePortionsIncluded.Month;
+            Month = month;
+
+            return this;
+        }
+
+        protected enum DeliveryPeriodBuilderType
+        {
+            None,
+            WithDate,
+            WithDateComponents,
+        }
+
+        [Flags]
+        protected enum DatePortionsIncluded
+        {
+            None,
+            Year,
+            Month,
         }
     }
 }
