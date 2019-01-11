690a4391a4392c0a0cd18a2a28f29b847ed43cd0
ESFA-Pawan <38136494+ESFA-Pawan@users.noreply.github.com>
2019-01-11 17:19:49 +0000

    Release/release 490 (#129)
    * TTL setting and queue configuration
    
    * update existing queues

--- src/SFA.DAS.Payments.AcceptanceTests.Core/Infrastructure/TestsConfiguration.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.Core/Infrastructure/TestsConfiguration.cs
@@ -12,6 +12,7 @@ namespace SFA.DAS.Payments.AcceptanceTests.Core.Infrastructure
         public TimeSpan TimeToWait => TimeSpan.Parse(ConfigurationManager.AppSettings["TimeToWait"] ?? "00:00:30");
         public TimeSpan TimeToWaitForUnexpected => TimeSpan.Parse(ConfigurationManager.AppSettings["TimeToWaitForUnexpected"] ?? "00:00:30");
         public TimeSpan TimeToPause => TimeSpan.Parse(ConfigurationManager.AppSettings["TimeToPause"] ?? "00:00:05");
+        public TimeSpan DefaultMessageTimeToLive => TimeSpan.Parse(ConfigurationManager.AppSettings["DefaultMessageTimeToLive"] ?? "00:20:00");
         public string GetAppSetting(string keyName) => ConfigurationManager.AppSettings[keyName] ?? throw new InvalidOperationException($"{keyName} not found in app settings.");
         public string GetConnectionString(string name) => ConfigurationManager.ConnectionStrings[name].ConnectionString ?? throw new InvalidOperationException($"{name} not found in connection strings.");
     }
