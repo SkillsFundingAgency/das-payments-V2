9a5a5bf8e4c115feec9f6e3308834329e0bdb5af
abroodie <abroodie@outlook.com>
2019-01-13 22:27:07 +0000

    added telemetry operation to required payments actor

--- src/SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService/ProviderPaymentsService.cs
+++ src/SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService/ProviderPaymentsService.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.ServiceFabric.Actors.Runtime;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
-using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
 using System.Fabric;
 using Autofac;
 using SFA.DAS.Payments.Audit.Application.PaymentsEventProcessing;
@@ -17,56 +16,4 @@ namespace SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService
         {
         }
     }
-
-
-    //[StatePersistence(StatePersistence.Volatile)]
-    //public class ProviderPaymentsService : Actor, Interfaces.IProviderPaymentsService
-    //{
-    //    private readonly ActorId actorId;
-    //    private readonly IProviderPaymentsRepository providerPaymentsRepository;
-    //    private readonly IValidateIlrSubmission validateIlrSubmission;
-    //    private readonly IPaymentLogger paymentLogger;
-    //    private IProviderPaymentsService paymentsService;
-    //    private IHandleIlrSubmissionService handleIlrSubmissionService;
-    //    private IMonthEndService monthEndService;
-    //    private long ukprn;
-
-    //    public ProviderPaymentsService(ActorService actorService, ActorId actorId,
-    //        IProviderPaymentsRepository providerPaymentsRepository,
-    //        IValidateIlrSubmission validateIlrSubmission,
-    //        IPaymentLogger paymentLogger)
-    //        : base(actorService, actorId)
-    //    {
-    //        this.actorId = actorId;
-    //        this.providerPaymentsRepository = providerPaymentsRepository ?? throw new ArgumentNullException(nameof(providerPaymentsRepository));
-    //        this.validateIlrSubmission = validateIlrSubmission ?? throw new ArgumentNullException(nameof(validateIlrSubmission));
-    //        this.paymentLogger = paymentLogger ?? throw new ArgumentNullException(nameof(paymentLogger));
-    //    }
-
-    //    public async Task HandlePayment(PaymentModel message, CancellationToken cancellationToken)
-    //    {
-    //        await paymentsService.ProcessPayment(message, cancellationToken);
-    //    }
-
-    //    public async Task<List<PaymentModel>> GetMonthEndPayments(short collectionYear, byte collectionPeriod, CancellationToken cancellationToken)
-    //    {
-    //        return await monthEndService.GetMonthEndPayments(collectionYear, collectionPeriod, ukprn, cancellationToken);
-    //    }
-
-    //    public async Task HandleIlrSubMission(IlrSubmittedEvent message, CancellationToken cancellationToken)
-    //    {
-    //        await handleIlrSubmissionService.Handle(message, cancellationToken);
-    //    }
-
-    //    protected override async Task OnActivateAsync()
-    //    {
-    //        if (!long.TryParse(actorId.GetStringId(), out ukprn)) throw new Exception("Unable to cast Actor Id to Ukprn");
-
-    //        var reliableCollectionCache = new ReliableCollectionCache<IlrSubmittedEvent>(StateManager);
-    //        paymentsService = new Application.Services.ProviderPaymentsService(providerPaymentsRepository, reliableCollectionCache, validateIlrSubmission, paymentLogger);
-    //        handleIlrSubmissionService = new HandleIlrSubmissionService(providerPaymentsRepository, reliableCollectionCache, validateIlrSubmission, paymentLogger);
-    //        monthEndService = new MonthEndService(providerPaymentsRepository);
-    //        await base.OnActivateAsync().ConfigureAwait(false);
-    //    }
-    //}
 }
