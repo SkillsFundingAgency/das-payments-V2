1de0551888354693c60a65e0c83d5c5a3d5c2229
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-01-16 15:42:38 +0000

    Added Datalocks.ServiceFabric app Datalocks.Proxy Service Datalocks.Actor Service
    Deploys locally with errors in Proxy service

---
+++ src/SFA.DAS.Payments.Datalocks.DatalocksProxyService/DatalocksProxyService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Fabric;
+using Autofac;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.ServiceFabric.Core;
+
+namespace SFA.DAS.Payments.Datalocks.DatalocksProxyService
+{
+    /// <summary>
+    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    public class DatalocksProxyService : StatelessService
+    {
+        private IEndpointCommunicationListener listener;
+        private readonly ILifetimeScope lifetimeScope;
+        private readonly IPaymentLogger paymentLogger;
+
+        public DatalocksProxyService(StatelessServiceContext context, ILifetimeScope lifetimeScope, IPaymentLogger paymentLogger) : base(context)
+        {
+            this.lifetimeScope = lifetimeScope;
+            this.paymentLogger = paymentLogger;
+        }
+
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            paymentLogger.LogInfo("Creating Service Instance Listeners For DatalocksProxyService");
+
+            return new List<ServiceInstanceListener>
+            {
+                new ServiceInstanceListener(context =>listener = lifetimeScope.Resolve<IEndpointCommunicationListener>())
+            };
+        }
+    }
+}
---
+++ src/SFA.DAS.Payments.Datalocks.DatalocksProxyService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using Castle.Core.Internal;
+using SFA.DAS.Payments.ServiceFabric.Core.Infrastructure.Ioc;
+
+[assembly: InternalsVisibleTo(InternalsVisible.ToDynamicProxyGenAssembly2)]
+
+namespace SFA.DAS.Payments.Datalocks.DatalocksProxyService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        private static void Main()
+        {
+            try
+            {
+                using (ServiceFabricContainerFactory.CreateContainerForStatelessService<DatalocksProxyService>())
+                {
+                    Thread.Sleep(Timeout.Infinite);
+                }
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
+        }
+    }
+}
