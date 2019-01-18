bffe93efda11413781c7cbfb817fdf9415915f75
Maxim Beliy <maximbel@gmail.com>
2019-01-18 16:03:31 +0000

    PV2-559: handler for payable earning events

--- src/SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService/Handlers/ApprenticeshipContractType2EarningEventHandler.cs
+++ src/SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService/Handlers/ApprenticeshipContractType2EarningEventHandler.cs
@@ -1,14 +1,14 @@
 ﻿using ESFA.DC.Logging.Interfaces;
 using Microsoft.ServiceFabric.Actors.Client;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
-using SFA.DAS.Payments.RequiredPayments.Domain;
 using SFA.DAS.Payments.EarningEvents.Messages.Events;
+using SFA.DAS.Payments.RequiredPayments.Domain;
 
 namespace SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService.Handlers
 {
-    public class FunctionalSkillEarningsEventHandler : EarningEventHandlerBase<FunctionalSkillEarningsEvent>
+    public class ApprenticeshipContractType2EarningEventHandler : EarningEventHandlerBase<ApprenticeshipContractType2EarningEvent>
     {
-        public FunctionalSkillEarningsEventHandler(IApprenticeshipKeyService apprenticeshipKeyService, IActorProxyFactory proxyFactory, IPaymentLogger paymentLogger, IExecutionContext executionContext) 
+        public ApprenticeshipContractType2EarningEventHandler(IApprenticeshipKeyService apprenticeshipKeyService, IActorProxyFactory proxyFactory, IPaymentLogger paymentLogger, IExecutionContext executionContext) 
             : base(apprenticeshipKeyService, proxyFactory, paymentLogger, executionContext)
         {
         }
--- src/SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService/Handlers/PayableEarningEventHandler.cs
+++ src/SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService/Handlers/PayableEarningEventHandler.cs
@@ -1,14 +1,14 @@
 ﻿using ESFA.DC.Logging.Interfaces;
 using Microsoft.ServiceFabric.Actors.Client;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.DataLocks.Messages;
 using SFA.DAS.Payments.RequiredPayments.Domain;
-using SFA.DAS.Payments.EarningEvents.Messages.Events;
 
 namespace SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService.Handlers
 {
-    public class FunctionalSkillEarningsEventHandler : EarningEventHandlerBase<FunctionalSkillEarningsEvent>
+    public class PayableEarningEventHandler : EarningEventHandlerBase<PayableEarningEvent>
     {
-        public FunctionalSkillEarningsEventHandler(IApprenticeshipKeyService apprenticeshipKeyService, IActorProxyFactory proxyFactory, IPaymentLogger paymentLogger, IExecutionContext executionContext) 
+        public PayableEarningEventHandler(IApprenticeshipKeyService apprenticeshipKeyService, IActorProxyFactory proxyFactory, IPaymentLogger paymentLogger, IExecutionContext executionContext) 
             : base(apprenticeshipKeyService, proxyFactory, paymentLogger, executionContext)
         {
         }
