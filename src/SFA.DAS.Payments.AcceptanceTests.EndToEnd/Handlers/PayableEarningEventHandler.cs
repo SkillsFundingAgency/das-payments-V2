fdc6d86aa91c4b2dfdeac867835087befc193995
davehowlett <dave_howlett@hotmail.com>
2019-01-23 14:20:31 +0000

    PV2-593 - new payable earning event handler

---
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Handlers/PayableEarningEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.Payments.DataLocks.Messages;
+
+namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Handlers
+{
+    public class PayableEarningEventHandler : IHandleMessages<PayableEarning>
+    {
+        public static ConcurrentBag<PayableEarning> ReceivedEvents { get; } = new ConcurrentBag<PayableEarning>();
+
+        public Task Handle(PayableEarning message, IMessageHandlerContext context)
+        {
+            ReceivedEvents.Add(message);
+            return Task.CompletedTask;
+        }
+    }
+}
