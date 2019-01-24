5346bcae981194d623d68af76acadbb0fc17c883
kola-samuel <samuel.dev.dba@gmail.com>
2019-01-24 10:27:01 +0000

    Event Matcher factory done

--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/EventMatchers/EventMatcherFactory.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/EventMatchers/EventMatcherFactory.cs
@@ -1,36 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
+﻿using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
 using SFA.DAS.Payments.AcceptanceTests.Core.Automation;
+using SFA.DAS.Payments.AcceptanceTests.Core.Data;
 using SFA.DAS.Payments.AcceptanceTests.EndToEnd.Data;
-using SFA.DAS.Payments.EarningEvents.Messages.Events;
 using SFA.DAS.Payments.Model.Core;
 using SFA.DAS.Payments.Model.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.EventMatchers
 {
     public static class EventMatcherFactory
     {
-        public static BaseEarningEventMatcher BuildEarningEventMatcher(IList<Earning> earningSpecs, TestSession testSession, CollectionPeriod collectionPeriod, IList<FM36Learner> learnerSpecs)
+        public static BaseEarningEventMatcher BuildEarningEventMatcher(
+            List<Price> currentPriceEpisodes,
+            List<Training> currentIlr,
+            IList<Earning> earningSpecs,
+            TestSession testSession,
+            CollectionPeriod collectionPeriod,
+            IList<FM36Learner> learnerSpecs)
         {
-            var testLearner = testSession.Learner ?? testSession.Learners.FirstOrDefault();
-            var contractType = testLearner.Aims.FirstOrDefault().PriceEpisodes.FirstOrDefault().ContractType;
+            ContractType contractType;
+
+            if (currentIlr == null || !currentIlr.Any())
+            {
+                if (currentPriceEpisodes == null) throw  new Exception("No valid current Price Episodes found");
+
+                contractType = currentPriceEpisodes.Last().ContractType;
+            }
+            else
+            {
+                contractType = currentIlr.Last().ContractType;
+            }
 
             switch (contractType)
             {
                 case ContractType.Act1:
-                     return new ApprenticeshipContractType2EarningEventMatcher(earningSpecs, testSession, collectionPeriod, learnerSpecs);
+                    return new ApprenticeshipContractType1EarningEventMatcher(earningSpecs, testSession, collectionPeriod, learnerSpecs);
 
                 case ContractType.Act2:
-                    return null;
+                    return new ApprenticeshipContractType2EarningEventMatcher(earningSpecs, testSession, collectionPeriod, learnerSpecs);
 
                 default:
-                    throw new InvalidOperationException($"Cannot create the EarningEventMatcher invalid contract type ");
+                    throw new InvalidOperationException("Cannot create the EarningEventMatcher invalid contract type ");
             }
-            
+
         }
     }
 }
