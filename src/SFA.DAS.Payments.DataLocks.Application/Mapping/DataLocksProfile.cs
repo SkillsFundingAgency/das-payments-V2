9d265bed311752e143621ffed675d18dc2eaad26
kola-samuel <samuel.dev.dba@gmail.com>
2019-01-28 14:09:05 +0000

    Mapping code changes

---
+++ src/SFA.DAS.Payments.DataLocks.Application/Mapping/DataLocksProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SFA.DAS.Payments.DataLocks.Messages;
+using SFA.DAS.Payments.EarningEvents.Messages.Events;
+
+namespace SFA.DAS.Payments.DataLocks.Application.Mapping
+{
+    public class DataLocksProfile : Profile
+    {
+        public DataLocksProfile()
+        {
+            CreateMap<ApprenticeshipContractType1EarningEvent, PayableEarningEvent>()
+                .ForMember(destinationMember => destinationMember.Priority, opt => opt.Ignore())
+                .ForMember(destinationMember => destinationMember.EmployerAccountId, opt => opt.Ignore())
+                .ForMember(destinationMember => destinationMember.CommitmentId, opt => opt.Ignore())
+                .ForMember(destinationMember => destinationMember.CommitmentVersion, opt => opt.Ignore());
+        }
+    }
+}
