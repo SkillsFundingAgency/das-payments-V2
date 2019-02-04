be756a1bbabafccd67518fd5194bb45c908fab6f
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-02-04 16:09:24 +0000

    Added test project for the datalocks service Datalocks service now returning passthough mapping Removed readonly list from events (issues with mapping)

--- src/SFA.DAS.Payments.Datalocks.Messages/DataLockEvent.cs
+++ src/SFA.DAS.Payments.Datalocks.Messages/DataLockEvent.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
 using SFA.DAS.Payments.Messages.Core.Events;
 using SFA.DAS.Payments.Model.Core;
 using SFA.DAS.Payments.Model.Core.Incentives;
@@ -8,11 +8,11 @@ namespace SFA.DAS.Payments.DataLocks.Messages
 {
     public abstract class DataLockEvent : PaymentsEvent, IContractType1EarningEvent
     {
-        public ReadOnlyCollection<PriceEpisode> PriceEpisodes { get; set; }
+        public List<PriceEpisode> PriceEpisodes { get; set; }
         public short CollectionYear { get; set; }
         public string AgreementId { get; set; }
         public decimal SfaContributionPercentage { get; set; }
-        public ReadOnlyCollection<OnProgrammeEarning> OnProgrammeEarnings { get; set; }
-        public ReadOnlyCollection<IncentiveEarning> IncentiveEarnings { get; set; }
+        public List<OnProgrammeEarning> OnProgrammeEarnings { get; set; }
+        public List<IncentiveEarning> IncentiveEarnings { get; set; }
     }
 }
