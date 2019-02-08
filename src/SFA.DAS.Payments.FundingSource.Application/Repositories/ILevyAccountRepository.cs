fbd8b5251cef82178228ee73e13e7957ad9df7c9
Maxim Beliy <maximbel@gmail.com>
2019-02-08 11:52:50 +0000

    fix for EF caching problem

--- src/SFA.DAS.Payments.FundingSource.Application/Repositories/ILevyAccountRepository.cs
+++ src/SFA.DAS.Payments.FundingSource.Application/Repositories/ILevyAccountRepository.cs
@@ -23,7 +23,7 @@ namespace SFA.DAS.Payments.FundingSource.Application.Repositories
 
         public async Task<LevyAccountModel> GetLevyAccount(long employerAccountId, CancellationToken cancellationToken = default(CancellationToken))
         {
-            return await dataContext.LevyAccount
+            return await dataContext.LevyAccount.AsNoTracking()
                 .Where(levyAccount => levyAccount.AccountId == employerAccountId)
                 .SingleOrDefaultAsync(cancellationToken).ConfigureAwait(false);
         }
