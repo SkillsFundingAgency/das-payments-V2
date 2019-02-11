d06d96e37975a4344219138e23cbba5d8790ff98
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-02-11 16:07:59 +0000

    Fixed test for datalock service

--- src/SFA.DAS.Payments.DataLocks.DataLockService.UnitTests/GivenADataLockService/WhenCallingHandlePayment.cs
+++ src/SFA.DAS.Payments.DataLocks.DataLockService.UnitTests/GivenADataLockService/WhenCallingHandlePayment.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -8,10 +9,13 @@ using Moq;
 using NUnit.Framework;
 using ServiceFabric.Mocks;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Application.Repositories;
 using SFA.DAS.Payments.DataLocks.Application.Mapping;
 using SFA.DAS.Payments.DataLocks.Application.Repositories;
 using SFA.DAS.Payments.DataLocks.Messages.Events;
 using SFA.DAS.Payments.EarningEvents.Messages.Events;
+using SFA.DAS.Payments.Model.Core;
+using SFA.DAS.Payments.Model.Core.Entities;
 
 namespace SFA.DAS.Payments.DataLocks.DataLockService.UnitTests.GivenADataLockService
 {
@@ -37,13 +41,33 @@ namespace SFA.DAS.Payments.DataLocks.DataLockService.UnitTests.GivenADataLockSer
             var actorService = MockActorServiceFactory.CreateActorServiceForActor<DataLockService>();
             var paymentLoggerMock = Mock.Of<IPaymentLogger>();
             var commitmentRepositoryMock = Mock.Of<ICommitmentRepository>();
+            var datacacheList = Mock.Of<IDataCache<List<CommitmentModel>>>();
 
+            var commitments = new List<CommitmentModel>
+            {
+                new CommitmentModel
+                {
+                    AccountId = 456,
+                }
+            };
 
-            var testEarning = new ApprenticeshipContractType1EarningEvent();
+            Mock.Get(datacacheList).Setup(x => x.TryGet(It.IsAny<string>(), CancellationToken.None))
+                .ReturnsAsync(() => new ConditionalValue<List<CommitmentModel>>(true, commitments));
+
+            var testEarning = new ApprenticeshipContractType1EarningEvent
+            {
+                Learner = new Learner
+                {
+                    Uln = 123,
+                }
+            };
+
             var actual = await (new DataLockService(actorService, new ActorId(Guid.Empty), mapper, paymentLoggerMock,
-                    null, null))
+                    commitmentRepositoryMock, datacacheList))
                 .HandleEarning(testEarning, default(CancellationToken));
+
             actual.Should().BeOfType<PayableEarningEvent>();
+            (actual as PayableEarningEvent).EmployerAccountId.Should().Be(456);
         }
     }
 }
