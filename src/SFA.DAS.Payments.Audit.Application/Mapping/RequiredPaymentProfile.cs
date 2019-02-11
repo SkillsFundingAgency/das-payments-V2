0b2051ebf2a285c3556dcc4b35d8f6b8bcce7e73
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-02-11 15:43:06 +0000

    Writing UKPRN to commitment from test Including correct mapping for audit service Correct config f

--- src/SFA.DAS.Payments.Audit.Application/Mapping/RequiredPaymentProfile.cs
+++ src/SFA.DAS.Payments.Audit.Application/Mapping/RequiredPaymentProfile.cs
@@ -15,26 +15,28 @@ namespace SFA.DAS.Payments.Audit.Application.Mapping
                 .Include<ApprenticeshipContractType1RequiredPaymentEvent, RequiredPaymentEventModel>()
                 .MapPeriodisedCommon()
                 .ForMember(dest => dest.EarningEventId, opt => opt.MapFrom(source => source.EarningEventId))
-                .ForMember(dest => dest.ContractType, opt => opt.Ignore())
-                .ForMember(dest => dest.TransactionType, opt => opt.Ignore())
-                .ForMember(dest => dest.SfaContributionPercentage, opt => opt.Ignore());
+                ;
 
             CreateMap<IncentiveRequiredPaymentEvent, RequiredPaymentEventModel>()
                 .ForMember(dest => dest.TransactionType, opt => opt.MapFrom(source => (TransactionType)source.Type))
                 .ForMember(dest => dest.ContractType, opt => opt.MapFrom(source => source.ContractType))
-                .ForMember(dest => dest.SfaContributionPercentage, opt => opt.UseValue(1M));
+                .ForMember(dest => dest.SfaContributionPercentage, opt => opt.UseValue(1M))
+                ;
 
             CreateMap<ApprenticeshipContractTypeRequiredPaymentEvent, RequiredPaymentEventModel>()
                 .Include<ApprenticeshipContractType2RequiredPaymentEvent, RequiredPaymentEventModel>()
                 .Include<ApprenticeshipContractType1RequiredPaymentEvent, RequiredPaymentEventModel>()
-                .ForMember(dest => dest.TransactionType, opt => opt.MapFrom(source => (TransactionType) source.OnProgrammeEarningType));
+                .ForMember(dest => dest.TransactionType, opt => opt.MapFrom(source => (TransactionType) source.OnProgrammeEarningType))
+                ;
 
             CreateMap<ApprenticeshipContractType2RequiredPaymentEvent, RequiredPaymentEventModel>()
-                .ForMember(dest => dest.ContractType, opt => opt.UseValue(ContractType.Act2));
+                .ForMember(dest => dest.ContractType, opt => opt.UseValue(ContractType.Act2))
+                ;
 
             CreateMap<ApprenticeshipContractType1RequiredPaymentEvent, RequiredPaymentEventModel>()
                 .ForMember(dest => dest.ContractType, opt => opt.UseValue(ContractType.Act1))
-                .ForMember(dest => dest.AgreementId, opt => opt.MapFrom(source => source.AgreementId));
+                .ForMember(dest => dest.AgreementId, opt => opt.MapFrom(source => source.AgreementId))
+                ;
 
         }
     }
