432c898920f5c9070c9ee7b844229e28131c9999
Maxim Beliy <maximbel@gmail.com>
2019-02-13 16:40:59 +0000

    Send month end command for levy service at the right moment

---
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Handlers/FundingSourcePaymentEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.Payments.FundingSource.Messages.Events;
+
+namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Handlers
+{
+    public class FundingSourcePaymentEventHandler : IHandleMessages<FundingSourcePaymentEvent>
+    {
+        public static ConcurrentBag<FundingSourcePaymentEvent> ReceivedEvents { get; } = new ConcurrentBag<FundingSourcePaymentEvent>();
+
+        public Task Handle(FundingSourcePaymentEvent message, IMessageHandlerContext context)
+        {
+            ReceivedEvents.Add(message);
+            return Task.CompletedTask;
+        }
+    }
+}
