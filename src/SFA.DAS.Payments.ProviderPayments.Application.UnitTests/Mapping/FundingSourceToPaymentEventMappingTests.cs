67e1f30c9c0fe6710272d1b4f93ec45198d703b2
kola-samuel <samuel.dev.dba@gmail.com>
2019-02-18 11:06:14 +0000

    Clean Up

--- src/SFA.DAS.Payments.ProviderPayments.Application.UnitTests/Mapping/FundingSourceToPaymentEventMappingTests.cs
+++ src/SFA.DAS.Payments.ProviderPayments.Application.UnitTests/Mapping/FundingSourceToPaymentEventMappingTests.cs
@@ -22,7 +22,6 @@ namespace SFA.DAS.Payments.ProviderPayments.Application.UnitTests.Mapping
             Mapper.AssertConfigurationIsValid();
         }
 
-
         [Test]
         public void CanMapFromEmployerCoInvestedFundingSourceEventToPayment()
         {
@@ -191,34 +190,6 @@ namespace SFA.DAS.Payments.ProviderPayments.Application.UnitTests.Mapping
             fundingSourceEvent.EventTime = DateTimeOffset.UtcNow;
             fundingSourceEvent.RequiredPaymentEventId = Guid.NewGuid();
 
-            //var employerCoInvested = new EmployerCoInvestedFundingSourcePaymentEvent
-            //{
-            //    EventId = Guid.NewGuid(),
-            //    CollectionPeriod = new CollectionPeriod { Period = 12, AcademicYear = 1819 },
-            //    Learner = new Learner { ReferenceNumber = "1234-ref", Uln = 123456 },
-            //    TransactionType = TransactionType.Completion,
-            //    Ukprn = 12345,
-            //    ContractType = ContractType.Act1,
-            //    SfaContributionPercentage = 0.9m,
-            //    PriceEpisodeIdentifier = "pe-1",
-            //    JobId = 123,
-            //    AmountDue = 300,
-            //    FundingSourceType = FundingSourceType.CoInvestedEmployer,
-            //    DeliveryPeriod = 12,
-            //    LearningAim = new LearningAim
-            //    {
-            //        PathwayCode = 12,
-            //        FrameworkCode = 1245,
-            //        FundingLineType = "Non-DAS 16-18 Learner",
-            //        StandardCode = 1209,
-            //        ProgrammeType = 7890,
-            //        Reference = "1234567-aim-ref"
-            //    },
-            //    IlrSubmissionDateTime = DateTime.UtcNow,
-            //    EventTime = DateTimeOffset.UtcNow,
-            //    RequiredPaymentEventId = Guid.NewGuid()
-            //};
-            //var payment = Mapper.Map<ProviderPaymentEvent>(employerCoInvested);
             var payment = Mapper.Map<ProviderPaymentEvent>(fundingSourceEvent);
             payment.Should().NotBeNull();
             payment.Should().BeAssignableTo(destType);
