391f84cbf8ee4219e8ecaf073997bfbc79dc8be0
abroodie <abroodie@outlook.com>
2019-03-06 16:55:49 +0000

    removed commented out code

--- src/SFA.DAS.Payments.Monitoring.Jobs.Application.UnitTests/SqlExceptionServiceTests.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Application.UnitTests/SqlExceptionServiceTests.cs
@@ -25,31 +25,5 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.UnitTests
                 service.IsConstraintViolation(new DbUpdateException("Not a violation", (SqlException) null));
             isConstraintViolation.Should().BeFalse();
         }
-
-
-        //TODO: Find way to properly mock sqlexception. (No public constructor and class is sealed)
-        //[TestCase(2627)]
-        //[TestCase(547)]
-        //[TestCase(2601)]
-        //public void IsConstraintViolation_Returns_True_If_SqlException_Number_Refers_To_Constraint_Error(int sqlExceptionNumber)
-        //{
-        //    var service= mocker.Create<SqlExceptionService>();
-        //    var mockSqlException = mocker.Mock<SqlException>();
-        //    mockSqlException.Setup(ex => ex.Number).Returns(sqlExceptionNumber);
-        //    var isConstraintViolation =
-        //        service.IsConstraintViolation(new DbUpdateException("Not a violation", mockSqlException.Object));
-        //    isConstraintViolation.Should().BeTrue();
-        //}
-
-        //[Test]
-        //public void IsConstraintViolation_Returns_False_If_SqlException_Number_Does_Not_Refer_To_Constraint_Error()
-        //{
-        //    var service= mocker.Create<SqlExceptionService>();
-        //    var mockSqlException = mocker.Mock<SqlException>();
-        //    mockSqlException.Setup(ex => ex.Number).Returns(1);
-        //    var isConstraintViolation =
-        //        service.IsConstraintViolation(new DbUpdateException("Not a violation", mockSqlException.Object));
-        //    isConstraintViolation.Should().BeFalse();
-        //}
     }
 }
