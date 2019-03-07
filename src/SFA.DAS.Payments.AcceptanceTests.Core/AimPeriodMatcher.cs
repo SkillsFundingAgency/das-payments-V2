ce7f301452773ebf99176f089b528dd71ef764ec
Dave Howlett <dave_howlett@hotmail.com>
2019-03-07 16:03:32 +0000

    PV2-639 - addition of aim period matcher unit tests

--- src/SFA.DAS.Payments.AcceptanceTests.Core/AimPeriodMatcher.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.Core/AimPeriodMatcher.cs
@@ -11,37 +11,104 @@ namespace SFA.DAS.Payments.AcceptanceTests.Core
     public static class AimPeriodMatcher
     {
         public static bool IsStartDateValidForCollectionPeriod(
-            string startDate, 
-            CollectionPeriod collectionPeriod, 
-            TimeSpan? plannedDurationAsTimeSpan, 
-            TimeSpan? actualDurationAsTimeSpan, 
-            CompletionStatus completionStatus, 
+            string startDate,
+            CollectionPeriod collectionPeriod,
+            TimeSpan? plannedDurationAsTimeSpan,
+            TimeSpan? actualDurationAsTimeSpan,
+            CompletionStatus completionStatus,
             string aimReference)
         {
             var aimStartDate = startDate.ToDate();
             var aimStartPeriod = new CollectionPeriodBuilder().WithDate(aimStartDate).Build();
             var aimDuration = actualDurationAsTimeSpan ?? plannedDurationAsTimeSpan;
             var collectionPeriodReferenceDate = DateFromCollectionPeriod(collectionPeriod);
+            const string zProgAim = "ZPROG001";
 
-            return (aimStartDate + aimDuration >= collectionPeriodReferenceDate && 
-                   (aimReference == "ZPROG001" && completionStatus == CompletionStatus.Completed || !GetInactiveStatuses().Contains(completionStatus))) ||
-                    aimStartPeriod.AcademicYear == collectionPeriod.AcademicYear;
+            var isZProg = aimReference == zProgAim;
+
+            if (GetInactiveStatuses().Contains(completionStatus))
+            {
+                if (
+                    (aimStartPeriod.AcademicYear < collectionPeriod.AcademicYear &&
+                     (DurationGreaterThanCollectionPeriod(aimStartDate, plannedDurationAsTimeSpan, collectionPeriodReferenceDate) || DurationGreaterThanCollectionPeriod(aimStartDate, actualDurationAsTimeSpan, collectionPeriodReferenceDate))) ||
+                    (aimStartPeriod.AcademicYear == collectionPeriod.AcademicYear &&
+                     aimStartDate <= collectionPeriodReferenceDate && ((DurationGreaterThanCollectionPeriod(aimStartDate, plannedDurationAsTimeSpan, collectionPeriodReferenceDate) || DurationGreaterThanCollectionPeriod(aimStartDate, actualDurationAsTimeSpan, collectionPeriodReferenceDate)))))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            if (GetActiveStatuses().Contains(completionStatus) &&
+                (aimStartPeriod.AcademicYear < collectionPeriod.AcademicYear &&
+                 DurationGreaterThanCollectionPeriod(aimStartDate, aimDuration, collectionPeriodReferenceDate)))
+            {
+                return true;
+            }
+
+            if (completionStatus == CompletionStatus.Completed &&
+                (aimStartPeriod.AcademicYear == collectionPeriod.AcademicYear &&
+                 aimStartDate <= collectionPeriodReferenceDate &&
+                 aimStartDate + aimDuration < collectionPeriodReferenceDate))
+            {
+                return true;
+            }
+
+            if (completionStatus == CompletionStatus.Completed &&
+                (aimStartPeriod.AcademicYear < collectionPeriod.AcademicYear &&
+                 aimStartDate <= collectionPeriodReferenceDate &&
+                 DurationGreaterThanCollectionPeriod(aimStartDate, aimDuration, collectionPeriodReferenceDate)))
+            {
+                return true;
+            }
+
+            return aimStartPeriod.AcademicYear == collectionPeriod.AcademicYear &&
+                   aimStartDate <= collectionPeriodReferenceDate &&
+                   DurationGreaterThanCollectionPeriod(aimStartDate, aimDuration, collectionPeriodReferenceDate);
+
+            //var aimCouldBeActive = //(isZProg && aimStartDate <= collectionPeriodReferenceDate) || 
+            //                        (isZProg && aimStartPeriod.AcademicYear < collectionPeriod.AcademicYear && aimStartDate + aimDuration >= collectionPeriodReferenceDate) ||
+            //                        (isZProg && aimStartPeriod.AcademicYear == collectionPeriod.AcademicYear && aimStartDate <= collectionPeriodReferenceDate) ||
+            //                       (!isZProg && aimStartPeriod.AcademicYear == collectionPeriod.AcademicYear);
+
+            // var isZProg = aimReference == zProgAim;// && GetInactiveStatuses().Contains(completionStatus);// completionStatus == CompletionStatus.Completed;
+
+            //var isNotContinuing = aimReference != zProgAim && !GetInactiveStatuses().Contains(completionStatus);
+
+            //return aimCouldBeActive && (isZProg || isNotContinuing);
         }
 
         private static DateTime DateFromCollectionPeriod(CollectionPeriod collectionPeriod)
         {
-            var year = collectionPeriod.AcademicYear / 100 + 2000;
+            var year = Convert.ToInt16(collectionPeriod.AcademicYear.ToString().Substring(0, 2)) + 2000;
 
-            return collectionPeriod.Period < 6 
-                ? new DateTime(year, collectionPeriod.Period + 7, 1) 
-                : new DateTime(year + 1, collectionPeriod.Period - 5, 1);
+            return collectionPeriod.Period < 6
+                ? new DateTime(year, collectionPeriod.Period + 7, 1).AddMonths(1).AddDays(-1)
+                : new DateTime(year + 1, collectionPeriod.Period - 5, 1).AddMonths(1).AddDays(-1);
+        }
+
+        private static bool DurationGreaterThanCollectionPeriod(DateTime startDate, TimeSpan? duration,
+            DateTime collectionPeriod)
+        {
+            return startDate.FirstDay() + duration >= collectionPeriod.FirstDay();
         }
 
         private static IEnumerable<CompletionStatus> GetInactiveStatuses()
         {
             yield return CompletionStatus.BreakInLearning;
-            yield return CompletionStatus.Completed;
             yield return CompletionStatus.Withdrawn;
+        }
+
+        private static IEnumerable<CompletionStatus> GetActiveStatuses()
+        {
+            yield return CompletionStatus.Continuing;
+            yield return CompletionStatus.Completed;
+        }
+
+        public static DateTime FirstDay(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, 1);
         }
     }
 }
---
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Tests/AimPeriodMatcherTest.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using NUnit.Framework;
+using SFA.DAS.Payments.AcceptanceTests.Core;
+using SFA.DAS.Payments.AcceptanceTests.Core.Data;
+using SFA.DAS.Payments.Model.Core;
+using FluentAssertions;
+
+namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Tests
+{
+    [TestFixture]
+    public class AimPeriodMatcherTest
+    {
+        private const string ZProgAim = "ZPROG001";
+        const string StartOfCurrentAcademicYear = "03/Aug/Current Academic Year";
+        const string StartOfPreviousAcademicYear = "01/Aug/Last Academic Year";
+        private readonly CollectionPeriod firstPeriod = new CollectionPeriod { AcademicYear = 1819, Period = 1 };
+        private readonly CollectionPeriod fourthPeriod = new CollectionPeriod { AcademicYear = 1819, Period = 4 };
+        private readonly CollectionPeriod sixthPeriod = new CollectionPeriod { AcademicYear = 1819, Period = 6 };
+        private readonly TimeSpan annualDuration = new TimeSpan(365, 0, 0, 0);
+        private readonly TimeSpan threeMonthDuration = new TimeSpan(92, 0, 0, 0);
+
+        [Test]
+        public void ContinuingZProgAimStartingInPreviousYearReturnsTrueWhenActiveInR01InCurrentYear()
+        {
+            var plannedDuration = new TimeSpan(730, 0, 0, 0);
+            var completionStatus = CompletionStatus.Continuing;
+
+            var isStartDateValid =
+                AimPeriodMatcher.IsStartDateValidForCollectionPeriod(
+                    StartOfPreviousAcademicYear,
+                    firstPeriod,
+                    plannedDuration,
+                    null,
+                    completionStatus,
+                    ZProgAim);
+
+            isStartDateValid.Should().Be(true);
+        }
+
+        [Test]
+        public void ContinuingZProgAimStartingInR04ReturnsFalseWhenInR01InCurrentYear()
+        {
+            var plannedDuration = new TimeSpan(30*9, 0, 0, 0);
+            var completionStatus = CompletionStatus.Continuing;
+
+            var isStartDateValid =
+                AimPeriodMatcher.IsStartDateValidForCollectionPeriod(
+                    "03/Nov/Current Academic Year",
+                    firstPeriod,
+                    plannedDuration,
+                    null,
+                    completionStatus,
+                    ZProgAim);
+
+            isStartDateValid.Should().Be(false);
+        }
+
+        [Test]
+        public void ContinuingZProgAimStartingInR04ReturnsTrueWhenInR04InCurrentYear()
+        {
+            var completionStatus = CompletionStatus.Continuing;
+
+            var isStartDateValid =
+                AimPeriodMatcher.IsStartDateValidForCollectionPeriod(
+                    "03/Nov/Current Academic Year",
+                    fourthPeriod,
+                    annualDuration,
+                    null,
+                    completionStatus,
+                    ZProgAim);
+
+            isStartDateValid.Should().Be(true);
+        }
+
+        [Test]
+        public void WithdrawnZProgAimReturnsTrueWhenWithdrawnButActiveInR01()
+        {
+            var completionStatus = CompletionStatus.Withdrawn;
+
+            var isStartDateValid =
+                AimPeriodMatcher.IsStartDateValidForCollectionPeriod(
+                    StartOfCurrentAcademicYear,
+                    firstPeriod,
+                    annualDuration,
+                    threeMonthDuration,
+                    completionStatus,
+                    ZProgAim);
+
+            isStartDateValid.Should().Be(true);
+        }
+
+        [Test]
+        public void WithdrawnZProgAimReturnsTrueWhenWithdrawnBeforeR04()
+        {
+            var completionStatus = CompletionStatus.Withdrawn;
+
+            var isStartDateValid =
+                AimPeriodMatcher.IsStartDateValidForCollectionPeriod(
+                    StartOfCurrentAcademicYear,
+                    fourthPeriod,
+                    annualDuration,
+                    threeMonthDuration,
+                    completionStatus,
+                    ZProgAim);
+
+            isStartDateValid.Should().Be(true);
+        }
+
+        [Test]
+        public void CompletedLastYearZProgAimReturnsTrueWhenInPeriodR01()
+        {
+            var completionStatus = CompletionStatus.Completed;
+
+            var isStartDateValid =
+                AimPeriodMatcher.IsStartDateValidForCollectionPeriod(
+                    StartOfPreviousAcademicYear,
+                    firstPeriod,
+                    annualDuration,
+                    null,
+                    completionStatus,
+                    ZProgAim);
+
+            isStartDateValid.Should().Be(true);
+        }
+    }
+}
