cdce577c94ea8485bc99797a69924cf7283420f9
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-03-08 08:54:50 +0000

    Blocking to complete PV2-747 (add SFA contribution % to payment history)

--- src/SFA.DAS.Payments.RequiredPayments.Domain.UnitTests/Services/RefundServiceTests.cs
+++ src/SFA.DAS.Payments.RequiredPayments.Domain.UnitTests/Services/RefundServiceTests.cs
@@ -1,10 +1,217 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Payments.Model.Core.Entities;
+using SFA.DAS.Payments.RequiredPayments.Domain.Entities;
+using SFA.DAS.Payments.RequiredPayments.Domain.Services;
 
 namespace SFA.DAS.Payments.RequiredPayments.Domain.UnitTests.Services
 {
     [TestFixture]
     public class RefundServiceTests
     {
+        [Test]
+        public void NoEarningReturnsEmptyResult()
+        {
+            var sut = new RefundService();
+            var actual = sut.GetRefund(new Earning(), new List<Payment>());
+            actual.Should().BeEmpty();
+        }
 
+        [Test]
+        public void NoHistoryReturnsEmptyResult()
+        {
+            var sut = new RefundService();
+            var actual = sut.GetRefund(new Earning
+            {
+                Amount = -100,
+                EarningType = EarningType.Incentive,
+                SfaContributionPercentage = 1,
+            }, new List<Payment>());
+            actual.Should().BeEmpty();
+        }
+
+        [Test]
+        [TestCase(EarningType.CoInvested)]
+        [TestCase(EarningType.Incentive)]
+        [TestCase(EarningType.Levy)]
+        public void EarningsFromASingleFundingSourceReturnRequiredPaymentsFromTheSameSource(EarningType testType)
+        {
+            var sut = new RefundService();
+
+            var expectedAmount = -123.2m;
+            var expectedSfaPercent = 0.76m;
+
+            var actual = sut.GetRefund(new Earning
+            {
+                EarningType = testType,
+                Amount = expectedAmount,
+                SfaContributionPercentage = expectedSfaPercent,
+            }, new List<Payment>
+            {
+                new Payment
+                {
+                    Amount = -1 * expectedAmount,
+                    FundingSource = ConvertToFundingSource(testType),
+                }
+            });
+
+            actual.Where(x => x.EarningType == testType).Should().HaveCount(1);
+            actual.Single().Amount.Should().Be(expectedAmount);
+            actual.Single().SfaContributionPercentage.Should().Be(expectedSfaPercent);
+        }
+
+        FundingSourceType ConvertToFundingSource(EarningType earningType)
+        {
+            switch (earningType)
+            {
+                case EarningType.CoInvested:
+                    return FundingSourceType.CoInvestedSfa;
+                case EarningType.Levy:
+                    return FundingSourceType.Levy;
+                case EarningType.Incentive:
+                    return FundingSourceType.FullyFundedSfa;
+            }
+            throw new ApplicationException($"Unknown earning type: {earningType}");
+        }
+
+        [Test]
+        public void CoInvestmentRefund()
+        {
+            var sut = new RefundService();
+
+            var testEarning = new Earning
+            {
+                Amount = -50,
+                EarningType = EarningType.CoInvested,
+                SfaContributionPercentage = 0.789m,
+            };
+
+            var testHistory = new List<Payment>
+            {
+                new Payment {Amount = 90, FundingSource = FundingSourceType.CoInvestedSfa},
+                new Payment {Amount = 10, FundingSource = FundingSourceType.CoInvestedEmployer},
+            };
+
+            var actual = sut.GetRefund(testEarning, testHistory);
+
+            actual.Should().HaveCount(1);
+            actual[0].EarningType.Should().Be(EarningType.CoInvested);
+            actual[0].Amount.Should().Be(-50);
+            actual[0].SfaContributionPercentage.Should().Be(0.9m);
+        }
+
+        [Test]
+        public void CoInvestmentRefundFromLevyEarning()
+        {
+            var sut = new RefundService();
+
+            var testEarning = new Earning
+            {
+                Amount = -50,
+                EarningType = EarningType.Levy,
+                SfaContributionPercentage = 0.789m,
+            };
+
+            var testHistory = new List<Payment>
+            {
+                new Payment {Amount = 90, FundingSource = FundingSourceType.CoInvestedSfa},
+                new Payment {Amount = 10, FundingSource = FundingSourceType.CoInvestedEmployer},
+            };
+
+            var actual = sut.GetRefund(testEarning, testHistory);
+
+            actual.Should().HaveCount(1);
+            actual[0].EarningType.Should().Be(EarningType.CoInvested);
+            actual[0].Amount.Should().Be(-50);
+            actual[0].SfaContributionPercentage.Should().Be(0.9m);
+        }
+
+        [Test]
+        public void MixedRefundOneSfaContribution()
+        {
+            var sut = new RefundService();
+
+            var testEarning = new Earning
+            {
+                Amount = -50,
+                EarningType = EarningType.Levy,
+                SfaContributionPercentage = 0.789m,
+            };
+
+            var testHistory = new List<Payment>
+            {
+                new Payment {Amount = 50, FundingSource = FundingSourceType.Levy},
+                new Payment {Amount = 45, FundingSource = FundingSourceType.CoInvestedSfa},
+                new Payment {Amount = 5, FundingSource = FundingSourceType.CoInvestedEmployer},
+            };
+
+            var actual = sut.GetRefund(testEarning, testHistory);
+
+            actual.Should().HaveCount(2);
+            actual.Single(x => x.EarningType == EarningType.CoInvested).Amount.Should().Be(-25);
+            actual.Single(x => x.EarningType == EarningType.CoInvested).SfaContributionPercentage.Should().Be(0.9m);
+
+            actual.Single(x => x.EarningType == EarningType.Levy).Amount.Should().Be(-25);
+            actual.Single(x => x.EarningType == EarningType.Levy).SfaContributionPercentage.Should().Be(0.789m);
+        }
+
+        [Test]
+        public void MixedRefundTwoSfaContribution()
+        {
+            var sut = new RefundService();
+
+            var testEarning = new Earning
+            {
+                Amount = -200,
+                EarningType = EarningType.Levy,
+                SfaContributionPercentage = 0.789m,
+            };
+
+            var testHistory = new List<Payment>
+            {
+                new Payment {Amount = 50, FundingSource = FundingSourceType.Levy},
+                new Payment {Amount = 45, FundingSource = FundingSourceType.CoInvestedSfa},
+                new Payment {Amount = 5, FundingSource = FundingSourceType.CoInvestedEmployer},
+                new Payment {Amount = 40, FundingSource = FundingSourceType.CoInvestedSfa},
+                new Payment {Amount = 10, FundingSource = FundingSourceType.CoInvestedEmployer},
+                new Payment {Amount = 50, FundingSource = FundingSourceType.CoInvestedSfa},
+            };
+
+            var actual = sut.GetRefund(testEarning, testHistory);
+
+            actual.Should().HaveCount(2);
+            actual.Single(x => x.EarningType == EarningType.CoInvested).Amount.Should().Be(-150);
+            actual.Single(x => x.EarningType == EarningType.CoInvested).SfaContributionPercentage.Should().Be(0.9m);
+
+            actual.Single(x => x.EarningType == EarningType.Levy).Amount.Should().Be(-50);
+            actual.Single(x => x.EarningType == EarningType.CoInvested).SfaContributionPercentage.Should().Be(0.789m);
+        }
+
+        [Test]
+        public void LevyRefund()
+        {
+            var sut = new RefundService();
+
+            var testEarning = new Earning
+            {
+                Amount = -50,
+                EarningType = EarningType.Levy,
+                SfaContributionPercentage = 0.789m,
+            };
+
+            var testHistory = new List<Payment>
+            {
+                new Payment {Amount = 100, FundingSource = FundingSourceType.Levy},
+            };
+
+            var actual = sut.GetRefund(testEarning, testHistory);
+
+            actual.Should().HaveCount(1);
+            actual[0].EarningType.Should().Be(EarningType.Levy);
+            actual[0].Amount.Should().Be(-50);
+        }
     }
 }
---
+++ src/SFA.DAS.Payments.RequiredPayments.Domain/Services/RefundService.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SFA.DAS.Payments.Model.Core.Entities;
+using SFA.DAS.Payments.RequiredPayments.Domain.Entities;
+
+namespace SFA.DAS.Payments.RequiredPayments.Domain.Services
+{
+    public class RefundService
+    {
+        public List<RequiredPayment> GetRefund(Earning earning, List<Payment> paymentHistory)
+        {
+            var result = new List<RequiredPayment>();
+
+            if (earning.Amount >= 0)
+            {
+                return result;
+            }
+
+            var totalPaidInHistory = paymentHistory.Sum(x => x.Amount);
+            if (totalPaidInHistory <= 0)
+            {
+                return result;
+            }
+
+            var paymentsBySource = paymentHistory.ToLookup(x => x.FundingSource);
+
+            var totalPaidForLevy = paymentHistory.Where(x => x.FundingSource == FundingSourceType.Levy).Sum(x => x.Amount);
+            var totalPaidForEmployer = paymentHistory.Where(x => x.FundingSource == FundingSourceType.CoInvestedEmployer).Sum(x => x.Amount);
+            var totalPaidForProvider = paymentHistory.Where(x => x.FundingSource == FundingSourceType.CoInvestedSfa).Sum(x => x.Amount);
+            var totalPaidForIncentives = paymentHistory.Where(x => x.FundingSource == FundingSourceType.FullyFundedSfa).Sum(x => x.Amount);
+
+            var totalRefundPercent = (-1 * earning.Amount) / totalPaidInHistory; // Earning amount is a negative
+
+            var levyRefund = totalPaidForLevy * totalRefundPercent;
+            if (levyRefund > 0)
+            {
+                result.Add(new RequiredPayment
+                {
+                    EarningType = EarningType.Levy,
+                    Amount = -1 * levyRefund,
+                    SfaContributionPercentage = earning.SfaContributionPercentage,
+                });
+            }
+
+            var incentiveRefund = totalPaidForIncentives * totalRefundPercent;
+            if (incentiveRefund > 0)
+            {
+                result.Add(new RequiredPayment
+                {
+                    EarningType = EarningType.Incentive,
+                    Amount = -1 * incentiveRefund,
+                    SfaContributionPercentage = earning.SfaContributionPercentage,
+                });
+            }
+
+            //var coInvestedRefund = 
+
+            return result;
+        }
+    }
+}
