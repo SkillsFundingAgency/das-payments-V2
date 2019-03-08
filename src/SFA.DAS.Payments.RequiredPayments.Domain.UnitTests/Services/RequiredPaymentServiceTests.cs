8b4526625b0728b87dc8853166741f26c83959d9
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-03-08 14:14:37 +0000

    Added tests - failing build

---
+++ src/SFA.DAS.Payments.RequiredPayments.Domain.UnitTests/Services/RequiredPaymentServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace SFA.DAS.Payments.RequiredPayments.Domain.UnitTests.Services
+{
+    [TestFixture]
+    public class RequiredPaymentServiceTests
+    {
+        [Test]
+        public void CallsPaymentsDueProcessor()
+        {
+            var sut = new RequiredPaymentService();
+
+        }
+
+        [Test]
+        public void DoesNotCallRefundServiceForGreaterThanZeroAmount()
+        {
+
+        }
+
+        [Test]
+        public void ReturnsTheResultOfPaymentsDueServiceForGreaterThanZeroAmount()
+        {
+
+        }
+
+        [Test]
+        public void CallsRefundServiceForLessThanZeroAmount()
+        {
+
+        }
+
+        [Test]
+        public void ReturnsTheResultOfTheRefundServiceForLessThanZeroAmount()
+        {
+
+        }
+
+        [Test]
+        public void ReutrnsAnEmptyListForZeroAmount()
+        {
+
+        }
+    }
+}
