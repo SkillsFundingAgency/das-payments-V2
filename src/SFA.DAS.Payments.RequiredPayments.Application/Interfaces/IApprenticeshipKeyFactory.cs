96174fff49a2b22c37bd71f069466a31050c4189
Maxim Beliy <maximbel@gmail.com>
2019-03-27 13:43:10 +0000

    ApprenticeshipKeyFactory

---
+++ src/SFA.DAS.Payments.RequiredPayments.Application/Interfaces/IApprenticeshipKeyFactory.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.Payments.RequiredPayments.Domain.Services;
+
+namespace SFA.DAS.Payments.RequiredPayments.Domain
+{
+    public interface IApprenticeshipKeyFactory
+    {
+        ApprenticeshipKey GetCurrentKey();
+    }
+}
