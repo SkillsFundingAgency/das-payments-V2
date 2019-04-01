b10c56ab2eb02a3bfa0dc4f90670f5dbae0abb63
Dave Howlett <dave_howlett@hotmail.com>
2019-04-01 15:50:19 +0100

    PV2-853 - setting defaults into the earnings info

--- src/SFA.DAS.Payments.Application/Data/Configurations/PaymentModelConfiguration.cs
+++ src/SFA.DAS.Payments.Application/Data/Configurations/PaymentModelConfiguration.cs
@@ -38,6 +38,13 @@ namespace SFA.DAS.Payments.Application.Data.Configurations
             builder.Property(x => x.IlrSubmissionDateTime).HasColumnName(@"IlrSubmissionDateTime").IsRequired();
             builder.Property(x => x.SfaContributionPercentage).HasColumnName(@"SfaContributionPercentage").IsRequired();
             builder.Property(x => x.JobId).HasColumnName(@"JobId").IsRequired();
+            builder.Property(x => x.StartDate).HasColumnName(@"EarningsStartDate").IsRequired();
+            builder.Property(x => x.PlannedEndDate).HasColumnName(@"EarningsPlannedEndDate").IsRequired();
+            builder.Property(x => x.ActualEndDate).HasColumnName(@"EarningsActualEndDate");
+            builder.Property(x => x.CompletionStatus).HasColumnName(@"EarningsCompletionStatus").IsRequired();
+            builder.Property(x => x.CompletionAmount).HasColumnName(@"EarningsCompletionAmount").IsRequired();
+            builder.Property(x => x.InstalmentAmount).HasColumnName(@"EarningsInstalmentAmount").IsRequired();
+            builder.Property(x => x.NumberOfInstalments).HasColumnName(@"EarningsNumberOfInstalments").IsRequired();
         }
     }
 }
