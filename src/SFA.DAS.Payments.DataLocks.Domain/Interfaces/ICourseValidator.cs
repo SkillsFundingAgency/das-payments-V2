f65c7858095730ebbbf20c4a62218137c5cf4ebf
Dave Howlett <dave_howlett@hotmail.com>
2019-04-04 14:59:14 +0100

    PV2-763 - initial classes

---
+++ src/SFA.DAS.Payments.DataLocks.Domain/Interfaces/ICourseValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.Payments.DataLocks.Domain.Interfaces
+{
+    public interface ICourseValidator
+    {
+
+    }
+}
---
+++ src/SFA.DAS.Payments.DataLocks.Domain/Models/CourseValidationResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.Payments.DataLocks.Domain.Models
+{
+    public class CourseValidationResult
+    {
+        public List<ValidationResult> ValidationResults { get; set; }
+    }
+}
