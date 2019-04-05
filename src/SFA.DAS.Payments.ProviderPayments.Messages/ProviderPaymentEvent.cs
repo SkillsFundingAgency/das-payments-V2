28c8ee6ed4ef0fa7ac2c0d3330a536ebc30eeccb
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-04-05 15:33:23 +0100

    Fixed spec to not double up on month end Removed Contract Type from subclasses that had duplicates

--- src/SFA.DAS.Payments.ProviderPayments.Messages/ProviderPaymentEvent.cs
+++ src/SFA.DAS.Payments.ProviderPayments.Messages/ProviderPaymentEvent.cs
@@ -17,7 +17,6 @@ namespace SFA.DAS.Payments.ProviderPayments.Messages
                        .ToArray());
         }
 
-        public byte ContractType { get; set; } //Backwards Compatibility //TODO: we will eventually have events: ACT1Transfer, ACT1Levy, ACT1EmployerCoInvested, ACT1SfaCoInvested, ACT2EmployerCoInvested, etc
         public TransactionType TransactionType { get; set; }
         public FundingSourceType FundingSourceType { get; set; }  //Backwards Compatibility
         public decimal SfaContributionPercentage { get; set; }
