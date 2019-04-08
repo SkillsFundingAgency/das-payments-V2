c071931f3c6fc93fe3854e221c9e72ab5e805933
kola-samuel <samuel.dev.dba@gmail.com>
2019-04-08 11:26:34 +0100

    Learner Matcher

--- src/SFA.DAS.Payments.DataLocks.Domain/Services/LearnerMatcher.cs
+++ src/SFA.DAS.Payments.DataLocks.Domain/Services/LearnerMatcher.cs
@@ -1,13 +1,37 @@
-﻿using System.Threading.Tasks;
+﻿using SFA.DAS.Payments.DataLocks.Domain.Interfaces;
 using SFA.DAS.Payments.DataLocks.Domain.Models;
+using System.Threading.Tasks;
 
-namespace SFA.DAS.Payments.DataLocks.Domain.Interfaces
+namespace SFA.DAS.Payments.DataLocks.Domain.Services
 {
     public class LearnerMatcher : ILearnerMatcher
     {
+        private readonly IUlnLearnerMatcher ulnLearnerMatcher;
+        private readonly IUkprnMatcher ukprnMatcher;
+
+        public LearnerMatcher(IUkprnMatcher ukprnMatcher, IUlnLearnerMatcher ulnLearnerMatcher)
+        {
+            this.ulnLearnerMatcher = ulnLearnerMatcher;
+            this.ukprnMatcher = ukprnMatcher;
+        }
+
         public async Task<LearnerMatchResult> MatchLearner(long ukprn, long uln)
         {
-            throw new System.NotImplementedException();
+            var dataLockErrorCode = await ukprnMatcher.MatchUkprn();
+            if (dataLockErrorCode.HasValue)
+                return new LearnerMatchResult
+                {
+                    DataLockErrorCode = dataLockErrorCode
+                };
+
+            var ulnMatchResult = await ulnLearnerMatcher.MatchUln(uln);
+            if (ulnMatchResult.DataLockErrorCode.HasValue)
+                return new LearnerMatchResult
+                {
+                    DataLockErrorCode = ulnMatchResult.DataLockErrorCode
+                };
+
+            return ulnMatchResult;
         }
     }
 }
