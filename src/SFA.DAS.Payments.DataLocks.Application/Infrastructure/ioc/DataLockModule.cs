656a8ea173d0e6919756a02ff787e024e71f004b
Dave Howlett <dave_howlett@hotmail.com>
2019-04-08 10:37:43 +0100

    PV2-763 - merge issue with registrations

--- src/SFA.DAS.Payments.DataLocks.Application/Infrastructure/ioc/DataLockModule.cs
+++ src/SFA.DAS.Payments.DataLocks.Application/Infrastructure/ioc/DataLockModule.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using Autofac;
-using SFA.DAS.Payments.Application.Repositories;
 using SFA.DAS.Payments.DataLocks.Application.Cache;
 using SFA.DAS.Payments.DataLocks.Domain.Interfaces;
 using SFA.DAS.Payments.DataLocks.Domain.Services;
@@ -14,9 +13,9 @@ namespace SFA.DAS.Payments.DataLocks.Application.Infrastructure.ioc
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<ActorReliableCollectionCache<List<ApprenticeshipModel>>>().AsImplementedInterfaces().InstancePerLifetimeScope();
-            builder.RegisterType<DataLockLearnerCache>().As<IDataLockLearnerCache>().InstancePerLifetimeScope();
-            builder.RegisterType<UkprnMatcher>().As<IUkprnMatcher>().InstancePerLifetimeScope();
-            builder.RegisterType<UlnLearnerMatcher>().As<IUlnLearnerMatcher>().InstancePerLifetimeScope();
+            builder.RegisterType<DataLockLearnerCache>().AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<UkprnMatcher>().InstancePerMatchingLifetimeScope().InstancePerLifetimeScope();
+            builder.RegisterType<UlnLearnerMatcher>().AsImplementedInterfaces().InstancePerLifetimeScope();
             builder.RegisterType<LearnerMatcher>().AsImplementedInterfaces().InstancePerLifetimeScope();
             builder.RegisterType<CourseValidator>().AsImplementedInterfaces().InstancePerLifetimeScope();
         }
