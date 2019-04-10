8133f2f10ca28c81beabd30f5d0999d7511c5481
Dave Howlett <dave_howlett@hotmail.com>
2019-04-10 12:31:55 +0100

    PV2-830 - changed signature

--- src/SFA.DAS.Payments.DataLocks.Domain/Interfaces/IValidator.cs
+++ src/SFA.DAS.Payments.DataLocks.Domain/Interfaces/IValidator.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
 using SFA.DAS.Payments.DataLocks.Domain.Models;
-using SFA.DAS.Payments.Model.Core;
 using SFA.DAS.Payments.Model.Core.Entities;
 
 namespace SFA.DAS.Payments.DataLocks.Domain.Interfaces
 {
     public interface IValidator
     {
-        CourseValidationResult Validate(CourseValidation courseValidation, List<ApprenticeshipModel> apprenticeships);
+        List<ValidationResult> Validate(CourseValidation courseValidation, List<ApprenticeshipModel> apprenticeships);
     }
 }
--- src/SFA.DAS.Payments.DataLocks.Domain/Services/StartDateValidator.cs
+++ src/SFA.DAS.Payments.DataLocks.Domain/Services/StartDateValidator.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using SFA.DAS.Payments.DataLocks.Domain.Interfaces;
 using SFA.DAS.Payments.DataLocks.Domain.Models;
-using SFA.DAS.Payments.Model.Core;
 using SFA.DAS.Payments.Model.Core.Entities;
 
 namespace SFA.DAS.Payments.DataLocks.Domain.Services
 {
     public class StartDateValidator : IValidator
     {
-        public CourseValidationResult Validate(PriceEpisode priceEpisode, List<ApprenticeshipModel> apprenticeships)
+        public List<ValidationResult> Validate(CourseValidation courseValidation, List<ApprenticeshipModel> apprenticeships)
         {
             throw new NotImplementedException();
         }
