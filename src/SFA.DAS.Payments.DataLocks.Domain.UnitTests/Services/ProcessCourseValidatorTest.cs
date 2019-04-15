a79aa5bdb9a23e9fc4854bbb57f5cb6be98f0476
kola-samuel <samuel.dev.dba@gmail.com>
2019-04-15 09:15:15 +0100

    Clean Up

--- src/SFA.DAS.Payments.DataLocks.Domain.UnitTests/Services/ProcessCourseValidatorTest.cs
+++ src/SFA.DAS.Payments.DataLocks.Domain.UnitTests/Services/ProcessCourseValidatorTest.cs
@@ -9,6 +9,8 @@ using Moq;
 using SFA.DAS.Payments.DataLocks.Domain.Interfaces;
 using SFA.DAS.Payments.DataLocks.Domain.Models;
 using SFA.DAS.Payments.DataLocks.Domain.Services;
+using SFA.DAS.Payments.Model.Core;
+using SFA.DAS.Payments.Model.Core.Entities;
 
 namespace SFA.DAS.Payments.DataLocks.Domain.UnitTests.Services
 {
@@ -17,10 +19,32 @@ namespace SFA.DAS.Payments.DataLocks.Domain.UnitTests.Services
     {
         private List<ICourseValidator> courseValidators;
         private List<ValidationResult>  courseValidationResults ;
+        private DataLockValidation dataLockValidation;
+
 
         [SetUp]
         public void Prepare()
         {
+            var earningPeriod = new EarningPeriod
+            {
+                Period = 1
+            };
+
+            dataLockValidation = new DataLockValidation
+            {
+                EarningPeriod = earningPeriod,
+                Uln = 100,
+                PriceEpisode = new PriceEpisode(),
+                Apprenticeships = new List<ApprenticeshipModel> {new ApprenticeshipModel()}
+            };
+
+            var courseValidation = new CourseValidation
+            {
+                Period = dataLockValidation.EarningPeriod.Period,
+                PriceEpisode = dataLockValidation.PriceEpisode,
+                Apprenticeships = dataLockValidation.Apprenticeships
+            };
+
              courseValidationResults = new List<ValidationResult>
             {
                 new ValidationResult()
@@ -42,7 +66,7 @@ namespace SFA.DAS.Payments.DataLocks.Domain.UnitTests.Services
         public void ValidateCourseShouldReturnValidationResults()
         {
             var processCourseValidator = new ProcessCourseValidator(courseValidators);
-            var actualResults = processCourseValidator.ValidateCourse(new DataLockValidation());
+            var actualResults = processCourseValidator.ValidateCourse(dataLockValidation);
 
             actualResults.Should().NotBeNull();
             actualResults.Should().HaveCount(courseValidators.Count);
