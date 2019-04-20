f94ab62b6e7f04b2a015dd3686c43a447192100c
kola-samuel <samuel.dev.dba@gmail.com>
2019-04-20 12:40:53 +0100

    DLOCK 12 Changes

---
+++ src/SFA.DAS.Payments.DataLocks.Domain.UnitTests/Services/ApprenticeshipPausedValidatorTest.cs
@@ -0,0 +1,97 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Payments.DataLocks.Domain.Models;
+using SFA.DAS.Payments.DataLocks.Domain.Services;
+using SFA.DAS.Payments.Model.Core;
+using SFA.DAS.Payments.Model.Core.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SFA.DAS.Payments.DataLocks.Domain.UnitTests.Services
+{
+    [TestFixture]
+    public class ApprenticeshipPausedValidatorTest
+    {
+        private string priceEpisodeIdentifier = "pe-1";
+        private byte period = 1;
+
+        [Test]
+        public void WhenStatusIsNotPauseReturnNoDataLockErrors()
+        {
+            var apprenticeship = new ApprenticeshipModel
+            {
+                Status = ApprenticeshipPaymentStatus.Active,
+                ApprenticeshipPriceEpisodes = new List<ApprenticeshipPriceEpisodeModel>
+                {
+                    new ApprenticeshipPriceEpisodeModel
+                    {
+                        Id = 1
+                    }
+                }
+            };
+
+            var apprenticeships = new List<ApprenticeshipModel> { apprenticeship };
+
+            var validation = new CourseValidationModel
+            {
+                PriceEpisode = new PriceEpisode { Identifier = priceEpisodeIdentifier },
+                Period = period,
+                Apprenticeships = apprenticeships
+            };
+
+            var validator = new ApprenticeshipPauseValidator();
+            var result = validator.Validate(validation);
+
+            result.Any().Should().BeFalse();
+        }
+
+        [Test]
+        public void GivenStatusIsPausedReturnDataLockErrors()
+        {
+
+            var apprenticeships = new List<ApprenticeshipModel>
+            {
+                new ApprenticeshipModel
+                {
+                    Status = ApprenticeshipPaymentStatus.Paused,
+                    ApprenticeshipPriceEpisodes = new List<ApprenticeshipPriceEpisodeModel>
+                    {
+                        new ApprenticeshipPriceEpisodeModel
+                        {
+                            Id = 1
+                        }
+                    }
+                },
+                new ApprenticeshipModel
+                {
+                    Status = ApprenticeshipPaymentStatus.Paused,
+                    ApprenticeshipPriceEpisodes = new List<ApprenticeshipPriceEpisodeModel>
+                    {
+                        new ApprenticeshipPriceEpisodeModel
+                        {
+                            Id = 2
+                        }
+                    }
+                 }
+            };
+
+            var validation = new CourseValidationModel
+            {
+                PriceEpisode = new PriceEpisode { Identifier = priceEpisodeIdentifier },
+                Period = period,
+                Apprenticeships = apprenticeships
+            };
+
+            var validator = new ApprenticeshipPauseValidator();
+
+            var result = validator.Validate(validation);
+
+            result.Should().HaveCount(2);
+            result[0].DataLockErrorCode.Should().Be(DataLockErrorCode.DLOCK_12);
+            result[0].ApprenticeshipPriceEpisodeIdentifier.Should().Be(1);
+            result[1].DataLockErrorCode.Should().Be(DataLockErrorCode.DLOCK_12);
+            result[1].ApprenticeshipPriceEpisodeIdentifier.Should().Be(2);
+        }
+
+    }
+}
---
+++ src/SFA.DAS.Payments.DataLocks.Domain/Services/ApprenticeshipPausedValidator.cs
@@ -0,0 +1,34 @@
+﻿using SFA.DAS.Payments.DataLocks.Domain.Interfaces;
+using SFA.DAS.Payments.DataLocks.Domain.Models;
+using SFA.DAS.Payments.Model.Core.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SFA.DAS.Payments.DataLocks.Domain.Services
+{
+    public class ApprenticeshipPauseValidator : ICourseValidator
+    {
+        public List<ValidationResult> Validate(CourseValidationModel validationModel)
+        {
+            var result = new List<ValidationResult>();
+            foreach (var apprenticeshipModel in validationModel.Apprenticeships)
+            {
+                if (apprenticeshipModel.Status == ApprenticeshipPaymentStatus.Paused)
+                {
+                    foreach(var priceEpisode in apprenticeshipModel.ApprenticeshipPriceEpisodes)
+                    {
+                        result.Add(new ValidationResult
+                        {
+                            DataLockErrorCode = DataLockErrorCode.DLOCK_12,
+                            Period = validationModel.Period,
+                            ApprenticeshipPriceEpisodeIdentifier = priceEpisode.Id,
+                            ApprenticeshipId = apprenticeshipModel.Id
+                        });
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
