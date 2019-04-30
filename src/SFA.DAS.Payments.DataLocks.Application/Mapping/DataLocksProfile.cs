870dc892226a2c89413261b6a2ac00556ca181cd
abroodie <abroodie@outlook.com>
2019-05-01 00:02:40 +0100

    fixed dlock mapping for nonpayableearning event

--- src/SFA.DAS.Payments.DataLocks.Application/Mapping/DataLocksProfile.cs
+++ src/SFA.DAS.Payments.DataLocks.Application/Mapping/DataLocksProfile.cs
@@ -17,6 +17,8 @@ namespace SFA.DAS.Payments.DataLocks.Application.Mapping
                 .ForMember(destinationMember => destinationMember.CommitmentVersion, opt => opt.Ignore());
 
             CreateMap<ApprenticeshipContractType1EarningEvent, NonPayableEarningEvent>()
+                .ForMember(destinationMember => destinationMember.EarningEventId, opt => opt.MapFrom(source => source.EventId))
+                .ForMember(destinationMember => destinationMember.EventId, opt => opt.Ignore())
                 .ForMember(x => x.Errors, opt => opt.Ignore());
         }
     }
