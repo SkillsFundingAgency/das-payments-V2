38e8915ae411cb181ebd6b27a76c6a2b698450ee
Carl Abrahams <carl@charmsolutions.co.uk>
2019-05-02 12:00:46 +0100

    Updated UlnService to handle invalid index exception

--- src/SFA.DAS.Payments.AcceptanceTests.Core/Services/UlnService.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.Core/Services/UlnService.cs
@@ -1,7 +1,20 @@
 ﻿namespace SFA.DAS.Payments.AcceptanceTests.Core.Services
 {
+    using System;
+    using DCT.TestDataGenerator;
+
     public class UlnService : IUlnService
     {
-        public long GenerateUln(long index) => DCT.TestDataGenerator.ListOfULNs.ULN(index);
+        public long GenerateUln(long index)
+        {
+            try
+            {
+                return ListOfULNs.ULN(index);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return ListOfULNs.ULN(index + 1);
+            }
+        }
     }
 }
