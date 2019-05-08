dc87b1c0279668a49f9405c486968304c5cf3bf5
abroodie <abroodie@outlook.com>
2019-05-08 12:20:19 +0100

    added more tests to cover removed learning aim processor

--- src/SFA.DAS.Payments.RequiredPayments.Application.UnitTests/Application/Processors/RefundRemovedLearningAimProcessorTests.cs
+++ src/SFA.DAS.Payments.RequiredPayments.Application.UnitTests/Application/Processors/RefundRemovedLearningAimProcessorTests.cs
@@ -13,6 +13,7 @@ using SFA.DAS.Payments.Model.Core;
 using SFA.DAS.Payments.Model.Core.Entities;
 using SFA.DAS.Payments.Model.Core.Incentives;
 using SFA.DAS.Payments.Model.Core.OnProgramme;
+using SFA.DAS.Payments.RequiredPayments.Application.Extensions;
 using SFA.DAS.Payments.RequiredPayments.Application.Infrastructure.Mapping;
 using SFA.DAS.Payments.RequiredPayments.Application.Processors;
 using SFA.DAS.Payments.RequiredPayments.Domain;
@@ -27,6 +28,7 @@ namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.Application.Pr
     {
         private AutoMock mocker;
         private List<PaymentHistoryEntity> history;
+        private IdentifiedRemovedLearningAim identifiedLearner;
 
         [SetUp]
         public void SetUp()
@@ -37,12 +39,7 @@ namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.Application.Pr
             config.AssertConfigurationIsValid();
             var mapper = new Mapper(config);
             mocker.Provide<IMapper>(mapper);
-        }
-
-        [Test]
-        public async Task Refunds_Required_Levy_Payments()
-        {
-            var identifiedLearner = new IdentifiedRemovedLearningAim
+            identifiedLearner = identifiedLearner = new IdentifiedRemovedLearningAim
             {
                 CollectionPeriod = new CollectionPeriod
                 {
@@ -69,11 +66,15 @@ namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.Application.Pr
                 },
                 Ukprn = 7
             };
-            var historicPayment = new PaymentHistoryEntity
+        }
+
+        private PaymentHistoryEntity CreatePaymentHistoryEntity(FundingSourceType fundingSource)
+        {
+            return new PaymentHistoryEntity
             {
                 Amount = 10,
                 SfaContributionPercentage = .9M,
-                TransactionType = (int) OnProgrammeEarningType.Learning,
+                TransactionType = (int)OnProgrammeEarningType.Learning,
                 CollectionPeriod = new CollectionPeriod
                 {
                     AcademicYear = 1819,
@@ -88,12 +89,19 @@ namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.Application.Pr
                 CompletionAmount = 3000,
                 CompletionStatus = 1,
                 ExternalId = Guid.NewGuid(),
-                FundingSource = FundingSourceType.Levy,
+                FundingSource = fundingSource,
                 InstalmentAmount = 1000,
                 NumberOfInstalments = 12,
                 PlannedEndDate = DateTime.Today,
                 StartDate = DateTime.Today.AddMonths(-12),
             };
+
+        }
+
+        [Test]
+        public async Task Refunds_Required_Levy_Payments()
+        {
+            var historicPayment = CreatePaymentHistoryEntity(FundingSourceType.FullyFundedSfa);
             history.Add(historicPayment);
             mocker.Mock<IDataCache<PaymentHistoryEntity[]>>()
                 .Setup(x => x.TryGet(It.IsAny<string>(), It.IsAny<CancellationToken>()))
@@ -115,89 +123,17 @@ namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.Application.Pr
             var refunds = await processor.RefundLearningAim(identifiedLearner, mocker.Mock<IDataCache<PaymentHistoryEntity[]>>().Object, CancellationToken.None).ConfigureAwait(false);
             refunds.Count.Should().Be(1);
             var refund = refunds.First();
-            refund.AmountDue.Should().Be(-10);
-            refund.Should().BeOfType<CalculatedRequiredLevyAmount>();
-            refund.EarningEventId.Should().Be(Guid.Empty);
-            refund.AccountId.Should().Be(historicPayment.AccountId);
-            refund.EventId.Should().NotBe(historicPayment.ExternalId);
-            refund.EventId.Should().NotBe(Guid.Empty);
-            refund.ActualEndDate.Should().Be(historicPayment.ActualEndDate);
-            refund.CollectionPeriod.AcademicYear.Should().Be(identifiedLearner.CollectionPeriod.AcademicYear);
-            refund.CollectionPeriod.Period.Should().Be(identifiedLearner.CollectionPeriod.Period);
-            refund.CompletionAmount.Should().Be(historicPayment.CompletionAmount);
-            refund.ContractType.Should().Be(historicPayment.ContractType);
-            refund.CompletionStatus.Should().Be(historicPayment.CompletionStatus);
-            refund.DeliveryPeriod.Should().Be(historicPayment.DeliveryPeriod);
-            refund.IlrSubmissionDateTime.Should().Be(identifiedLearner.IlrSubmissionDateTime);
-            refund.InstalmentAmount.Should().Be(historicPayment.InstalmentAmount);
-            refund.ActualEndDate.Should().Be(historicPayment.ActualEndDate);
-            refund.JobId.Should().Be(identifiedLearner.JobId);
-            refund.Learner.ReferenceNumber.Should().Be(identifiedLearner.Learner.ReferenceNumber);
-            refund.Learner.Uln.Should().Be(identifiedLearner.Learner.Uln);
-            refund.LearningAim.Reference.Should().Be(identifiedLearner.LearningAim.Reference);
-            refund.LearningAim.FrameworkCode.Should().Be(identifiedLearner.LearningAim.FrameworkCode);
-            refund.LearningAim.FundingLineType.Should().Be(identifiedLearner.LearningAim.FundingLineType);
-            refund.LearningAim.PathwayCode.Should().Be(identifiedLearner.LearningAim.PathwayCode);
-            refund.LearningAim.ProgrammeType.Should().Be(identifiedLearner.LearningAim.ProgrammeType);
-            refund.LearningAim.StandardCode.Should().Be(identifiedLearner.LearningAim.StandardCode);
-
+            var levyRefund = refund as CalculatedRequiredLevyAmount;
+            levyRefund.Should().NotBeNull();
+            levyRefund.AmountDue.Should().Be(-10);
+            levyRefund.ShouldBeMappedTo(historicPayment);
+            levyRefund.ShouldBeMappedTo(identifiedLearner);
         }
 
         [Test]
         public async Task Refunds_Required_CoInvested_Payments()
         {
-            var identifiedLearner = new IdentifiedRemovedLearningAim
-            {
-                CollectionPeriod = new CollectionPeriod
-                {
-                    AcademicYear = 1819,
-                    Period = 1
-                },
-                EventId = Guid.NewGuid(),
-                EventTime = DateTimeOffset.UtcNow,
-                IlrSubmissionDateTime = DateTime.Now,
-                JobId = 1,
-                Learner = new Learner
-                {
-                    ReferenceNumber = "learner-ref-123",
-                    Uln = 2
-                },
-                LearningAim = new LearningAim
-                {
-                    FrameworkCode = 3,
-                    FundingLineType = "funding line type",
-                    PathwayCode = 4,
-                    ProgrammeType = 5,
-                    Reference = "learning-ref-456",
-                    StandardCode = 6
-                },
-                Ukprn = 7
-            };
-            var historicPayment = new PaymentHistoryEntity
-            {
-                Amount = 10,
-                SfaContributionPercentage = .9M,
-                TransactionType = (int) OnProgrammeEarningType.Learning,
-                CollectionPeriod = new CollectionPeriod
-                {
-                    AcademicYear = 1819,
-                    Period = 1
-                },
-                LearnAimReference = "aim-ref-123",
-                LearnerReferenceNumber = "learning-ref-456",
-                PriceEpisodeIdentifier = "pe-1",
-                DeliveryPeriod = 1,
-                Ukprn = 7,
-                ActualEndDate = null,
-                CompletionAmount = 3000,
-                CompletionStatus = 1,
-                ExternalId = Guid.NewGuid(),
-                FundingSource = FundingSourceType.CoInvestedSfa,
-                InstalmentAmount = 1000,
-                NumberOfInstalments = 12,
-                PlannedEndDate = DateTime.Today,
-                StartDate = DateTime.Today.AddMonths(-12),
-            };
+            var historicPayment = CreatePaymentHistoryEntity(FundingSourceType.CoInvestedSfa);
             history.Add(historicPayment);
             mocker.Mock<IDataCache<PaymentHistoryEntity[]>>()
                 .Setup(x => x.TryGet(It.IsAny<string>(), It.IsAny<CancellationToken>()))
@@ -219,89 +155,17 @@ namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.Application.Pr
             var refunds = await processor.RefundLearningAim(identifiedLearner, mocker.Mock<IDataCache<PaymentHistoryEntity[]>>().Object, CancellationToken.None).ConfigureAwait(false);
             refunds.Count.Should().Be(1);
             var refund = refunds.First();
-            refund.AmountDue.Should().Be(-10);
-            refund.Should().BeOfType<CalculatedRequiredCoInvestedAmount>();
-            refund.EarningEventId.Should().Be(Guid.Empty);
-            refund.AccountId.Should().Be(historicPayment.AccountId);
-            refund.EventId.Should().NotBe(historicPayment.ExternalId);
-            refund.EventId.Should().NotBe(Guid.Empty);
-            refund.ActualEndDate.Should().Be(historicPayment.ActualEndDate);
-            refund.CollectionPeriod.AcademicYear.Should().Be(identifiedLearner.CollectionPeriod.AcademicYear);
-            refund.CollectionPeriod.Period.Should().Be(identifiedLearner.CollectionPeriod.Period);
-            refund.CompletionAmount.Should().Be(historicPayment.CompletionAmount);
-            refund.ContractType.Should().Be(historicPayment.ContractType);
-            refund.CompletionStatus.Should().Be(historicPayment.CompletionStatus);
-            refund.DeliveryPeriod.Should().Be(historicPayment.DeliveryPeriod);
-            refund.IlrSubmissionDateTime.Should().Be(identifiedLearner.IlrSubmissionDateTime);
-            refund.InstalmentAmount.Should().Be(historicPayment.InstalmentAmount);
-            refund.ActualEndDate.Should().Be(historicPayment.ActualEndDate);
-            refund.JobId.Should().Be(identifiedLearner.JobId);
-            refund.Learner.ReferenceNumber.Should().Be(identifiedLearner.Learner.ReferenceNumber);
-            refund.Learner.Uln.Should().Be(identifiedLearner.Learner.Uln);
-            refund.LearningAim.Reference.Should().Be(identifiedLearner.LearningAim.Reference);
-            refund.LearningAim.FrameworkCode.Should().Be(identifiedLearner.LearningAim.FrameworkCode);
-            refund.LearningAim.FundingLineType.Should().Be(identifiedLearner.LearningAim.FundingLineType);
-            refund.LearningAim.PathwayCode.Should().Be(identifiedLearner.LearningAim.PathwayCode);
-            refund.LearningAim.ProgrammeType.Should().Be(identifiedLearner.LearningAim.ProgrammeType);
-            refund.LearningAim.StandardCode.Should().Be(identifiedLearner.LearningAim.StandardCode);
-
+            var conInvestRefund = refund as CalculatedRequiredCoInvestedAmount;
+            conInvestRefund.Should().NotBeNull();
+            conInvestRefund.AmountDue.Should().Be(-10);
+            conInvestRefund.ShouldBeMappedTo(historicPayment);
+            conInvestRefund.ShouldBeMappedTo(identifiedLearner);
         }
 
-[Test]
+        [Test]
         public async Task Refunds_Required_Incentive_Payments()
         {
-            var identifiedLearner = new IdentifiedRemovedLearningAim
-            {
-                CollectionPeriod = new CollectionPeriod
-                {
-                    AcademicYear = 1819,
-                    Period = 1
-                },
-                EventId = Guid.NewGuid(),
-                EventTime = DateTimeOffset.UtcNow,
-                IlrSubmissionDateTime = DateTime.Now,
-                JobId = 1,
-                Learner = new Learner
-                {
-                    ReferenceNumber = "learner-ref-123",
-                    Uln = 2
-                },
-                LearningAim = new LearningAim
-                {
-                    FrameworkCode = 3,
-                    FundingLineType = "funding line type",
-                    PathwayCode = 4,
-                    ProgrammeType = 5,
-                    Reference = "learning-ref-456",
-                    StandardCode = 6
-                },
-                Ukprn = 7
-            };
-            var historicPayment = new PaymentHistoryEntity
-            {
-                Amount = 10,
-                SfaContributionPercentage = .9M,
-                TransactionType = (int) IncentiveEarningType.Balancing16To18FrameworkUplift,
-                CollectionPeriod = new CollectionPeriod
-                {
-                    AcademicYear = 1819,
-                    Period = 1
-                },
-                LearnAimReference = "aim-ref-123",
-                LearnerReferenceNumber = "learning-ref-456",
-                PriceEpisodeIdentifier = "pe-1",
-                DeliveryPeriod = 1,
-                Ukprn = 7,
-                ActualEndDate = null,
-                CompletionAmount = 3000,
-                CompletionStatus = 1,
-                ExternalId = Guid.NewGuid(),
-                FundingSource = FundingSourceType.FullyFundedSfa,
-                InstalmentAmount = 1000,
-                NumberOfInstalments = 12,
-                PlannedEndDate = DateTime.Today,
-                StartDate = DateTime.Today.AddMonths(-12),
-            };
+            var historicPayment = CreatePaymentHistoryEntity(FundingSourceType.FullyFundedSfa);
             history.Add(historicPayment);
             mocker.Mock<IDataCache<PaymentHistoryEntity[]>>()
                 .Setup(x => x.TryGet(It.IsAny<string>(), It.IsAny<CancellationToken>()))
@@ -323,33 +187,64 @@ namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.Application.Pr
             var refunds = await processor.RefundLearningAim(identifiedLearner, mocker.Mock<IDataCache<PaymentHistoryEntity[]>>().Object, CancellationToken.None).ConfigureAwait(false);
             refunds.Count.Should().Be(1);
             var refund = refunds.First();
-            refund.AmountDue.Should().Be(-10);
-            refund.Should().BeOfType<CalculatedRequiredIncentiveAmount>();
-            refund.EarningEventId.Should().Be(Guid.Empty);
-            refund.AccountId.Should().Be(historicPayment.AccountId);
-            refund.EventId.Should().NotBe(historicPayment.ExternalId);
-            refund.EventId.Should().NotBe(Guid.Empty);
-            refund.ActualEndDate.Should().Be(historicPayment.ActualEndDate);
-            refund.CollectionPeriod.AcademicYear.Should().Be(identifiedLearner.CollectionPeriod.AcademicYear);
-            refund.CollectionPeriod.Period.Should().Be(identifiedLearner.CollectionPeriod.Period);
-            refund.CompletionAmount.Should().Be(historicPayment.CompletionAmount);
-            refund.ContractType.Should().Be(historicPayment.ContractType);
-            refund.CompletionStatus.Should().Be(historicPayment.CompletionStatus);
-            refund.DeliveryPeriod.Should().Be(historicPayment.DeliveryPeriod);
-            refund.IlrSubmissionDateTime.Should().Be(identifiedLearner.IlrSubmissionDateTime);
-            refund.InstalmentAmount.Should().Be(historicPayment.InstalmentAmount);
-            refund.ActualEndDate.Should().Be(historicPayment.ActualEndDate);
-            refund.JobId.Should().Be(identifiedLearner.JobId);
-            refund.Learner.ReferenceNumber.Should().Be(identifiedLearner.Learner.ReferenceNumber);
-            refund.Learner.Uln.Should().Be(identifiedLearner.Learner.Uln);
-            refund.LearningAim.Reference.Should().Be(identifiedLearner.LearningAim.Reference);
-            refund.LearningAim.FrameworkCode.Should().Be(identifiedLearner.LearningAim.FrameworkCode);
-            refund.LearningAim.FundingLineType.Should().Be(identifiedLearner.LearningAim.FundingLineType);
-            refund.LearningAim.PathwayCode.Should().Be(identifiedLearner.LearningAim.PathwayCode);
-            refund.LearningAim.ProgrammeType.Should().Be(identifiedLearner.LearningAim.ProgrammeType);
-            refund.LearningAim.StandardCode.Should().Be(identifiedLearner.LearningAim.StandardCode);
-
+            var incentiveRefund = refund as CalculatedRequiredIncentiveAmount;
+            incentiveRefund.Should().NotBeNull();
+            incentiveRefund.AmountDue.Should().Be(-10);
+            incentiveRefund.ShouldBeMappedTo(historicPayment);
+            incentiveRefund.ShouldBeMappedTo(identifiedLearner);
         }
 
+        [Test]
+        public async Task Refunds_All_Required_Payments()
+        {
+            var historicLevyPayment = CreatePaymentHistoryEntity(FundingSourceType.Levy);
+            historicLevyPayment.Amount = 100;
+            var historicCoInvestedPayment = CreatePaymentHistoryEntity(FundingSourceType.CoInvestedSfa);
+            historicCoInvestedPayment.Amount = 50;
+            var historicIncentivePayment = CreatePaymentHistoryEntity(FundingSourceType.FullyFundedSfa);
+            historicIncentivePayment.Amount = 25;
+            history.Add(historicLevyPayment);
+            history.Add(historicCoInvestedPayment);
+            history.Add(historicIncentivePayment);
+            mocker.Mock<IDataCache<PaymentHistoryEntity[]>>()
+                .Setup(x => x.TryGet(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ConditionalValue<PaymentHistoryEntity[]>(true, history.ToArray()));
+            mocker.Mock<IRefundRemovedLearningAimService>()
+                .Setup(x => x.RefundLearningAim(It.IsAny<List<Payment>>()))
+                .Returns(new List<RequiredPayment>
+                {
+                    new RequiredPayment{Amount = historicLevyPayment.Amount*-1, SfaContributionPercentage = .95M, EarningType = EarningType.Levy, PriceEpisodeIdentifier = "pe-1"},
+                    new RequiredPayment{Amount = historicCoInvestedPayment.Amount*-1, SfaContributionPercentage = .95M, EarningType = EarningType.CoInvested, PriceEpisodeIdentifier = "pe-1"},
+                    new RequiredPayment{Amount = historicIncentivePayment.Amount*-1, SfaContributionPercentage = .95M, EarningType = EarningType.Incentive, PriceEpisodeIdentifier = "pe-1"}
+                });
+
+            mocker.Mock<IPeriodisedRequiredPaymentEventFactory>()
+                .Setup(x => x.Create(It.Is<EarningType>(et => et == EarningType.Levy), It.IsAny<int>()))
+                .Returns(new CalculatedRequiredLevyAmount
+                {
+                    OnProgrammeEarningType = OnProgrammeEarningType.Learning,
+                });
+
+            mocker.Mock<IPeriodisedRequiredPaymentEventFactory>()
+                .Setup(x => x.Create(It.Is<EarningType>(et => et == EarningType.CoInvested), It.IsAny<int>()))
+                .Returns(new CalculatedRequiredCoInvestedAmount
+                {
+                    OnProgrammeEarningType = OnProgrammeEarningType.Learning
+                });
+
+            mocker.Mock<IPeriodisedRequiredPaymentEventFactory>()
+                .Setup(x => x.Create(It.Is<EarningType>(et => et == EarningType.Incentive), It.IsAny<int>()))
+                .Returns(new CalculatedRequiredIncentiveAmount
+                {
+                    Type = IncentivePaymentType.Balancing16To18FrameworkUplift
+                });
+
+            var processor = mocker.Create<RefundRemovedLearningAimProcessor>();
+            var refunds = await processor.RefundLearningAim(identifiedLearner, mocker.Mock<IDataCache<PaymentHistoryEntity[]>>().Object, CancellationToken.None).ConfigureAwait(false);
+            refunds.Count.Should().Be(3);
+            refunds.Count(refund => refund.AmountDue == historicLevyPayment.Amount * -1 && refund is CalculatedRequiredLevyAmount).Should().Be(1);
+            refunds.Count(refund => refund.AmountDue == historicCoInvestedPayment.Amount * -1 && refund is CalculatedRequiredCoInvestedAmount).Should().Be(1);
+            refunds.Count(refund => refund.AmountDue == historicIncentivePayment.Amount * -1 && refund is CalculatedRequiredIncentiveAmount).Should().Be(1);
+        }
     }
 }
