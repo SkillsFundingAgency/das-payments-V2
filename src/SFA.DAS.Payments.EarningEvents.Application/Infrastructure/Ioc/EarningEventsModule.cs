d685b1226e694da15f3c25f9cf0c902e5bbfb930
Maxim Beliy <maximbel@gmail.com>
2019-05-13 13:45:39 +0100

    IoC and settings update for earning events

--- src/SFA.DAS.Payments.EarningEvents.Application/Infrastructure/Ioc/EarningEventsModule.cs
+++ src/SFA.DAS.Payments.EarningEvents.Application/Infrastructure/Ioc/EarningEventsModule.cs
@@ -1,4 +1,6 @@
 ﻿using Autofac;
+using SFA.DAS.Payments.Application.Data.Configurations;
+using SFA.DAS.Payments.Application.Repositories;
 using SFA.DAS.Payments.EarningEvents.Application.Interfaces;
 using SFA.DAS.Payments.EarningEvents.Application.Mapping;
 using SFA.DAS.Payments.EarningEvents.Application.Services;
@@ -6,6 +8,7 @@ using SFA.DAS.Payments.EarningEvents.Domain;
 using SFA.DAS.Payments.EarningEvents.Domain.Mapping;
 using SFA.DAS.Payments.EarningEvents.Domain.Validation.Global;
 using SFA.DAS.Payments.EarningEvents.Domain.Validation.Learner;
+using SFA.DAS.Payments.Model.Core.Entities;
 
 namespace SFA.DAS.Payments.EarningEvents.Application.Infrastructure.Ioc
 {
@@ -33,6 +36,15 @@ namespace SFA.DAS.Payments.EarningEvents.Application.Infrastructure.Ioc
                 .InstancePerLifetimeScope();
             builder.RegisterType<ApprenticeshipContractType2EarningEventsService>()
                 .As<IEarningEventsProcessingService>()
+                .InstancePerLifetimeScope();
+            builder.RegisterType<SubmittedLearnerAimBuilder>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+            builder.RegisterType<SubmittedLearnerAimBulkCopyConfiguration>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+            builder.RegisterType<BulkWriter<SubmittedLearnerAimModel>>()
+                .AsImplementedInterfaces()
                 .InstancePerLifetimeScope();
             //builder.Register(c => new ApprenticeshipContractType2EarningEventsService
             //(
