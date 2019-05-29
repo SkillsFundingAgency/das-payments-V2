3944239c76f03c6d28186d8d1cdc987a9c0328db
Carl Abrahams <carl@charmsolutions.co.uk>
2019-05-29 10:13:52 +0100

    Added back code to allow tests to pass in DAS only mode

--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/EventMatchers/BaseMatcher.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/EventMatchers/BaseMatcher.cs
@@ -19,9 +19,15 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.EventMatchers
         {
             var actualPayments = GetActualEvents();
 
+            actualPayments = actualPayments.Where(x => (x is FunctionalSkillEarningsEvent &&
+                                                        (x as FunctionalSkillEarningsEvent).Earnings.Any()) ||
+                                                       (x as FunctionalSkillEarningsEvent) == null)
+                .ToList();
+
+
             var expectedPayments = GetExpectedEvents();
 
-            // remove any FunctionalSkillEarningsEvent when we are not expecting any (only if they have all 0 values)
+            //remove any FunctionalSkillEarningsEvent when we are not expecting any(only if they have all 0 values)
             if (expectedPayments.All(x => x.GetType() != typeof(FunctionalSkillEarningsEvent)))
             {
                 actualPayments = RemoveEmptyFunctionSkillEarningEvent(actualPayments);
