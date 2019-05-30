e1edc768099e4d040aef4eb2e441c1c60edf445e
Tony <abroodie@gmail.com>
2019-05-30 18:16:48 +0100

    tidy up

--- src/SFA.DAS.Payments.FundingSource.LevyFundedProxyService/Handlers/MonthEndCommandHandler.cs
+++ src/SFA.DAS.Payments.FundingSource.LevyFundedProxyService/Handlers/MonthEndCommandHandler.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Actors;
 using Microsoft.ServiceFabric.Actors.Client;
@@ -8,7 +7,6 @@ using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
 using SFA.DAS.Payments.FundingSource.LevyFundedService.Interfaces;
 using SFA.DAS.Payments.FundingSource.Messages.Commands;
-using SFA.DAS.Payments.FundingSource.Messages.Events;
 using SFA.DAS.Payments.FundingSource.Messages.Internal.Commands;
 
 namespace SFA.DAS.Payments.FundingSource.LevyFundedProxyService.Handlers
