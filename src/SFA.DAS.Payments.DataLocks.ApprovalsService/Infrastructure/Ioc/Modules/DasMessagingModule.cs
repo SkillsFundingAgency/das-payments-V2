4c8387933ad5c7af78bd2518b6673f76138611ce
Tony <abroodie@gmail.com>
2019-06-05 12:47:47 +0100

    Added das messaging module

---
+++ src/SFA.DAS.Payments.DataLocks.ApprovalsService/Infrastructure/Ioc/Modules/DasMessagingModule.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Autofac;
+using NServiceBus;
+using NServiceBus.Features;
+using SFA.DAS.Payments.Application.Messaging;
+using SFA.DAS.Payments.Application.Messaging.Telemetry;
+using SFA.DAS.Payments.Core.Configuration;
+
+namespace SFA.DAS.Payments.DataLocks.ApprovalsService.Infrastructure.Ioc.Modules
+{
+    public class DasMessagingModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register((c, p) =>
+            {
+                var config = c.Resolve<IApplicationConfiguration>();
+                var configHelper = c.Resolve<IConfigurationHelper>();
+                var endpointConfiguration = new EndpointConfiguration(config.EndpointName);
+
+                var conventions = endpointConfiguration.Conventions();
+                conventions.DefiningMessagesAs(type => (type.Namespace?.StartsWith("SFA.DAS.Payments") ?? false) && (type.Namespace?.Contains(".Messages") ?? false));
+                conventions.DefiningCommandsAs(type => (type.Namespace?.StartsWith("SFA.DAS.Payments") ?? false) && (type.Namespace?.Contains(".Messages.Commands") ?? false));
+                conventions.DefiningEventsAs(type => (type.Namespace?.StartsWith("SFA.DAS.Payments") ?? false) && (type.Namespace?.Contains(".Messages.Events") ?? false));
+
+                var persistence = endpointConfiguration.UsePersistence<AzureStoragePersistence>();
+                persistence.ConnectionString(config.StorageConnectionString);
+
+                endpointConfiguration.DisableFeature<TimeoutManager>();
+                var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+                transport
+                    .ConnectionString(configHelper.GetConnectionString("DASServiceBusConnectionString"))
+                    .Transactions(TransportTransactionMode.ReceiveOnly)
+                    .RuleNameShortener(ruleName => ruleName.Split('.').LastOrDefault() ?? ruleName);
+                builder.RegisterInstance(transport)
+                    .As<TransportExtensions<AzureServiceBusTransport>>()
+                    .SingleInstance();
+                EndpointConfigurationEvents.OnConfiguringTransport(transport);  //TODO: find AutoFac & NSB way to do this
+                endpointConfiguration.SendFailedMessagesTo(config.FailedMessagesQueue);
+                endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+                endpointConfiguration.EnableInstallers();
+                endpointConfiguration.Pipeline.Register(typeof(TelemetryHandlerBehaviour), "Sends handler timing to telemetry service.");
+                endpointConfiguration.EnableCallbacks(makesRequests: false);
+
+                if(config.ProcessMessageSequentially) endpointConfiguration.LimitMessageProcessingConcurrencyTo(1);
+                
+                endpointConfiguration.Pipeline.Register(typeof(ExceptionHandlingBehavior),"Logs exceptions to the payments logger");
+                return endpointConfiguration;
+            })
+            .As<EndpointConfiguration>()
+            .Named<EndpointConfiguration>("DASEndpointConfiguration")
+            .SingleInstance();
+        }
+    }
+}
