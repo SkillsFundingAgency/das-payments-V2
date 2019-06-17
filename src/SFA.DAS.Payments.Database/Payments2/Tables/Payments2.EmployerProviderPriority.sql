849c4b042b8146bc919411cdcfcf141940344382
kola-samuel <samuel.dev.dba@gmail.com>
2019-06-17 11:03:21 +0100

    Employer Payment Priority Database Changes

---
+++ src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.EmployerProviderPriority.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Payments2].[EmployerProviderPriority]
+(
+	Id bigint not null identity Constraint Pk_EmployerProviderPriority Primary Key,
+	EmployerAccountId bigint not null,
+	Ukprn bigint not null,
+	[Order] int not null
+)
