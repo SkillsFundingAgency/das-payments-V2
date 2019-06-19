27e5b6b6e6c80dfaa5db00a48c6dfddb1e0c1f0d
Carl Abrahams <carl@charmsolutions.co.uk>
2019-06-19 16:35:30 +0100

    Updates for 330

--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/LearnerMutators/LearnerMutatorFactory.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/LearnerMutators/LearnerMutatorFactory.cs
@@ -34,6 +34,7 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.LearnerMutators
                 case "487":
                 case "326":
                 case "329":
+                case "330":
                     return new Framework403Learner(learners, featureNumber);
                 case "443":
                     return new Standard57Learner(learners, featureNumber);
