c2a4bff3452cd4571883af1f121a8d69a790b6cd
Maxim Beliy <maximbel@gmail.com>
2019-06-28 15:23:17 +0100

    new table

---
+++ src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.DataLockFailure.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [Payments2].[DataLockFailure]
+(
+	[Id] BIGINT NOT NULL CONSTRAINT PK_DataLockFailure PRIMARY KEY IDENTITY, 
+    [Ukprn] BIGINT NOT NULL, 
+    [LearnerReferenceNumber] NVARCHAR(50) NOT NULL, 
+    [LearningAimReference] NVARCHAR(8) NOT NULL, 
+    [LearningAimProgrammeType] INT NOT NULL, 
+    [LearningAimStandardCode] INT NOT NULL, 
+    [LearningAimFrameworkCode] INT NOT NULL, 
+    [LearningAimPathwayCode] INT NOT NULL, 
+    [AcademicYear] SMALLINT NOT NULL, 
+    [DeliveryPeriod] TINYINT NOT NULL, 
+    [Errors] NVARCHAR(MAX) NOT NULL
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_DataLockFailure_Key] ON [Payments2].[DataLockFailure] ([Ukprn], [LearnerReferenceNumber], [LearningAimFrameworkCode], [LearningAimPathwayCode], [LearningAimProgrammeType], [LearningAimReference], [LearningAimStandardCode], [AcademicYear], [DeliveryPeriod])
