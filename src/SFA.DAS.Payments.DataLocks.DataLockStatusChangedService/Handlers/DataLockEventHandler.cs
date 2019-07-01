1922e2353640f65d640c59ec4aa542b6d6ee3863
Maxim Beliy <maximbel@gmail.com>
2019-07-01 15:08:22 +0100

    PV2-1234: stateless service for datalockstatuschange

---
+++ src/SFA.DAS.Payments.DataLocks.DataLockStatusChangedService/Handlers/DataLockEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.DataLocks.Messages.Events;
+
+namespace SFA.DAS.Payments.DataLocks.DataLockStatusChangedService.Handlers
+{
+    public class DataLockEventHandler : IHandleMessages<DataLockEvent>
+    {
+        private readonly IPaymentLogger paymentLogger;
+
+        public DataLockEventHandler(IPaymentLogger paymentLogger)
+        {
+            this.paymentLogger = paymentLogger;
+        }
+
+        public Task Handle(DataLockEvent message, IMessageHandlerContext context)
+        {
+            paymentLogger.LogDebug($"Processing {message.GetType().Name} event for UKPRN {message.Ukprn}");
+            return Task.CompletedTask;
+        }
+    }
+}
