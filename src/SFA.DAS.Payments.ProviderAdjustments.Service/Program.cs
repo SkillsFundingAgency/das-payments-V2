bd2b0468392ea643f4b3bedb2acefb928d74c9e1
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-07-01 15:28:34 +0100

    Updated to reflect the fact that the provider adjustment service will use NSB over ASB

--- src/SFA.DAS.Payments.ProviderAdjustments.Service/Program.cs
+++ src/SFA.DAS.Payments.ProviderAdjustments.Service/Program.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Diagnostics;
-using System.Fabric;
 using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.ServiceFabric.Services.Runtime;
+using SFA.DAS.Payments.ServiceFabric.Core.Infrastructure.Ioc;
 
 namespace SFA.DAS.Payments.ProviderAdjustments.Service
 {
@@ -16,18 +13,10 @@ namespace SFA.DAS.Payments.ProviderAdjustments.Service
         {
             try
             {
-                // The ServiceManifest.XML file defines one or more service type names.
-                // Registering a service maps a service type name to a .NET type.
-                // When Service Fabric creates an instance of this service type,
-                // an instance of the class is created in this host process.
-
-                ServiceRuntime.RegisterServiceAsync("SFA.DAS.Payments.ProviderAdjustments.ServiceType",
-                    context => new Service(context)).GetAwaiter().GetResult();
-
-                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(Service).Name);
-
-                // Prevents this host process from terminating so services keep running.
-                Thread.Sleep(Timeout.Infinite);
+                using (ServiceFabricContainerFactory.CreateContainerForStatelessService<Service>())
+                {
+                    Thread.Sleep(Timeout.Infinite);
+                }
             }
             catch (Exception e)
             {
--- src/SFA.DAS.Payments.ProviderAdjustments.Service/Service.cs
+++ src/SFA.DAS.Payments.ProviderAdjustments.Service/Service.cs
@@ -1,22 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.Fabric;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Autofac;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.ServiceFabric.Core;
 
 namespace SFA.DAS.Payments.ProviderAdjustments.Service
 {
     /// <summary>
     /// An instance of this class is created for each service instance by the Service Fabric runtime.
     /// </summary>
-    internal sealed class Service : StatelessService
+    public class Service : StatelessService
     {
-        public Service(StatelessServiceContext context)
+        private readonly ILifetimeScope lifetimeScope;
+        private readonly IPaymentLogger logger;
+
+        public Service(StatelessServiceContext context, ILifetimeScope lifetimeScope, IPaymentLogger logger)
             : base(context)
-        { }
+        {
+            this.lifetimeScope = lifetimeScope ?? throw new ArgumentNullException(nameof(lifetimeScope));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
 
         /// <summary>
         /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
@@ -24,7 +32,10 @@ namespace SFA.DAS.Payments.ProviderAdjustments.Service
         /// <returns>A collection of listeners.</returns>
         protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
         {
-            return new ServiceInstanceListener[0];
+            return new List<ServiceInstanceListener>
+            {
+                new ServiceInstanceListener(context =>lifetimeScope.Resolve<IStatelessEndpointCommunicationListener>())
+            };
         }
 
         /// <summary>
@@ -33,18 +44,26 @@ namespace SFA.DAS.Payments.ProviderAdjustments.Service
         /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
         protected override async Task RunAsync(CancellationToken cancellationToken)
         {
-            // TODO: Replace the following sample code with your own logic 
-            //       or remove this RunAsync override if it's not needed in your service.
+            var initialised = false;
 
-            long iterations = 0;
-
-            while (true)
+            try
             {
-                cancellationToken.ThrowIfCancellationRequested();
-
-                ServiceEventSource.Current.ServiceMessage(this.Context, "Working-{0}", ++iterations);
-
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+                logger.LogDebug("Starting the Provider Ajustments service.");
+                initialised = true;
+                logger.LogInfo("Started the Provider Adjustments service.");
+                await Task.Delay(Timeout.Infinite, cancellationToken);
+            }
+            catch (Exception exception) when (!(exception is TaskCanceledException))
+            {
+                // Ignore, as an exception is only really thrown on cancellation of the token.
+                logger.LogError($"Reference Data Stateless Service Exception. Error: {exception}.", exception);
+            }
+            finally
+            {
+                if (initialised)
+                {
+                    logger.LogInfo("Provider Adjustments Stateless Service End");
+                }
             }
         }
     }
