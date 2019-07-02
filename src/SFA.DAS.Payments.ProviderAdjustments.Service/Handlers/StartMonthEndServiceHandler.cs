3f4b5cded409ab0659a46d88d62bb7f15ef06ced
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-07-02 08:37:39 +0100

    Added projects for the different layers

---
+++ src/SFA.DAS.Payments.ProviderAdjustments.Service/Handlers/StartMonthEndServiceHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace SFA.DAS.Payments.ProviderAdjustments.Service.Handlers
+{
+    class StartMonthEndServiceHandler : IHandleMessages<StartMonthEndServiceHandler>
+    {
+        public Task Handle(StartMonthEndServiceHandler message, IMessageHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
