c3f029b8330042d427c17adad66319ef98b75142
Carl Abrahams <carl@charmsolutions.co.uk>
2019-07-03 08:57:18 +0100

    Changes for 489

--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/LearnerMutators/LearnerMutatorFactory.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/LearnerMutators/LearnerMutatorFactory.cs
@@ -39,6 +39,7 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.LearnerMutators
                 case "326":
                     return new Framework403Learner(learners, featureNumber);
                 case "443":
+                case "489":
                     return new Standard57Learner(learners, featureNumber);
                 case "277":
                     return new Framework593LearnerWithdrawsAfterQualifyingPeriod(learners);
