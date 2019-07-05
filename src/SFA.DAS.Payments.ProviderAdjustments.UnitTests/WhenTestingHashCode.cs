34b8a55e5a62366ea9b4b14c3e5d48f1ee4987d9
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-07-05 16:16:50 +0100

    Added first test. Not building

---
+++ src/SFA.DAS.Payments.ProviderAdjustments.UnitTests/WhenTestingHashCode.cs
@@ -0,0 +1,79 @@
+﻿
+
+using NUnit.Framework;
+
+namespace SFA.DAS.Payments.Calc.ProviderAdjustments.UnitTests.DomainTests.GivenAProviderPaymentsGroup
+{
+    [TestFixture]
+    public class WhenTestingHashCode
+    {
+        [Test]
+        [AutoData]
+        public void WithDifferringUkprnAreNotEqual(
+            AdjustmentEntity earning1,
+            AdjustmentEntity earning2)
+        {
+            earning2.PaymentType = earning1.PaymentType;
+            earning2.SubmissionCollectionPeriod = earning1.SubmissionCollectionPeriod;
+
+            var sut = new ProviderPaymentsGroup(earning1);
+            var test = new ProviderPaymentsGroup(earning2);
+
+            var actual = sut.GetHashCode() == test.GetHashCode();
+
+            actual.Should().BeFalse();
+        }
+
+        [Test]
+        [AutoData]
+        public void WithDifferringPaymentTypeAreNotEqual(
+            AdjustmentEntity earning1,
+            AdjustmentEntity earning2)
+        {
+            earning2.Ukprn = earning1.Ukprn;
+            earning2.SubmissionCollectionPeriod = earning1.SubmissionCollectionPeriod;
+
+            var sut = new ProviderPaymentsGroup(earning1);
+            var test = new ProviderPaymentsGroup(earning2);
+
+            var actual = sut.GetHashCode() == test.GetHashCode();
+
+            actual.Should().BeFalse();
+        }
+
+        [Test]
+        [AutoData]
+        public void WithDifferringPeriodAreNotEqual(
+            AdjustmentEntity earning1,
+            AdjustmentEntity earning2)
+        {
+            earning2.PaymentType = earning1.PaymentType;
+            earning2.Ukprn = earning1.Ukprn;
+
+            var sut = new ProviderPaymentsGroup(earning1);
+            var test = new ProviderPaymentsGroup(earning2);
+
+            var actual = sut.GetHashCode() == test.GetHashCode();
+
+            actual.Should().BeFalse();
+        }
+
+        [Test]
+        [AutoData]
+        public void WithSameUkprnPaymentTypeAndPeriodAreSame(
+            AdjustmentEntity earning1,
+            AdjustmentEntity earning2)
+        {
+            earning2.PaymentType = earning1.PaymentType;
+            earning2.SubmissionCollectionPeriod = earning1.SubmissionCollectionPeriod;
+            earning2.Ukprn = earning1.Ukprn;
+
+            var sut = new ProviderPaymentsGroup(earning1);
+            var test = new ProviderPaymentsGroup(earning2);
+
+            var actual = sut.GetHashCode() == test.GetHashCode();
+
+            actual.Should().BeTrue();
+        }
+    }
+}
