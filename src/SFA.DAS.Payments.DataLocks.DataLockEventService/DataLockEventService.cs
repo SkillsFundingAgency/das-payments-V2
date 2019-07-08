82e53aeee93ae263482b88817789c39bdc20ce31
Maxim Beliy <maximbel@gmail.com>
2019-07-08 15:46:55 +0100

    service fabric app

---
+++ src/SFA.DAS.Payments.DataLocks.DataLockEventService/DataLockEventService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Audit.Application.ServiceFabric.Infrastructure;
+using SFA.DAS.Payments.ServiceFabric.Core;
+
+namespace SFA.DAS.Payments.DataLocks.DataLockEventService
+{
+    [StatePersistence(StatePersistence.Persisted)]
+    internal sealed class DataLockEventService : AuditStatefulService<DataLockStatusChanged>
+    {
+        private IStatefulEndpointCommunicationListener listener;
+        private readonly ILifetimeScope lifetimeScope;
+        private readonly IPaymentLogger paymentLogger;
+
+        public DataLockEventService(StatefulServiceContext context)
+            : base(context)
+        { }
+
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            paymentLogger.LogInfo("Creating Service Replica Listeners For DataLockEventService");
+            return new List<ServiceReplicaListener>
+            {
+                new ServiceReplicaListener(context => listener = lifetimeScope.Resolve<IStatefulEndpointCommunicationListener>())
+            };
+        }
+    }
+}
