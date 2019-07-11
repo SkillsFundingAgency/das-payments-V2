57b03955376c99ada58a514ec9b51164ae60caf5
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-07-11 11:25:14 +0100

    Added ApprenticeshipPause table

---
+++ src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.ApprenticeshipPause.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Payments2].[ApprenticeshipPause]
+(
+	[Id] BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	ApprenticeshipId BIGINT NOT NULL,
+	PauseDate DATE NOT NULL,
+	ResumeDate DATE NULL
+)
+
+GO
+
+CREATE INDEX [IX_ApprenticeshipPause_ApprenticeshipId] ON [Payments2].[ApprenticeshipPause] (ApprenticeshipId)
