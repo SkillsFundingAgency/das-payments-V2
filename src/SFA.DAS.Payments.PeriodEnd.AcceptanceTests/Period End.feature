34445c78579c6adeecac5bd6af966d962bd8446d
Tony <abroodie@gmail.com>
2019-07-14 23:39:01 +0100

    adding acceptance tests for period end service

---
+++ src/SFA.DAS.Payments.PeriodEnd.AcceptanceTests/Period End.feature
@@ -0,0 +1,11 @@
+﻿Feature: Period End
+	To allow Payments System Administrators and other interested stake-holders to know the current status of the payments infrastructure 
+	the Payments Job Monitoring records the completion status of jobs.
+
+Background:
+	Given the payments are for the current collection year
+	And the current collection period is R01
+
+Scenario: Period End Started Job
+	When the period end service is notified the the period end has started
+	Then the period end service should publish a period end started event
---
+++ src/SFA.DAS.Payments.PeriodEnd.AcceptanceTests/PeriodEndSteps.cs
@@ -0,0 +1,40 @@
+﻿using SFA.DAS.Payments.AcceptanceTests.Core;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Payments.PeriodEnd.AcceptanceTests
+{
+    [Binding]
+    public class PeriodEndSteps: StepsBase
+    {
+        public PeriodEndSteps(ScenarioContext context) : base(context)
+        {
+
+        }
+
+        [Given(@"the payments are for the current collection year")]
+        public void GivenThePaymentsAreForTheCurrentCollectionYear()
+        {
+            SetCurrentCollectionYear();
+        }
+
+        [Given(@"the current collection period is R(.*)")]
+        [Given(@"the current processing period is (.*)")]
+        public void GivenTheCurrentProcessingPeriodIs(byte period)
+        {
+            CollectionPeriod = period;
+        }
+
+        [When(@"the period end service is notified the the period end has started")]
+        public void WhenThePeriodEndServiceIsNotifiedTheThePeriodEndHasStarted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the period end service should publish a period end started event")]
+        public void ThenThePeriodEndServiceShouldPublishAPeriodEndStartedEvent()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
