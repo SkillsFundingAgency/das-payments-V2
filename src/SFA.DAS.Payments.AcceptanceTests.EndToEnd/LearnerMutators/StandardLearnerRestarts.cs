aab217e26ec65d54e951c73330fbcfe05f48e068
Mark Keene <mark@keenesolutions.co.uk>
2019-07-17 14:58:45 +0000

    Updates for 897

---
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/LearnerMutators/StandardLearnerRestarts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Model.Loose;
+using SFA.DAS.Payments.AcceptanceTests.Core.Data;
+
+namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.LearnerMutators
+{
+    public class StandardLearnerRestarts :StandardLearner
+    {
+        public StandardLearnerRestarts(IEnumerable<Learner> learners, string featureNumber) : base(learners, featureNumber)
+        {
+        }
+
+        protected override void DoSpecificMutate(MessageLearner learner, Learner request)
+        {
+            base.DoSpecificMutate(learner, request);
+            DCT.TestDataGenerator.Helpers.AddLearningDeliveryRestartFAM(learner);
+        }
+    }
+}
