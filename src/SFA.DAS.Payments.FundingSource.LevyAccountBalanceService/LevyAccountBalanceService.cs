342815431ea936da814b1726290866dc06276bda
kola-samuel <samuel.dev.dba@gmail.com>
2019-07-25 08:52:07 +0100

    Clean Up

--- src/SFA.DAS.Payments.FundingSource.LevyAccountBalanceService/LevyAccountBalanceService.cs
+++ src/SFA.DAS.Payments.FundingSource.LevyAccountBalanceService/LevyAccountBalanceService.cs
@@ -22,30 +22,17 @@ namespace SFA.DAS.Payments.FundingSource.LevyAccountBalanceService
         
         protected override async Task RunAsync(CancellationToken cancellationToken)
         {
-            // TODO: Replace the following sample code with your own logic 
-            //       or remove this RunAsync override if it's not needed in your service.
-
-            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
-
             while (true)
             {
                 cancellationToken.ThrowIfCancellationRequested();
 
-                using (var tx = this.StateManager.CreateTransaction())
-                {
-                    var result = await myDictionary.TryGetValueAsync(tx, "Counter");
 
-                    ServiceEventSource.Current.ServiceMessage(this.Context, "Current Counter Value: {0}",
-                        result.HasValue ? result.Value.ToString() : "Value does not exist.");
 
-                    await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
 
-                    // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
-                    // discarded, and nothing is saved to the secondary replicas.
-                    await tx.CommitAsync();
-                }
 
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+
+
+                await Task.Delay(TimeSpan.FromHours(23), cancellationToken);
             }
         }
     }
