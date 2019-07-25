f95781a58af222dabac62f87981ddd930d477c22
Mark Keene <mark@keenesolutions.co.uk>
2019-07-25 13:29:33 +0000

    Changes for 274

--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/LearnerMutators/LearnerMutatorFactory.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/LearnerMutators/LearnerMutatorFactory.cs
@@ -22,6 +22,7 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.LearnerMutators
                 case "258":
                 case "261":
                 case "262":
+                case "274":
                 case "282":
                 case "283":
                 case "334":
