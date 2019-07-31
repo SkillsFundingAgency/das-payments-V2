b7ae770e74d30d9068f35443059846d4408f0250
Tony <abroodie@gmail.com>
2019-07-31 14:59:21 +0100

    Tidied up TestBase in Perf tests  project

--- src/SFA.DAS.Payments.DataLocks.DataLockProxyService/Handlers/ResetCacheHandler.cs
+++ src/SFA.DAS.Payments.DataLocks.DataLockProxyService/Handlers/ResetCacheHandler.cs
@@ -11,36 +11,6 @@ using SFA.DAS.Payments.DataLocks.Messages.Internal;
 
 namespace SFA.DAS.Payments.DataLocks.DataLockProxyService.Handlers
 {
-    public class ResetActorsHandler: IHandleMessages<ResetActorsCommand>
-    {
-        private readonly IActorProxyFactory proxyFactory;
-        private readonly IPaymentLogger logger;
-
-        public ResetActorsHandler(IPaymentLogger logger, IActorProxyFactory proxyFactory)
-        {
-            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            this.proxyFactory = proxyFactory ?? throw new ArgumentNullException(nameof(proxyFactory));
-        }
-
-        public async Task Handle(ResetActorsCommand message, IMessageHandlerContext context)
-        {
-            logger.LogDebug("Resetting cache for list of ULNs.");
-            var resetTasks = new List<Task>();
-            foreach (var uln in message.Ulns)
-            {
-                var actorId = new ActorId(uln);
-                logger.LogVerbose($"Creating actor proxy for uln starting: {uln}.");
-                var actor = proxyFactory.CreateActorProxy<IDataLockService>(new Uri("fabric:/SFA.DAS.Payments.DataLocks.ServiceFabric/DataLockServiceActorService"), actorId);
-                logger.LogVerbose($"Actor proxy created for uln starting {uln}, now resetting the cache.");
-                resetTasks.Add(actor.Reset());
-            }
-
-            await Task.WhenAll(resetTasks).ConfigureAwait(false);
-            logger.LogInfo("Finished resetting the cache for ulns");
-        }
-    }
-
-
     public class ResetCacheHandler : IHandleMessages<ResetCacheCommand>
     {
         private readonly IPaymentLogger logger;
