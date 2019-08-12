d5ebb043dc005ea976cd985f10f19cd5b59d7ae8
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-08-12 15:43:41 +0100

    Aded legacy payments database and models

---
+++ src/SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService/Handlers/ProcessPaymentsToV1Handler.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.Payments.ProviderPayments.Application.Services;
+using SFA.DAS.Payments.ProviderPayments.Messages.Internal.Commands;
+
+namespace SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService.Handlers
+{
+    public class ProcessPaymentsToV1Handler : IHandleMessages<ProcessProviderMonthEndCommand>
+    {
+        public IMonthEndService MonthEndService { get; set; }
+
+        public async Task Handle(ProcessProviderMonthEndCommand message, IMessageHandlerContext context)
+        {
+            var payments = await MonthEndService.GetMonthEndPayments(message.CollectionPeriod, message.Ukprn)
+                .ConfigureAwait(false);
+
+            
+        }
+    }
+}
