2c09486bddece00eba12145653fa6fff99de426b
Carl <Carl@cabm.co.uk>
2019-08-23 15:44:58 +0100

    Ensuring that the connection is open prior to writing single records Added batch size and timeout to IBulkWriter<>

--- src/SFA.DAS.Payments.Audit.Application/PaymentsEventProcessing/PaymentsEventModelBatchProcessor.cs
+++ src/SFA.DAS.Payments.Audit.Application/PaymentsEventProcessing/PaymentsEventModelBatchProcessor.cs
@@ -68,6 +68,8 @@ namespace SFA.DAS.Payments.Audit.Application.PaymentsEventProcessing
                         {
                             bulkCopy.ColumnMappings.Clear();
                             bulkCopy.DestinationTableName = data.Count > 1 ? table.TableName : dataTable.TableName;
+                            bulkCopy.BulkCopyTimeout = 0;
+                            bulkCopy.BatchSize = 1000;
 
                             foreach (DataRow tableRow in table.Rows)
                             {
@@ -86,7 +88,7 @@ namespace SFA.DAS.Payments.Audit.Application.PaymentsEventProcessing
                                 logger.LogWarning($"Error bulk writing to server. Processing single records. \n\n" +
                                                   $"{ex.Message}\n\n" +
                                                   $"{ex.StackTrace}");
-                                TrySingleRecord(bulkCopy, table);
+                                await TrySingleRecord(bulkCopy, table, sqlConnection).ConfigureAwait(false);
                             }
                         }
 
@@ -105,7 +107,7 @@ namespace SFA.DAS.Payments.Audit.Application.PaymentsEventProcessing
             return batch.Count;
         }
 
-        private int TrySingleRecord(SqlBulkCopy bulkCopy, DataTable table)
+        private async Task<int> TrySingleRecord(SqlBulkCopy bulkCopy, DataTable table, SqlConnection connection)
         {
             bulkCopy.BatchSize = 1;
             var errors = 0;
@@ -128,7 +130,11 @@ namespace SFA.DAS.Payments.Audit.Application.PaymentsEventProcessing
 
                     try
                     {
-                        bulkCopy.WriteToServer(singleRecordTable);
+                        if (connection.State != ConnectionState.Open)
+                        {
+                            await connection.OpenAsync().ConfigureAwait(false);
+                        }
+                        await bulkCopy.WriteToServerAsync(singleRecordTable).ConfigureAwait(false);
                     }
                     catch (SystemException ex)
                     {
