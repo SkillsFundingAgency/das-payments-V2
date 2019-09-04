7baba5293b80dd77b2599f6c9ab9c975ef55437a
Carl Abrahams <carl@charmsolutions.co.uk>
2019-09-04 12:24:18 +0100

    Added tests for DataLockEvent mapping to DataLockEventModel

---
+++ src/SFA.DAS.Payments.Audit.Application.UnitTests/Mapping/DataLock/FunctionalSkillEarningFailedDataLockMatchingMappingTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Payments.Audit.Model;
+using SFA.DAS.Payments.DataLocks.Messages.Events;
+
+namespace SFA.DAS.Payments.Audit.Application.UnitTests.Mapping.DataLock
+{
+    [TestFixture]
+    public class FunctionalSkillEarningFailedDataLockMatchingMappingTests : FunctionalSkillDataLockEventMappingTests<FunctionalSkillEarningFailedDataLockMatching>
+    {
+        protected override FunctionalSkillEarningFailedDataLockMatching CreatePaymentEvent()
+        {
+            return new FunctionalSkillEarningFailedDataLockMatching();
+        }
+
+        [Test]
+        public void Maps_IsPayable_ToTrue()
+        {
+            Mapper.Map<DataLockEventModel>(PaymentEvent).IsPayable.Should().BeFalse();
+        }
+    }
+}
---
+++ src/SFA.DAS.Payments.Audit.Application.UnitTests/Mapping/DataLock/PayableFunctionalSkillEarningEventMappingTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Payments.Audit.Model;
+using SFA.DAS.Payments.DataLocks.Messages.Events;
+
+namespace SFA.DAS.Payments.Audit.Application.UnitTests.Mapping.DataLock
+{
+    [TestFixture]
+    public class PayableFunctionalSkillEarningEventMappingTests : FunctionalSkillDataLockEventMappingTests<PayableFunctionalSkillEarningEvent>
+    {
+        protected override PayableFunctionalSkillEarningEvent CreatePaymentEvent()
+        {
+            return new PayableFunctionalSkillEarningEvent
+                   {
+                       StartDate = DateTime.Today
+                   };
+        }
+
+        [Test]
+        public void Maps_IsPayable_ToTrue()
+        {
+            Mapper.Map<DataLockEventModel>(PaymentEvent).IsPayable.Should().BeTrue();
+        }
+
+        [Test]
+        public void Maps_StartDate()
+        {
+            Mapper.Map<DataLockEventModel>(PaymentEvent).StartDate.Should().Be(DateTime.Today);
+        }
+    }
+}
