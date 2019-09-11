5ee96e672460092543cc0cb50e6220ba86d29d63
Ben Arnold <ben@arnoldfamily.me.uk>
2019-09-11 11:33:37 +0100

    Only pass NServiceBus logs that are above the minimum log level

--- src/SFA.DAS.Payments.Application/Infrastructure/Logging/MessagingLoggerFactory.cs
+++ src/SFA.DAS.Payments.Application/Infrastructure/Logging/MessagingLoggerFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ESFA.DC.Logging.Config.Interfaces;
 using NServiceBus.Logging;
 
 namespace SFA.DAS.Payments.Application.Infrastructure.Logging
@@ -6,20 +7,22 @@ namespace SFA.DAS.Payments.Application.Infrastructure.Logging
     internal class MessagingLoggerFactory : ILoggerFactory
     {
         private readonly IPaymentLogger logger;
+        private readonly IApplicationLoggerSettings settings;
 
-        public MessagingLoggerFactory(IPaymentLogger logger)
+        public MessagingLoggerFactory(IPaymentLogger logger, IApplicationLoggerSettings settings)
         {
             this.logger = logger;
+            this.settings = settings;
         }
 
         public ILog GetLogger(Type type)
         {
-            return new MessagingLogger(logger);
+            return new MessagingLogger(logger, settings);
         }
 
         public ILog GetLogger(string name)
         {
-            return new MessagingLogger(logger);
+            return new MessagingLogger(logger, settings);
         }
     }
 }
