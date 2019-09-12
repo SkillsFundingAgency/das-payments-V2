fe1f8e7d7e9fc21b89574daa2b796ffe99517353
Ben Arnold <ben@arnoldfamily.me.uk>
2019-09-12 15:27:18 +0100

    Automatically add common properties to MessageProcessingFailedException
    The exception message will now come through as
    `SFA.DAS.Payments.Application.Messaging.MessageProcessingFailedException: Couldn't process message `ProcessLearnerCommand` from `sfa-das-payments-earningevents`. Ukprn: 344666, JobId: 297741 ---> <original exception and stack trace>`
    
    Properties automatically added:
    * Id
    *  EmployerAccountId
    *  AccountId
    *  Ukprn
    *  JobId
    *  EventId
    *  ContractType
    *  TransactionType

--- src/SFA.DAS.Payments.Application/Messaging/ExceptionHandlingBehavior.cs
+++ src/SFA.DAS.Payments.Application/Messaging/ExceptionHandlingBehavior.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using NServiceBus.Pipeline;
+using NServiceBus.Transport;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.Core;
 
 namespace SFA.DAS.Payments.Application.Messaging
 {
-    public class ExceptionHandlingBehavior : Behavior<ITransportReceiveContext>
+    public class ExceptionHandlingBehavior : Behavior<IIncomingLogicalMessageContext>
     {
         private readonly IPaymentLogger logger;
 
@@ -17,7 +19,7 @@ namespace SFA.DAS.Payments.Application.Messaging
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        public override async Task Invoke(ITransportReceiveContext context, Func<Task> next)
+        public override async Task Invoke(IIncomingLogicalMessageContext context, Func<Task> next)
         {
             try
             {
@@ -25,17 +27,47 @@ namespace SFA.DAS.Payments.Application.Messaging
             }
             catch (Exception ex)
             {
-                if (!(context.Message.Headers.TryGetValue("NServiceBus.EnclosedMessageTypes", out var messageType) 
+                if (!(context.Headers.TryGetValue("NServiceBus.EnclosedMessageTypes", out var messageType) 
                     && TypeString.TryParseTypeName(messageType, out messageType)))
                 {
                     messageType = "No enclosed messages";
                 }
 
-                if (!context.Message.Headers.TryGetValue("NServiceBus.OriginatingEndpoint", out var sendingEndpoint))
+                if (!context.Headers.TryGetValue("NServiceBus.OriginatingEndpoint", out var sendingEndpoint))
                     sendingEndpoint = "No sending endpoint";
 
-                throw new MessageProcessingFailedException($"Couldn't process message `{messageType}` from `{sendingEndpoint}`.", context.Message, ex);
+                var add = AdditionalProperties(context.Message);
+                var props = string.Join(", ", add.Select(x => $"{x.Key}: {x.Value}"));
+
+                throw new MessageProcessingFailedException($"Couldn't process message `{messageType}` from `{sendingEndpoint}`.\n{props}", context.Message, ex);
             }
+        }
+
+        private Dictionary<string,string> AdditionalProperties(LogicalMessage message)
+        {
+            var props = new List<string> { "Id", "EmployerAccountId", "AccountId", "Ukprn", "JobId", "EventId", "ContractType", "TransactionType" };
+
+            return props.ToDictionaryWithoutNullValues(key => key, key => GetMessageProperty(message, key));
+        }
+
+        private static string GetMessageProperty(LogicalMessage message, string propertyName)
+        {
+            return message.Instance.GetType().GetProperty(propertyName)?.GetValue(message.Instance, null)?.ToString();
+        }
+    }
+
+    public static class DictionaryExtensions
+    {
+        public static Dictionary<TKey, TValue> ToDictionaryWithoutNullValues<TSource, TKey, TValue>(this IEnumerable<TSource> keys, Func<TSource, TKey> keySelector, Func<TSource, TValue> valueSelector)
+        {
+            // message.Learner?.ReferenceNumber
+            // message.Learner.LearnRefNumber
+            //{message.CollectionPeriod.Period:00}-{message.CollectionPeriod.AcademicYear:0000}"
+             
+            return keys
+                .Select(x => (key: keySelector(x), value: valueSelector(x)))
+                .Where(x => x.value != null)
+                .ToDictionary(x => x.key, x => x.value);
         }
     }
 
