b13cfed87cc0907614993cdcc06ae0ec1552a54c
Ben Arnold <ben@arnoldfamily.me.uk>
2019-09-12 11:51:49 +0100

    Embellish exceptions that leave message handlers more details
    NServiceBus already logs completely failed messages by default, it is unnecessary to replicate this behaviour.  NServiceBus also only logs the final failure as Error, all retries are either Info or Warnings.
    
    Instead, include the message name and origination endpoint that would have been logged in the exception that bubbles up to NServiceBus so it is still visible for easy diagnostics.

---
+++ src/SFA.DAS.Payments.EarningEvents.Domain.UnitTests/ExceptionHandlingBehaviourTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using SFA.DAS.Payments.Core;
+
+namespace SFA.DAS.Payments.EarningEvents.Domain.UnitTests
+{
+    [TestFixture]
+    public class ExceptionHandlingBehaviourTests
+    { 
+        [Test]
+        public void Test_typename_extraction()
+        {
+            var messageType = "SFA.DAS.Payments.EarningEvents.Messages.Internal.Commands.ProcessLearnerCommand, SFA.DAS.Payments.EarningEvents.Messages.Internal, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null;SFA.DAS.Payments.Messages.Core.Commands.IPaymentsCommand, SFA.DAS.Payments.Messages.Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null;SFA.DAS.Payments.Messages.Core.IJobMessage, SFA.DAS.Payments.Messages.Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null;SFA.DAS.Payments.Messages.Core.Commands.ICommand, SFA.DAS.Payments.Messages.Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null;SFA.DAS.Payments.Messages.Core.Commands.PaymentsCommand, SFA.DAS.Payments.Messages.Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null;SFA.DAS.Payments.Messages.Core.IPaymentsMessage, SFA.DAS.Payments.Messages.Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+            var actual = TypeString.TryParseTypeName(messageType, out var name);
+            Assert.IsTrue(actual);
+            Assert.AreEqual("ProcessLearnerCommand", name);
+        }
+    }
+}
