63b692705275f78cc416429bcd9f1475d0cec253
Tony <abroodie@gmail.com>
2019-09-18 21:26:33 +0100

    added missing configuration to audit services

--- src/SFA.DAS.Payments.Monitoring.Jobs.JobsProxyService/Handlers/BaseJobMessageHandler.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.JobsProxyService/Handlers/BaseJobMessageHandler.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Services.Client;
@@ -18,16 +20,17 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.JobsProxyService.Handlers
         protected BaseJobMessageHandler(IServiceProxyFactory proxyFactory, IPaymentLogger logger)
         {
             this.proxyFactory = proxyFactory ?? throw new ArgumentNullException(nameof(proxyFactory));
-            this.Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task Handle(T message, IMessageHandlerContext context)
         {
             try
             {
+                var hashedJobId = GetHashJobId(message.JobId); //TO ensure jobs aren't placed into the same partition
                 Logger.LogVerbose($"Getting actor for job: {message.JobId}");
                 var service = proxyFactory.CreateServiceProxy<IJobService>(new Uri(ServiceUris.JobService), 
-                    new ServicePartitionKey(message.JobId));
+                    new ServicePartitionKey(hashedJobId));
                 await HandleMessage(message, context, service, CancellationToken.None).ConfigureAwait(false);
             }
             catch (Exception ex)
@@ -36,6 +39,15 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.JobsProxyService.Handlers
                 throw;
             }
         }
+
+        protected virtual long GetHashJobId(long jobId)
+        {
+            var guidBytes = Encoding.UTF8.GetBytes(jobId.ToString());
+            var hasher = new SHA1CryptoServiceProvider();
+            var hashedBytes = hasher.ComputeHash(guidBytes);
+            return BitConverter.ToInt64(hashedBytes, 0);
+        }
+
         protected abstract Task HandleMessage(T message, IMessageHandlerContext context, IJobService jobService, CancellationToken cancellationToken);
     }
 }
