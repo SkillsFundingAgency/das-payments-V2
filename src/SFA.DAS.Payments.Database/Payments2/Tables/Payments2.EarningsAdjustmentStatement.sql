17358e77f2162de8310e62141b8590e215dc5084
Michael Young <mick.young1981@googlemail.com>
2019-09-19 08:20:32 +0100

    Added earning adjustment statement table

---
+++ src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.EarningsAdjustmentStatement.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Payments2].[EarningsAdjustmentStatement]
+(
+	[Ukprn] [bigint] NOT NULL,
+	[SubmissionId] [uniqueidentifier] NOT NULL,
+	[SubmissionCollectionPeriod] [int] NOT NULL,
+	[SubmissionAcademicYear] [int] NOT NULL,
+	[PaymentType] [int] NOT NULL,
+	[PaymentTypeName] [nvarchar](250) NOT NULL,
+	[Amount] [decimal](15, 5) NOT NULL,
+	[CollectionPeriodName] [varchar](8) NOT NULL,
+	[CollectionPeriodMonth] [int] NOT NULL,
+	[CollectionPeriodYear] [int] NOT NULL
+)
