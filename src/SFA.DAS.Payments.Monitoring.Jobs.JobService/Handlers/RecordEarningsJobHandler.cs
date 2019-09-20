45fc61032d63b3f5753b7979bb31230ec5efc949
Tony <abroodie@gmail.com>
2019-09-20 15:27:31 +0100

    EarningsJobHandler now calls earningsJobService directly rather than via proxy & JobService

--- src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Handlers/RecordEarningsJobHandler.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Handlers/RecordEarningsJobHandler.cs
@@ -1,23 +1,47 @@
-﻿using Microsoft.ServiceFabric.Services.Remoting.Client;
-using NServiceBus;
-using SFA.DAS.Payments.Application.Infrastructure.Logging;
-using SFA.DAS.Payments.Monitoring.Jobs.JobService.Interfaces;
-using SFA.DAS.Payments.Monitoring.Jobs.Messages.Commands;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Application.Infrastructure.UnitOfWork;
+using SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing;
+using SFA.DAS.Payments.Monitoring.Jobs.Messages.Commands;
+using SFA.DAS.Payments.Monitoring.Jobs.Model;
 
-namespace SFA.DAS.Payments.Monitoring.Jobs.JobsService.Handlers
+namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.Handlers
 {
-    public class RecordEarningsJobHandler: BaseJobMessageHandler<RecordEarningsJob>
+    public class RecordEarningsJobHandler: IHandleMessages<RecordEarningsJob>
     {
-        public RecordEarningsJobHandler(IServiceProxyFactory proxyFactory, IPaymentLogger logger): base(proxyFactory,logger)
+        private readonly IPaymentLogger logger;
+        private readonly IUnitOfWorkScopeFactory scopeFactory;
+        private readonly IJobStatusManager jobStatusManager;
+
+        public RecordEarningsJobHandler(IPaymentLogger logger, IUnitOfWorkScopeFactory scopeFactory, IJobStatusManager jobStatusManager)
         {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.scopeFactory = scopeFactory ?? throw new ArgumentNullException(nameof(scopeFactory));
+            this.jobStatusManager = jobStatusManager ?? throw new ArgumentNullException(nameof(jobStatusManager));
         }
 
-        protected override Task HandleMessage(RecordEarningsJob message, IMessageHandlerContext context, IJobService jobService,
-            CancellationToken cancellationToken)
+        public async Task Handle(RecordEarningsJob message, IMessageHandlerContext context)
         {
-            return jobService.RecordEarningsJob(message, cancellationToken);
+
+            using (var scope = scopeFactory.Create("JobService.RecordEarningsJob"))
+            {
+                try
+                {
+                    var earningsJobService = scope.Resolve<IEarningsJobService>();
+                    await earningsJobService.RecordNewJob(message, CancellationToken.None).ConfigureAwait(false);
+                    await scope.Commit();
+                }
+                catch (Exception ex)
+                {
+                    scope.Abort();
+                    logger.LogWarning($"Failed to record earnings job: {message.JobId}. Error: {ex.Message}, {ex}");
+                    throw;
+                }
+            }
+            jobStatusManager.StartMonitoringJob(message.JobId, JobType.EarningsJob);
         }
     }
 }
