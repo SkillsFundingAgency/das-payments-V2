e7a8b6fc0cab68a76eb000cbc5dd29f404709200
Tony <abroodie@gmail.com>
2019-09-30 19:51:36 +0100

    temp removed named provides from partition class

--- src/SFA.DAS.Payments.Monitoring.Jobs.Client/JobMonitorPartition.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Client/JobMonitorPartition.cs
@@ -6,11 +6,11 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Client
     {
         public static long[] InvidivualNodes = new long[]
         {
-            10003915,
-            10033440,
-            10003161,
-            10012467,
-            10000446,
+            //10003915,
+            //10033440,
+            //10003161,
+            //10012467,
+            //10000446,
         };
 
         public long PartitionNameForJob(long jobId, long Ukprn)
