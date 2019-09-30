afb8b63389e1c5cd089466f39421496314abd237
Tony <abroodie@gmail.com>
2019-09-30 20:21:40 +0100

    added handlers for period end jobs

--- src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Infrastructure/Ioc/JobServiceModule.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Infrastructure/Ioc/JobServiceModule.cs
@@ -1,8 +1,5 @@
 ﻿using Autofac;
-using SFA.DAS.Payments.Application.Messaging;
 using SFA.DAS.Payments.Monitoring.Jobs.Application;
-using SFA.DAS.Payments.Monitoring.Jobs.JobService.Handlers;
-using SFA.DAS.Payments.Monitoring.Jobs.Messages.Commands;
 
 namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.Infrastructure.Ioc
 {
@@ -12,24 +9,6 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.Infrastructure.Ioc
         {
             builder.RegisterType<JobStorageService>()
                 .As<IJobStorageService>()
-                .InstancePerLifetimeScope();
-        }
-    }
-
-    public class BatchMessageHandlersModule : Module
-    {
-        protected override void Load(ContainerBuilder builder)
-        {
-            builder.RegisterType<RecordJobMessageProcessingStatusBatchHandler>()
-                .As<IHandleMessageBatches<RecordJobMessageProcessingStatus>>()
-                .InstancePerLifetimeScope();
-
-            builder.RegisterType<RecordEarningsJobBatchHandler>()
-                .As<IHandleMessageBatches<RecordEarningsJob>>()
-                .InstancePerLifetimeScope();
-
-            builder.RegisterType<RecordEarningsJobAdditionalMessageBatchHandler>()
-                .As<IHandleMessageBatches<RecordEarningsJobAdditionalMessages>>()
                 .InstancePerLifetimeScope();
         }
     }
