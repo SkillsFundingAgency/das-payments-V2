1280816b7bbdc16269ae7e5d3144120546d8d6ae
Ben Arnold <ben@arnoldfamily.me.uk>
2019-10-14 13:32:51 +0100

    Refactor tests to Builder pattern

---
+++ src/SFA.DAS.Payments.EarningEvents.Domain.UnitTests/EarningsEventBuilderTests.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Payments.EarningEvents.Application.Mapping;
+using SFA.DAS.Payments.EarningEvents.Messages.Events;
+using System.Linq;
+
+namespace SFA.DAS.Payments.EarningEvents.Domain.UnitTests
+{
+    [TestFixture]
+    public class EarningsEventBuilderTests
+    {
+        ApprenticeshipContractTypeEarningsEventBuilder sut = new ApprenticeshipContractTypeEarningsEventBuilder(
+            new ApprenticeshipContractTypeEarningsEventFactory(),
+            new Mapper(new MapperConfiguration(c => { })));
+
+        [Test]
+        public void Generate_No_Earnings_When_Fm36_Only_Has_Invalid_Contracts()
+        {
+            var cmd = new LearnerBuilder()
+                .Build();
+
+            // Arrange
+            cmd.Learner.PriceEpisodes.First().PriceEpisodeValues.PriceEpisodeContractType = "";
+
+            // Act
+            var earningsEvents = sut.Build(cmd);
+
+            // Assert
+            earningsEvents.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Generate_Valid_Earnings_When_Ignoring_Invalid_Contracts_In_Fm36()
+        {
+            var cmd = new LearnerBuilder()
+                .WithMultipleDeliveries()
+                .Build();
+
+            // Arrange
+            cmd.Learner.PriceEpisodes.First().PriceEpisodeValues.PriceEpisodeContractType = "";
+
+            // Act
+            var earningsEvents = sut.Build(cmd);
+
+            // Assert
+            var expectedEarnings = cmd.Learner.PriceEpisodes.Skip(1).Select(x => new ApprenticeshipContractType1EarningEvent
+            {
+                StartDate = x.PriceEpisodeValues.EpisodeStartDate.GetValueOrDefault()
+            });
+            
+            earningsEvents.Should().BeEquivalentTo(expectedEarnings);
+        }
+    }
+}
