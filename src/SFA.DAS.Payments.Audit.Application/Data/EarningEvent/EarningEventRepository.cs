8f72adf15ee878d7dd8e0b503259bc69fe72d70e
Tony <abroodie@gmail.com>
2019-10-16 14:23:58 +0100

    Added unit tests to cover deleting earning events after job succeeded

---
+++ src/SFA.DAS.Payments.Audit.Application/Data/EarningEvent/EarningEventRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.Payments.Application.Repositories;
+
+namespace SFA.DAS.Payments.Audit.Application.Data.EarningEvent
+{
+    public interface IEarningEventRepository
+    {
+        Task RemovePriorEvents(long ukprn, DateTime latestIlrSubmission);
+    }
+
+    public class EarningEventRepository: IEarningEventRepository
+    {
+        private readonly IPaymentsDataContext dataContext;
+
+        public EarningEventRepository(IPaymentsDataContext dataContext)
+        {
+            this.dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
+        }
+
+        public async Task RemovePriorEvents(long ukprn, DateTime latestIlrSubmission)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SFA.DAS.Payments.Audit.Application/PaymentsEventProcessing/EarningEvent/EarningEventSubmissionSucceededProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.Payments.EarningEvents.Messages.Events;
+
+namespace SFA.DAS.Payments.Audit.Application.PaymentsEventProcessing.EarningEvent
+{
+    public interface IEarningEventSubmissionSucceededProcessor
+    {
+        Task Process(SubmissionSucceededEvent message);
+    }
+
+    public class EarningEventSubmissionSucceededProcessor: IEarningEventSubmissionSucceededProcessor
+    {
+        public Task Process(SubmissionSucceededEvent message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
