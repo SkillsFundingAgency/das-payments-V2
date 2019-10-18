ec5cba84c45b03e1c4293e86814b36603c3050ab
Ben Arnold <ben@arnoldfamily.me.uk>
2019-10-18 15:53:13 +0100

    Add UnpaidReason to PeriodisedPaymentsEventModel

---
+++ src/SFA.DAS.Payments.Audit.Application.UnitTests/Mapping/RequiredPayments/CompletionPaymentHeldBackEventTests.cs
@@ -0,0 +1,21 @@
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Payments.Audit.Model;
+using SFA.DAS.Payments.Model.Core.Entities;
+using SFA.DAS.Payments.RequiredPayments.Messages.Events;
+
+namespace SFA.DAS.Payments.Audit.Application.UnitTests.Mapping.RequiredPayments
+{
+    [TestFixture]
+    public class CompletionPaymentHeldBackEventTests : RequiredPaymentsMappingTests<CompletionPaymentHeldBackEvent>
+    {
+        protected override CompletionPaymentHeldBackEvent CreatePaymentEvent() 
+            => new CompletionPaymentHeldBackEvent();
+
+        [Test]
+        public void Maps_CompletionWithheld_As_CompletionWithheldReason()
+        {
+            Mapper.Map<RequiredPaymentEventModel>(PaymentEvent).UnpaidReason.Should().Be(UnpaidReason.Withheld);
+        }
+    }
+}
--- src/SFA.DAS.Payments.Audit.Model/PeriodisedPaymentsEventModel.cs
+++ src/SFA.DAS.Payments.Audit.Model/PeriodisedPaymentsEventModel.cs
@@ -1,29 +1,29 @@
-﻿using System;
 using SFA.DAS.Payments.Model.Core.Audit;
 using SFA.DAS.Payments.Model.Core.Entities;
+using System;
 
 namespace SFA.DAS.Payments.Audit.Model
 {
-    public interface IPeriodisedPaymentsEventModel: IPaymentsEventModel
+    public interface IPeriodisedPaymentsEventModel : IPaymentsEventModel
     {
-         Guid EarningEventId { get; set; }
-         string PriceEpisodeIdentifier { get; set; }
-         ContractType ContractType { get; set; }
-         TransactionType TransactionType { get; set; }
-         decimal Amount { get; set; }
-         byte DeliveryPeriod { get; set; }
-         decimal SfaContributionPercentage { get; set; }
-         string AgreementId { get; set; }
-         long? AccountId { get; set; }
-         long? TransferSenderAccountId { get; set; }
-         long? ApprenticeshipId { get; set; }
-         long? ApprenticeshipPriceEpisodeId { get; set; }
+        Guid EarningEventId { get; set; }
+        string PriceEpisodeIdentifier { get; set; }
+        ContractType ContractType { get; set; }
+        TransactionType TransactionType { get; set; }
+        decimal Amount { get; set; }
+        byte DeliveryPeriod { get; set; }
+        decimal SfaContributionPercentage { get; set; }
+        string AgreementId { get; set; }
+        long? AccountId { get; set; }
+        long? TransferSenderAccountId { get; set; }
+        long? ApprenticeshipId { get; set; }
+        long? ApprenticeshipPriceEpisodeId { get; set; }
         ApprenticeshipEmployerType ApprenticeshipEmployerType { get; set; }
+        UnpaidReason? UnpaidReason { get; set; }
     }
 
-    public abstract class PeriodisedPaymentsEventModel: PaymentsEventModel, IPeriodisedPaymentsEventModel
+    public abstract class PeriodisedPaymentsEventModel : PaymentsEventModel, IPeriodisedPaymentsEventModel
     {
-        //public long Id { get; set; }
         public Guid EarningEventId { get; set; }
         public string PriceEpisodeIdentifier { get; set; }
         public ContractType ContractType { get; set; }
@@ -36,5 +36,7 @@ namespace SFA.DAS.Payments.Audit.Model
         public long? TransferSenderAccountId { get; set; }
         public long? ApprenticeshipId { get; set; }
         public long? ApprenticeshipPriceEpisodeId { get; set; }
-        public ApprenticeshipEmployerType ApprenticeshipEmployerType { get; set; }    }
+        public ApprenticeshipEmployerType ApprenticeshipEmployerType { get; set; }
+        public UnpaidReason? UnpaidReason { get; set; }
+    }
 }
