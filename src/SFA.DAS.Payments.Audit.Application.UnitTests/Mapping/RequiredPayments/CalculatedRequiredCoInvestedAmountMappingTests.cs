e32a566693a0f6b70d24a9a704ef45a8b5aea113
kola-samuel <samuel.dev.dba@gmail.com>
2019-11-01 16:25:39 +0000

    Fix Contract Type for Refund

---
+++ src/SFA.DAS.Payments.Audit.Application.UnitTests/Mapping/RequiredPayments/CalculatedRequiredCoInvestedAmountMappingTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Payments.Audit.Model;
+using SFA.DAS.Payments.Model.Core.Entities;
+using SFA.DAS.Payments.RequiredPayments.Messages.Events;
+
+namespace SFA.DAS.Payments.Audit.Application.UnitTests.Mapping.RequiredPayments
+{
+    [TestFixture]
+    public class CalculatedRequiredCoInvestedAmountMappingTests : RequiredPaymentsMappingTests<CalculatedRequiredCoInvestedAmount>
+    {
+        protected override CalculatedRequiredCoInvestedAmount CreatePaymentEvent()
+        {
+            return new CalculatedRequiredCoInvestedAmount
+            {
+                ContractType = ContractType.Act1,
+                ApprenticeshipId = 400L,
+                ApprenticeshipPriceEpisodeId = 800L
+            };
+        }
+
+
+        [Test]
+        public void Maps_ContractType()
+        {
+            var payment = CreatePaymentEvent();
+            Mapper.Map<RequiredPaymentEventModel>(payment).ContractType.Should().Be(payment.ContractType);
+        }
+    }
+}
