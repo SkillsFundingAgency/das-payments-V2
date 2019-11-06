f0952b98510d321edad992fdf47febddee89c51e
Mark Keene <Mark@keenesolutions.co.uk>
2019-11-06 11:35:28 +0000

    Updates to metrics

--- src/Scripts/Database/Reports/MetricsReportQuery.sql
+++ src/Scripts/Database/Reports/MetricsReportQuery.sql
@@ -1,6 +1,247 @@
 DECLARE @academicYear SMALLINT = 1920
 DECLARE @collectionPeriod TINYINT = 3
-DECLARE @ukprn bigInt = null -- 10000060 /*Set ukprn to required value to get metrics for a single provider*/
+
+DECLARE @ValidUkprns TABLE
+(
+	ukprn bigInt,
+	lastDcJobId bigInt
+)
+
+
+INSERT INTO @ValidUkprns
+SELECT 
+	j.ukprn,
+	j.dcjobid
+FROM 
+	payments2.job j 
+INNER JOIN
+	(
+	SELECT 
+		ukprn, 
+		MAX(ilrSubmissionTime) lastIlrSubmission
+	FROM 
+		payments2.job
+	WHERE 
+		AcademicYear = @academicYear
+	AND
+		CollectionPeriod = @collectionPeriod
+	GROUP BY 
+		ukprn
+	) lastSubmission
+ON 
+	lastSubmission.ukprn = j.ukprn
+AND 
+	lastsubmission.lastIlrSubmission = j.ilrSubmissionTime
+AND 
+	j.[status] = 2
+
+--removed any failed ukprns from DC
+DELETE @ValidUkprns FROM @ValidUkprns
+WHERE UKPRN IN (
+10000715
+,10001143
+,10001148
+,10001436
+,10001696
+,10001778
+,10001800
+,10002131
+,10002167
+,10002424
+,10002463
+,10003240
+,10003354
+,10003981
+,10003990
+,10004124
+,10004180
+,10004285
+,10004351
+,10004399
+,10004499
+,10004527
+,10006574
+,10007013
+,10007100
+,10007137
+,10007140
+,10007144
+,10007146
+,10007152
+,10007154
+,10007318
+,10007788
+,10007977
+,10010134
+,10010548
+,10010549
+,10010586
+,10011138
+,10011147
+,10011159
+,10011332
+,10019048
+,10019210
+,10019217
+,10019383
+,10020019
+,10020068
+,10021172
+,10021481
+,10021539
+,10021602
+,10021754
+,10022567
+,10022689
+,10022763
+,10023592
+,10023705
+,10023776
+,10023787
+,10023918
+,10024054
+,10024293
+,10024633
+,10024686
+,10025700
+,10026442
+,10026702
+,10027269
+,10027662
+,10028269
+,10028342
+,10028441
+,10028909
+,10028930
+,10029332
+,10029676
+,10029887
+,10030265
+,10030520
+,10030670
+,10030740
+,10031424
+,10031544
+,10031912
+,10032250
+,10032449
+,10032616
+,10032658
+,10033440
+,10033536
+,10033608
+,10033670
+,10033815
+,10034022
+,10034128
+,10034387
+,10034767
+,10035735
+,10036036
+,10036049
+,10036106
+,10036240
+,10037119
+,10037126
+,10037213
+,10037757
+,10037909
+,10038201
+,10038829
+,10039793
+,10039859
+,10039882
+,10039956
+,10040011
+,10040368
+,10040391
+,10040417
+,10040440
+,10040525
+,10040812
+,10040883
+,10041069
+,10041165
+,10041236
+,10041319
+,10041332
+,10042014
+,10042119
+,10042166
+,10042291
+,10043126
+,10043250
+,10043575
+,10043661
+,10044749
+,10045119
+,10045159
+,10045305
+,10045348
+,10045505
+,10045935
+,10046498
+,10046705
+,10046777
+,10047090
+,10048409
+,10048788
+,10048801
+,10048806
+,10048827
+,10048848
+,10049099
+,10052538
+,10052574
+,10052606
+,10053742
+,10054055
+,10054118
+,10054451
+,10054814
+,10054898
+,10054962
+,10055259
+,10055479
+,10056190
+,10056323
+,10056694
+,10056711
+,10056912
+,10057175
+,10057328
+,10058007
+,10058111
+,10061144
+,10061302
+,10061312
+,10061438
+,10061540
+,10061548
+,10061591
+,10061640
+,10061808
+,10061840
+,10061842
+,10062041
+,10062166
+,10062335
+,10063272
+,10063286
+,10063477
+,10063587
+,10063769
+,10064216
+,10065535
+,10065593
+,10065752
+,10065814
+,10065941
+)
+
+
+
+
 
 ;WITH IncludedTransactionTypes AS
 (
@@ -9,6 +250,7 @@ DECLARE @ukprn bigInt = null -- 10000060 /*Set ukprn to required value to get me
 EarningsYTD AS
 (
 	SELECT TransactionType
+			
 		   ,COUNT(DISTINCT e.LearnerUln) [EarningLearnerCount]
 		   ,SUM(CASE WHEN Amount > 0 THEN Amount END) [EarningsYTD]
 		   ,SUM(CASE WHEN ContractType = 1
@@ -23,9 +265,11 @@ EarningsYTD AS
 	FROM [Payments2].[EarningEvent] e WITH (NOLOCK)
 	INNER JOIN [Payments2].[EarningEventPeriod] p WITH (NOLOCK)
 		   ON p.EarningEventId = e.EventId
+	INNER JOIN @ValidUkprns UKPRNS
+			ON UKPRNS.ukprn = e.ukprn  and ukprns.lastDcJobId = e.jobid
 	WHERE e.AcademicYear = @academicYear
 		   AND e.CollectionPeriod = @collectionPeriod
-		   AND ((@ukprn IS NULL) or (@ukprn = ukprn))
+	
 	GROUP BY p.TransactionType
 ),
 RequiredPaymentsYTD AS
@@ -36,9 +280,10 @@ RequiredPaymentsYTD AS
 		   ,sum(CASE WHEN ContractType = 1 THEN Amount END) [RequiredPaymentsACT1]
 		   ,sum(CASE WHEN ContractType = 2 THEN Amount END) [RequiredPaymentsACT2]
 	FROM [Payments2].[RequiredPaymentEvent] RP WITH (NOLOCK)
+	INNER JOIN @ValidUkprns UKPRNS
+			ON UKPRNS.ukprn = rp.ukprn
 	WHERE CollectionPeriod <= @collectionPeriod
 		   AND AcademicYear = @academicYear
-		   AND ((@ukprn IS NULL) or (@ukprn = ukprn))
 	GROUP BY TransactionType
 ),
 ActualPaymentsYTD AS
@@ -48,11 +293,12 @@ ActualPaymentsYTD AS
 		   ,sum(Amount) [ActualPaymentsYTD]
 		   ,sum(CASE WHEN ContractType = 1 THEN Amount END) [ActualPaymentsACT1]
 		   ,sum(CASE WHEN ContractType = 2 THEN Amount END) [ActualPaymentsACT2]
-	FROM Payments2.Payment WITH (NOLOCK)
+	FROM Payments2.Payment  p WITH (NOLOCK)
+		INNER JOIN @ValidUkprns UKPRNS
+			ON UKPRNS.ukprn = p.ukprn
 	WHERE AcademicYear = @academicYear
 		   AND CollectionPeriod <= @collectionPeriod
-		   AND ((@ukprn IS NULL) or (@ukprn = ukprn))
-	GROUP BY TransactionType
+		   GROUP BY TransactionType
 ),
 DataLocks AS
 (
@@ -62,11 +308,12 @@ DataLocks AS
 	FROM [Payments2].[DataLockEvent] dle
 	INNER JOIN [Payments2].[DataLockEventNonPayablePeriod] dlenpp
 		   ON dle.EventId = dlenpp.DataLockEventId
+	INNER JOIN @ValidUkprns UKPRNS
+			ON UKPRNS.ukprn = dle.ukprn
 	WHERE dle.CollectionPeriod = @collectionPeriod
 		   AND dle.AcademicYear = @academicYear
 		   AND dle.IsPayable = 0
 		   AND dlenpp.amount > 0
-		   AND ((@ukprn IS NULL) or (@ukprn = ukprn))
 	GROUP BY dlenpp.TransactionType
 ), 
 HeldBackCompletionPayments AS
@@ -91,11 +338,12 @@ HeldBackCompletionPayments AS
 		   ON f.EarningEventId = e.EventId
 				  AND f.DeliveryPeriod = ep.DeliveryPeriod
 				  AND f.TransactionType = ep.TransactionType
+	INNER JOIN @ValidUkprns UKPRNS
+			ON UKPRNS.ukprn = e.ukprn
 	WHERE ep.TransactionType = 2
 		   AND ep.Amount <> 0
 		   AND e.AcademicYear = @academicYear
 		   AND e.CollectionPeriod = @collectionPeriod
-		   AND ((@ukprn IS NULL) or (@ukprn = e.ukprn))
 	GROUP BY ep.TransactionType
 )
 
