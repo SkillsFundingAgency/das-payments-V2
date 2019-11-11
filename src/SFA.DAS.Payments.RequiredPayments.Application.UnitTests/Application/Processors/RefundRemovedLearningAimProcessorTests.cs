387e9a1a77df59a189d0506538beb41c6b90dd15
Carl Abrahams <carl@charmsolutions.co.uk>
2019-11-11 09:16:00 +0000

    Added test for scenario

--- src/SFA.DAS.Payments.RequiredPayments.Application.UnitTests/Application/Processors/RefundRemovedLearningAimProcessorTests.cs
+++ src/SFA.DAS.Payments.RequiredPayments.Application.UnitTests/Application/Processors/RefundRemovedLearningAimProcessorTests.cs
@@ -95,7 +95,7 @@ namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.Application.Pr
                 InstalmentAmount = 1000,
                 NumberOfInstalments = 12,
                 PlannedEndDate = DateTime.Today,
-                StartDate = DateTime.Today.AddMonths(-12),
+                StartDate = DateTime.Today.AddMonths(-12)
             };
 
         }
@@ -294,6 +294,48 @@ namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.Application.Pr
                 refunds.Count.Should().Be(2);
                 refunds.Cast<CalculatedRequiredIncentiveAmount>().Count(x => x.Type == IncentivePaymentType.BalancingMathsAndEnglish).Should().Be(1);
                 refunds.Cast<CalculatedRequiredIncentiveAmount>().Count(x => x.Type == IncentivePaymentType.OnProgrammeMathsAndEnglish).Should().Be(1);
+            }
+        }
+
+        [Test]
+        public async Task Clawback_OnProgramme_And_Completion_Payments_WhenLearnerRefNumber_Changes()
+        {
+            // arrange
+            mocker.Provide<IRefundService, RefundService>();
+            mocker.Provide<IPaymentDueProcessor, PaymentDueProcessor>();
+            mocker.Provide<IPeriodisedRequiredPaymentEventFactory, PeriodisedRequiredPaymentEventFactory>();
+            mocker.Provide<IRefundRemovedLearningAimService, RefundRemovedLearningAimService>();
+
+            var historicOnProgrammePayment = CreatePaymentHistoryEntity(FundingSourceType.Levy, 2, 1);
+            historicOnProgrammePayment.Amount = 133.33333m;
+            history.Add(historicOnProgrammePayment);
+
+            var historicCompletionPayment = CreatePaymentHistoryEntity(FundingSourceType.Levy, 2, 2);
+            historicCompletionPayment.Amount = 400.0m;
+            historicCompletionPayment.TransactionType = (int) TransactionType.Completion;
+            history.Add(historicCompletionPayment);
+
+            var mockPaymentHistoryCache = mocker.Mock<IDataCache<PaymentHistoryEntity[]>>();
+            mockPaymentHistoryCache.Setup(x => x.TryGet(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                                   .ReturnsAsync(new ConditionalValue<PaymentHistoryEntity[]>(true, history.ToArray()));
+            identifiedLearner.CollectionPeriod.Period = 3;
+            identifiedLearner.Learner.ReferenceNumber = "KEY053";
+
+
+            // act
+            var processor = mocker.Create<RefundRemovedLearningAimProcessor>();
+            var refunds = await processor
+                                .RefundLearningAim(identifiedLearner,
+                                                   mockPaymentHistoryCache.Object,
+                                                   CancellationToken.None)
+                                .ConfigureAwait(false);
+
+            // assert
+            using (new AssertionScope())
+            {
+                refunds.Count.Should().Be(2);
+                refunds.Cast<CalculatedRequiredLevyAmount>().Count(x => x.OnProgrammeEarningType == OnProgrammeEarningType.Learning).Should().Be(1);
+                refunds.Cast<CalculatedRequiredLevyAmount>().Count(x => x.OnProgrammeEarningType == OnProgrammeEarningType.Completion).Should().Be(1);
             }
         }
     }
