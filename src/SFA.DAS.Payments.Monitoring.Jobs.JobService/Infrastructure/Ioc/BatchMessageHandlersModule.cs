ca377bf7524a6f00ceb02dcfce0458f4153ab403
Tony <abroodie@gmail.com>
2019-11-17 09:00:37 +0000

    fixed config: monitoring svc now uses MonitoringServiceBusConnectionString VSTS variable

--- src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Infrastructure/Ioc/BatchMessageHandlersModule.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Infrastructure/Ioc/BatchMessageHandlersModule.cs
@@ -1,10 +1,14 @@
 ﻿using Autofac;
+using SFA.DAS.Payments.Application.Infrastructure.Ioc;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
 using SFA.DAS.Payments.Application.Messaging;
-using SFA.DAS.Payments.EarningEvents.Messages.Events;
+using SFA.DAS.Payments.Core.Configuration;
 using SFA.DAS.Payments.Monitoring.Jobs.JobService.Handlers;
 using SFA.DAS.Payments.Monitoring.Jobs.JobService.Handlers.PeriodEnd;
 using SFA.DAS.Payments.Monitoring.Jobs.JobService.Handlers.Submission;
 using SFA.DAS.Payments.Monitoring.Jobs.Messages.Commands;
+using SFA.DAS.Payments.ServiceFabric.Core;
 
 namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.Infrastructure.Ioc
 {
@@ -12,6 +16,20 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.Infrastructure.Ioc
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.Register(c =>
+                {
+                    var appConfig = c.Resolve<IApplicationConfiguration>();
+                    var configHelper = c.Resolve<IConfigurationHelper>();
+                    return new  ServiceBusBatchCommunicationListener(configHelper.GetConnectionString("MonitoringServiceBusConnectionString"),
+                        appConfig.EndpointName,
+                        appConfig.FailedMessagesQueue, 
+                        c.Resolve<IPaymentLogger>(),
+                        c.Resolve<IContainerScopeFactory>(),
+                        c.Resolve<ITelemetry>());
+                })
+                .As<IServiceBusBatchCommunicationListener>()
+                .SingleInstance();
+
             builder.RegisterType<RecordJobMessageProcessingStatusBatchHandler>()
                 .As<IHandleMessageBatches<RecordJobMessageProcessingStatus>>()
                 .InstancePerLifetimeScope();
