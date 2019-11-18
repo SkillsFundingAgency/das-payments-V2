1fcc5ff2eada68b3cbf91cb14496db9090fbdf4d
kola-samuel <samuel.dev.dba@gmail.com>
2019-11-18 11:06:36 +0000

    Delete Dlock Approval Service

--- src/SFA.DAS.Payments.DataLocks.DataLockProxyService/Handlers/PeriodEndStartedHandler.cs
+++ src/SFA.DAS.Payments.DataLocks.DataLockProxyService/Handlers/PeriodEndStartedHandler.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Fabric;
+using System.Fabric.Description;
 using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
 using NServiceBus;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.Core;
@@ -19,7 +22,39 @@ namespace SFA.DAS.Payments.DataLocks.DataLockProxyService.Handlers
         public Task Handle(PeriodEndStartedEvent message, IMessageHandlerContext context)
         {
             logger.LogInfo($"Received period end started event. Details: {message.ToJson()}");
-            //TODO: when regenerate dlock after apprenticeship updated feature is added use this to disable until period end finished
+
+            long iterations = 0;
+            var appName = "SFA.DAS.Payments.DataLocks.ApprovalsServiceType";
+            var serviceName = "ApprovalsService";
+            var serviceType = "ApprovalsServiceType";
+
+            var fabricClient = new FabricClient();
+            var partitionDescription = new SingletonPartitionSchemeDescription();
+            var serviceDescription = new StatelessServiceDescription()
+            {
+                ApplicationName = new Uri(appName),
+                PartitionSchemeDescription = partitionDescription,
+                ServiceName = new Uri(serviceName),
+                ServiceTypeName = serviceType
+            };
+
+            // Create the service instance.  If the service is declared as a default service in the ApplicationManifest.xml,
+            // the service instance is already running and this call will fail.
+            try
+            {
+                await fabricClient.ServiceManager.DeleteServiceAsync(serviceDescription);
+            }
+            catch (AggregateException ae)
+            {
+                Console.WriteLine($"Failed to deploy the new service. {ae}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to deploy the new service. {e}");
+            }
+
+
+
             return Task.CompletedTask;
         }
     }
