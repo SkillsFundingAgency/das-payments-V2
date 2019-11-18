476dc2b7a3ed14065bdb9b494a1d82423e48faa1
Ben Arnold <ben@arnoldfamily.me.uk>
2019-11-18 16:36:21 +0000

    Match a received Price Episode event to current price episodes

---
+++ src/SFA.DAS.Payments.DataLocks.Domain.UnitTests/Services/PriceEpisodeEventMatcherTest.cs
@@ -0,0 +1,91 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Payments.DataLocks.Domain.Services;
+using SFA.DAS.Payments.DataLocks.Messages.Events;
+using SFA.DAS.Payments.Model.Core;
+using System.Collections.Generic;
+
+namespace SFA.DAS.Payments.DataLocks.Domain.UnitTests.Services
+{
+    public class PriceEpisodeEventMatcherTest
+    {
+        [Theory, AutoData]
+        public void When_price_episode_cannot_be_found_then_return_New(
+            PriceEpisode priceEpisode,
+            PriceEpisodeEventMatcher sut)
+        {
+            var currentPriceEpisodes = new[]
+            {
+                new CurrentPriceEpisode
+                {
+                    PriceEpisodeIdentifier = "something else",
+                    AgreedPrice = priceEpisode.AgreedPrice,
+                }
+            };
+
+            var r = sut.Match(currentPriceEpisodes, priceEpisode);
+
+            r.Should().Be(PriceEpisodeStatus.New);
+        }
+
+        [Theory, AutoData]
+        public void When_price_episode_is_stored_and_has_not_changed_then_return_New(
+            PriceEpisode priceEpisode,
+            PriceEpisodeEventMatcher sut)
+        {
+            var currentPriceEpisodes = new[]
+            {
+                new CurrentPriceEpisode
+                {
+                    PriceEpisodeIdentifier = priceEpisode.Identifier,
+                    AgreedPrice = priceEpisode.AgreedPrice,
+                }
+            };
+
+            var r = sut.Match(currentPriceEpisodes, priceEpisode);
+
+            r.Should().Be(PriceEpisodeStatus.New);
+        }
+
+        [Theory, AutoData]
+        public void When_price_episode_is_stored_and_changes_case_then_return_New(
+            PriceEpisode priceEpisode,
+            PriceEpisodeEventMatcher sut)
+        {
+            priceEpisode.Identifier = priceEpisode.Identifier.ToLower();
+            
+            var currentPriceEpisodes = new[]
+            {
+                new CurrentPriceEpisode
+                {
+                    PriceEpisodeIdentifier = priceEpisode.Identifier.ToUpper(),
+                    AgreedPrice = priceEpisode.AgreedPrice,
+                }
+            };
+
+            var r = sut.Match(currentPriceEpisodes, priceEpisode);
+
+            r.Should().Be(PriceEpisodeStatus.New);
+        }
+
+        [Theory, AutoData]
+        public void When_price_episode_price_changes_then_return_Updated(
+            PriceEpisode priceEpisode,
+            PriceEpisodeEventMatcher sut)
+        {
+            var currentPriceEpisodes = new[]
+            {
+                new CurrentPriceEpisode
+                {
+                    PriceEpisodeIdentifier = priceEpisode.Identifier,
+                    AgreedPrice = 999,
+                }
+            };
+
+            var r = sut.Match(currentPriceEpisodes, priceEpisode);
+
+            r.Should().Be(PriceEpisodeStatus.Updated);
+        }
+    }
+}
---
+++ src/SFA.DAS.Payments.DataLocks.Domain/Services/PriceEpisodeEventMatcher.cs
@@ -0,0 +1,42 @@
+﻿using SFA.DAS.Payments.Model.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SFA.DAS.Payments.DataLocks.Domain.Services
+{
+    public class CurrentPriceEpisode
+    {
+        public string PriceEpisodeIdentifier { get; set; }
+        public decimal AgreedPrice { get; set; }
+        public long Uln { get; set; }
+        public long Ukprn { get; set; }
+        public long JobId { get; set; }
+    }
+
+    public enum PriceEpisodeStatus
+    {
+        New = 1,
+        Updated,
+        Removed,
+    }
+
+    public class PriceEpisodeEventMatcher
+    {
+        public PriceEpisodeStatus Match(IEnumerable<CurrentPriceEpisode> recordedPriceEpisodes, PriceEpisode newPriceEpisode)
+        {
+            var previous = recordedPriceEpisodes.FirstOrDefault(MatchById(newPriceEpisode));
+
+            if (previous == null)
+                return PriceEpisodeStatus.New;
+            
+            if (previous.AgreedPrice == newPriceEpisode.AgreedPrice)
+                return PriceEpisodeStatus.New;
+            
+            return PriceEpisodeStatus.Updated;
+        }
+
+        private Func<CurrentPriceEpisode, bool> MatchById(PriceEpisode find) =>
+            x => x.PriceEpisodeIdentifier.Equals(find.Identifier, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
