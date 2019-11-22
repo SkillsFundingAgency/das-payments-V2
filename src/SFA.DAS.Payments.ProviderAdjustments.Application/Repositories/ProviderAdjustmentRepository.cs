8bf3e2fd500607cb39a6d7ed35f341910ddd65b0
Carl <Carl@cabm.co.uk>
2019-11-22 16:13:39 +0000

    Added settings for username/password. Added request for the token before connecting to the API

--- src/SFA.DAS.Payments.ProviderAdjustments.Application/Repositories/ProviderAdjustmentRepository.cs
+++ src/SFA.DAS.Payments.ProviderAdjustments.Application/Repositories/ProviderAdjustmentRepository.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Serilog.Enrichers;
 using SFA.DAS.Payments.Application.Batch;
 using SFA.DAS.Payments.Application.Repositories;
+using SFA.DAS.Payments.Core.Configuration;
 using SFA.DAS.Payments.ProviderAdjustments.Domain;
 
 namespace SFA.DAS.Payments.ProviderAdjustments.Application.Repositories
@@ -23,31 +29,50 @@ namespace SFA.DAS.Payments.ProviderAdjustments.Application.Repositories
         private readonly IPaymentsDataContext dataContext;
         private readonly IMapper mapper;
         private readonly IBulkWriter<ProviderAdjustment> bulkWriter;
-        private readonly string easApiEndpoint;
-
+        private readonly string apiUsername;
+        private readonly string apiPassword;
+       
         public ProviderAdjustmentRepository(
             IBulkWriter<ProviderAdjustment> bulkWriter, 
             IPaymentsDataContext dataContext, 
-            HttpClient client, 
+            IConfigurationHelper configHelper,
             IMapper mapper)
         {
             this.bulkWriter = bulkWriter;
             this.dataContext = dataContext;
-            this.client = client;
+            client = new HttpClient{BaseAddress = new Uri(configHelper.GetSetting("EasApiEndpoint"))};
+            apiUsername = configHelper.GetSetting("EasApiUsername");
+            apiPassword = configHelper.GetSetting("EasApiPassword");
             this.mapper = mapper;
-            easApiEndpoint = "";
         }
 
         public async Task<List<ProviderAdjustment>> GetCurrentProviderAdjustments()
         {
-            var httpResponse = await client.GetStringAsync("requesturi").ConfigureAwait(false);
+            var token = await GetToken();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+            var httpResponse = await client.GetStringAsync("api/v1/Eas/1920").ConfigureAwait(false);
             var results = JsonConvert.DeserializeObject<List<ProviderAdjustment>>(httpResponse);
             return results;
         }
 
+        public async Task<string> GetToken()
+        {
+            var body = $"{{\"userName\":\"{apiUsername}\", \"apiPassword\": \"{apiPassword}\"}}";
+            var content = new ByteArrayContent(Encoding.UTF8.GetBytes(body));
+
+            var httpResponse = await client.PostAsync($"api/v1/token", content);
+            var responseContent = await httpResponse.Content.ReadAsStringAsync();
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                return responseContent;
+            }
+            
+            throw new InvalidOperationException($"Error getting API token: {responseContent}");
+        }
+
         public async Task<List<ProviderAdjustment>> GetPreviousProviderAdjustments()
         {
-            var results = mapper.Map<List<ProviderAdjustment>>(dataContext.ProviderAdjustments);
+            var results = mapper.Map<List<ProviderAdjustment>>(await dataContext.ProviderAdjustments.ToListAsync());
             return results;
         }
 
