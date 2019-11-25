0ebfc6d1da001f5ed7db22daf1d6223d8ed4cc80
Tony <abroodie@gmail.com>
2019-11-25 21:15:25 +0000

    Tidied up the earnings job service

--- src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/Earnings/EarningsJobService.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/Earnings/EarningsJobService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
@@ -17,17 +14,12 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing
         Task RecordDcJobCompleted(long jobId, bool succeeded, CancellationToken cancellationToken);
     }
 
-    public class EarningsJobService : IEarningsJobService
-    {
-        private readonly IPaymentLogger logger;
-        private readonly IJobStorageService jobStorageService;
-        private readonly ITelemetry telemetry;
 
+    public class EarningsJobService : JobService, IEarningsJobService
+    {
         public EarningsJobService(IPaymentLogger logger, IJobStorageService jobStorageService, ITelemetry telemetry)
+        :base(logger,jobStorageService, telemetry)
         {
-            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            this.jobStorageService = jobStorageService ?? throw new ArgumentNullException(nameof(jobStorageService));
-            this.telemetry = telemetry ?? throw new ArgumentNullException(nameof(telemetry));
         }
 
         public async Task RecordNewJob(RecordEarningsJob earningsJobRequest, CancellationToken cancellationToken = default(CancellationToken))
@@ -45,24 +37,8 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing
                 Status = JobStatus.InProgress,
                 LearnerCount = earningsJobRequest.LearnerCount
             };
-            var isNewJob = await jobStorageService.StoreNewJob(jobDetails, CancellationToken.None);
-            logger.LogDebug($"Finished storing new job: {jobDetails.Id} for dc job id: {jobDetails.DcJobId}, Ukprn: {earningsJobRequest.Ukprn}. Now storing the job messages.");
-            await RecordJobInProgressMessages(earningsJobRequest.JobId,earningsJobRequest.GeneratedMessages,cancellationToken).ConfigureAwait(false);
-            logger.LogDebug($"Finished storing new job messages for job: {jobDetails.Id} for dc job id: {jobDetails.DcJobId}, Ukprn: {earningsJobRequest.Ukprn}.");
-            if (isNewJob)
-                SendTelemetry(earningsJobRequest, jobDetails);
-            logger.LogInfo($"Finished saving the job info.  DC Job Id: {earningsJobRequest.JobId}, Ukprn: {earningsJobRequest.Ukprn}.");
-        }
-
-        private async Task RecordJobInProgressMessages(long jobId, List<GeneratedMessage> generatedMessages, CancellationToken cancellationToken)
-        {
-            await jobStorageService.StoreInProgressMessages(jobId,
-                generatedMessages.Select(message => new InProgressMessage
-                {
-                    MessageId = message.MessageId,
-                    JobId = jobId,
-                    MessageName = message.MessageName
-                }).ToList(), cancellationToken);
+            await RecordNewJob(jobDetails, earningsJobRequest.GeneratedMessages, cancellationToken)
+                .ConfigureAwait(false);
         }
 
         public async Task RecordNewJobAdditionalMessages(RecordEarningsJobAdditionalMessages earningsJobRequest, CancellationToken cancellationToken)
@@ -76,23 +52,6 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing
             logger.LogDebug($"Now storing the completion status of the submission job. Id: {jobId}, succeeded: {succeeded}");
             await jobStorageService.StoreDcJobStatus(jobId, succeeded, cancellationToken).ConfigureAwait(false);
             logger.LogInfo($"Finished storing the completion status of the submission job. Id: {jobId}, succeeded: {succeeded}");
-        }
-
-
-        private void SendTelemetry(RecordEarningsJob startedEvent, JobModel jobDetails)
-        {
-            telemetry.TrackEvent("Started Job",
-                new Dictionary<string, string>
-                {
-                    { "JobType", JobType.EarningsJob.ToString("G") },
-                    { "Ukprn", startedEvent.Ukprn.ToString() },
-                    { "Id", jobDetails.Id.ToString() },
-                    { "ExternalJobId", startedEvent.JobId.ToString() },
-                    { "CollectionPeriod", startedEvent.CollectionPeriod.ToString() },
-                    { "CollectionYear", startedEvent.CollectionYear.ToString() }
-                },
-                new Dictionary<string, double> { { "LearnerCount", startedEvent.LearnerCount } }
-            );
         }
     }
 }
---
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/JobService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
+using SFA.DAS.Payments.Monitoring.Jobs.Messages.Commands;
+using SFA.DAS.Payments.Monitoring.Jobs.Model;
+
+namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing
+{
+    public abstract class JobService
+    {
+        protected readonly IPaymentLogger logger;
+        protected readonly IJobStorageService jobStorageService;
+        protected readonly ITelemetry telemetry;
+
+        protected JobService(IPaymentLogger logger, IJobStorageService jobStorageService, ITelemetry telemetry)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.jobStorageService = jobStorageService ?? throw new ArgumentNullException(nameof(jobStorageService));
+            this.telemetry = telemetry ?? throw new ArgumentNullException(nameof(telemetry));
+        }
+
+        protected virtual async Task RecordNewJob(JobModel jobDetails, List<GeneratedMessage> generatedMessages, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (!jobDetails.DcJobId.HasValue)
+                throw new InvalidOperationException("No DcJob Id found on the job.");
+
+            var isNewJob = await jobStorageService.StoreNewJob(jobDetails, CancellationToken.None);
+            logger.LogDebug($"Finished storing new job: {jobDetails.Id} for dc job id: {jobDetails.DcJobId}. Now storing the job messages.");
+            await RecordJobInProgressMessages(jobDetails.DcJobId.Value, generatedMessages, cancellationToken).ConfigureAwait(false);
+            logger.LogDebug($"Finished storing new job messages for job: {jobDetails.Id} for dc job id: {jobDetails.DcJobId}.");
+            if (isNewJob)
+                SendTelemetry(jobDetails);
+            logger.LogInfo($"Finished saving the job info.  DC Job Id: {jobDetails.DcJobId}");
+        }
+
+        protected async Task RecordJobInProgressMessages(long jobId, List<GeneratedMessage> generatedMessages, CancellationToken cancellationToken)
+        {
+            await jobStorageService.StoreInProgressMessages(jobId,
+                generatedMessages.Select(message => new InProgressMessage
+                {
+                    MessageId = message.MessageId,
+                    JobId = jobId,
+                    MessageName = message.MessageName
+                }).ToList(), cancellationToken);
+        }
+
+
+        private void SendTelemetry(JobModel jobDetails)
+        {
+            telemetry.TrackEvent("Started Job",
+                new Dictionary<string, string>
+                {
+                    { TelemetryKeys.JobType, jobDetails.JobType.ToString("G") },
+                    { TelemetryKeys.Ukprn, (jobDetails.Ukprn ?? 0).ToString() },
+                    { TelemetryKeys.InternalJobId , jobDetails.Id.ToString() },
+                    { TelemetryKeys.JobId, jobDetails.DcJobId.ToString() },
+                    { TelemetryKeys.CollectionPeriod , jobDetails.CollectionPeriod.ToString() },
+                    { TelemetryKeys.AcademicYear , jobDetails.AcademicYear.ToString() }
+                },
+                new Dictionary<string, double> { { "LearnerCount", jobDetails.LearnerCount ?? 0 } }
+            );
+        }
+    }
+}
