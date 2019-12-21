f614c4ba4a2f3b9ed1f16704bd83f83a79796377
Tony <abroodie@gmail.com>
2019-12-21 19:41:58 +0000

    metrics.submissionservice now uses nsb transport

--- src/SFA.DAS.Payments.Monitoring.Metrics.SubmissionService/SubmissionService.cs
+++ src/SFA.DAS.Payments.Monitoring.Metrics.SubmissionService/SubmissionService.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Fabric;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+using Autofac;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.ServiceFabric.Core;
 
 namespace SFA.DAS.Payments.Monitoring.Metrics.SubmissionService
 {
@@ -14,9 +14,16 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.SubmissionService
     /// </summary>
     public class SubmissionService : StatelessService
     {
-        public SubmissionService(StatelessServiceContext context)
+        private IStatelessEndpointCommunicationListener listener;
+        private readonly ILifetimeScope lifetimeScope;
+        private readonly IPaymentLogger logger;
+
+        public SubmissionService(StatelessServiceContext context, ILifetimeScope lifetimeScope, IPaymentLogger logger)
             : base(context)
-        { }
+        {
+            this.lifetimeScope = lifetimeScope ?? throw new ArgumentNullException(nameof(lifetimeScope));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
 
         /// <summary>
         /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
@@ -24,28 +31,12 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.SubmissionService
         /// <returns>A collection of listeners.</returns>
         protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
         {
-            return new ServiceInstanceListener[0];
-        }
+            logger.LogInfo("Creating Service Instance Listeners For SubmissionService");
 
-        /// <summary>
-        /// This is the main entry point for your service instance.
-        /// </summary>
-        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
-        protected override async Task RunAsync(CancellationToken cancellationToken)
-        {
-            // TODO: Replace the following sample code with your own logic 
-            //       or remove this RunAsync override if it's not needed in your service.
-
-            long iterations = 0;
-
-            while (true)
+            return new List<ServiceInstanceListener>
             {
-                cancellationToken.ThrowIfCancellationRequested();
-
-                ServiceEventSource.Current.ServiceMessage(this.Context, "Working-{0}", ++iterations);
-
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
-            }
+                new ServiceInstanceListener(context =>listener = lifetimeScope.Resolve<IStatelessEndpointCommunicationListener>())
+            };
         }
     }
 }
