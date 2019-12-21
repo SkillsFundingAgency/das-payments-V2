abbeb7ffdfc9a0077394461318ccb69f2f01a22e
Tony <abroodie@gmail.com>
2019-12-21 19:48:50 +0000

    metrics.submission service now subscribes to SubmissionJobSucceeded events

---
+++ src/SFA.DAS.Payments.Monitoring.Metrics.SubmissionService/Handlers/SubmissionJobSucceededHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Core;
+using SFA.DAS.Payments.Monitoring.Jobs.Messages.Events;
+
+namespace SFA.DAS.Payments.Monitoring.Metrics.SubmissionService.Handlers
+{
+    public class SubmissionJobSucceededHandler: IHandleMessages<SubmissionJobSucceeded>
+    {
+        private readonly IPaymentLogger logger;
+
+        public SubmissionJobSucceededHandler(IPaymentLogger logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Handle(SubmissionJobSucceeded message, IMessageHandlerContext context)
+        {
+            logger.LogInfo($"Finished handling the SubmissionJobSucceeded event: {message.ToJson()}");
+        }
+    }
+}
