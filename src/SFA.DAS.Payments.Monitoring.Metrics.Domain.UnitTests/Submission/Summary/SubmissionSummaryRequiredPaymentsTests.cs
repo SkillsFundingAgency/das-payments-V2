d4efc0001e053bcd257ed19e34d3fc3a5ae7d1d2
Tony <abroodie@gmail.com>
2019-12-30 22:06:00 +0000

    added failing test for summary percentage

--- src/SFA.DAS.Payments.Monitoring.Metrics.Domain.UnitTests/Submission/Summary/SubmissionSummaryRequiredPaymentsTests.cs
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Domain.UnitTests/Submission/Summary/SubmissionSummaryRequiredPaymentsTests.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using NUnit.Framework;
 using SFA.DAS.Payments.Model.Core.Entities;
 using SFA.DAS.Payments.Monitoring.Metrics.Domain.Submission;
+using SFA.DAS.Payments.Monitoring.Metrics.Model.Submission;
 
 namespace SFA.DAS.Payments.Monitoring.Metrics.Domain.UnitTests.Submission.Summary
 {
@@ -10,7 +11,7 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Domain.UnitTests.Submission.Summar
     public class SubmissionSummaryRequiredPaymentsTests
     {
         private SubmissionSummary GetSubmissionSummary => TestsHelper.DefaultSubmissionSummary;
-        
+
 
         [Test]
         public void Calculates_Correct_Metrics_For_RequiredPayments()
@@ -36,6 +37,35 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Domain.UnitTests.Submission.Summar
             metrics.RequiredPaymentsMetrics.Count.Should().Be(2);
             metrics.RequiredPayments.ContractType1.Should().Be(requiredPayments.FirstOrDefault(x => x.ContractType == ContractType.Act1).Total);
             metrics.RequiredPayments.ContractType2.Should().Be(requiredPayments.FirstOrDefault(x => x.ContractType == ContractType.Act2).Total);
+        }
+    }
+
+    [TestFixture]
+    public class SubmissionSummaryTests
+    {
+        private SubmissionSummary GetSubmissionSummary()
+        {
+            var summary = TestsHelper.DefaultSubmissionSummary;
+            summary.AddEarnings(TestsHelper.DefaultDcEarnings, TestsHelper.DefaultDasEarnings);
+            summary.AddDataLockedEarnings(TestsHelper.DefaultDataLockedEarnings);
+            summary.AddRequiredPayments(TestsHelper.DefaultRequiredPayments);
+            summary.AddHeldBackCompletionPayments(TestsHelper.DefaultHeldBackCompletionPayments);
+            return summary;
+        }
+
+        private SubmissionSummaryModel GetSubmissionSummaryMetrics()
+        {
+            var summary = GetSubmissionSummary();
+            return summary.GetMetrics();
+        }
+
+
+        [Test]
+        public void Calculates_Correct_Percentage()
+        {
+            var metrics = GetSubmissionSummaryMetrics();
+            metrics.Percentage.Should().Be(100);
+            metrics.Difference.Should().Be(0);
         }
     }
 }
