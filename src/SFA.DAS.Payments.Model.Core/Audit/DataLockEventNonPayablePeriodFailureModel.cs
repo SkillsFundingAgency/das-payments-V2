8ce58e2a1091900a467ac3ea13a08c67b29be796
Tony <abroodie@gmail.com>
2020-01-02 13:46:55 +0000

    Added models for DataLocks

---
+++ src/SFA.DAS.Payments.Model.Core/Audit/DataLockEventNonPayablePeriodFailureModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.Payments.Model.Core.Audit
+{
+    public class DataLockEventNonPayablePeriodFailureModel
+    {
+        public long Id { get; set; }
+        public virtual DataLockEventNonPayablePeriodModel DataLockEventNonPayablePeriod { get; set; }
+        public DataLockErrorCode DataLockFailure { get; set; }
+    }
+}
