28e5b7a1c6a5e119bfa9e55bd62a232cba3f2096
Tony <abroodie@gmail.com>
2020-01-02 14:27:41 +0000

    Added configurations for DataLockModel emtities

---
+++ src/SFA.DAS.Payments.Application/Data/Configurations/DataLockEventNonPayablePeriodFailureModelConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SFA.DAS.Payments.Model.Core.Audit;
+
+namespace SFA.DAS.Payments.Application.Data.Configurations
+{
+    public class DataLockEventNonPayablePeriodFailureModelConfiguration : IEntityTypeConfiguration<DataLockEventNonPayablePeriodFailureModel>
+    {
+        public void Configure(EntityTypeBuilder<DataLockEventNonPayablePeriodFailureModel> builder)
+        {
+            builder.ToTable("DataLockEventNonPayablePeriodFailures", "Payments2");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).HasColumnName(@"Id").IsRequired();
+            builder.Property(x => x.DataLockFailure).HasColumnName(@"DataLockFailureId").IsRequired();
+            builder.Property(x => x.DataLockEventNonPayablePeriodId).HasColumnName(@"DataLockEventNonPayablePeriod").IsRequired();
+            builder.Property(x => x.ApprenticeshipId).HasColumnName(@"ApprenticeshipId").IsRequired();
+
+            builder.HasOne(x => x.DataLockEventNonPayablePeriod).WithMany(dl => dl.Failures).HasForeignKey(x => x.DataLockEventNonPayablePeriodId);
+            builder.HasOne(x => x.Apprenticeship).WithMany().HasForeignKey(x => x.ApprenticeshipId);
+        }
+    }
+}
