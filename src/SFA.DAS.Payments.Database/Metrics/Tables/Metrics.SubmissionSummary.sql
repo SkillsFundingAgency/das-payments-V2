6d991d959720c8410a9d681435a21af0ee0ed18b
Tony <abroodie@gmail.com>
2020-01-04 10:32:13 +0000

    fixed typo in column names on SubmissionSummary table

--- src/SFA.DAS.Payments.Database/Metrics/Tables/Metrics.SubmissionSummary.sql
+++ src/SFA.DAS.Payments.Database/Metrics/Tables/Metrics.SubmissionSummary.sql
@@ -5,16 +5,18 @@
 	,CollectionPeriod tinyint not null
 	,JobId bigint not null
 	,[Percentage] decimal(15,5) not null
-	,PercentageContractType1 decimal(15,5) not null
-	,PercentageContractType2 decimal(15,5) not null
+	,ContractType1 decimal(15,5) not null
+	,ContractType2 decimal(15,5) not null
 	,DifferenceContractType1 decimal(15,5) not  null
 	,DifferenceContractType2 decimal(15,5) not  null
+	,PercentageContractType1 decimal(15,5) not null
+	,PercentageContractType2 decimal(15,5) not null
 	,EarningsDCContractType1 decimal(15,5) not null
 	,EarningsDCContractType2 decimal(15,5) not null
 	,EarningsDASContractType1 decimal(15,5) not null
 	,EarningsDASContractType2 decimal(15,5) not null
-	,EaringsDifferenceContractType1 decimal(15,5) not  null
-	,EaringsDifferenceContractType2 decimal(15,5) not  null
+	,EarningsDifferenceContractType1 decimal(15,5) not  null
+	,EarningsDifferenceContractType2 decimal(15,5) not  null
 	,EarningsPercentageContractType1 decimal(15,5) not null
 	,EarningsPercentageContractType2 decimal(15,5) not null
 	,RequiredPaymentsContractType1 decimal(15,5) not  null
