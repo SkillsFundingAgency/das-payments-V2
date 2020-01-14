e1dd9575ffd18faa8d8c7d45db84a4465b9f7cf9
Mark Keene <Mark@keenesolutions.co.uk>
2020-01-14 11:34:20 +0000

    commented out event handler

--- src/SFA.DAS.Payments.Monitoring.AcceptanceTests/Handlers/RecordPeriodEndStartFailedHandler.cs
+++ src/SFA.DAS.Payments.Monitoring.AcceptanceTests/Handlers/RecordPeriodEndStartFailedHandler.cs
@@ -8,11 +8,18 @@ namespace SFA.DAS.Payments.Monitoring.AcceptanceTests.Handlers
 {
     public class RecordPeriodEndStartFailedHandler : IHandleMessages<RecordPeriodEndRunStartJobFailed>
     {
+
+
         public static ConcurrentBag<RecordPeriodEndRunStartJobFailed> ReceivedEvents { get; } = new ConcurrentBag<RecordPeriodEndRunStartJobFailed>();
 
         public Task Handle(RecordPeriodEndRunStartJobFailed message, IMessageHandlerContext context)
         {
-            ReceivedEvents.Add(message);
+
+            //This should be using a custom event rather than command. Need to create the event objects
+            // and also check where these will be handled
+
+
+            //ReceivedEvents.Add(message);
             return Task.CompletedTask;
         }
     }
