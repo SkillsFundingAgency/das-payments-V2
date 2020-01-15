93988b2d445c7ec1fc69c7e36a10777ad5363529
Carl Berisford-Murray <carl@techcellence.org.uk>
2020-01-15 07:45:05 +0000

    Updated datamatch report to only use the most recent job id for each provider

---
+++ src/SFA.DAS.Payments.Database/Payments2/Views/Payments2.LatestSuccessfulJobs.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [Payments2].[LatestSuccessfulJobs]
+AS
+
+with validJobs (DcJobId, Ukprn)
+as
+(
+Select
+max(DcJobId),
+Ukprn
+from Payments2.Job
+where status in (2,3)
+group by Ukprn
+)
+Select
+j.*
+from Payments2.Job j
+join validJobs vj on j.DCJobId = vj.DcJobId
+
