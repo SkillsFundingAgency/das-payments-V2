3bbbf2e173c9ef3b53f00d559baa038c9a9e2f0a
kola <samuel.dev.dba@gmail.com>
2020-01-16 10:25:42 +0000

    Add lock timeout  message to log

--- src/SFA.DAS.Payments.Application/Messaging/MessageTimedOutBehaviour.cs
+++ src/SFA.DAS.Payments.Application/Messaging/MessageTimedOutBehaviour.cs
@@ -1,22 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.ServiceBus;
 using NServiceBus.Pipeline;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
 
 namespace SFA.DAS.Payments.Application.Messaging
 {
     public class MessageTimedOutBehaviour : Behavior<IIncomingLogicalMessageContext>
     {
+        private readonly IPaymentLogger logger;
+
+        public MessageTimedOutBehaviour(IPaymentLogger logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         public override async Task Invoke(IIncomingLogicalMessageContext context, Func<Task> next)
         {
             var lockedUntil = context.Extensions.Get<Message>().SystemProperties.LockedUntilUtc;
             if (DateTime.UtcNow > lockedUntil)
-                throw new InvalidOperationException($"Message has timed out before processing. Locked until: {lockedUntil}, current time: {DateTime.UtcNow} ");
+            {
+                var timeoutMesssage = $"Message has timed out before processing. Locked until: {lockedUntil}, current time: {DateTime.UtcNow} ";
+                logger.LogWarning(timeoutMesssage);
+                throw new InvalidOperationException();
+            }
+
             await next().ConfigureAwait(false);
             if (DateTime.UtcNow > lockedUntil)
-                throw new InvalidOperationException($"Message has timed out after processing. Locked until: {lockedUntil}, current time: {DateTime.UtcNow} ");
+            {
+                var lockTimeoutMessage = $"Message has timed out after processing. Locked until: {lockedUntil}, current time: {DateTime.UtcNow} ";
+                logger.LogWarning(lockTimeoutMessage);
+                throw new InvalidOperationException(lockTimeoutMessage);
+            }
+
         }
     }
 }
