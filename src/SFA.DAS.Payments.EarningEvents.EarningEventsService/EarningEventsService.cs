ae72a6e5232b5781092a254bac2554ed428847b7
Carl Berisford-Murray <carl@techcellence.org.uk>
2020-02-12 09:51:58 +0000

    Haven't worked out how to distinguish between a stateful service and an actor service, but ignoring the exceptions from normal stateful services

--- src/SFA.DAS.Payments.EarningEvents.EarningEventsService/EarningEventsService.cs
+++ src/SFA.DAS.Payments.EarningEvents.EarningEventsService/EarningEventsService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Fabric;
+using System.Fabric.Query;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,16 +9,15 @@ using Autofac;
 using ESFA.DC.JobContextManager.Interface;
 using ESFA.DC.JobContextManager.Model;
 using Microsoft.ServiceFabric.Actors;
-using Microsoft.ServiceFabric.Actors.Client;
 using Microsoft.ServiceFabric.Actors.Query;
 using Microsoft.ServiceFabric.Services.Client;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Remoting;
 using Microsoft.ServiceFabric.Services.Remoting.Client;
 using Microsoft.ServiceFabric.Services.Remoting.FabricTransport;
-using Microsoft.ServiceFabric.Services.Runtime;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.ServiceFabric.Core;
+using StatelessService = Microsoft.ServiceFabric.Services.Runtime.StatelessService;
 
 [assembly: FabricTransportServiceRemotingProvider(RemotingListenerVersion = RemotingListenerVersion.V2_1, 
     RemotingClientVersion = RemotingClientVersion.V2_1)]
@@ -52,43 +52,51 @@ namespace SFA.DAS.Payments.EarningEvents.EarningEventsService
 
                 foreach (var application in applications)
                 {
-                    logger.LogInfo($"APPLICATION - {application.ApplicationTypeName}");
                     var services = await client.QueryManager.GetServiceListAsync(application.ApplicationName);
                     foreach (var service in services)
                     {
-                        logger.LogInfo($"\tSERVICE - {service.ServiceTypeName}");
-                        var partitions = await client.QueryManager.GetPartitionListAsync(service.ServiceName);
-                        foreach (var partition in partitions)
+                        if (service.ServiceKind == ServiceKind.Stateful)
                         {
-                            logger.LogInfo($"\t\tPARTITION - {partition.PartitionInformation.Id}: {partition.PartitionInformation.Kind}");
-                            if (partition.PartitionInformation.Kind == ServicePartitionKind.Int64Range)
+                            var partitions = await client.QueryManager.GetPartitionListAsync(service.ServiceName);
+                            foreach (var partition in partitions)
                             {
-                                var partitionInformation = (Int64RangePartitionInformation)partition.PartitionInformation;
-                                logger.LogInfo($"\t\tRANGED PARTITION: {partitionInformation.LowKey} - {partitionInformation.HighKey}");
-                                var actorServiceProxy = ServiceProxy.Create<IActorService>(service.ServiceName,
-                                    new ServicePartitionKey(partitionInformation.LowKey));
-
-                                ContinuationToken token = null;
-                                var activeActors = new List<ActorInformation>();
-                                var allActors = new List<ActorInformation>();
-
-                                do
+                                logger.LogInfo($"\t\tPARTITION - {partition.PartitionInformation.Id}: {partition.PartitionInformation.Kind}");
+                                if (partition.PartitionInformation.Kind == ServicePartitionKind.Int64Range)
                                 {
-                                    try
+                                    var partitionInformation = (Int64RangePartitionInformation) partition.PartitionInformation;
+                                    logger.LogInfo($"\t\tRANGED PARTITION: {partitionInformation.LowKey} - {partitionInformation.HighKey}");
+                                    var actorServiceProxy = ServiceProxy.Create<IActorService>(service.ServiceName,
+                                        new ServicePartitionKey(partitionInformation.LowKey));
+
+                                    ContinuationToken token = null;
+                                    var activeActors = new List<ActorInformation>();
+                                    var allActors = new List<ActorInformation>();
+                                    var exception = false;
+                                    do
                                     {
-                                        var page = await actorServiceProxy.GetActorsAsync(token,
-                                            default(CancellationToken));
-                                        activeActors.AddRange(page.Items.Where(x => x.IsActive));
-                                        allActors.AddRange(page.Items);
-                                        token = page.ContinuationToken;
-                                    }
-                                    catch (Exception e)
+                                        try
+                                        {
+                                            var page = await actorServiceProxy.GetActorsAsync(token,
+                                                default(CancellationToken));
+                                            activeActors.AddRange(page.Items.Where(x => x.IsActive));
+                                            allActors.AddRange(page.Items);
+                                            token = page.ContinuationToken;
+                                        }
+                                        catch (Exception e)
+                                        {
+                                            logger.LogError("Error enumerating actors", e);
+                                            token = null;
+                                            exception = true;
+                                        }
+                                    } while (token != null);
+
+                                    if (!exception)
                                     {
-                                        logger.LogError("Error enumerating actors", e);
-                                        token = null;
+                                        logger.LogInfo($"{service.ServiceName}\n" +
+                                                     $"RANGED PARTITION: {partitionInformation.LowKey} - {partitionInformation.HighKey}\n" +
+                                                     $"{activeActors.Count} active actors out of {allActors.Count} actors");
                                     }
-                                } while (token != null);
-                                logger.LogInfo($"\t\t\t{activeActors.Count} active actors out of {allActors.Count} actors");
+                                }
                             }
                         }
                     }
