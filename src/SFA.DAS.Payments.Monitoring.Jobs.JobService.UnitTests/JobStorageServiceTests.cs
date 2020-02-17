9b0fb39bcdb385e5785f740d2b7270bc91d567ed
tom-gough <tom@tgsd.co.uk>
2020-02-17 16:07:02 +0000

    Fixed test file naming

--- src/SFA.DAS.Payments.Monitoring.Jobs.JobService.UnitTests/JobStorageServiceTests.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.JobService.UnitTests/JobStorageServiceTests.cs
@@ -12,7 +12,7 @@ using FluentAssertions;
 namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.UnitTests
 {
     [TestFixture]
-    public class JobStorageService_Tests
+    public class JobStorageServiceTests
     {
         private JobStorageService jobsStorageService;
         private Mock<IJobModelRepository> jobModelRepository;
