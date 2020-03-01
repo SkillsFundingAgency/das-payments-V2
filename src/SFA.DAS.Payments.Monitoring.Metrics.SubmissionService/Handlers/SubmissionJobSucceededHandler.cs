ad527e72b92fe96dc5905a6e8a9aa35575e04fb0
Tony <abroodie@gmail.com>
2020-03-01 23:29:40 +0000

    Added delay before triggering generation to account for reading from replica

--- src/SFA.DAS.Payments.Monitoring.Metrics.SubmissionService/Handlers/SubmissionJobSucceededHandler.cs
+++ src/SFA.DAS.Payments.Monitoring.Metrics.SubmissionService/Handlers/SubmissionJobSucceededHandler.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using NServiceBus;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.Core;
+using SFA.DAS.Payments.Core.Configuration;
 using SFA.DAS.Payments.Monitoring.Jobs.Messages.Events;
 using SFA.DAS.Payments.Monitoring.Metrics.Application.Submission;
 
@@ -13,15 +14,31 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.SubmissionService.Handlers
     {
         private readonly IPaymentLogger logger;
         private readonly ISubmissionMetricsService submissionMetricsService;
+        private readonly IConfigurationHelper configurationHelper;
 
-        public SubmissionJobSucceededHandler(IPaymentLogger logger, ISubmissionMetricsService submissionMetricsService)
+        public SubmissionJobSucceededHandler(IPaymentLogger logger, ISubmissionMetricsService submissionMetricsService,IConfigurationHelper configurationHelper)
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this.submissionMetricsService = submissionMetricsService ?? throw new ArgumentNullException(nameof(submissionMetricsService));
+            this.configurationHelper = configurationHelper ?? throw new ArgumentNullException(nameof(configurationHelper));
         }
 
         public async Task Handle(SubmissionJobSucceeded message, IMessageHandlerContext context)
         {
+            if (!context.MessageHeaders.ContainsKey("Delayed"))
+            {
+                var delay = TimeSpan.Parse(
+                    configurationHelper.GetSettingOrDefault("MetricsGenerationDelay", "00:05:00"));
+                logger.LogInfo($"Delaying metrics generation for {delay.TotalSeconds} seconds for job: {message.JobId}");
+                var options = new SendOptions();
+                options.SetHeader("Delayed", DateTimeOffset.UtcNow.ToString("G")+delay);
+                options.RouteToThisEndpoint();
+                options.DelayDeliveryWith(delay);
+                await context.Send(message, options).ConfigureAwait(false);
+                context.DoNotContinueDispatchingCurrentMessageToHandlers();
+                return;
+            }
+
             logger.LogDebug($"Handling message to build metrics for: {message.ToJson()}");
             await submissionMetricsService.BuildMetrics(message.Ukprn, message.JobId, message.AcademicYear,
                 message.CollectionPeriod, CancellationToken.None).ConfigureAwait(false);
