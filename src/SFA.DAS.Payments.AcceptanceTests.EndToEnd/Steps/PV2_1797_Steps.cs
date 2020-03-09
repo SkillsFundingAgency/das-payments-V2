e0bf6f25a65c59b460857e2ca17c13ea56f69ffd
kola <samuel.dev.dba@gmail.com>
2020-03-09 13:05:59 +0000

    Clean Up

--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/PV2_1797_Steps.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/PV2_1797_Steps.cs
@@ -29,38 +29,6 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
         [Given(@"there is a single price episode in the ILR, (.*)")]
         public void GivenThereIsASinglePriceEpisodeInTheILRPE(string p0) { }
 
-
-        [Given(@"the (.*) in the ILR matches to both Commitments (.*) and (.*), on ULN and UKPRN")]
-        public async Task GivenThePEInTheILRMatchesToBothCommitmentsAAndBOnULNAndUKPRN(string priceEpisodeIdentifier1, string commitmentIdentifier1, string commitmentIdentifier2)
-        {
-            var learner = TestSession.FM36Global.Learners.Single();
-            learner.ULN = TestSession.Learner.Uln;
-            learner.LearnRefNumber = TestSession.Learner.LearnRefNumber;
-
-            var priceEpisode1 = learner.PriceEpisodes.Single(y => y.PriceEpisodeIdentifier == priceEpisodeIdentifier1);
-            var learningDelivery1 = learner.LearningDeliveries.Single(x => x.AimSeqNumber == priceEpisode1.PriceEpisodeValues.PriceEpisodeAimSeqNumber);
-
-            var ids = new List<long> { TestSession.GenerateId(), TestSession.GenerateId() };
-            var commitment1 = new ApprenticeshipBuilder().BuildSimpleApprenticeship(TestSession, learningDelivery1.LearningDeliveryValues, ids.Min()).WithALevyPayingEmployer().WithApprenticeshipPriceEpisode(priceEpisode1.PriceEpisodeValues).ToApprenticeshipModel();
-            var commitment2 = new ApprenticeshipBuilder().BuildSimpleApprenticeship(TestSession, learningDelivery1.LearningDeliveryValues, ids.Max()).WithALevyPayingEmployer().WithApprenticeshipPriceEpisode(priceEpisode1.PriceEpisodeValues).ToApprenticeshipModel();
-
-            TestSession.Apprenticeships.GetOrAdd(commitmentIdentifier1, commitment1);
-            testDataContext.Apprenticeship.Add(commitment1);
-            testDataContext.ApprenticeshipPriceEpisode.AddRange(commitment1.ApprenticeshipPriceEpisodes);
-
-            TestSession.Apprenticeships.GetOrAdd(commitmentIdentifier2, commitment2);
-            testDataContext.Apprenticeship.Add(commitment2);
-            testDataContext.ApprenticeshipPriceEpisode.AddRange(commitment2.ApprenticeshipPriceEpisodes);
-
-            var levyModel = TestSession.Employer.ToModel();
-            levyModel.Balance = 1000000000;
-            testDataContext.LevyAccount.Add(levyModel);
-            await testDataContext.SaveChangesAsync();
-
-            TestSession.FM36Global.UKPRN = TestSession.Provider.Ukprn;
-
-        }
-
         [Given(@"the start date in the (.*) is on or after the start date for Commitment (.*)")]
         public async Task GivenTheStartDateInThePEIsOnOrAfterTheStartDateForCommitmentA(string priceEpisodeIdentifier, string commitmentIdentifier)
         {
--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Tests/DataLocks/PV2-1797-Data Lock09-Single price episode-Happy-Path.feature
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Tests/DataLocks/PV2-1797-Data Lock09-Single price episode-Happy-Path.feature
@@ -3,7 +3,7 @@ Scenario: PV2-1797-DataLock09-Single-Price-Episode-Happy-Path-R02
 
 Given there are 2 Commitments in DAS, Commitment A and Commitment B in collection period R02/current academic year 
 And there is a single price episode in the ILR, PE-1
-And the PE-1 in the ILR matches to both Commitments A and B, on ULN and UKPRN
+And PE-1 in the ILR matches to both Commitments A and B, on ULN and UKPRN
 And the start date in the PE-1 is on or after the start date for Commitment A
 And the start date in the PE-1 is before the start date for Commitment B
 When the Provider submits the single price episode PE-1 in the ILR
