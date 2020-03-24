965e224c9e265ef209aef13154cc28f7ba8cc246
Tony <abroodie@gmail.com>
2020-03-24 22:24:23 +0000

    added registration for communication listener

--- src/SFA.DAS.Payments.Audit.EarningEventsService/Infrastructure/Ioc/AuditEarningEventServiceModule.cs
+++ src/SFA.DAS.Payments.Audit.EarningEventsService/Infrastructure/Ioc/AuditEarningEventServiceModule.cs
@@ -1,7 +1,12 @@
 ﻿using Autofac;
+using SFA.DAS.Payments.Application.Infrastructure.Ioc;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
 using SFA.DAS.Payments.Application.Messaging;
 using SFA.DAS.Payments.Audit.EarningEventsService.Handlers;
+using SFA.DAS.Payments.Core.Configuration;
 using SFA.DAS.Payments.EarningEvents.Messages.Events;
+using SFA.DAS.Payments.ServiceFabric.Core;
 
 namespace SFA.DAS.Payments.Audit.EarningEventsService.Infrastructure.Ioc
 {
@@ -9,6 +14,20 @@ namespace SFA.DAS.Payments.Audit.EarningEventsService.Infrastructure.Ioc
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.Register(c =>
+                {
+                    var appConfig = c.Resolve<IApplicationConfiguration>();
+                    var configHelper = c.Resolve<IConfigurationHelper>();
+                    return new StatelessServiceBusBatchCommunicationListener(configHelper.GetConnectionString("ServiceBusConnectionString"),
+                        appConfig.EndpointName,
+                        appConfig.FailedMessagesQueue,
+                        c.Resolve<IPaymentLogger>(),
+                        c.Resolve<IContainerScopeFactory>(),
+                        c.Resolve<ITelemetry>());
+                })
+                .As<IStatelessServiceBusBatchCommunicationListener>()
+                .SingleInstance();
+
             builder.RegisterType<ApprenticeshipContractType1EarningEventHandler>()
                 .As<IHandleMessageBatches<ApprenticeshipContractType1EarningEvent>>()
                 .InstancePerLifetimeScope();
