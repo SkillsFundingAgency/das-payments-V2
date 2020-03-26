ce993cebbb01c8e57e0624f98d836c3f89e9ea03
Hardik Desai <hardik.desai@digital.education.gov.uk>
2020-03-26 11:09:53 +0000

    use consistent formatting

--- src/Scripts/Database/Audit tables clean up during submission.sql
+++ src/Scripts/Database/Audit tables clean up during submission.sql
@@ -1,139 +1,118 @@
-begin tran
+BEGIN TRAN;
 
-print 'start ' + convert(varchar(150), SYSDATETIME())
+PRINT 'start ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
-declare @collectionPeriod as tinyint = 8
-declare @academicYear as smallint = 1920
+DECLARE @collectionPeriod AS TINYINT= 8;
+DECLARE @academicYear AS SMALLINT= 1920;
 
-IF OBJECT_ID('tempdb..#JobDataToBeDeleted') IS NOT NULL DROP TABLE #JobDataToBeDeleted
-
-SELECT DISTINCT JobId INTO #JobDataToBeDeleted FROM Payments2.EarningEvent 
-Where CollectionPeriod = @collectionPeriod AND AcademicYear = @academicYear
+IF OBJECT_ID('tempdb..#JobDataToBeDeleted') IS NOT NULL DROP TABLE #JobDataToBeDeleted;
+SELECT DISTINCT JobId INTO #JobDataToBeDeleted FROM Payments2.EarningEvent
+WHERE CollectionPeriod = @collectionPeriod AND AcademicYear = @academicYear
 UNION
-SELECT DISTINCT JobId FROM Payments2.RequiredPaymentEvent 
-Where CollectionPeriod = @collectionPeriod AND AcademicYear = @academicYear
+SELECT DISTINCT JobId FROM Payments2.RequiredPaymentEvent
+WHERE CollectionPeriod = @collectionPeriod AND AcademicYear = @academicYear
 UNION
-SELECT DISTINCT JobId FROM Payments2.FundingSourceEvent 
-Where CollectionPeriod = @collectionPeriod AND AcademicYear = @academicYear
+SELECT DISTINCT JobId FROM Payments2.FundingSourceEvent
+WHERE CollectionPeriod = @collectionPeriod AND AcademicYear = @academicYear
 UNION
-SELECT DISTINCT JobId FROM Payments2.DataLockEvent 
-Where CollectionPeriod = @collectionPeriod AND AcademicYear = @academicYear
+SELECT DISTINCT JobId FROM Payments2.DataLockEvent
+WHERE CollectionPeriod = @collectionPeriod AND AcademicYear = @academicYear;
 
 --keep all sucessful jobs and all in progress jobs, 
-DELETE FROM #JobDataToBeDeleted WHERE JobId in (SELECT DcJobId FROM Payments2.LatestSuccessfulDataLockJobs)
+DELETE FROM #JobDataToBeDeleted WHERE JobId IN ( SELECT DcJobId FROM Payments2.LatestSuccessfulDataLockJobs);
 
-print 'select jobid finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'select jobid finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
-IF OBJECT_ID('tempdb..#EarningEventIdsToDelete') IS NOT NULL DROP TABLE #EarningEventIdsToDelete
+IF OBJECT_ID('tempdb..#EarningEventIdsToDelete') IS NOT NULL DROP TABLE #EarningEventIdsToDelete;
 
-SELECT EventId INTO #EarningEventIdsToDelete 
-FROM (
+SELECT EventId INTO #EarningEventIdsToDelete
+FROM
+(
     SELECT EventId FROM Payments2.EarningEvent EE
-    WHERE EE.JobId IN (SELECT JobId FROM #JobDataToBeDeleted)
-    AND CollectionPeriod = @collectionPeriod
-    AND AcademicYear = @academicYear
-) q
+    WHERE EE.JobId IN ( SELECT JobId FROM #JobDataToBeDeleted )
+    AND CollectionPeriod = @collectionPeriod AND AcademicYear = @academicYear
+) q;
 
-print 'select earningeventid finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'select earningeventid finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
 DELETE Payments2.EarningEventPeriod
-WHERE EarningEventId IN (
-    SELECT EventId FROM #EarningEventIdsToDelete
-)
+WHERE EarningEventId IN ( SELECT EventId FROM #EarningEventIdsToDelete );
 
-print 'delete EarningEventPeriod finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'delete EarningEventPeriod finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
 DELETE Payments2.EarningEventPriceEpisode
-WHERE EarningEventId IN (
-    SELECT EventId FROM #EarningEventIdsToDelete
-)
+WHERE EarningEventId IN ( SELECT EventId FROM #EarningEventIdsToDelete );
 
-print 'delete EarningEventPriceEpisode finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'delete EarningEventPriceEpisode finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
 DELETE Payments2.EarningEvent
-WHERE EventId IN (
-    SELECT EventId FROM #EarningEventIdsToDelete
-)
+WHERE EventId IN ( SELECT EventId FROM #EarningEventIdsToDelete );
 
-print 'delete EarningEvent finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'delete EarningEvent finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
-IF OBJECT_ID('tempdb..#RequiredPaymentsToDelete') IS NOT NULL DROP TABLE #RequiredPaymentsToDelete
+IF OBJECT_ID('tempdb..#RequiredPaymentsToDelete') IS NOT NULL DROP TABLE #RequiredPaymentsToDelete;
 
 SELECT EventId INTO #RequiredPaymentsToDelete
-FROM (
-	SELECT EventId 
-    FROM Payments2.RequiredPaymentEvent
-    WHERE JobId IN (
-	    SELECT JobId FROM #JobDataToBeDeleted
-    )
-    AND CollectionPeriod = @collectionPeriod
-    AND AcademicYear = @academicYear
-) q​
+FROM
+(
+    SELECT EventId FROM Payments2.RequiredPaymentEvent 
+	WHERE JobId IN ( SELECT JobId FROM #JobDataToBeDeleted )
+          AND CollectionPeriod = @collectionPeriod
+          AND AcademicYear = @academicYear
+) q;​
 ​
-print 'SELECT RequiredPaymentEventid finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'SELECT RequiredPaymentEventid finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
 DELETE Payments2.FundingSourceEvent
-WHERE RequiredPaymentEventId IN (
-    SELECT EventId FROM #RequiredPaymentsToDelete
-)
+WHERE RequiredPaymentEventId IN ( SELECT EventId FROM #RequiredPaymentsToDelete );
 ​
-print 'delete FundingSourceEvent finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'delete FundingSourceEvent finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
 DELETE Payments2.RequiredPaymentEvent
-WHERE EventId IN (
-	SELECT EventId FROM #RequiredPaymentsToDelete
-)
+WHERE EventId IN ( SELECT EventId FROM #RequiredPaymentsToDelete );
 ​
-print 'delete RequiredPaymentEvent finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'delete RequiredPaymentEvent finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
-IF OBJECT_ID('tempdb..#DatalocksToDelete') IS NOT NULL DROP TABLE #DatalocksToDelete
+IF OBJECT_ID('tempdb..#DatalocksToDelete') IS NOT NULL DROP TABLE #DatalocksToDelete;
 ​
 SELECT EventId INTO #DatalocksToDelete
-FROM (
+FROM
+(
 	SELECT EventId FROM Payments2.DataLockEvent DLE
-	WHERE DLE.JobId IN (SELECT JobId FROM #JobDataToBeDeleted)
-	AND CollectionPeriod = @collectionPeriod
-    AND AcademicYear = @academicYear
-) q
+	WHERE DLE.JobId IN(SELECT JobId FROM #JobDataToBeDeleted)
+		AND CollectionPeriod = @collectionPeriod
+		AND AcademicYear = @academicYear
+) q;
 
-print 'SELECT DataLockEventid finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'SELECT DataLockEventid finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
 DELETE Payments2.DataLockEventNonPayablePeriodFailures
-WHERE DataLockEventNonPayablePeriodId IN (
-	SELECT DataLockEventNonPayablePeriodId FROM Payments2.DataLockEventNonPayablePeriod
-	WHERE DataLockEventId IN (
-		SELECT EventId FROM #DatalocksToDelete
-	)
-)
+WHERE DataLockEventNonPayablePeriodId IN
+(
+    SELECT DataLockEventNonPayablePeriodId FROM Payments2.DataLockEventNonPayablePeriod
+    WHERE DataLockEventId IN ( SELECT EventId FROM #DatalocksToDelete )
+);
 
-print 'delete DataLockEventNonPayablePeriodFailures finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'delete DataLockEventNonPayablePeriodFailures finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
 DELETE Payments2.DataLockEventNonPayablePeriod
-WHERE DataLockEventId IN (
-	SELECT EventId FROM #DatalocksToDelete
-)
+WHERE DataLockEventId IN ( SELECT EventId FROM #DatalocksToDelete );
 
-print 'delete DataLockEventNonPayablePeriod finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'delete DataLockEventNonPayablePeriod finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
 DELETE Payments2.DataLockEventPayablePeriod
-WHERE DataLockEventId IN (
-	SELECT EventId FROM #DatalocksToDelete
-)
+WHERE DataLockEventId IN ( SELECT EventId FROM #DatalocksToDelete );
 
-print 'delete DataLockEventPayablePeriod finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'delete DataLockEventPayablePeriod finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
 DELETE Payments2.DataLockEventPriceEpisode
-WHERE DataLockEventId IN (
-	SELECT EventId FROM #DatalocksToDelete
-)
+WHERE DataLockEventId IN ( SELECT EventId FROM #DatalocksToDelete );
 
-print 'delete DataLockEventPriceEpisode finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'delete DataLockEventPriceEpisode finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
 DELETE Payments2.DataLockEvent
-WHERE EventId IN (
-	SELECT EventId FROM #DatalocksToDelete
-)
+WHERE EventId IN ( SELECT EventId FROM #DatalocksToDelete );
 
-print 'delete DataLockEvent finished ' + convert(varchar(150), SYSDATETIME())
+PRINT 'delete DataLockEvent finished ' + CONVERT(VARCHAR(150), SYSDATETIME());
 
-rollback tran
+ROLLBACK TRAN;
