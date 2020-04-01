67b6ef9b3192e7a2550ae4ef5a72f58d06fb4677
tom-gough <tom@tgsd.co.uk>
2020-04-01 07:56:44 +0100

    Entity configuration fixes

--- src/SFA.DAS.Payments.Application/Data/Configurations/ApprenticeshipModelConfiguration.cs
+++ src/SFA.DAS.Payments.Application/Data/Configurations/ApprenticeshipModelConfiguration.cs
@@ -10,15 +10,15 @@ namespace SFA.DAS.Payments.Application.Data.Configurations
         {
             builder.ToTable("Apprenticeship", "Payments2");
             builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id).HasColumnName(@"Id").IsRequired();
-            builder.Property(x => x.AccountId).HasColumnName(@"AccountId").IsRequired();
+            builder.Property(x => x.Id).HasColumnName(@"Id");
+            builder.Property(x => x.AccountId).HasColumnName(@"AccountId");
             builder.Property(x => x.AgreementId).HasColumnName(@"AgreementId");
-            builder.Property(x => x.AgreedOnDate).HasColumnName(@"AgreedOnDate").IsRequired();
-            builder.Property(x => x.Uln).HasColumnName(@"Uln").IsRequired();
-            builder.Property(x => x.Ukprn).HasColumnName(@"Ukprn").IsRequired();
-            builder.Property(x => x.EstimatedStartDate).HasColumnName(@"EstimatedStartDate").IsRequired();
-            builder.Property(x => x.EstimatedEndDate).HasColumnName(@"EstimatedEndDate").IsRequired();
-            builder.Property(x => x.Priority).HasColumnName(@"Priority").IsRequired();
+            builder.Property(x => x.AgreedOnDate).HasColumnName(@"AgreedOnDate");
+            builder.Property(x => x.Uln).HasColumnName(@"Uln");
+            builder.Property(x => x.Ukprn).HasColumnName(@"Ukprn");
+            builder.Property(x => x.EstimatedStartDate).HasColumnName(@"EstimatedStartDate");
+            builder.Property(x => x.EstimatedEndDate).HasColumnName(@"EstimatedEndDate");
+            builder.Property(x => x.Priority).HasColumnName(@"Priority");
             builder.Property(x => x.StandardCode).HasColumnName(@"StandardCode");
             builder.Property(x => x.ProgrammeType).HasColumnName(@"ProgrammeType");
             builder.Property(x => x.FrameworkCode).HasColumnName(@"FrameworkCode");
@@ -26,9 +26,9 @@ namespace SFA.DAS.Payments.Application.Data.Configurations
             builder.Property(x => x.LegalEntityName).HasColumnName(@"LegalEntityName");
             builder.Property(x => x.TransferSendingEmployerAccountId).HasColumnName(@"TransferSendingEmployerAccountId");
             builder.Property(x => x.StopDate).HasColumnName(@"StopDate");
-            builder.Property(x => x.Status).HasColumnName(@"Status").IsRequired();
-            builder.Property(x => x.IsLevyPayer).HasColumnName(@"IsLevyPayer").IsRequired();
-            builder.Property(x => x.ApprenticeshipEmployerType).HasColumnName(@"ApprenticeshipEmployerType").IsRequired();
+            builder.Property(x => x.Status).HasColumnName(@"Status");
+            builder.Property(x => x.IsLevyPayer).HasColumnName(@"IsLevyPayer");
+            builder.Property(x => x.ApprenticeshipEmployerType).HasColumnName(@"ApprenticeshipEmployerType");
 
             builder.HasMany<ApprenticeshipPriceEpisodeModel>(x => x.ApprenticeshipPriceEpisodes).WithOne().HasForeignKey(p => p.ApprenticeshipId);
         }
--- src/SFA.DAS.Payments.Application/Data/Configurations/PaymentModelConfiguration.cs
+++ src/SFA.DAS.Payments.Application/Data/Configurations/PaymentModelConfiguration.cs
@@ -11,37 +11,37 @@ namespace SFA.DAS.Payments.Application.Data.Configurations
             builder.ToTable("Payment", "Payments2");
             builder.HasKey(x => x.Id);
 
-            builder.Property(x => x.Id).HasColumnName(@"Id").IsRequired();
-            builder.Property(x => x.EventId).HasColumnName(@"EventId").IsRequired();
-            builder.Property(x => x.EarningEventId).HasColumnName(@"EarningEventId").IsRequired();
-            builder.Property(x => x.FundingSourceEventId).HasColumnName(@"FundingSourceEventId").IsRequired();
-            builder.Property(x => x.EventTime).HasColumnName(@"EventTime").IsRequired();
-            builder.Property(x => x.PriceEpisodeIdentifier).HasColumnName(@"PriceEpisodeIdentifier").IsRequired();
-            builder.Property(x => x.Amount).HasColumnName(@"Amount").IsRequired();
+            builder.Property(x => x.Id).HasColumnName(@"Id");
+            builder.Property(x => x.EventId).HasColumnName(@"EventId");
+            builder.Property(x => x.EarningEventId).HasColumnName(@"EarningEventId");
+            builder.Property(x => x.FundingSourceEventId).HasColumnName(@"FundingSourceEventId");
+            builder.Property(x => x.EventTime).HasColumnName(@"EventTime");
+            builder.Property(x => x.PriceEpisodeIdentifier).HasColumnName(@"PriceEpisodeIdentifier");
+            builder.Property(x => x.Amount).HasColumnName(@"Amount");
             builder.OwnsOne(p => p.CollectionPeriod, cp =>
             {
-                cp.Property(x => x.Period).HasColumnName(@"CollectionPeriod").IsRequired();
-                cp.Property(x => x.AcademicYear).HasColumnName(@"AcademicYear").IsRequired();
+                cp.Property(x => x.Period).HasColumnName(@"CollectionPeriod");
+                cp.Property(x => x.AcademicYear).HasColumnName(@"AcademicYear");
             });
-            builder.Property(x => x.DeliveryPeriod).HasColumnName("DeliveryPeriod").IsRequired();
-            builder.Property(x => x.Ukprn).HasColumnName(@"Ukprn").IsRequired();
-            builder.Property(x => x.LearnerReferenceNumber).HasColumnName(@"LearnerReferenceNumber").IsRequired();
-            builder.Property(x => x.LearnerUln).HasColumnName(@"LearnerUln").IsRequired();
-            builder.Property(x => x.LearningAimReference).HasColumnName(@"LearningAimReference").IsRequired();
-            builder.Property(x => x.LearningAimProgrammeType).HasColumnName(@"LearningAimProgrammeType").IsRequired();
-            builder.Property(x => x.LearningAimStandardCode).HasColumnName(@"LearningAimStandardCode").IsRequired();
-            builder.Property(x => x.LearningAimFrameworkCode).HasColumnName(@"LearningAimFrameworkCode").IsRequired();
-            builder.Property(x => x.LearningAimPathwayCode).HasColumnName(@"LearningAimPathwayCode").IsRequired();
-            builder.Property(x => x.LearningAimFundingLineType).HasColumnName(@"LearningAimFundingLineType").IsRequired();
-            builder.Property(x => x.ContractType).HasColumnName(@"ContractType").IsRequired();
-            builder.Property(x => x.TransactionType).HasColumnName(@"TransactionType").IsRequired();
-            builder.Property(x => x.FundingSource).HasColumnName(@"FundingSource").IsRequired();
+            builder.Property(x => x.DeliveryPeriod).HasColumnName("DeliveryPeriod");
+            builder.Property(x => x.Ukprn).HasColumnName(@"Ukprn");
+            builder.Property(x => x.LearnerReferenceNumber).HasColumnName(@"LearnerReferenceNumber");
+            builder.Property(x => x.LearnerUln).HasColumnName(@"LearnerUln");
+            builder.Property(x => x.LearningAimReference).HasColumnName(@"LearningAimReference");
+            builder.Property(x => x.LearningAimProgrammeType).HasColumnName(@"LearningAimProgrammeType");
+            builder.Property(x => x.LearningAimStandardCode).HasColumnName(@"LearningAimStandardCode");
+            builder.Property(x => x.LearningAimFrameworkCode).HasColumnName(@"LearningAimFrameworkCode");
+            builder.Property(x => x.LearningAimPathwayCode).HasColumnName(@"LearningAimPathwayCode");
+            builder.Property(x => x.LearningAimFundingLineType).HasColumnName(@"LearningAimFundingLineType");
+            builder.Property(x => x.ContractType).HasColumnName(@"ContractType");
+            builder.Property(x => x.TransactionType).HasColumnName(@"TransactionType");
+            builder.Property(x => x.FundingSource).HasColumnName(@"FundingSource");
             builder.Property(x => x.AccountId).HasColumnName(@"AccountId");
             builder.Property(x => x.TransferSenderAccountId).HasColumnName(@"TransferSenderAccountId");
-            builder.Property(x => x.IlrSubmissionDateTime).HasColumnName(@"IlrSubmissionDateTime").IsRequired();
-            builder.Property(x => x.SfaContributionPercentage).HasColumnName(@"SfaContributionPercentage").IsRequired();
-            builder.Property(x => x.JobId).HasColumnName(@"JobId").IsRequired();
-            builder.Property(x => x.StartDate).HasColumnName(@"EarningsStartDate").IsRequired();
+            builder.Property(x => x.IlrSubmissionDateTime).HasColumnName(@"IlrSubmissionDateTime");
+            builder.Property(x => x.SfaContributionPercentage).HasColumnName(@"SfaContributionPercentage");
+            builder.Property(x => x.JobId).HasColumnName(@"JobId");
+            builder.Property(x => x.StartDate).HasColumnName(@"EarningsStartDate");
             builder.Property(x => x.PlannedEndDate).HasColumnName(@"EarningsPlannedEndDate");
             builder.Property(x => x.ActualEndDate).HasColumnName(@"EarningsActualEndDate");
             builder.Property(x => x.CompletionStatus).HasColumnName(@"EarningsCompletionStatus");
@@ -52,8 +52,8 @@ namespace SFA.DAS.Payments.Application.Data.Configurations
             builder.Property(x => x.LearningStartDate).HasColumnName(@"LearningStartDate");
             builder.Property(x => x.ApprenticeshipId).HasColumnName(@"ApprenticeshipId");
             builder.Property(x => x.ApprenticeshipPriceEpisodeId).HasColumnName(@"ApprenticeshipPriceEpisodeId");
-            builder.Property(x => x.ApprenticeshipEmployerType).HasColumnName(@"ApprenticeshipEmployerType").IsRequired();
-            builder.Property(x => x.ReportingAimFundingLineType).HasColumnName(@"ReportingAimFundingLineType").IsRequired();
+            builder.Property(x => x.ApprenticeshipEmployerType).HasColumnName(@"ApprenticeshipEmployerType");
+            builder.Property(x => x.ReportingAimFundingLineType).HasColumnName(@"ReportingAimFundingLineType");
         }
     }
 }
