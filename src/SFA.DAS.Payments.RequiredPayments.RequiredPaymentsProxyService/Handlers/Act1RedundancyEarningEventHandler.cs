baaac1b1a5654ccc45ff8847bbe429f1bc9ebb84
Mark Keene <Mark@keenesolutions.co.uk>
2020-04-03 20:16:03 +0100

    Creation of ACT 1 redundancy earning handler

--- src/SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService/Handlers/Act1RedundancyEarningEventHandler.cs
+++ src/SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService/Handlers/Act1RedundancyEarningEventHandler.cs
@@ -1,7 +1,15 @@
-﻿namespace SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService.Handlers
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.Payments.EarningEvents.Messages.Events;
+
+namespace SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService.Handlers
 {
-    public class Act1RedundancyEarningEventHandler
+    public class Act1RedundancyEarningEventHandler : IHandleMessages<ApprenticeshipContractType1EarningEvent>
     {
-        
+        public Task Handle(ApprenticeshipContractType1EarningEvent message, IMessageHandlerContext context)
+        {
+            throw new System.NotImplementedException();
+            //mimic Datalocks here and produce payable earning events then send local
+        }
     }
 }
