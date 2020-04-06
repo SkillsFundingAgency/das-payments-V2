f71fe51e4569d0c099c94f7141bdccd3a7cbcb0e
Tony <abroodie@gmail.com>
2020-04-06 16:54:18 +0100

    use nsb to create subscriptions for audit earning events

--- src/SFA.DAS.Payments.Audit.EarningEventsService/EarningEventsService.cs
+++ src/SFA.DAS.Payments.Audit.EarningEventsService/EarningEventsService.cs
@@ -6,6 +6,10 @@ using SFA.DAS.Payments.ServiceFabric.Core;
 using System;
 using System.Collections.Generic;
 using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.Payments.Application.Messaging;
 
 namespace SFA.DAS.Payments.Audit.EarningEventsService
 {
@@ -32,6 +36,19 @@ namespace SFA.DAS.Payments.Audit.EarningEventsService
             };
             logger.LogInfo("Created Service Instance Listener For Audit.EarningEventsService");
             return listeners;
+        }
+
+        protected override Task RunAsync(CancellationToken cancellationToken)
+        {
+            return Task.WhenAll(RunSendOnlyEndpoint());
+        }
+
+        private async Task RunSendOnlyEndpoint()
+        {
+            var endpoint = lifetimeScope.Resolve<EndpointConfiguration>();
+            endpoint.SendOnly();
+            var factory = lifetimeScope.Resolve<IEndpointInstanceFactory>();
+            await factory.GetEndpointInstance();
         }
     }
 }
