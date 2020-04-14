20a6259e69415388a238aaceae5bebe89124e40f
tom-gough <tom@tgsd.co.uk>
2020-04-14 09:08:34 +0100

    WIP events helpers

--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/EarningEventsHelper.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/EarningEventsHelper.cs
@@ -4,6 +4,7 @@ using SFA.DAS.Payments.AcceptanceTests.Core.Automation;
 using SFA.DAS.Payments.AcceptanceTests.EndToEnd.Handlers;
 using SFA.DAS.Payments.DataLocks.Messages.Events;
 using SFA.DAS.Payments.Model.Core;
+using SFA.DAS.Payments.ProviderPayments.Messages;
 
 namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
 {
@@ -34,12 +35,17 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
         }
     }
 
-    //public static class PaymentEventsHelper
-    //{
-    //    public static IEnumerable<PayableEarningEvent> PayableEarningsReceivedForLearner(TestSession session)
-    //    {
-    //        return ProviderPaymentEventHandler.ReceivedEvents.Where(event =>
-    //            )
-    //    }
-    //}
+    public static class PaymentEventsHelper
+    {
+        public static IEnumerable<ProviderPaymentEvent> ProviderPaymentsReceivedForLearner(string priceEpisodeIdentifier, short academicYear, TestSession session)
+        {
+            return ProviderPaymentEventHandler.ReceivedEvents.Where(ppEvent =>
+                ppEvent.Ukprn == session.Provider.Ukprn
+                && ppEvent.Learner.Uln == session.Learner.Uln
+                && ppEvent.Learner.ReferenceNumber == session.Learner.LearnRefNumber
+                && ppEvent.CollectionPeriod.AcademicYear == academicYear
+                && ppEvent.CollectionPeriod.Period == session.CollectionPeriod.Period
+                && ppEvent.PriceEpisodeIdentifier == priceEpisodeIdentifier);
+        }
+    }
 }
