765eb2b1ca6fd6b2c07392ca5df490607c0ffb3a
Hardik Desai <hardik.desai@digital.education.gov.uk>
2020-04-17 14:33:00 +0100

    use custom Diagnostic writer

--- src/SFA.DAS.Payments.ScheduledJobs/Infrastructure/IoC/Modules/MessagingModule.cs
+++ src/SFA.DAS.Payments.ScheduledJobs/Infrastructure/IoC/Modules/MessagingModule.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using Autofac;
 using NServiceBus;
 using NServiceBus.Features;
@@ -15,42 +16,42 @@ namespace SFA.DAS.Payments.ScheduledJobs.Infrastructure.IoC.Modules
         {
             builder.RegisterType<MessagingLoggerFactory>();
             builder.RegisterType<MessagingLogger>();
-            
+
             builder.Register((c, p) =>
-                {
-                    var config = c.Resolve<IScheduledJobsConfiguration>();
-                    var endpointConfiguration = new EndpointConfiguration(config.EndpointName);
+                             {
+                                 var config = c.Resolve<IScheduledJobsConfiguration>();
+                                 var endpointConfiguration = new EndpointConfiguration(config.EndpointName);
 
-                    var conventions = endpointConfiguration.Conventions();
-                    conventions.DefiningCommandsAs(type => (type.Namespace?.StartsWith("SFA.DAS.Payments") ?? false) && (bool) type.Namespace?.Contains(".Messages.Commands"));
+                                 endpointConfiguration.CustomDiagnosticsWriter(diagnostics =>
+                                                                               {
+                                                                                   var logger = c.Resolve<MessagingLogger>();
+                                                                                   logger.Info(diagnostics);
+                                                                                   return Task.CompletedTask;
+                                                                               });
 
-                    endpointConfiguration.DisableFeature<TimeoutManager>();
-                    if (!string.IsNullOrEmpty(config.NServiceBusLicense))
-                    {
-                        var license = WebUtility.HtmlDecode(config.NServiceBusLicense);
-                        endpointConfiguration.License(license);
-                    }
+                                 var conventions = endpointConfiguration.Conventions();
+                                 conventions.DefiningCommandsAs(type => ( type.Namespace?.StartsWith("SFA.DAS.Payments") ?? false ) && (bool) type.Namespace?.Contains(".Messages.Commands"));
 
-                    var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
-                    transport
-                        .ConnectionString(config.ServiceBusConnectionString)
-                        .Transactions(TransportTransactionMode.ReceiveOnly)
-                        .RuleNameShortener(ruleName => ruleName.Split('.').LastOrDefault() ?? ruleName);
-                    transport.PrefetchCount(20);
-                    builder.RegisterInstance(transport)
-                        .As<TransportExtensions<AzureServiceBusTransport>>()
-                        .SingleInstance();
-                    endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
-                    endpointConfiguration.EnableInstallers();
+                                 endpointConfiguration.DisableFeature<TimeoutManager>();
+                                 if (!string.IsNullOrEmpty(config.NServiceBusLicense))
+                                 {
+                                     var license = WebUtility.HtmlDecode(config.NServiceBusLicense);
+                                     endpointConfiguration.License(license);
+                                 }
 
-                    return endpointConfiguration;
-                })
-            .As<EndpointConfiguration>()
-            .InstancePerLifetimeScope();
+                                 var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+                                 transport.ConnectionString(config.ServiceBusConnectionString).Transactions(TransportTransactionMode.ReceiveOnly).RuleNameShortener(ruleName => ruleName.Split('.').LastOrDefault() ?? ruleName);
+                                 transport.PrefetchCount(20);
+                                 builder.RegisterInstance(transport).As<TransportExtensions<AzureServiceBusTransport>>().SingleInstance();
+                                 endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+                                 endpointConfiguration.EnableInstallers();
 
-            builder.RegisterType<EndpointInstanceFactory>()
-                .As<IEndpointInstanceFactory>()
-                .InstancePerLifetimeScope();
+                                 return endpointConfiguration;
+                             })
+                   .As<EndpointConfiguration>()
+                   .InstancePerLifetimeScope();
+
+            builder.RegisterType<EndpointInstanceFactory>().As<IEndpointInstanceFactory>().InstancePerLifetimeScope();
         }
     }
 }
