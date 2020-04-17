3876c51802312017415247303c42464a77433c61
Hardik Desai <hardik.desai@digital.education.gov.uk>
2020-04-17 13:45:35 +0100

    Added Http Function to test NSB

--- src/SFA.DAS.Payments.ScheduledJobs/LevyAccountImport.cs
+++ src/SFA.DAS.Payments.ScheduledJobs/LevyAccountImport.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Threading.Tasks;
 using AzureFunctions.Autofac;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using NServiceBus;
 using SFA.DAS.Payments.Application.Messaging;
@@ -17,6 +19,22 @@ namespace SFA.DAS.Payments.ScheduledJobs
     {
         [FunctionName("LevyAccountImport")]
         public static async Task Run([TimerTrigger("%LevyAccountSchedule%", RunOnStartup=true)]TimerInfo myTimer, [Inject]IEndpointInstanceFactory endpointInstanceFactory, [Inject]IScheduledJobsConfiguration config, ILogger log)
+        {
+            try
+            {
+                var command = new ImportEmployerAccounts();
+                var endpointInstance = await endpointInstanceFactory.GetEndpointInstance().ConfigureAwait(false);
+                await endpointInstance.Send(config.LevyAccountBalanceEndpoint, command).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                log.Log(LogLevel.Error, e, "Error in LevyAccountImport");
+                throw;
+            }
+        }
+        
+        [FunctionName("HttpLevyAccountImport")]
+        public static async Task HttpLevyAccountImport([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, [Inject]IEndpointInstanceFactory endpointInstanceFactory, [Inject]IScheduledJobsConfiguration config, ILogger log)
         {
             try
             {
