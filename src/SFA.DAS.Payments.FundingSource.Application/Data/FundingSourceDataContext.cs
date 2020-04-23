5972f13ab4a0b54dc437a370856ec589b7d35418
Mark Keene <Mark@keenesolutions.co.uk>
2020-04-23 15:49:21 +0100

    Add of new FundingSourceDataContext

---
+++ src/SFA.DAS.Payments.FundingSource.Application/Data/FundingSourceDataContext.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Payments.Application.Data.Configurations;
+using SFA.DAS.Payments.Model.Core.Entities;
+
+namespace SFA.DAS.Payments.FundingSource.Application.Data
+{
+    public interface IFundingSourceDataContext
+    {
+        DbSet<LevyAccountModel> LevyAccounts { get; }
+
+        DbSet<EmployerProviderPriorityModel>
+            EmployerProviderPriorities { get; }
+
+        // DbSet<LevyTransactionModel> LevyTransactions { get; }
+        // Task<int> SaveChanges(CancellationToken cancellationToken);
+        // Task<List<LevyTransactionModel>> GetEmployerLevyTransactions(long employerAccountId);
+        // Task SaveBatch(IList<LevyTransactionModel> batch, CancellationToken cancellationToken);
+    }
+
+
+    public class FundingSourceDataContext : DbContext, IFundingSourceDataContext
+    {
+        protected readonly string connectionString;
+        public virtual DbSet<LevyAccountModel> LevyAccounts { get; protected set; }
+        public virtual DbSet<EmployerProviderPriorityModel> EmployerProviderPriorities { get; protected set; }
+        public virtual DbSet<LevyTransactionModel> LevyTransactions { get; protected set; }
+
+        public async Task<int> SaveChanges(CancellationToken cancellationToken)
+        {
+            return await SaveChangesAsync(cancellationToken);
+        }
+
+        public FundingSourceDataContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("Payments2");
+            modelBuilder.ApplyConfiguration(new LevyAccountModelConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployerProviderPriorityModelConfiguration());
+            modelBuilder.ApplyConfiguration(new LevyTransactionModelConfiguration());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (connectionString != null) optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public async Task<List<LevyTransactionModel>> GetEmployerLevyTransactions(long employerAccountId)
+        {
+            return await LevyTransactions.Where(transaction => transaction.AccountId == employerAccountId)
+                .ToListAsync();
+        }
+
+        public async Task SaveBatch(IList<LevyTransactionModel> batch, CancellationToken cancellationToken)
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+            await LevyTransactions.AddRangeAsync(batch, cancellationToken).ConfigureAwait(false);
+            await SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/SFA.DAS.Payments.Model.Core/Entities/LevyTransactionModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SFA.DAS.Payments.Model.Core.Entities
+{
+    public class LevyTransactionModel
+    {
+        public long Id { get; set; }
+        public long Ukprn { get; set; }
+        public byte CollectionPeriod { get; set; }
+        public short AcademicYear { get; set; }
+        public byte DeliveryPeriod { get; set; }
+        public long JobId { get; set; }
+        public long AccountId { get; set; }
+        public long? TransferSenderAccountId { get; set; }
+        public Guid RequiredPaymentEventId { get; set; }
+        public Guid EarningEventId { get; set; }
+        public decimal Amount { get; set; }
+        public string MessagePayload { get; set; }
+        public string MessageType { get; set; }
+        
+    }
+}
