6afe34f0236bf8c8c981859ffa0abad22b31234e
Carl Berisford-Murray <carl@cabm.co.uk>
2020-05-11 16:07:39 +0100

    Tests show failing functionality

---
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/PV2_1953_Steps.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using SFA.DAS.Payments.AcceptanceTests.Core.Automation;
+using SFA.DAS.Payments.Model.Core.Entities;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
+{
+    [Binding]
+    [Scope(Feature = "PV2-1953-small-employer-retrospective")]
+    // ReSharper disable once InconsistentNaming
+    public class PV2_1953_Steps : FM36_ILR_Base_Steps
+    {
+        public PV2_1953_Steps(FeatureContext context) : base(context)
+        {
+        }
+
+        private const string PriceEpisodeIdentifier = "20-593-1-06/08/2019";
+        private const string CommitmentIdentifier = "A-1953";
+
+        [Given("a learner submits an ILR when not employed by a small employer in R03")]
+        public async Task R03NotSmallEmployer()
+        {
+            GetFm36LearnerForCollectionPeriod("R03/current academic year");
+            await SetupTestData(PriceEpisodeIdentifier, null, CommitmentIdentifier, null);
+            var dcHelper = Scope.Resolve<IDcHelper>();
+            await dcHelper.SendIlrSubmission(TestSession.FM36Global.Learners,
+                TestSession.Provider.Ukprn,
+                TestSession.CollectionPeriod.AcademicYear,
+                TestSession.CollectionPeriod.Period,
+                TestSession.Provider.JobId);
+
+            await WaitForPayments(12);
+        }
+        [When("a learner submits an ILR when employed by a small employer in R04")]
+        public async Task R04SmallEmployerFromBeginning()
+        {
+            GetFm36LearnerForCollectionPeriod("R04/current academic year");
+            await SetupTestData(PriceEpisodeIdentifier, null, CommitmentIdentifier, null);
+            var dcHelper = Scope.Resolve<IDcHelper>();
+            await dcHelper.SendIlrSubmission(TestSession.FM36Global.Learners,
+                TestSession.Provider.Ukprn,
+                TestSession.CollectionPeriod.AcademicYear,
+                TestSession.CollectionPeriod.Period,
+                TestSession.Provider.JobId);
+
+            await WaitForPayments(20);
+        }
+
+        private bool HasPaymentsTillR03(short academicYear)
+        {
+            return FundingSourcePaymentEventsHelper
+                .FundingSourcePaymentsReceivedForLearner(PriceEpisodeIdentifier, academicYear, TestSession)
+                .Count(x =>
+                    x.ContractType == ContractType.Act2 &&
+                    x.TransactionType == TransactionType.Learning) == 3;
+        }
+
+        [Then(@"there should be payments refunding and repaying periods 1-3")]
+        public async Task ThenFundTheRemainingInstallmentsCorrectly(int fundingSource, int sfaPercentage)
+        {
+            await WaitForIt(() => HasPaymentsTillR03(short.Parse(TestSession.FM36Global.Year)),
+                "Failed to find payments till R03");
+        }
+    }
+}
