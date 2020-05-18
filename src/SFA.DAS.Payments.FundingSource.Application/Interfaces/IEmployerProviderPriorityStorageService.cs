62ade5588ba192df4cf3e14d1a4f9f43ef32bcba
Mark Keene <Mark@keenesolutions.co.uk>
2020-05-18 13:54:33 +0100

    Removed usings

--- src/SFA.DAS.Payments.FundingSource.Application/Interfaces/IEmployerProviderPriorityStorageService.cs
+++ src/SFA.DAS.Payments.FundingSource.Application/Interfaces/IEmployerProviderPriorityStorageService.cs
@@ -1,7 +1,5 @@
 ﻿using System.Threading.Tasks;
 using SFA.DAS.Payments.DataLocks.Messages.Events;
-using SFA.DAS.Payments.Model.Core;
-using SFA.DAS.Payments.Model.Core.Entities;
 
 namespace SFA.DAS.Payments.FundingSource.Application.Interfaces
 {
