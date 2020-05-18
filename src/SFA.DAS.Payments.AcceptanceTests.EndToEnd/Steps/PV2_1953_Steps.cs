3867009f3ae29782ae3be3857a45461f5d2def93
Carl Berisford-Murray <carl@cabm.co.uk>
2020-05-18 12:18:38 +0100

    Removed erroneous file addition Now checking for refunds Fixed merge issue

--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/PV2_1953_Steps.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/PV2_1953_Steps.cs
@@ -23,7 +23,7 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
         public async Task R03NotSmallEmployer()
         {
             GetFm36LearnerForCollectionPeriod("R03/current academic year");
-            await SetupTestData(PriceEpisodeIdentifier, null, CommitmentIdentifier, null);
+            await SetupTestCommitmentData(CommitmentIdentifier, PriceEpisodeIdentifier);
             var dcHelper = Scope.Resolve<IDcHelper>();
             await dcHelper.SendIlrSubmission(TestSession.FM36Global.Learners,
                 TestSession.Provider.Ukprn,
@@ -37,7 +37,7 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
         public async Task R04SmallEmployerFromBeginning()
         {
             GetFm36LearnerForCollectionPeriod("R04/current academic year");
-            await SetupTestData(PriceEpisodeIdentifier, null, CommitmentIdentifier, null);
+            await SetupTestCommitmentData(CommitmentIdentifier, PriceEpisodeIdentifier);
             var dcHelper = Scope.Resolve<IDcHelper>();
             await dcHelper.SendIlrSubmission(TestSession.FM36Global.Learners,
                 TestSession.Provider.Ukprn,
@@ -49,9 +49,20 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
         }
 
         [Then(@"there should be payments refunding and repaying periods 1-3")]
-        public async Task ThenFundTheRemainingInstallmentsCorrectly()
+        public async Task ThenThereShouldBeRefundPaymentsForPeriodsOneToThree()
         {
-            
+            await WaitForRefunds(6);
+        }
+
+        protected async Task WaitForRefunds(int count)
+        {
+            await WaitForIt(() =>
+                {
+                    var payments = Scope.Resolve<IPaymentsHelper>()
+                               .GetPayments(TestSession.Provider.Ukprn, TestSession.CollectionPeriod);
+                    return payments.Count(x => x.Amount < 0) == count;
+                },
+                "Failed to wait for expected number of payments");
         }
     }
 }
