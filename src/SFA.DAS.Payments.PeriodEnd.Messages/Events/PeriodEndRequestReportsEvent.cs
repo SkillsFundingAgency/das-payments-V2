5e05129ebbed7b0a9586b406aa2fd0801128d4e2
Mark Keene <Mark@keenesolutions.co.uk>
2020-07-08 16:32:09 +0100

    Fix of build error

--- src/SFA.DAS.Payments.PeriodEnd.Messages/Events/PeriodEndRequestReportsEvent.cs
+++ src/SFA.DAS.Payments.PeriodEnd.Messages/Events/PeriodEndRequestReportsEvent.cs
@@ -1,13 +1,7 @@
-﻿using SFA.DAS.Payments.Application.Infrastructure.Logging;
-
-namespace SFA.DAS.Payments.PeriodEnd.Messages.Events
+﻿namespace SFA.DAS.Payments.PeriodEnd.Messages.Events
 {
     public class PeriodEndRequestReportsEvent : PeriodEndEvent
-    {  
-        private readonly IPaymentLogger logger;
-        public PeriodEndRequestReportsEvent( IPaymentLogger logger;
-        {
-            
-        }
+    {
+        
     }
 }
