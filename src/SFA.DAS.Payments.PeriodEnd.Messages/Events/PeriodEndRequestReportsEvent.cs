738d380ccde877fc67f331c6b397df0504b11133
Mark Keene <Mark@keenesolutions.co.uk>
2020-07-08 16:27:13 +0100

    Updates to references and conversion to .NET core 2.1 from 3.1

--- src/SFA.DAS.Payments.PeriodEnd.Messages/Events/PeriodEndRequestReportsEvent.cs
+++ src/SFA.DAS.Payments.PeriodEnd.Messages/Events/PeriodEndRequestReportsEvent.cs
@@ -1,7 +1,13 @@
-﻿namespace SFA.DAS.Payments.PeriodEnd.Messages.Events
+﻿using SFA.DAS.Payments.Application.Infrastructure.Logging;
+
+namespace SFA.DAS.Payments.PeriodEnd.Messages.Events
 {
     public class PeriodEndRequestReportsEvent : PeriodEndEvent
-    {
-        
+    {  
+        private readonly IPaymentLogger logger;
+        public PeriodEndRequestReportsEvent( IPaymentLogger logger;
+        {
+            
+        }
     }
 }
