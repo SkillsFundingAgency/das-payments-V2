181fab7a22fdc47fb9ff3871c76e3980d0da4e2e
Mark Keene <Mark@keenesolutions.co.uk>
2020-07-13 11:28:45 +0100

    Creation of 4 new tables in DB schema

---
+++ src/SFA.DAS.Payments.Database/Metrics/Tables/Metrics.ProviderPaymentFundingSource.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [Metrics].[ProviderPaymentFundingSource] (
+    [Id]                         BIGINT             IDENTITY (1, 1) NOT NULL,
+    [ProviderPeriodEndSummaryId] BIGINT             NOT NULL,
+    [ContractType]               TINYINT            NOT NULL,
+    [FundingSource1]             DECIMAL (15, 5)    NOT NULL,
+    [FundingSource2]             DECIMAL (15, 5)    NOT NULL,
+    [FundingSource3]             DECIMAL (15, 5)    NOT NULL,
+    [FundingSource4]             DECIMAL (15, 5)    NOT NULL,
+    [FundingSource5]             DECIMAL (15, 5)    NOT NULL,
+    [CreationDate]               DATETIMEOFFSET (7) CONSTRAINT [DF_ProviderPaymentFundingSource__CreationDate] DEFAULT (sysdatetimeoffset()) NOT NULL,
+    CONSTRAINT [PK_ProviderPaymentFundingSource] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_ProviderPaymentFundingSource__ProviderPeriodEndSummary_Id] FOREIGN KEY ([ProviderPeriodEndSummaryId]) REFERENCES [Metrics].[ProviderPeriodEndSummary] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ProviderPaymentFundingSource]
+    ON [Metrics].[ProviderPaymentFundingSource]([ProviderPeriodEndSummaryId] ASC, [ContractType] ASC, [CreationDate] ASC);
+
