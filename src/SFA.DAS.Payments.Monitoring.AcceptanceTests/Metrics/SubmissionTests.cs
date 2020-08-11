791bbd370ae599d94eb4210daee9e5d3aeb1f0a7
Tom <Tom>
2020-08-11 13:12:21 +0100

    Stripped unneeded tests out of submissions tests leaving just the test harness used

--- src/SFA.DAS.Payments.Monitoring.AcceptanceTests/Metrics/SubmissionTests.cs
+++ src/SFA.DAS.Payments.Monitoring.AcceptanceTests/Metrics/SubmissionTests.cs
@@ -7,15 +7,11 @@ using Microsoft.EntityFrameworkCore;
 using NServiceBus;
 using NServiceBus.Features;
 using NUnit.Framework;
-using SFA.DAS.Payments.AcceptanceTests.Core.Automation;
-using SFA.DAS.Payments.AcceptanceTests.Core.Data;
 using SFA.DAS.Payments.AcceptanceTests.Core.Infrastructure;
 using SFA.DAS.Payments.Messages.Core;
 using SFA.DAS.Payments.Messages.Core.Commands;
 using SFA.DAS.Payments.Messages.Core.Events;
-using SFA.DAS.Payments.Monitoring.Jobs.Client;
 using SFA.DAS.Payments.Monitoring.Jobs.Data;
-using SFA.DAS.Payments.Monitoring.Jobs.Messages.Commands;
 using SFA.DAS.Payments.Monitoring.Jobs.Model;
 using SFA.DAS.Payments.Monitoring.Metrics.Messages.Commands;
 
@@ -80,38 +76,15 @@ namespace SFA.DAS.Payments.Monitoring.AcceptanceTests.Metrics
             MessageSession = await Endpoint.Start(EndpointConfiguration);
         }
 
-        [TestCase(10003915, 358799, 12, 1920)]
-        [TestCase(10003915, 358799, 12, 1920)]
-        [TestCase(10033440, 358280, 12, 1920)]
-        [TestCase(10000446, 357281, 12, 1920)]
-        [TestCase(10003161, 357483, 12, 1920)]
-        [TestCase(10012467, 357979, 12, 1920)]
-        [TestCase(10003375, 357505, 12, 1920)]
-        [TestCase(10036952, 358348, 12, 1920)]
-        [TestCase(10029952, 358208, 12, 1920)]
-        [TestCase(10001149, 336758, 11, 1920)]
-        public async Task GenerateMetrics(long ukprn, long jobId, byte collectionPeriod, short academicYear)
-        {
-            var metricsCommand = new GenerateSubmissionSummary
-            {
-                CollectionPeriod = collectionPeriod,
-                JobId = jobId,
-                AcademicYear = academicYear,
-                Ukprn = ukprn
-            };
-            await MessageSession.Send(metricsCommand).ConfigureAwait(false);
-        }
-
 
         [TestCase(1)]
-        public async Task GenerateMetricsFromLatestSuccessfulJobs(int count)
+        public async Task GenerateMetricsFromLatestJob(int count)
         {
             var dataContext = Container.Resolve<JobsDataContext>();
             var jobs = await dataContext.Jobs
                 .Where(job =>
                     job.JobType == JobType.EarningsJob &&
-                    (job.Status == JobStatus.Completed || job.Status == JobStatus.CompletedWithErrors)// &&
-                    //job.DcJobSucceeded != null && job.DcJobSucceeded == true)
+                    (job.Status == JobStatus.Completed || job.Status == JobStatus.CompletedWithErrors)
                 ).OrderByDescending(job => job.LearnerCount)
                 .Take(count)
                 .ToListAsync();
