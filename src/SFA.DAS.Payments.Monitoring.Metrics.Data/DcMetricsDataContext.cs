0bee1a8f1249e8ab5517696fb350f00f9648c4ff
Tom <Tom>
2020-08-11 13:02:14 +0100

    Uses correct factory

--- src/SFA.DAS.Payments.Monitoring.Metrics.Data/DcMetricsDataContext.cs
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Data/DcMetricsDataContext.cs
@@ -8,23 +8,31 @@ using SFA.DAS.Payments.Monitoring.Metrics.Model;
 
 namespace SFA.DAS.Payments.Monitoring.Metrics.Data
 {
-    public interface IDcMetricsDataContextConnectionStringProvider
+    public interface IDcMetricsDataContextFactory
     {
-        string GetConnectionString();
+        IDcMetricsDataContext Get(short academicYear);
     }
 
-    public class DcMetricsDataContextConnectionStringProvider : IDcMetricsDataContextConnectionStringProvider
+    public class DcMetricsDataContextFactory : IDcMetricsDataContextFactory
     {
         private readonly string connectionString;
+        private readonly Dictionary<short, IDcMetricsDataContext> contexts;
 
-        public DcMetricsDataContextConnectionStringProvider(string connectionString)
+        public DcMetricsDataContextFactory(string connectionString)
         {
             this.connectionString = connectionString;
+            contexts = new Dictionary<short, IDcMetricsDataContext>();
         }
 
-        public string GetConnectionString()
+        public IDcMetricsDataContext Get(short academicYear)
         {
-            return connectionString;
+            if (contexts.ContainsKey(academicYear))
+                return contexts[academicYear];
+
+            var builder = new SqlConnectionStringBuilder(connectionString);
+            builder["Database"] = $"ILR{academicYear}DataStore";
+            contexts.Add(academicYear, new DcMetricsDataContext(builder.ConnectionString));
+            return contexts[academicYear];
         }
     }
 
@@ -35,8 +43,6 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Data
 
     public class DcMetricsDataContext : DbContext, IDcMetricsDataContext
     {
-        public delegate DcMetricsDataContext Factory(short academicYear);
-
         private static string DcEarningsQuery = @"
 ;WITH 
 RawEarnings AS (
@@ -194,12 +200,9 @@ order by UKPRN,ApprenticeshipContractType";
 
         public DbQuery<TransactionTypeAmounts> Earnings { get; set; }
 
-        public DcMetricsDataContext(short academicYear, IDcMetricsDataContextConnectionStringProvider provider)
+        public DcMetricsDataContext(string connectionString)
         {
-            if (provider == null) throw new ArgumentNullException(nameof(provider));
-            var builder = new SqlConnectionStringBuilder(provider.GetConnectionString());
-            builder["Database"] = $"ILR{academicYear}DataStore";
-            this.connectionString = builder.ConnectionString;
+            this.connectionString = connectionString;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
