4bf75fc9463ef73f422d13e10e48dcb9d53c0f43
Tom <Tom>
2020-08-11 13:10:21 +0100

    Factory now in it's on file

---
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Data/DcMetricsDataContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace SFA.DAS.Payments.Monitoring.Metrics.Data
+{
+    public interface IDcMetricsDataContextFactory
+    {
+        IDcMetricsDataContext Get(short academicYear);
+    }
+
+    public class DcMetricsDataContextFactory : IDcMetricsDataContextFactory
+    {
+        private readonly string connectionString;
+        private readonly Dictionary<short, IDcMetricsDataContext> contexts;
+
+        public DcMetricsDataContextFactory(string connectionString)
+        {
+            this.connectionString = connectionString;
+            contexts = new Dictionary<short, IDcMetricsDataContext>();
+        }
+
+        public IDcMetricsDataContext Get(short academicYear)
+        {
+            if (contexts.ContainsKey(academicYear))
+                return contexts[academicYear];
+
+            var builder = new SqlConnectionStringBuilder(connectionString);
+            builder["Database"] = $"ILR{academicYear}DataStore";
+            contexts.Add(academicYear, new DcMetricsDataContext(builder.ConnectionString));
+            return contexts[academicYear];
+        }
+    }
+}
