1de17485b7cd83d42f20f496d4a9e5c9e19d1220
Akshay Chugh <akshay.chugh30@outlook.com>
2020-08-26 11:34:07 +0100

    PV2-1845 fm36 file fix

--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/PV2_1845_Steps.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/PV2_1845_Steps.cs
@@ -33,7 +33,6 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
         public async Task LevyLearnerMadeRedundant()
         {
             ImportR07Fm36ForNonRedundantLevyLearner();
-            TestSession.CollectionPeriod.AcademicYear = 1920; //this should be handled by the base steps...
 
             await SetUpMatchingCommitment();
 
@@ -60,7 +59,6 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
         public async Task ThereAreLessThan6MonthsRemainingOfPlannedLearning()
         {
             ImportR12Fm36ToMakeLearnerRedundant();
-            TestSession.CollectionPeriod.AcademicYear = 1920; //this should be handled by the base steps...
 
             await SetUpMatchingCommitment();
 
@@ -75,9 +73,9 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
                 TestSession.Provider.JobId);
         }
 
-        private void ImportR07Fm36ForNonRedundantLevyLearner() { GetFm36LearnerForCollectionPeriod("R07/1920"); }
+        private void ImportR07Fm36ForNonRedundantLevyLearner() { GetFm36LearnerForCollectionPeriod("R07/current academic year"); }
 
-        private void ImportR12Fm36ToMakeLearnerRedundant() { GetFm36LearnerForCollectionPeriod("R12/1920"); }
+        private void ImportR12Fm36ToMakeLearnerRedundant() { GetFm36LearnerForCollectionPeriod("R12/current academic year"); }
 
         private async Task SetUpMatchingCommitment() { await SetupTestCommitmentData(CommitmentIdentifier, PriceEpisodeIdentifier); }
 
