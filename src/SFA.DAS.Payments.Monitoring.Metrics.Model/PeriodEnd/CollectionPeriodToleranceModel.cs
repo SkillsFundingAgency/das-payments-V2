45fd2cf434327bf80c1c635813bcff9d754808c5
Akshay Chugh <akshay.chugh30@outlook.com>
2020-10-21 17:06:23 +0100

    PV2-2161 CollectionPeriodTolerance entity model & configuration

---
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Model/PeriodEnd/CollectionPeriodToleranceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.Payments.Monitoring.Metrics.Model.PeriodEnd
+{
+    public class CollectionPeriodToleranceModel
+    {
+		public int Id { get; set; }
+		public byte CollectionPeriod { get; set; }
+		public decimal SubmissionToleranceLower { get; set; }
+		public decimal SubmissionToleranceUpper { get; set; }
+		public decimal PeriodEndToleranceLower { get; set; }
+		public decimal PeriodEndToleranceUpper { get; set; }
+	}
+}
