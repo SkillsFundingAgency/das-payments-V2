8ebf53d6e43c1cf82b8d39adc1f17a1db5d1caca
Akshay Chugh <akshay.chugh30@outlook.com>
2020-10-21 17:02:18 +0100

    PV2-2161 new CollectionPeriodTolerance table

---
+++ src/SFA.DAS.Payments.Database/Metrics/Tables/Metrics.CollectionPeriodTolerance.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Metrics].[CollectionPeriodTolerance]
+(
+	[Id]								INT				IDENTITY(1,1) NOT NULL		  CONSTRAINT PK_CollectionPeriodTolerance PRIMARY KEY CLUSTERED,
+	[AcademicYear]						SMALLINT		NOT NULL,
+	[CollectionPeriod]					TINYINT			NOT NULL,
+	[SubmissionToleranceLower]			DECIMAL(15,5)	NOT NULL,
+	[SubmissionToleranceUpper]			DECIMAL(15,5)	NOT NULL,
+	[PeriodEndToleranceLower]			DECIMAL(15,5)	NOT NULL,
+	[PeriodEndToleranceUpper]			DECIMAL(15,5)	NOT NULL,
+	
+	INDEX IX_CollectionPeriodTolerance__AcademicYear_CollectionPeriod (AcademicYear, CollectionPeriod),
+	CONSTRAINT UQ_CollectionPeriodTolerance UNIQUE (AcademicYear, CollectionPeriod)
+);
+GO
