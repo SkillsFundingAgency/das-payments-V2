ad298e487a647bb75285073d747a866743b9194e
Akshay Chugh <akshay.chugh30@outlook.com>
2020-10-22 11:39:19 +0100

    PV2-2061 Added AcademicYear to model

--- src/SFA.DAS.Payments.Monitoring.Metrics.Data/Configuration/CollectionPeriodToleranceModelConfiguration.cs
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Data/Configuration/CollectionPeriodToleranceModelConfiguration.cs
@@ -14,6 +14,7 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Data.Configuration
             builder.ToTable("CollectionPeriodTolerance", "Metrics");
             builder.HasKey(x => x.Id);
             builder.Property(x => x.CollectionPeriod).HasColumnName("CollectionPeriod");
+            builder.Property(x => x.AcademicYear).HasColumnName("AcademicYear");
             builder.Property(x => x.SubmissionToleranceLower).HasColumnName("SubmissionToleranceLower");
             builder.Property(x => x.SubmissionToleranceUpper).HasColumnName("SubmissionToleranceUpper");
             builder.Property(x => x.PeriodEndToleranceLower).HasColumnName("PeriodEndToleranceLower");
