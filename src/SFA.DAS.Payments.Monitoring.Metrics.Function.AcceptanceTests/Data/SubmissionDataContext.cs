6de400a3c6c5a99de65ef945fca57854264beaa2
Hardik Desai <hardik.desai@digital.education.gov.uk>
2020-10-22 11:05:56 +0100

    Monitoring.Metrics.Function.AcceptanceTests

---
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Function.AcceptanceTests/Data/SubmissionDataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Payments.Monitoring.Metrics.Data.Configuration;
+using SFA.DAS.Payments.Monitoring.Metrics.Model.Submission;
+
+namespace SFA.DAS.Payments.Monitoring.Metrics.Function.AcceptanceTests.Data
+{
+    public class SubmissionDataContext : DbContext
+    {
+        private readonly string connectionString;
+        public virtual DbSet<SubmissionSummaryModel> SubmissionSummaries { get; set; }
+
+        public SubmissionDataContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new SubmissionSummaryModelConfiguration());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
