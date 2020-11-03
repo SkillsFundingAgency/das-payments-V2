57b8f2b3d157d50121c004fd8a23667037e5648c
Tom <Tom>
2020-11-03 14:58:03 +0000

    Config changes - MetricsValidationService now pulls function url and api key from config correctly

--- src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/PeriodEnd/MetricsValidationService.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/PeriodEnd/MetricsValidationService.cs
@@ -7,13 +7,13 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.PeriodEnd
 {
     public class MetricsValidationService : IMetricsValidationService
     {
-        private readonly string _authCode;
-        private readonly string _functionAddress;
+        private readonly string authCode;
+        private readonly string functionAddress;
 
-        public MetricsValidationService()
+        public MetricsValidationService(string authCode, string functionAddress)
         {
-            _authCode = string.Empty; //todo set this from config and auto to string.empty if dev (can be in the config too so nothing clever here)
-            _functionAddress = "http://localhost:7071/api/ValidateSubmissionWindow"; //todo set this from config
+            this.authCode = authCode;
+            this.functionAddress = functionAddress;
         }
 
         public async Task<bool> Validate(long jobId, short academicYear, byte collectionPeriod)
@@ -24,36 +24,9 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.PeriodEnd
 
         private string BuildUriFromParameters(long jobId, short academicYear, byte collectionPeriod)
         {
-            return string.IsNullOrWhiteSpace(_authCode)
-                ? $"{_functionAddress}?jobId={jobId}&collectionPeriod={collectionPeriod}&AcademicYear={academicYear}"
-                : $"{_functionAddress}?code={_authCode}&jobId={jobId}&collectionPeriod={collectionPeriod}&AcademicYear={academicYear}";
-            //return string.IsNullOrWhiteSpace(_authCode)
-            //    ? $"{_functionAddress}?jobId={{{jobId}}}&collectionPeriod={{{collectionPeriod}}}&AcademicYear={{{academicYear}}}"
-            //    : $"{_functionAddress}?code={{{_authCode}}}&jobId={{{jobId}}}&collectionPeriod={{{collectionPeriod}}}&AcademicYear={{{academicYear}}}";
+            return string.IsNullOrWhiteSpace(authCode)
+                ? $"{functionAddress}?jobId={jobId}&collectionPeriod={collectionPeriod}&AcademicYear={academicYear}"
+                : $"{functionAddress}?code={authCode}&jobId={jobId}&collectionPeriod={collectionPeriod}&AcademicYear={academicYear}";
         }
     }
-
-    //public interface IMetricsValidationRequestBuilder
-    //{
-    //    string BuildFromParameters(long jobId, short academicYear, byte collectionPeriod);
-    //}
-
-    //public class MetricsValidationRequestBuilder : IMetricsValidationRequestBuilder
-    //{
-    //    private readonly string _authCode;
-    //    private readonly string _functionAddress;
-
-    //    public MetricsValidationRequestBuilder()
-    //    {
-    //        _authCode = string.Empty; //todo set this from config and auto to string.empty if dev
-    //        _functionAddress = "http://localhost:7071/api/ValidateSubmissionWindow";
-    //    }
-
-    //    public string BuildFromParameters(long jobId, short academicYear, byte collectionPeriod)
-    //    {
-    //        return string.IsNullOrWhiteSpace(_authCode)
-    //            ? $"{_functionAddress}?jobId={{{jobId}}}&collectionPeriod={{{collectionPeriod}}}&AcademicYear={{{academicYear}}}"
-    //            : $"{_functionAddress}?code={{{_authCode}}}&jobId={{{jobId}}}&collectionPeriod={{{collectionPeriod}}}&AcademicYear={{{academicYear}}}";
-    //    }
-    //}
 }
