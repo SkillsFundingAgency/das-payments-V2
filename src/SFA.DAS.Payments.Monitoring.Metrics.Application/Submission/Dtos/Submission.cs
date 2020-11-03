611e4cfb721a55b15bc30aca45684437ed980534
Carl <carl@techcellence.org.uk>
2020-11-03 07:57:51 +0000

    WIP - repo methods to get the successful jobs

---
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Application/Submission/Dtos/Submission.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Payments.Monitoring.Metrics.Application.Submission.Dtos
+{
+    public class Submission
+    {
+        public long Ukprn { get; set; }
+        public long DcJobId { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Application/Submission/Dtos/SuccessfulSubmissions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.Payments.Monitoring.Metrics.Application.Submission.Dtos
+{
+    public class SuccessfulSubmissions
+    {
+        public List<Submission> SuccessfulProviders { get; set; }
+    }
+}
