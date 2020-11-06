528b1bef359804608d80b6ac87c277e5344bf900
Hardik Desai <hardik.desai@digital.education.gov.uk>
2020-11-06 16:48:09 +0000

    override ManageStatus

--- src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/PeriodEnd/PeriodEndSubmissionWindowValidationJobStatusService.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/PeriodEnd/PeriodEndSubmissionWindowValidationJobStatusService.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
 using SFA.DAS.Payments.Monitoring.Jobs.Application.Infrastructure.Configuration;
-using SFA.DAS.Payments.Monitoring.Jobs.Data;
 using SFA.DAS.Payments.Monitoring.Jobs.Model;
 
 namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.PeriodEnd
@@ -15,21 +14,27 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.PeriodEnd
 
     public class PeriodEndSubmissionWindowValidationJobStatusService : PeriodEndJobStatusService, IPeriodEndSubmissionWindowValidationJobStatusService
     {
-        private readonly IPaymentLogger logger;
         private readonly IMetricsValidationService metricsValidationService;
 
-        public PeriodEndSubmissionWindowValidationJobStatusService(IJobStorageService jobStorageService, IPaymentLogger logger, ITelemetry telemetry, IJobStatusEventPublisher eventPublisher, IJobServiceConfiguration config, IJobsDataContext context, IMetricsValidationService metricsValidationService) : base(jobStorageService, logger, telemetry, eventPublisher, config)
+        public PeriodEndSubmissionWindowValidationJobStatusService(IJobStorageService jobStorageService, IPaymentLogger logger, ITelemetry telemetry, IJobStatusEventPublisher eventPublisher, IJobServiceConfiguration config, IMetricsValidationService metricsValidationService) : base(jobStorageService, logger, telemetry, eventPublisher, config)
         {
-            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this.metricsValidationService = metricsValidationService ?? throw new ArgumentNullException(nameof(metricsValidationService));
         }
 
-        protected override async Task<(bool IsComplete, JobStatus? OverriddenJobStatus, DateTimeOffset? completionTime)> PerformAdditionalJobChecks(JobModel job, CancellationToken cancellationToken)
+        public override async Task<bool> ManageStatus(long jobId, CancellationToken cancellationToken)
         {
-            logger.LogInfo($"Calling Metrics Validation Service for Job Id: {job.DcJobId}, Academic Year: {job.AcademicYear}, Collection Period: {job.CollectionPeriod}");
-            var validationResult = await metricsValidationService.Validate(job.DcJobId.Value, job.AcademicYear, job.CollectionPeriod);
-            logger.LogInfo($"Finished Calling Metrics Validation Service for Job Id: {job.DcJobId}, Academic Year: {job.AcademicYear}, Collection Period: {job.CollectionPeriod}. Result: {validationResult}");
-            return ( true, validationResult ? JobStatus.Completed : JobStatus.CompletedWithErrors, DateTimeOffset.Now );
+            Logger.LogVerbose($"Now determining if job {jobId} has finished.");
+            var job = await JobStorageService.GetJob(jobId, cancellationToken).ConfigureAwait(false);
+
+            Logger.LogDebug("Now using Metrics Validation Service to determine if submissions window metrics are within tolerance.");
+            var withinTolerance = await metricsValidationService.Validate(job.DcJobId.Value, job.AcademicYear, job.CollectionPeriod);
+
+            if (withinTolerance)
+                Logger.LogInfo("Submission window metrics are within tolerance.");
+            else
+                Logger.LogWarning("Submission window metrics are not within tolerance.");
+
+            return await CompleteJob(job, withinTolerance ? JobStatus.Completed : JobStatus.CompletedWithErrors, DateTimeOffset.UtcNow, cancellationToken).ConfigureAwait(false);
         }
     }
 }
