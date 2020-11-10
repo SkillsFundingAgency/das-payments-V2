74f475feb9714e8f0b91db6695144a4efd3782a0
Hardik Desai <hardik.desai@digital.education.gov.uk>
2020-11-10 14:29:02 +0000

    Added Finished Job telemetry

--- src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Handlers/PeriodEnd/RecordPeriodEndSubmissionWindowValidationJobHandler.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Handlers/PeriodEnd/RecordPeriodEndSubmissionWindowValidationJobHandler.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
 using SFA.DAS.Payments.Application.Messaging;
 using SFA.DAS.Payments.Core;
 using SFA.DAS.Payments.Monitoring.Jobs.Application;
@@ -18,30 +20,66 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.Handlers.PeriodEnd
         private readonly IPaymentLogger logger;
         private readonly IPeriodEndJobService periodEndJobService;
         private readonly IPeriodEndJobStatusManager jobStatusManager;
-        private readonly IMetricsValidationService metricsValidationService;
+        private readonly ISubmissionWindowValidationClient submissionWindowValidationClient;
         private readonly IJobStorageService jobStorageService;
 
+        private readonly ITelemetry telemetry;
 
-        public RecordPeriodEndSubmissionWindowValidationJobHandler(IPaymentLogger logger, IPeriodEndJobService periodEndJobService, IPeriodEndJobStatusManager jobStatusManager, IMetricsValidationService metricsValidationService, IJobStorageService jobStorageService)
+        public RecordPeriodEndSubmissionWindowValidationJobHandler(IPaymentLogger logger, IPeriodEndJobService periodEndJobService, IPeriodEndJobStatusManager jobStatusManager, ISubmissionWindowValidationClient submissionWindowValidationClient, IJobStorageService jobStorageService, ITelemetry telemetry)
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this.periodEndJobService = periodEndJobService ?? throw new ArgumentNullException(nameof(periodEndJobService));
             this.jobStatusManager = jobStatusManager ?? throw new ArgumentNullException(nameof(jobStatusManager));
-            this.metricsValidationService = metricsValidationService ?? throw new ArgumentNullException(nameof(metricsValidationService));
+            this.submissionWindowValidationClient = submissionWindowValidationClient ?? throw new ArgumentNullException(nameof(submissionWindowValidationClient));
             this.jobStorageService = jobStorageService ?? throw new ArgumentNullException(nameof(jobStorageService));
+            this.telemetry = telemetry ?? throw new ArgumentNullException(nameof(telemetry));
         }
 
         public async Task Handle(IList<RecordPeriodEndSubmissionWindowValidationJob> messages, CancellationToken cancellationToken)
         {
+            var stopwatch = new Stopwatch();
+
             foreach (var message in messages)
             {
+                stopwatch.Start();
+
                 logger.LogInfo($"Handling period end submission window validation job: {message.ToJson()}");
+                
                 await periodEndJobService.RecordPeriodEndJob(message, cancellationToken);
+                
                 jobStatusManager.StartMonitoringJob(message.JobId, JobType.PeriodEndSubmissionWindowValidationJob);
-                var metricsValid = await metricsValidationService.Validate(message.JobId, message.CollectionYear, message.CollectionPeriod);
-                await jobStorageService.SaveJobStatus(message.JobId, metricsValid ? JobStatus.Completed : JobStatus.CompletedWithErrors, DateTimeOffset.Now, cancellationToken);
+                
+                var metricsValid = await submissionWindowValidationClient.Validate(message.JobId, message.CollectionYear, message.CollectionPeriod);
+
+                var jobStatus = metricsValid ? JobStatus.Completed : JobStatus.CompletedWithErrors;
+
+                await jobStorageService.SaveJobStatus(message.JobId, jobStatus, DateTimeOffset.Now, cancellationToken);
+                
                 logger.LogInfo($"Handled period end submission window validation job: {message.JobId}");
+
+                stopwatch.Stop();
+                
+                SendTelemetry(message, jobStatus, stopwatch.Elapsed);
             }
+        }
+
+        private void SendTelemetry(RecordPeriodEndSubmissionWindowValidationJob job, JobStatus jobStatus, TimeSpan elapsedTime)
+        {
+            var properties = new Dictionary<string, string>
+            {
+                { TelemetryKeys.JobId, job.JobId.ToString()},
+                { TelemetryKeys.JobType, nameof(RecordPeriodEndSubmissionWindowValidationJob)},
+                { TelemetryKeys.CollectionPeriod, job.CollectionPeriod.ToString()},
+                { TelemetryKeys.AcademicYear, job.CollectionYear.ToString()},
+                { TelemetryKeys.Status, jobStatus.ToString("G")}
+            };
+
+            var metrics = new Dictionary<string, double>
+            {
+                { TelemetryKeys.Duration, elapsedTime.TotalMilliseconds},
+            };
+            
+            telemetry.TrackEvent("Finished Job", properties, metrics);
         }
     }
 }
