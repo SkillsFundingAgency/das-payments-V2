d8d4c086b75c62f86ef68db710757d8720226ed2
Hardik Desai <hardik.desai@digital.education.gov.uk>
2020-11-10 14:10:06 +0000

    Revert "JobStatusEventPublisher now correctly publishes events in the SubmissionWindowValidationJob scenario"
    This reverts commit 5411b45671eb563e5f205b7da36846c0d0adb641.

--- src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/JobStatusEventPublisher.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/JobStatusEventPublisher.cs
@@ -63,8 +63,7 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing
                     return succeeded ? (PeriodEndJobFinishedEvent) new PeriodEndRunJobSucceeded() : new PeriodEndRunJobFailed();
                 if (jobType  == JobType.PeriodEndStopJob)
                     return succeeded ? (PeriodEndJobFinishedEvent) new PeriodEndStopJobSucceeded() : new PeriodEndStopJobFailed();
-                if (jobType == JobType.PeriodEndSubmissionWindowValidationJob)
-                    return succeeded ? (PeriodEndJobFinishedEvent)new PeriodEndSubmissionWindowValidationJobSucceeded() : new PeriodEndSubmissionWindowValidationJobFailed();
+                //todo end of day Friday here - need to publish this
                 throw new InvalidOperationException($"Unhandled period end job type: {jobType}");
         }
 
