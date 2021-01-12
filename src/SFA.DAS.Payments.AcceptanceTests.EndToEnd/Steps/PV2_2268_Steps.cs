b5e08e5f62debda29ee3685c33ac150093c0590b
Tom <Tom>
2021-01-12 16:09:52 +0000

    Working acceptance test for PV2-2268

--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/PV2_2268_Steps.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/PV2_2268_Steps.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Autofac;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +10,7 @@ using TechTalk.SpecFlow;
 namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
 {
     [Binding]
-    [Scope(Feature = "PV2-2268-spike")]
+    [Scope(Feature = "PV2-2268-Break-in-Learning-after-redundancy-with-delivered-days-under-75-percent-then-re-employed-before-12-weeks-is-up")]
     // ReSharper disable once InconsistentNaming
     public class PV2_2268_Steps : FM36_ILR_Base_Steps
     {
@@ -19,16 +18,16 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
         {
         }
 
-        private const string PriceEpisodeIdentifier = "PE-2268";
-        private const string CommitmentIdentifier = "A-2268";
+        private const string PriceEpisodeIdentifierR04 = "PE-2268-R04";
 
-        [Given("run spike")]
-        public async Task RunSpike()
+        [Given("a Learner has been made redundant")]
+        public async Task LearnerMadeRedundant()
         {
             ImportR03Fm36();
-            //TestSession.Provider.Ukprn = TestSession.FM36Global.UKPRN; //hardcoded ukprn
-            TestSession.FM36Global.UKPRN = TestSession.Provider.Ukprn; //anonymised ukprn
-            TestSession.FM36Global.Learners.ForEach(x => x.ULN = TestSession.Learner.Uln); //one learner nuke all ulns to be the same from test session
+            
+            TestSession.FM36Global.UKPRN = TestSession.Provider.Ukprn;
+            TestSession.FM36Global.Learners.ForEach(x => x.ULN = TestSession.Learner.Uln);
+            TestSession.FM36Global.Learners.ForEach(x => x.LearnRefNumber = TestSession.Learner.LearnRefNumber);
 
             TestSession.Employers.Clear();
             TestSession.Employers.Add(new Employer { AccountId = TestSession.GenerateId(), Balance = 999999999, IsLevyPayer = true, AccountName = "employer 0"});
@@ -59,10 +58,18 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
 
             await WaitForPayments(6);
 
-            ImportR04Fm36();
-            TestSession.FM36Global.UKPRN = TestSession.Provider.Ukprn; //anonymised ukprn
-            TestSession.FM36Global.Learners.ForEach(x => x.ULN = TestSession.Learner.Uln); //one learner nuke all ulns to be the same from test session
+            
+        }
 
+        [When("the learner is re-employed before the 12 weeks redundancy period is exhausted")]
+        public async Task LearnerReEmployed()
+        {
+            ImportR04Fm36();
+            TestSession.FM36Global.UKPRN = TestSession.Provider.Ukprn;
+            TestSession.FM36Global.Learners.ForEach(x => x.ULN = TestSession.Learner.Uln);
+            TestSession.FM36Global.Learners.ForEach(x => x.LearnRefNumber = TestSession.Learner.LearnRefNumber);
+
+            var dcHelper = Scope.Resolve<IDcHelper>();
             await dcHelper.SendIlrSubmission(TestSession.FM36Global.Learners,
                 TestSession.Provider.Ukprn,
                 TestSession.CollectionPeriod.AcademicYear,
@@ -79,6 +86,31 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
                 MessageSession);
 
             await WaitForPayments(2);
+        }
+
+        [Given("a break in learning has also occurred")]
+        [Given("the redundancy and break in learning have been correctly recorded in the ILR")]
+        [Given("the delivered learning days before the break are under 75 percent")]
+        public void EmptyStep()
+        {
+            //covered by FM36s imported in first step
+        }
+
+        [Then("the learner must be funded from the employer levy funds")]
+        public async Task LearnerFundedFromLevy()
+        {
+            await WaitForIt(() => HasCorrectlyFundedPaymentFromLevyForR04(),
+                "Failed to find learning payment funded from Levy for R04");
+        }
+
+        private bool HasCorrectlyFundedPaymentFromLevyForR04()
+        {
+            return FundingSourcePaymentEventsHelper
+                .FundingSourcePaymentsReceivedForLearner(PriceEpisodeIdentifierR04, short.Parse(TestSession.FM36Global.Year), TestSession)
+                .Any(x =>
+                    x.FundingSourceType == FundingSourceType.Levy
+                    && x.DeliveryPeriod == 4
+                    && x.TransactionType == TransactionType.Learning);
         }
 
         private void ImportR03Fm36() { GetFm36LearnerForCollectionPeriod("R03/current academic year"); }
--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Tests/Redundancy/PV2-2268-spike.feature
+++
@@ -1,4 +0,0 @@
-﻿Feature: PV2-2268-spike
-Scenario: PV2-2268-spike
-
-Given run spike
