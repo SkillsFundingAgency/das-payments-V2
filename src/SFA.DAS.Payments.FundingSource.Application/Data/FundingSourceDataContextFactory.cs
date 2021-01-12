f0320d2cc67889964775d1235fe64d0034cc3ffe
Akshay Chugh <akshay.chugh30@outlook.com>
2021-01-12 09:56:25 +0000

    PV2-2271 FundingSourceDataContextFactory added

---
+++ src/SFA.DAS.Payments.FundingSource.Application/Data/FundingSourceDataContextFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace SFA.DAS.Payments.FundingSource.Application.Data
+{
+    public interface IFundingSourceDataContextFactory
+    {
+        IFundingSourceDataContext Create(DbTransaction transaction = null);
+    }
+    
+    public class FundingSourceDataContextFactory : IFundingSourceDataContextFactory
+    {
+
+        private readonly string connectionString;
+
+        public FundingSourceDataContextFactory(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public IFundingSourceDataContext Create(DbTransaction transaction = null)
+        {
+            var context = new FundingSourceDataContext(connectionString);
+
+            if (transaction != null)
+            {
+                context.Database.UseTransaction(transaction);
+            }
+
+            return context;
+        }
+    }
+
+}
