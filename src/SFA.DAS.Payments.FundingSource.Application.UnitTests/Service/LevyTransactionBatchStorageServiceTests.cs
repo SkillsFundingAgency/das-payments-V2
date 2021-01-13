6060b297525f598388ea1ea4d110a53951f325e3
Akshay Chugh <akshay.chugh30@outlook.com>
2021-01-13 16:38:33 +0000

    PV2-2271 Code Review Change 1

--- src/SFA.DAS.Payments.FundingSource.Application.UnitTests/Service/LevyTransactionBatchStorageServiceTests.cs
+++ src/SFA.DAS.Payments.FundingSource.Application.UnitTests/Service/LevyTransactionBatchStorageServiceTests.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Core;
 using SFA.DAS.Payments.FundingSource.Application.Data;
+using SFA.DAS.Payments.FundingSource.Application.Extensions;
 using SFA.DAS.Payments.FundingSource.Application.Repositories;
 using SFA.DAS.Payments.FundingSource.Application.Services;
 using SFA.DAS.Payments.Model.Core.Entities;
@@ -25,11 +28,11 @@ namespace SFA.DAS.Payments.FundingSource.Application.UnitTests.Service
         }
 
         [Test]
-        public async Task WhenStoringLevyTransactions_ThenSaveLevyTransactionsIsCalled()
+        public async Task WhenStoringLevyTransactions_ThenSaveLevyTransactionsIsCalled_AndModelsMappedCorrectly()
         {
             await fixture.StoreLevyTransactions();
 
-            fixture.Verify_SaveLevyTransactions_WasCalled();
+            fixture.Verify_SaveLevyTransactions_WasCalled_AndModelsMappedCorrectly();
         }
     }
 
@@ -56,9 +59,40 @@ namespace SFA.DAS.Payments.FundingSource.Application.UnitTests.Service
 
         public Task StoreLevyTransactions() => sut.StoreLevyTransactions(calculatedRequiredLevyAmounts, It.IsAny<CancellationToken>());
 
-        public void Verify_SaveLevyTransactions_WasCalled()
+        public void Verify_SaveLevyTransactions_WasCalled_AndModelsMappedCorrectly()
         {
-            mockLevyTransactionRepository.Verify(x => x.SaveLevyTransactions(It.IsAny<List<LevyTransactionModel>>(), It.IsAny<CancellationToken>()), Times.Once);
+            mockLevyTransactionRepository.Verify(x =>
+                x.SaveLevyTransactions(It.Is<List<LevyTransactionModel>>(y =>
+                    y.Count == calculatedRequiredLevyAmounts.Count &&
+                    y.TrueForAll(levyTransactionModel => calculatedRequiredLevyAmounts.Any(calculatedRequiredLevyAmount =>
+                        calculatedRequiredLevyAmount.CollectionPeriod.Period == levyTransactionModel.CollectionPeriod &&
+                        calculatedRequiredLevyAmount.CollectionPeriod.AcademicYear == levyTransactionModel.AcademicYear &&
+                        calculatedRequiredLevyAmount.JobId == levyTransactionModel.JobId &&
+                        calculatedRequiredLevyAmount.Ukprn == levyTransactionModel.Ukprn &&
+                        calculatedRequiredLevyAmount.AmountDue == levyTransactionModel.Amount &&
+                        calculatedRequiredLevyAmount.EarningEventId == levyTransactionModel.EarningEventId &&
+                        calculatedRequiredLevyAmount.DeliveryPeriod == levyTransactionModel.DeliveryPeriod &&
+                        (calculatedRequiredLevyAmount.AccountId == levyTransactionModel.AccountId || levyTransactionModel.AccountId == 0) &&
+                        calculatedRequiredLevyAmount.EventId == levyTransactionModel.RequiredPaymentEventId &&
+                        calculatedRequiredLevyAmount.TransferSenderAccountId == levyTransactionModel.TransferSenderAccountId &&
+                        calculatedRequiredLevyAmount.ToJson() == levyTransactionModel.MessagePayload &&
+                        calculatedRequiredLevyAmount.GetType().FullName == levyTransactionModel.MessageType &&
+                        calculatedRequiredLevyAmount.IlrSubmissionDateTime == levyTransactionModel.IlrSubmissionDateTime &&
+                        calculatedRequiredLevyAmount.CalculateFundingAccountId(false) == levyTransactionModel.FundingAccountId &&
+                        calculatedRequiredLevyAmount.ApprenticeshipEmployerType == levyTransactionModel.ApprenticeshipEmployerType &&
+                        calculatedRequiredLevyAmount.ApprenticeshipId == levyTransactionModel.ApprenticeshipId &&
+                        calculatedRequiredLevyAmount.Learner.ReferenceNumber == levyTransactionModel.LearnerReferenceNumber &&
+                        calculatedRequiredLevyAmount.LearningAim.FrameworkCode == levyTransactionModel.LearningAimFrameworkCode &&
+                        calculatedRequiredLevyAmount.LearningAim.PathwayCode == levyTransactionModel.LearningAimPathwayCode &&
+                        calculatedRequiredLevyAmount.LearningAim.FundingLineType == levyTransactionModel.LearningAimFundingLineType &&
+                        calculatedRequiredLevyAmount.LearningAim.ProgrammeType == levyTransactionModel.LearningAimProgrammeType &&
+                        calculatedRequiredLevyAmount.LearningAim.Reference == levyTransactionModel.LearningAimReference &&
+                        calculatedRequiredLevyAmount.LearningAim.StandardCode == levyTransactionModel.LearningAimStandardCode &&
+                        calculatedRequiredLevyAmount.LearningStartDate == levyTransactionModel.LearningStartDate &&
+                        calculatedRequiredLevyAmount.SfaContributionPercentage == levyTransactionModel.SfaContributionPercentage &&
+                        calculatedRequiredLevyAmount.TransactionType == levyTransactionModel.TransactionType
+                    ))), It.IsAny<CancellationToken>()), Times.Once);
+
         }
     }
 }
