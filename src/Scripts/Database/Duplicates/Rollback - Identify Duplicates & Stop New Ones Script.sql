a5f9b9a45e36efef3c6c428bbd02fd4f54513db5
Akshay Chugh <akshay.chugh30@outlook.com>
2021-01-15 16:44:40 +0000

    PV2-2271 Testing Feedback: Script extracted into new file

--- src/Scripts/Database/Duplicates/Rollback - Identify Duplicates & Stop New Ones Script.sql
+++ src/Scripts/Database/Duplicates/Rollback - Identify Duplicates & Stop New Ones Script.sql
@@ -37,12 +37,3 @@ GO
 IF EXISTS (SELECT * FROM sys.columns WHERE name = N'DuplicateNumber' AND object_id = OBJECT_ID(N'Payments2.Payment'))
 	ALTER TABLE Payments2.Payment DROP COLUMN DuplicateNumber
 GO
-
---FundingSourceLevyTransaction
-IF EXISTS (SELECT * FROM sys.indexes WHERE name = N'UX_FundingSourceLevyTransaction_LogicalDuplicates' AND object_id = OBJECT_ID(N'Payments2.FundingSourceLevyTransaction'))
-	DROP INDEX UX_FundingSourceLevyTransaction_LogicalDuplicates ON Payments2.FundingSourceLevyTransaction
-GO
-
-IF EXISTS (SELECT * FROM sys.columns WHERE name = N'DuplicateNumber' AND object_id = OBJECT_ID(N'Payments2.FundingSourceLevyTransaction'))
-	ALTER TABLE Payments2.FundingSourceLevyTransaction DROP COLUMN DuplicateNumber
-GO
