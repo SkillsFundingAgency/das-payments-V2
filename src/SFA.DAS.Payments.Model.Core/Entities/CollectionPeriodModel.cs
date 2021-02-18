b2ad096d17c0191aa86df5c48f7de06df22a00db
Tom <Tom>
2021-02-18 15:53:28 +0000

    Model & model configuration for CollectionPeriod

---
+++ src/SFA.DAS.Payments.Model.Core/Entities/CollectionPeriodModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SFA.DAS.Payments.Model.Core.Entities
+{
+    public class CollectionPeriodModel
+    {
+        public long Id { get; set; }
+        public short AcademicYear { get; set; }
+        public byte Period { get; set; }
+        public byte CalendarMonth { get; set; }
+        public short CalendarYear { get; set; }
+        public DateTime ReferenceDataValidationDate { get; set; }
+        public DateTime CompletionDate { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Payments.ProviderPayments.Application/Data/Configurations/CollectionPeriodModelConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SFA.DAS.Payments.Model.Core.Entities;
+
+namespace SFA.DAS.Payments.ProviderPayments.Application.Data.Configurations
+{
+    public class CollectionPeriodModelConfiguration : IEntityTypeConfiguration<CollectionPeriodModel>
+    {
+        public void Configure(EntityTypeBuilder<CollectionPeriodModel> builder)
+        {
+            builder.ToTable("CollectionPeriod", "Payments2");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasColumnName(@"Id").IsRequired();
+            builder.Property(x => x.AcademicYear).HasColumnName(@"AcademicYear").IsRequired();
+            builder.Property(x => x.Period).HasColumnName(@"Period").IsRequired();
+            builder.Property(x => x.CalendarMonth).HasColumnName(@"CalendarMonth").IsRequired();
+            builder.Property(x => x.CalendarYear).HasColumnName(@"CalendarYear").IsRequired();
+            builder.Property(x => x.ReferenceDataValidationDate).HasColumnName(@"ReferenceDataValidationDate").IsRequired();
+            builder.Property(x => x.CompletionDate).HasColumnName(@"CompletionDate").IsRequired();
+        }
+    }
+}
