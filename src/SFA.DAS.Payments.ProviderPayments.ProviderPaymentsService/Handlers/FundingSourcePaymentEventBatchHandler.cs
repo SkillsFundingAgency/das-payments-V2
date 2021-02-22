1103f1909771ea589ea0fc493abd2cded0c1b1e8
Tony <abroodie+kraken@github.com>
2021-02-22 23:20:42 +0000

    added batchhandler for FundingSourcePaymentEvents to automatic subscription creation working

---
+++ src/SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService/Handlers/FundingSourcePaymentEventBatchHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.Payments.Application.Messaging;
+using SFA.DAS.Payments.FundingSource.Messages.Events;
+
+namespace SFA.DAS.Payments.ProviderPayments.ProviderPaymentsService.Handlers
+{
+    public class FundingSourcePaymentEventBatchHandler: IHandleMessageBatches<FundingSourcePaymentEvent>
+    {
+        public Task Handle(IList<FundingSourcePaymentEvent> messages, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
