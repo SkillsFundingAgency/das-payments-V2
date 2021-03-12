130610850d50476cd64c6dadae8e01a15abb8769
Akshay Chugh <akshay.chugh30@outlook.com>
2021-03-12 14:52:57 +0000

    PV2-2294 new Repository, service and references

---
+++ src/SFA.DAS.Payments.PeriodEnd.Application/Repositories/SubmissionsJobsRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Payments.Model.Core.Entities;
+using SFA.DAS.Payments.PeriodEnd.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Payments.PeriodEnd.Application.Repositories
+{
+    public interface ISubmissionJobsRepository
+    {
+        Task<List<LatestSuccessfulJobModel>> GetLatestSuccessfulJobs();
+    }
+
+    public class SubmissionJobsRepository : ISubmissionJobsRepository
+    {
+        private readonly IPeriodEndDataContext dataContext;
+
+        public SubmissionJobsRepository(IPeriodEndDataContext dataContext)
+        {
+            this.dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
+        }
+
+        public async Task<List<LatestSuccessfulJobModel>> GetLatestSuccessfulJobs()
+        {
+            var latestCollectionPeriod = await dataContext.LatestSuccessfulJobs
+                .OrderByDescending(x => x.AcademicYear)
+                .ThenByDescending(x => x.CollectionPeriod)
+                .Select(x => new { x.AcademicYear, x.CollectionPeriod })
+                .FirstOrDefaultAsync();
+
+            if (latestCollectionPeriod == null)
+            {
+                return new List<LatestSuccessfulJobModel>();
+            }
+
+            return await dataContext.LatestSuccessfulJobs
+                .Where(x =>
+                    x.AcademicYear == latestCollectionPeriod.AcademicYear &&
+                    x.CollectionPeriod == latestCollectionPeriod.CollectionPeriod)
+                .ToListAsync();
+        }
+    }
+}
---
+++ src/SFA.DAS.Payments.PeriodEnd.Application/Services/SubmissionJobsService.cs
@@ -0,0 +1,36 @@
+﻿using SFA.DAS.Payments.PeriodEnd.Application.Repositories;
+using SFA.DAS.Payments.PeriodEnd.Model.SubmissionJobs;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Payments.PeriodEnd.Application.Services
+{
+    public interface ISubmissionJobsService
+    {
+        Task<SubmissionJobs> SuccessfulSubmissions();
+    }
+
+    public class SubmissionJobsService : ISubmissionJobsService
+    {
+        private readonly ISubmissionJobsRepository repository;
+
+        public SubmissionJobsService(ISubmissionJobsRepository repository)
+        {
+            this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public async Task<SubmissionJobs> SuccessfulSubmissions()
+        {
+            var providers = await repository.GetLatestSuccessfulJobs();
+            return new SubmissionJobs
+            {
+                SuccessfulSubmissionJobs = providers.Select(x => new SubmissionJob
+                {
+                    JobId = x.DcJobId,
+                    Ukprn = x.Ukprn,
+                }).ToList(),
+            };
+        }
+    }
+}
