88b4d7e588ac65468cd9bef0aeb51f6f91def497
Akshay Chugh <akshay.chugh30@outlook.com>
2021-03-12 14:49:57 +0000

    PV2-2294 new Models added

---
+++ src/SFA.DAS.Payments.PeriodEnd.Model/SubmissionJobs/SubmissionJob.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Payments.PeriodEnd.Model.SubmissionJobs
+{
+    public class SubmissionJob
+    {
+        public long Ukprn { get; set; }
+        public long JobId { get; set; }
+    }
+}
