c891adceaf69f9da588ff15088203140635fa6d4
Carl <carl@techcellence.org.uk>
2021-03-15 10:07:55 +0000

    Fixed merge issues

--- src/SFA.DAS.Payments.PeriodEnd.Application/Repositories/PeriodEndRepository.cs
+++ src/SFA.DAS.Payments.PeriodEnd.Application/Repositories/PeriodEndRepository.cs
@@ -6,15 +6,11 @@ using Microsoft.EntityFrameworkCore;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.Model.Core.Entities;
 using SFA.DAS.Payments.PeriodEnd.Data;
-using SFA.DAS.Payments.PeriodEnd.Model;
 
 namespace SFA.DAS.Payments.PeriodEnd.Application.Repositories
 {
     public interface IPeriodEndRepository
     {
-        Task RemoveUkrpnFromReprocessingList(long ukprn);
-        Task<ProviderRequiringReprocessingEntity> RecordForProviderThatRequiresReprocessing(long ukprn);
-        Task AddProviderThatRequiredReprocessing(long ukprn);
         Task<List<LatestSuccessfulJobModel>> GetLatestSuccessfulJobs();
     }
 
@@ -28,40 +24,6 @@ namespace SFA.DAS.Payments.PeriodEnd.Application.Repositories
             this.dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
-
-        public async Task RemoveUkrpnFromReprocessingList(long ukprn)
-        {
-            logger.LogDebug($"Removing UKPRN from ProviderRequiringReprocessing for ukprn: {ukprn}");
-            var record = await dataContext.ProvidersRequiringReprocessing
-                .FirstOrDefaultAsync(x => x.Ukprn == ukprn);
-            if (record != null)
-                dataContext.ProvidersRequiringReprocessing.Remove(record);
-            await dataContext.SaveChanges();
-
-            //dataContext.ProvidersRequiringReprocessing.FromSql($"DELETE [Payments2].[ProviderRequiringReprocessing] WHERE Ukprn = {ukprn}");
-            logger.LogInfo($"Removed UKPRN from ProviderRequiringReprocessing for ukprn: {ukprn}");
-        }
-
-        public async Task<ProviderRequiringReprocessingEntity> RecordForProviderThatRequiresReprocessing(long ukprn)
-        {
-            logger.LogDebug($"Getting ProviderRequiringReprocessing entity for Ukprn: {ukprn}");
-            var record = await dataContext.ProvidersRequiringReprocessing.FirstOrDefaultAsync(x => x.Ukprn == ukprn);
-            logger.LogInfo($"Finished getting ProviderRequiringReprocessing entity for Ukprn: {ukprn}");
-            return record;
-        }
-
-        public async Task AddProviderThatRequiredReprocessing(long ukprn)
-        {
-            logger.LogDebug($"Adding ProviderRequiringReprocessing entity for provider: {ukprn}");
-            var record = new ProviderRequiringReprocessingEntity
-            {
-                Ukprn = ukprn,
-            };
-            await dataContext.ProvidersRequiringReprocessing.AddAsync(record);
-            await dataContext.SaveChanges();
-            logger.LogInfo($"Finished adding ProviderRequiringReprocessing entity for Ukprn: {ukprn}");
-        }
-
 
         public async Task<List<LatestSuccessfulJobModel>> GetLatestSuccessfulJobs()
         {
