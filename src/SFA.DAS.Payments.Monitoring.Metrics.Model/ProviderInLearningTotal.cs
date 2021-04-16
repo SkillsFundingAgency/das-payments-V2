3cbfe9bdf4a27438ea5ec3fb3fc7e72a63981cb0
Tom <Tom>
2021-04-16 09:49:33 +0100

    Metrics model added for ProviderInLearningTotal

---
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Model/ProviderInLearningTotal.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Payments.Monitoring.Metrics.Model
+{
+    public class ProviderInLearningTotal
+    {
+        public long Ukprn { get; set; }
+        public int InLearningCount { get; set; }
+    }
+}
