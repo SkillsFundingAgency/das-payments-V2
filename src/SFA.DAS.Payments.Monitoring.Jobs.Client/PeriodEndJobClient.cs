f32fe7c6e5a8ecde2c70588ff9d03d94f7e332c5
Tom <Tom>
2021-05-06 10:53:09 +0100

    job client changes

--- src/SFA.DAS.Payments.Monitoring.Jobs.Client/PeriodEndJobClient.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Client/PeriodEndJobClient.cs
@@ -19,6 +19,8 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Client
             List<GeneratedMessage> generatedMessages);
         Task RecordPeriodEndSubmissionWindowValidation(long jobId, short collectionYear, byte collectionPeriod,
             List<GeneratedMessage> generatedMessages);
+        Task RecordPeriodEndRequestReportsJob(long jobId, short collectionYear, byte collectionPeriod,
+            List<GeneratedMessage> generatedMessages);
     }
 
     public class PeriodEndJobClient : IPeriodEndJobClient
@@ -84,6 +86,15 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Client
             await StartJob<RecordPeriodEndSubmissionWindowValidationJob>(jobId, collectionYear, collectionPeriod, generatedMessages)
                 .ConfigureAwait(false);
             logger.LogInfo($"Sent request to record period end submission window validation. Job Id: {jobId}, collection period: {collectionYear}-{collectionPeriod}");
+        }
+
+        public async Task RecordPeriodEndRequestReportsJob(long jobId, short collectionYear, byte collectionPeriod,
+            List<GeneratedMessage> generatedMessages)
+        {
+            logger.LogDebug($"Sending request to request period end reports. Job Id: {jobId}, collection period: {collectionYear}-{collectionPeriod}");
+            await StartJob<RecordPeriodEndRequestReportsJob>(jobId, collectionYear, collectionPeriod, generatedMessages)
+                .ConfigureAwait(false); //todo the handler for this job should call the new function using the client using the same pattern as RecordPeriodEndSubmissionWindowValidationJob
+            logger.LogInfo($"Sent request to request period end reports. Job Id: {jobId}, collection period: {collectionYear}-{collectionPeriod}");
         }
     }
 }
