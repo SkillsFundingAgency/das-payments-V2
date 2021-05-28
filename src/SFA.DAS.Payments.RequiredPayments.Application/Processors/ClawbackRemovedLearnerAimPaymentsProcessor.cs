8bafe4f61d9678d7d4b20d734458530efca80f5b
Hardik Desai <hardik.desai@digital.education.gov.uk>
2021-05-28 12:02:44 +0100

    added Clawback Removed Learner Aim Payments Processor

---
+++ src/SFA.DAS.Payments.RequiredPayments.Application/Processors/ClawbackRemovedLearnerAimPaymentsProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.Payments.RequiredPayments.Messages.Events;
+
+namespace SFA.DAS.Payments.RequiredPayments.Application.Processors
+{
+    public interface IClawbackRemovedLearnerAimPaymentsProcessor
+    {
+        Task<IReadOnlyCollection<CalculatedRequiredLevyAmount>> GenerateClawbacksForRemovedLearnerAim(IdentifiedRemovedLearningAim identifiedRemovedLearningAim, CancellationToken cancellationToken);
+    }
+
+    public class ClawbackRemovedLearnerAimPaymentsProcessor : IClawbackRemovedLearnerAimPaymentsProcessor
+    {
+        public Task<IReadOnlyCollection<CalculatedRequiredLevyAmount>> GenerateClawbacksForRemovedLearnerAim(IdentifiedRemovedLearningAim identifiedRemovedLearningAim, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- src/SFA.DAS.Payments.RequiredPayments.ClawbackRemovedLearnerAimPaymentsService/Handlers/IdentifiedRemovedLearningAimEventHandler.cs
+++ src/SFA.DAS.Payments.RequiredPayments.ClawbackRemovedLearnerAimPaymentsService/Handlers/IdentifiedRemovedLearningAimEventHandler.cs
@@ -1,18 +1,46 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.Logging.Interfaces;
 using NServiceBus;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.RequiredPayments.Application.Processors;
 using SFA.DAS.Payments.RequiredPayments.Messages.Events;
+using ExecutionContext = ESFA.DC.Logging.ExecutionContext;
 
 namespace SFA.DAS.Payments.RequiredPayments.ClawbackRemovedLearnerAimPaymentsService.Handlers
 {
     public class IdentifiedRemovedLearningAimEventHandler : IHandleMessages<IdentifiedRemovedLearningAim>
     {
-        public IdentifiedRemovedLearningAimEventHandler()
+        private readonly IClawbackRemovedLearnerAimPaymentsProcessor clawbackRemovedLearnerAimPaymentsProcessor;
+        private readonly IPaymentLogger logger;
+        private readonly IExecutionContext executionContext;
+
+        public IdentifiedRemovedLearningAimEventHandler(IClawbackRemovedLearnerAimPaymentsProcessor clawbackRemovedLearnerAimPaymentsProcessor, IPaymentLogger logger, IExecutionContext executionContext)
         {
+            this.clawbackRemovedLearnerAimPaymentsProcessor = clawbackRemovedLearnerAimPaymentsProcessor ?? throw new ArgumentNullException(nameof(clawbackRemovedLearnerAimPaymentsProcessor));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.executionContext = executionContext ?? throw new ArgumentNullException(nameof(executionContext));
         }
 
-        public Task Handle(IdentifiedRemovedLearningAim message, IMessageHandlerContext context)
+        public async Task Handle(IdentifiedRemovedLearningAim message, IMessageHandlerContext context)
         {
-            throw new System.NotImplementedException();
+            logger.LogDebug("Processing 'IdentifiedRemovedLearningAim' message.");
+            ((ExecutionContext)executionContext).JobId = message.JobId.ToString();
+
+            var calculatedRequiredLevyAmount = await clawbackRemovedLearnerAimPaymentsProcessor.GenerateClawbacksForRemovedLearnerAim(message, CancellationToken.None).ConfigureAwait(false);
+
+            logger.LogDebug($"Got {calculatedRequiredLevyAmount?.Count ?? 0} required payments.");
+
+            if (calculatedRequiredLevyAmount != null)
+                await Task.WhenAll(calculatedRequiredLevyAmount.Select(context.Publish)).ConfigureAwait(false);
+
+            logger.LogInfo("Successfully processed IdentifiedRemovedLearningAim event for " +
+                           $"jobId:{message.JobId}, learnerRef:{message.Learner.ReferenceNumber}, frameworkCode:{message.LearningAim.FrameworkCode}, " +
+                           $"pathwayCode:{message.LearningAim.PathwayCode}, programmeType:{message.LearningAim.ProgrammeType}, " +
+                           $"standardCode:{message.LearningAim.StandardCode}, learningAimReference:{message.LearningAim.Reference}, " +
+                           $"academicYear:{message.CollectionPeriod.AcademicYear}, contractType:{message.ContractType}");
         }
     }
 }
