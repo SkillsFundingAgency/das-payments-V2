47e54f98d169d0f02748f4f965054232084b5b9d
Hardik Desai <hardik.desai@digital.education.gov.uk>
2021-05-28 11:48:51 +0100

    Added Event Handler

---
+++ src/SFA.DAS.Payments.RequiredPayments.ClawbackRemovedLearnerAimPaymentsService/Handlers/IdentifiedRemovedLearningAimEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.Payments.RequiredPayments.Messages.Events;
+
+namespace SFA.DAS.Payments.RequiredPayments.ClawbackRemovedLearnerAimPaymentsService.Handlers
+{
+    public class IdentifiedRemovedLearningAimEventHandler : IHandleMessages<IdentifiedRemovedLearningAim>
+    {
+        public IdentifiedRemovedLearningAimEventHandler()
+        {
+        }
+
+        public Task Handle(IdentifiedRemovedLearningAim message, IMessageHandlerContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
