4b6f2422156681ad95d1697a170a1e64aeb95dcf
Tom <Tom>
2021-06-09 10:43:29 +0100

    Fix to model configuration to reflect nullability of RequiredPaymentEventId on payment

--- src/SFA.DAS.Payments.Application/Data/Configurations/PaymentModelConfiguration.cs
+++ src/SFA.DAS.Payments.Application/Data/Configurations/PaymentModelConfiguration.cs
@@ -17,7 +17,7 @@ namespace SFA.DAS.Payments.Application.Data.Configurations
             builder.Property(x => x.EventId).HasColumnName(@"EventId").IsRequired();
             builder.Property(x => x.EarningEventId).HasColumnName(@"EarningEventId").IsRequired();
             builder.Property(x => x.FundingSourceEventId).HasColumnName(@"FundingSourceEventId").IsRequired();
-            builder.Property(x => x.RequiredPaymentEventId).HasColumnName(@"RequiredPaymentEventId").IsRequired();
+            builder.Property(x => x.RequiredPaymentEventId).HasColumnName(@"RequiredPaymentEventId");
             builder.Property(x => x.ClawbackSourcePaymentEventId).HasColumnName(@"ClawbackSourcePaymentEventId");
             builder.Property(x => x.EventTime).HasColumnName(@"EventTime").IsRequired();
             builder.Property(x => x.PriceEpisodeIdentifier).HasColumnName(@"PriceEpisodeIdentifier").IsRequired();
