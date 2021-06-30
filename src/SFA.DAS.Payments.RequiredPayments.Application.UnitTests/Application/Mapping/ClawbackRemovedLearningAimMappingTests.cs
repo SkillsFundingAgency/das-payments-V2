7778d31e60781f12df91a617fe00c21c0d92c511
Akshay Chugh <akshay.chugh30@outlook.com>
2021-06-30 16:30:21 +0100

    PV2-2568 RequiredPaymentsProfile PaymentModel -> PeriodisedRequiredPaymentEvent mapping tests

---
+++ src/SFA.DAS.Payments.RequiredPayments.Application.UnitTests/Application/Mapping/ClawbackRemovedLearningAimMappingTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoFixture;
+using AutoMapper;
+using NUnit.Framework;
+using SFA.DAS.Payments.Model.Core.Entities;
+using SFA.DAS.Payments.RequiredPayments.Application.Mapping;
+using SFA.DAS.Payments.RequiredPayments.Application.UnitTests.TestHelpers;
+using SFA.DAS.Payments.RequiredPayments.Messages.Events;
+
+namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.Application.Mapping
+{
+    [TestFixture]
+    internal class ClawbackRemovedLearningAimMappingTests
+    {
+
+        private IMapper sut;
+        private PaymentModel payment;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            var fixture = new Fixture();
+
+            var config = new MapperConfiguration(x => x.AddProfile<RequiredPaymentsProfile>());
+            
+            config.AssertConfigurationIsValid();
+
+            payment = fixture.Create<PaymentModel>();
+            sut = new Mapper(config);
+        }
+
+        [Test]
+        public void ThenMaps_PaymentModel_To_CalculatedRequiredIncentiveAmount()
+        {
+            //Act
+            var result = sut.Map<CalculatedRequiredIncentiveAmount>(payment);
+            
+            //Assert
+            result.AssertPeriodisedRequiredPaymentEventWasCorrectlyMappedFrom(payment);
+        }
+
+        [Test]
+        public void ThenMaps_PaymentModel_To_CalculatedRequiredLevyAmount()
+        {
+            //Act
+            var result = sut.Map<CalculatedRequiredLevyAmount>(payment);
+            
+            //Assert
+            result.AssertCalculatedRequiredLevyAmountWasCorrectlyMappedFrom(payment);
+        }
+
+        [Test]
+        public void ThenMaps_PaymentModel_To_CalculatedRequiredCoInvestedAmount()
+        {
+            //Act
+            var result = sut.Map<CalculatedRequiredCoInvestedAmount>(payment);
+            
+            //Assert
+            result.AssertCalculatedRequiredCoInvestedAmountWasCorrectlyMappedFrom(payment);
+        }
+    }
+}
