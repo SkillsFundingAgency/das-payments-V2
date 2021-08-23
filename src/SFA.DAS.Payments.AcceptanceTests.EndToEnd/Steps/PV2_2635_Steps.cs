ed88a95f120baed8d1c30ebae4f20c6c5a19c75a
Akshay Chugh <akshay.chugh30@outlook.com>
2021-08-23 14:31:29 +0100

    PV2-2635 WIP acceptance  test

---
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/PV2_2635_Steps.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Payments.AcceptanceTests.Core.Automation;
+using SFA.DAS.Payments.AcceptanceTests.Core.Data;
+using SFA.DAS.Payments.Model.Core.Entities;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
+{
+    [Binding]
+    [Scope(Feature = "PV2-2635-LearnStartDate-IsMapped-FromILR-ToPaymentsTable")]
+    // ReSharper disable once InconsistentNaming
+    public class PV2_2635_Steps : FM36_ILR_Base_Steps
+    {
+        public PV2_2635_Steps(FeatureContext context) : base(context)
+        {
+        }
+
+        [Then(@"the correct LearnStartDate is set for each generated payment")]
+        public void ThenTheCorrectLearnStartDateIsSetForEachGeneratedPayment()
+        {
+        }
+
+    }
+}
