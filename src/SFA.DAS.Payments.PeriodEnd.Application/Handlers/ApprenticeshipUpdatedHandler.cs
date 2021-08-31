de27814797df8cfe3ba6a8c1849c6dc6d15c08df
Akshay Chugh <akshay.chugh30@outlook.com>
2021-08-31 16:49:47 +0100

    PV2-2644 Catching and logging exception related to unique index

--- src/SFA.DAS.Payments.PeriodEnd.Application/Handlers/ApprenticeshipUpdatedHandler.cs
+++ src/SFA.DAS.Payments.PeriodEnd.Application/Handlers/ApprenticeshipUpdatedHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using NServiceBus;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.DataLocks.Messages.Events;
@@ -21,7 +22,16 @@ namespace SFA.DAS.Payments.PeriodEnd.Application.Handlers
         public async Task Handle(ApprenticeshipUpdated message, IMessageHandlerContext context)
         {
             logger.LogDebug($"Apprenticeship Updated. Ensuring Ukprn: {message.Ukprn} exists on the list for reprocessing");
-            await repository.Add(message.Ukprn);
+
+            try
+            {
+                await repository.Add(message.Ukprn);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                logger.LogInfo($"Ukprn: {message.Ukprn} already exists on the list of providers requiring reprocessing");
+            }
+
             logger.LogInfo($"Apprenticeship Updated. Finished ensuring that Ukprn: {message.Ukprn} exists on the list for reprocessing");
         }
     }
