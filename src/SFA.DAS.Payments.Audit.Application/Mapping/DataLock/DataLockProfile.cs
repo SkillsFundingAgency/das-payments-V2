17f77f22d9ce307b6a72dc6188566f91ad349ac7
Tom <Tom>
2021-09-15 12:15:30 +0100

    Backed out earning events mapping update and set data lock audit mapping to work out funding line type based on price episode

--- src/SFA.DAS.Payments.Audit.Application/Mapping/DataLock/DataLockProfile.cs
+++ src/SFA.DAS.Payments.Audit.Application/Mapping/DataLock/DataLockProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Linq;
+using AutoMapper;
 using SFA.DAS.Payments.DataLocks.Messages.Events;
 using SFA.DAS.Payments.Model.Core;
 using SFA.DAS.Payments.Model.Core.Audit;
@@ -20,7 +21,7 @@ namespace SFA.DAS.Payments.Audit.Application.Mapping.DataLock
                 .ForMember(dest => dest.LearningStartDate, opt => opt.MapFrom(src => src.LearningAim.StartDate))
                 .ForMember(dest => dest.IlrFileName, opt => opt.MapFrom(x => x.IlrFileName))
                 .ForMember(dest => dest.EventType, opt => opt.MapFrom(x => x.GetType().FullName))
-                .ForMember(dest => dest.LearningAimFundingLineType, opt => opt.MapFrom(x => x.LearningAim.FundingLineType))
+                .ForMember(dest => dest.LearningAimFundingLineType, opt => opt.MapFrom(x => x.PriceEpisodes.First(y => y.LearningAimSequenceNumber == x.LearningAim.SequenceNumber).FundingLineType))
                 ;
 
             CreateMap<PayableEarningEvent, DataLockEventModel>()
