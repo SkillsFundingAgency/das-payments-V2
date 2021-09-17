9b83964b0f8176bd1c3b5d819309c3e9ce702b61
Tom <Tom>
2021-09-17 14:18:50 +0100

    Performance improvement

--- src/SFA.DAS.Payments.Monitoring.Metrics.Model/ProviderFundingLineTypeAmounts.cs
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Model/ProviderFundingLineTypeAmounts.cs
@@ -5,6 +5,6 @@
         public long Ukprn { get; set; }
         public decimal FundingLineType16To18Amount { get; set; }
         public decimal FundingLineType19PlusAmount { get; set; }
-        public decimal Total => FundingLineType16To18Amount + FundingLineType19PlusAmount;
+        public decimal Total { get; set; }
     }
 }
