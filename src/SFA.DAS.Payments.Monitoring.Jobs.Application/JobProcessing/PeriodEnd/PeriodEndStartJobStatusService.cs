0fbc859d445c5188b53e76e3a7951fd9f7b08fd3
Hardik Desai <hardik.desai@digital.education.gov.uk>
2021-10-29 17:39:52 +0100

    Added additonal logging for PeriodEndStart Job Status Update

--- src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/PeriodEnd/PeriodEndStartJobStatusService.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/PeriodEnd/PeriodEndStartJobStatusService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
+using SFA.DAS.Payments.Core;
 using SFA.DAS.Payments.Monitoring.Jobs.Application.Infrastructure.Configuration;
 using SFA.DAS.Payments.Monitoring.Jobs.Data;
 using SFA.DAS.Payments.Monitoring.Jobs.Model;
@@ -15,17 +17,21 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.PeriodEnd
     {
         private readonly IJobsDataContext context;
 
-        public PeriodEndStartJobStatusService(IJobStorageService jobStorageService,
-            IPaymentLogger logger, ITelemetry telemetry, IJobStatusEventPublisher eventPublisher,
-            IJobServiceConfiguration config, IJobsDataContext context) : base(jobStorageService, logger, telemetry, eventPublisher, config)
+        public PeriodEndStartJobStatusService(
+            IJobStorageService jobStorageService,
+            IPaymentLogger logger, 
+            ITelemetry telemetry, 
+            IJobStatusEventPublisher eventPublisher,
+            IJobServiceConfiguration config, 
+            IJobsDataContext context) 
+            : base(jobStorageService, logger, telemetry, eventPublisher, config)
         {
             this.context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
         public override async Task<(bool IsComplete, JobStatus? OverriddenJobStatus, DateTimeOffset? completionTime)> PerformAdditionalJobChecks(JobModel job, CancellationToken cancellationToken)
         {
-            var outstandingJobs =
-                await context.GetOutstandingOrTimedOutJobs(job.DcJobId, job.StartTime, cancellationToken);
+            var outstandingJobs = await context.GetOutstandingOrTimedOutJobs(job.DcJobId, job.StartTime, cancellationToken);
 
             var timeoutsPresent = outstandingJobs.Any(x =>
                 (x.JobStatus == JobStatus.TimedOut ||
@@ -37,17 +43,52 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.PeriodEnd
                 return (true, JobStatus.CompletedWithErrors, outstandingJobs.Max(x => x.EndTime));
             }
 
-            var processingJobsPresent = outstandingJobs.Any(x => x.JobStatus == JobStatus.InProgress || x.DcJobSucceeded == null);
+            var processingJobsPresent = outstandingJobs.Where(x => x.JobStatus == JobStatus.InProgress || x.DcJobSucceeded == null).ToList();
 
-            if (processingJobsPresent)
-                return (false, (JobStatus?)null, (DateTimeOffset?)null);
+            if (processingJobsPresent.Any())
+            {
+                SendTelemetry(job, processingJobsPresent);
+                return (false, null, null);
+            }
 
             var jobsWithoutSubmissionSummariesPresent = context.DoSubmissionSummariesExistForJobs(outstandingJobs);
 
-            if (!jobsWithoutSubmissionSummariesPresent)
-                return (false, (JobStatus?)null, (DateTimeOffset?)null);
+            if (jobsWithoutSubmissionSummariesPresent.Any()) 
+            {
+                SendTelemetry(job, null, jobsWithoutSubmissionSummariesPresent);
+                return (false, null, null);
+            }
 
-            return (true, (JobStatus?)null, DateTimeOffset.UtcNow);
+            return (true, null, DateTimeOffset.UtcNow);
+        }
+
+        private void SendTelemetry(JobModel job, List<OutstandingJobResult> processingJobsPresent = null, List<long?> jobsWithoutSubmissionSummariesPresent = null)
+        {
+            var properties = new Dictionary<string, string>
+            {
+                { TelemetryKeys.JobId, job.DcJobId.Value.ToString()},
+                { TelemetryKeys.JobType, job.JobType.ToString("G")},
+                { TelemetryKeys.Ukprn, job.Ukprn?.ToString() ?? string.Empty},
+                { TelemetryKeys.InternalJobId, job.DcJobId.ToString()},
+                { TelemetryKeys.CollectionPeriod, job.CollectionPeriod.ToString()},
+                { TelemetryKeys.AcademicYear, job.AcademicYear.ToString()},
+                { TelemetryKeys.Status, job.Status.ToString("G")}
+            };
+
+            if (processingJobsPresent != null)
+            {
+                properties.Add("InProgressJobsCount", processingJobsPresent.Count.ToString());
+                properties.Add("InProgressJobsList", string.Join(", ", processingJobsPresent.Select(j => j.ToJson())));
+            }
+            
+            
+            if (jobsWithoutSubmissionSummariesPresent != null)
+            {
+                properties.Add("jobsWithoutSubmissionSummariesCount", jobsWithoutSubmissionSummariesPresent.Count.ToString());
+                properties.Add("jobsWithoutSubmissionSummaries", string.Join(", ", jobsWithoutSubmissionSummariesPresent.Select(j => j.ToJson())));
+            }
+            
+            Telemetry.TrackEvent("PeriodEndStart Job Status Update", properties, new Dictionary<string, double>());
         }
     }
 }
