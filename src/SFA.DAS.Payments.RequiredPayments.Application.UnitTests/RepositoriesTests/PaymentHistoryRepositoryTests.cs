31de0e8004e10a0eebe042c245b58c1ce063d4dd
Hardik Desai <hardik.desai@digital.education.gov.uk>
2021-11-01 15:08:51 +0000

    updated unit tests

--- src/SFA.DAS.Payments.RequiredPayments.Application.UnitTests/RepositoriesTests/PaymentHistoryRepositoryTests.cs
+++ src/SFA.DAS.Payments.RequiredPayments.Application.UnitTests/RepositoriesTests/PaymentHistoryRepositoryTests.cs
@@ -62,6 +62,40 @@ namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.RepositoriesTe
             actual.Should().Be(100);
         }
 
+        [TestCase(TransactionType.Completion)]
+        [TestCase(TransactionType.Balancing)]
+        [TestCase(TransactionType.First16To18EmployerIncentive)]
+        [TestCase(TransactionType.First16To18ProviderIncentive)]
+        [TestCase(TransactionType.Second16To18EmployerIncentive)]
+        [TestCase(TransactionType.Second16To18ProviderIncentive)]
+        [TestCase(TransactionType.OnProgramme16To18FrameworkUplift)]
+        [TestCase(TransactionType.Completion16To18FrameworkUplift)]
+        [TestCase(TransactionType.Balancing16To18FrameworkUplift)]
+        [TestCase(TransactionType.FirstDisadvantagePayment)]
+        [TestCase(TransactionType.SecondDisadvantagePayment)]
+        [TestCase(TransactionType.OnProgrammeMathsAndEnglish)]
+        [TestCase(TransactionType.BalancingMathsAndEnglish)]
+        [TestCase(TransactionType.LearningSupport)]
+        [TestCase(TransactionType.CareLeaverApprenticePayment)]
+        public async Task GetEmployerCoInvestedPaymentHistoryTotal_Only_includes_Learning_Payments(TransactionType transactionType)
+        {
+            var key = CreateTestApprenticeshipKey();
+            var payment1 = CreateTestPayment();
+            payment1.Amount = 50.99m;
+
+            var payment2 = CreateTestPayment();
+            payment2.TransactionType = transactionType;
+            payment2.Amount = 50.99m;
+
+            context.Payment.Add(payment1);
+            context.Payment.Add(payment2);
+            await context.SaveChangesAsync();
+
+            var actual = await sut.GetEmployerCoInvestedPaymentHistoryTotal(key);
+
+            actual.Should().Be(50);
+        }
+
         private PaymentModel CreateTestPayment()
         {
             return new PaymentModel
@@ -75,6 +109,7 @@ namespace SFA.DAS.Payments.RequiredPayments.Application.UnitTests.RepositoriesTe
                 LearningAimStandardCode = 105,
                 FundingSource = FundingSourceType.CoInvestedEmployer,
                 ContractType = ContractType.Act2,
+                TransactionType = TransactionType.Learning,
             };
         }
 
