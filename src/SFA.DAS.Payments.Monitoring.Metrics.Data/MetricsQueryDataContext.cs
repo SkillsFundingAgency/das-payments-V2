4678a3fab517034bc28b628442fd4ead11f1afa9
Akshay Chugh <akshay.chugh30@outlook.com>
2022-02-17 12:19:27 +0000

    PV2-2836 updated negative earnings data locks query

--- src/SFA.DAS.Payments.Monitoring.Metrics.Data/MetricsQueryDataContext.cs
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Data/MetricsQueryDataContext.cs
@@ -326,17 +326,35 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Data
             var results = new List<ProviderNegativeEarningsLearnerDataLockAmounts>();
             var batches = learnerUlns.SplitIntoBatchesOf(2000);
 
-            var sql = @"Select dle.ukprn as Ukprn,
-                            dle.LearnerUln,
-	                        SUM(npp.Amount) AS TotalAmount
+            var sql = @";WITH unGroupedEarnings AS 
+                        (Select
+	                        dle.ukprn as Ukprn,
+							dle.LearnerULn,
+                            CASE WHEN dle.LearningAimFundingLineType IN (
+			                        '16 - 18 Apprenticeship(From May 2017) Non - Levy Contract(non - procured)',
+			                        '16-18 Apprenticeship Non-Levy Contract (procured)',
+			                        '16-18 Apprenticeship (Employer on App Service)'
+		                        ) THEN npp.Amount ELSE 0 END AS FundingLineType16To18Amount,
+	                        CASE WHEN dle.LearningAimFundingLineType IN (
+			                        '19+ Apprenticeship (From May 2017) Non-Levy Contract (non-procured)',
+			                        '19+ Apprenticeship Non-Levy Contract (procured)',
+			                        '19+ Apprenticeship (Employer on App Service)'
+		                        ) THEN npp.Amount ELSE 0 END AS FundingLineType19PlusAmount,
+	                        npp.Amount AS Total
                         from Payments2.dataLockEventNonPayablePeriod npp
-                        join Payments2.dataLockEvent dle 
-                        on npp.DataLockEventId = dle.EventId
+                        join Payments2.dataLockEvent dle on npp.DataLockEventId = dle.EventId
                         where 		
 	                        dle.jobId in (select DcJobid from Payments2.LatestSuccessfulJobs Where AcademicYear = @academicYear AND CollectionPeriod = @collectionPeriod)
 	                        and npp.Amount <> 0
-                            and dle.LearnerUln in ({0})
-	                    GROUP BY dle.Ukprn, dle.LearnerUln";
+							and dle.LearnerUln in ({0})
+                        )
+                        SELECT Ukprn,
+						LearnerUln,
+	                        SUM(unGroupedEarnings.FundingLineType16To18Amount) AS FundingLineType16To18Amount, 
+	                        SUM(unGroupedEarnings.FundingLineType19PlusAmount) AS FundingLineType19PlusAmount,
+	                        SUM(unGroupedEarnings.Total) AS Total
+	                        FROM unGroupedEarnings
+	                        GROUP BY unGroupedEarnings.Ukprn, LearnerUln";
 
             foreach (var batch in batches)
             {
