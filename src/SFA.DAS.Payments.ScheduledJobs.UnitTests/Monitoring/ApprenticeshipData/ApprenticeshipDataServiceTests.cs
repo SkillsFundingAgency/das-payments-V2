7f24a2e56ee8939afaef544907ebabd7ddf20fd6
Hardik Desai <hardik.desai@education.gov.uk>
2022-04-05 13:54:32 +0100

    updated unit tests

--- src/SFA.DAS.Payments.ScheduledJobs.UnitTests/Monitoring/ApprenticeshipData/ApprenticeshipDataServiceTests.cs
+++ src/SFA.DAS.Payments.ScheduledJobs.UnitTests/Monitoring/ApprenticeshipData/ApprenticeshipDataServiceTests.cs
@@ -9,7 +9,6 @@ using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
 using SFA.DAS.Payments.Application.Repositories;
 using SFA.DAS.Payments.Model.Core.Entities;
 using SFA.DAS.Payments.ScheduledJobs.Monitoring.ApprenticeshipData;
-using ApprenticeshipModel = SFA.DAS.Payments.ScheduledJobs.Monitoring.ApprenticeshipData.ApprenticeshipModel;
 
 namespace SFA.DAS.Payments.ScheduledJobs.UnitTests.Monitoring.ApprenticeshipData
 {
@@ -17,7 +16,6 @@ namespace SFA.DAS.Payments.ScheduledJobs.UnitTests.Monitoring.ApprenticeshipData
     public class ApprenticeshipDataServiceTests
     {
         private IPaymentsDataContext paymentsDataContext;
-        private ICommitmentsApiClient commitmentsDataContext;
         private Mock<ITelemetry> telemetry;
         private ApprenticeshipDataService service;
 
@@ -28,41 +26,25 @@ namespace SFA.DAS.Payments.ScheduledJobs.UnitTests.Monitoring.ApprenticeshipData
         {
             telemetry = new Mock<ITelemetry>();
 
-            commitmentsDataContext = new CommitmentsDataContext(new DbContextOptionsBuilder<CommitmentsDataContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .Options);
+            var commitmentStats = new CommitmentStatsDto
+            {
+                Approved = 3,
+                Paused = 1,
+                Stopped = 3
+            };
+
+            var commitmentsApiClient = new Mock<ICommitmentsApiClient>();
+
+            commitmentsApiClient.Setup(x => x.GetStats(It.IsAny<int>())).ReturnsAsync(commitmentStats);
 
             paymentsDataContext = new PaymentsDataContext(new DbContextOptionsBuilder<PaymentsDataContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options);
 
-            var commitmentsDataContextFactory = new Mock<ICommitmentsDataContextFactory>();
-            commitmentsDataContextFactory.Setup(c => c.Create())
-                .Returns(commitmentsDataContext);
-
             var paymentsDataContextFactory = new Mock<IPaymentsDataContextFactory>();
             paymentsDataContextFactory.Setup(c => c.Create())
                 .Returns(paymentsDataContext);
 
-
-            var dasApprenticeships = new List<ApprenticeshipModel>
-            {
-                new ApprenticeshipModel { IsApproved = true, Commitment = new Commitment { EmployerAndProviderApprovedOn = DateTime.Now, Approvals = 3} },
-                new ApprenticeshipModel { IsApproved = false, Commitment = new Commitment { EmployerAndProviderApprovedOn = DateTime.Now, Approvals = 3} }, //assert we are using the correct new logic based on query in PV2-2215
-                new ApprenticeshipModel { IsApproved = true, Commitment = new Commitment { EmployerAndProviderApprovedOn = DateTime.Now, Approvals = 7} }, //assert we include transfers
-
-                new ApprenticeshipModel { PaymentStatus = PaymentStatus.Withdrawn, StopDate = DateTime.Now, Commitment = new Commitment { EmployerAndProviderApprovedOn = DateTime.Now } }, //expected in stopped count
-                new ApprenticeshipModel { PaymentStatus = PaymentStatus.Withdrawn, StopDate = DateTime.Now, Commitment = new Commitment { EmployerAndProviderApprovedOn = DateTime.Now } }, //expected in stopped count
-                new ApprenticeshipModel { PaymentStatus = PaymentStatus.Withdrawn, StopDate = DateTime.Now.AddDays(10), Commitment = new Commitment { EmployerAndProviderApprovedOn = DateTime.Now } }, //expected in stopped count
-                new ApprenticeshipModel { PaymentStatus = PaymentStatus.Withdrawn, StopDate = DateTime.Now.AddDays(-31), Commitment = new Commitment { EmployerAndProviderApprovedOn = DateTime.Now } }, //not expected in stopped count (stop date too old)
-                new ApprenticeshipModel { PaymentStatus = PaymentStatus.Withdrawn, Commitment = new Commitment { EmployerAndProviderApprovedOn = DateTime.Now } }, //not expected in stopped count (null stop date)
-                new ApprenticeshipModel { PaymentStatus = PaymentStatus.Completed, StopDate = DateTime.Now, Commitment = new Commitment { EmployerAndProviderApprovedOn = DateTime.Now } }, //not expected in stopped count (wrong status even though stop date is now)
-
-                new ApprenticeshipModel { PaymentStatus = PaymentStatus.Paused, IsApproved = true, PauseDate = DateTime.Now },
-                new ApprenticeshipModel { PaymentStatus = PaymentStatus.Withdrawn, IsApproved = true, PauseDate = DateTime.Now },
-                new ApprenticeshipModel { PaymentStatus = PaymentStatus.Paused, IsApproved = false, PauseDate = DateTime.Now },
-                new ApprenticeshipModel { PaymentStatus = PaymentStatus.Paused, IsApproved = true, PauseDate = DateTime.Now.AddDays(-31) },
-            };
 
             var paymentsApprenticeships = new List<Model.Core.Entities.ApprenticeshipModel>
             {
@@ -82,12 +64,10 @@ namespace SFA.DAS.Payments.ScheduledJobs.UnitTests.Monitoring.ApprenticeshipData
                 new Model.Core.Entities.ApprenticeshipModel { Status = ApprenticeshipStatus.Inactive, ApprenticeshipPauses = new List<ApprenticeshipPauseModel>{ new ApprenticeshipPauseModel{ PauseDate = DateTime.Now, ResumeDate = null } }}
             };
 
-            commitmentsDataContext.Apprenticeship.AddRange(dasApprenticeships);
-            commitmentsDataContext.SaveChanges();
             paymentsDataContext.Apprenticeship.AddRange(paymentsApprenticeships);
             paymentsDataContext.SaveChanges();
 
-            service = new ApprenticeshipDataService(paymentsDataContextFactory.Object, commitmentsDataContextFactory.Object, telemetry.Object);
+            service = new ApprenticeshipDataService(paymentsDataContextFactory.Object, commitmentsApiClient.Object, telemetry.Object);
         }
 
         [Test]
