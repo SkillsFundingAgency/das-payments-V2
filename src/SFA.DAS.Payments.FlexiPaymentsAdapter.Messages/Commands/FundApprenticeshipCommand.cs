43398e5f50f266cde8943aeb84fdb770b7806793
Tom <Tom>
2022-05-10 13:11:26 +0100

    WIP

---
+++ src/SFA.DAS.Payments.FlexiPaymentsAdapter.Messages/Commands/FundApprenticeshipCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.Payments.FlexiPaymentsAdapter.Messages.Commands
+{
+    //NB this may be an event owned by the Apprenticeship Payments Service but for the spike this will do
+    public class FundApprenticeshipCommand
+    {
+    }
+}
---
+++ src/SFA.DAS.Payments.FlexiPaymentsAdapter.Messages/Events/FlexiPaymentsReadyForProcessingEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFA.DAS.Payments.Model.Core;
+
+namespace SFA.DAS.Payments.FlexiPaymentsAdapter.Messages.Events
+{
+    public class FlexiPaymentsReadyForProcessingEvent
+    {
+        public long EmployerAccountId { get; set; }
+        public long JobId { get; set; }
+        public CollectionPeriod CollectionPeriod { get; set; }
+    }
+}
