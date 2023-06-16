9917d78b506b85d54400d62a1ddfd0dc03b569ed
Tom Gough <tom.gough@talentconsulting.uk>
2023-06-16 09:32:43 +0100

    Added option to publish event in line with the one output via unit tests from the apprenticeship funding payments component

--- src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/FLP_347_Spike_Steps.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.EndToEnd/Steps/FLP_347_Spike_Steps.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -25,8 +26,8 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
         private const string CommitmentIdentifier = "FA-347";
         private CollectionPeriod CollectionPeriod = new CollectionPeriod
         {
-            AcademicYear = 2324,
-            Period = 1
+            AcademicYear = 2223,
+            Period = 5
         };
 
         private long AccountId = 21063; //max levy balance employer
@@ -37,7 +38,7 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
             try
             {
                 var calculatedRequiredLevyAmount = CalculatedRequiredLevyAmountBuilder
-                    .BuildDefaultEvent(PriceEpisodeIdentifier, AccountId, CollectionPeriod)
+                    .BuildRealisticEventFromPaymentsUnitTest(AccountId, CollectionPeriod)
                     .AddSubmissionDate();
 
                 var options = new PublishOptions();
@@ -124,6 +125,59 @@ namespace SFA.DAS.Payments.AcceptanceTests.EndToEnd.Steps
                 //IlrSubmissionDateTime = new DateTime(2022, 8, 1), //
                 //IlrFileName = "Run 2", //
                 CollectionPeriod = new CollectionPeriod { AcademicYear = collectionPeriod.AcademicYear, Period = collectionPeriod.Period }
+            };
+        }
+
+        public static CalculatedRequiredLevyAmount BuildRealisticEventFromPaymentsUnitTest(long? accountId, CollectionPeriod collectionPeriod)
+        {
+            return new CalculatedRequiredLevyAmount
+            {
+                //EarningSource = "SubmitLearnerDataFundingPlatform",
+                Priority = 0,
+                AgreementId = null,
+                AgreedOnDate = null,
+                SfaContributionPercentage = 0.95m,
+                OnProgrammeEarningType = OnProgrammeEarningType.Learning,
+                EarningEventId = Guid.NewGuid(),
+                ClawbackSourcePaymentEventId = null,
+                PriceEpisodeIdentifier = "",
+                AmountDue = 1000,
+                DeliveryPeriod = 5,
+                AccountId = accountId,
+                TransferSenderAccountId = accountId,
+                ContractType = ContractType.Act1,
+                StartDate = new DateTime(2023, 03, 11, 08, 05, 12),
+                PlannedEndDate = new DateTime(2024, 03, 29, 01, 18, 32),
+                ActualEndDate = new DateTime(2024, 01, 30, 08, 29, 40),
+                CompletionStatus = 1,
+                CompletionAmount = 0,
+                InstalmentAmount = 1000,
+                NumberOfInstalments = 10,
+                LearningStartDate = new DateTime(2023, 03, 11, 08, 05, 12),
+                ApprenticeshipId = 115,
+                ApprenticeshipPriceEpisodeId = 5,
+                ApprenticeshipEmployerType = ApprenticeshipEmployerType.NonLevy,
+                ReportingAimFundingLineType = "",
+                LearningAimSequenceNumber = 0,
+                JobId = 0,
+                EventTime = DateTimeOffset.ParseExact("15/06/2023 10:12:31 +00:00", "dd/MM/yyyy HH:mm:ss zzz", CultureInfo.InvariantCulture),
+                EventId = Guid.Parse("b4eaea80-968e-4acf-8c91-d6f438c15c97"),
+                Ukprn = 193,
+                Learner = new Model.Core.Learner { ReferenceNumber = "2349874", Uln = 2304897 },
+                LearningAim = new LearningAim
+                {
+                    Reference = "AIM123",
+                    ProgrammeType = 1,
+                    StandardCode = 1234,
+                    FrameworkCode = 5678,
+                    PathwayCode = 9012,
+                    FundingLineType = "16-18 Apprenticeship (From May 2017) Levy Contract",
+                    SequenceNumber = 1,
+                    StartDate = new DateTime(2022, 8, 1)
+                },
+                IlrSubmissionDateTime = new DateTime(),
+                IlrFileName = null,
+                CollectionPeriod = collectionPeriod
             };
         }
 
