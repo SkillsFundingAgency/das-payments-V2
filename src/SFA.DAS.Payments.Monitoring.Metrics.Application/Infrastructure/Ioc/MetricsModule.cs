2d40908b4f81df84fc13213741e147115d61cfc1
Owen Campbell <owen@owencampbell.co.uk>
2023-07-10 13:45:32 +0100

    Added in new DcEarnings2324DataContext registered context

--- src/SFA.DAS.Payments.Monitoring.Metrics.Application/Infrastructure/Ioc/MetricsModule.cs
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Application/Infrastructure/Ioc/MetricsModule.cs
@@ -18,6 +18,20 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Application.Infrastructure.Ioc
                 .As<ISubmissionMetricsService>()
                 .InstancePerLifetimeScope();
 
+
+            builder.Register((c, p) =>
+                {
+                    var configHelper = c.Resolve<IConfigurationHelper>();
+
+                    var dbContextOptions = new DbContextOptionsBuilder().UseSqlServer(
+                        configHelper.GetConnectionString("DcEarnings2324ConnectionString"),
+                        optionsBuilder => optionsBuilder.CommandTimeout(270)).Options;
+
+                    return new DcMetricsDataContext(dbContextOptions);
+                })
+                .Named<IDcMetricsDataContext>("DcEarnings2324DataContext")
+                .InstancePerLifetimeScope();
+
             builder.Register((c, p) =>
             {
                 var configHelper = c.Resolve<IConfigurationHelper>();
--- src/SFA.DAS.Payments.Monitoring.Metrics.Function/Infrastructure/IoC/Modules/FunctionsModule.cs
+++ src/SFA.DAS.Payments.Monitoring.Metrics.Function/Infrastructure/IoC/Modules/FunctionsModule.cs
@@ -1,4 +1,4 @@
-﻿using Autofac;
+using Autofac;
 using Microsoft.EntityFrameworkCore;
 using SFA.DAS.Payments.Core.Configuration;
 using SFA.DAS.Payments.Monitoring.Metrics.Application.PeriodEnd;
@@ -57,6 +57,19 @@ namespace SFA.DAS.Payments.Monitoring.Metrics.Function.Infrastructure.IoC.Module
 
                     var dbContextOptions = new DbContextOptionsBuilder().UseSqlServer(
                         configHelper.GetConnectionString("DcEarnings2324ConnectionString"),
+                        optionsBuilder => optionsBuilder.CommandTimeout(270)).Options;
+
+                    return new DcMetricsDataContext(dbContextOptions);
+                })
+                .Named<IDcMetricsDataContext>("DcEarnings2324DataContext")
+                .InstancePerDependency();
+
+            builder.Register((c, p) =>
+                {
+                    var configHelper = c.Resolve<IConfigurationHelper>();
+
+                    var dbContextOptions = new DbContextOptionsBuilder().UseSqlServer(
+                        configHelper.GetConnectionString("DcEarnings2223ConnectionString"),
                         optionsBuilder => optionsBuilder.CommandTimeout(270)).Options;
 
                     return new DcMetricsDataContext(dbContextOptions);
