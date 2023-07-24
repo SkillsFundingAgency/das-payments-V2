f95cd20471827e477f39502f77d8b84df2536da0
abroodie <abroodie@outlook.com>
2023-07-24 09:06:11 +0100

    updating batch message receiver to target latest version of ASB nuget

--- src/SFA.DAS.Payments.ServiceFabric.Core/BatchMessageReceiver.cs
+++ src/SFA.DAS.Payments.ServiceFabric.Core/BatchMessageReceiver.cs
@@ -4,26 +4,32 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Azure.Core.Pipeline;
-using Microsoft.Azure.ServiceBus;
-using Microsoft.Azure.ServiceBus.Core;
 using Azure.Messaging.ServiceBus;
 
 namespace SFA.DAS.Payments.ServiceFabric.Core
 {
     public class BatchMessageReceiver
     {
-        private readonly MessageReceiver messageReceiver;
+        private readonly ServiceBusReceiver messageReceiver;
         private readonly List<ServiceBusReceivedMessage> messages;
-        public BatchMessageReceiver(ServiceBusConnection connection, string endpointName)
+
+        public BatchMessageReceiver(ServiceBusClient client, string endpointName)
         {
-            if (connection == null) throw new ArgumentNullException(nameof(connection));
+            if (client == null) throw new ArgumentNullException(nameof(client));
             if (endpointName == null) throw new ArgumentNullException(nameof(endpointName));
-            var r = new Azure.Messaging.ServiceBus.ServiceBusReceiver()
-            messageReceiver = new MessageReceiver(connection, endpointName, ReceiveMode.PeekLock,
-                RetryPolicy.Default, 0);
-            messages = new List<Message>();
+            messageReceiver = client.CreateReceiver(endpointName);
+            messages = new List<ServiceBusReceivedMessage>();
         }
+
+        //public BatchMessageReceiver(ServiceBusConnection connection, string endpointName)
+        //{
+        //    if (connection == null) throw new ArgumentNullException(nameof(connection));
+        //    if (endpointName == null) throw new ArgumentNullException(nameof(endpointName));
+        //    var r = new Azure.Messaging.ServiceBus.ServiceBusReceiver();
+        //    messageReceiver = new MessageReceiver(connection, endpointName, ReceiveMode.PeekLock,
+        //        RetryPolicy.Default, 0);
+        //    messages = new List<Message>();
+        //}
 
         public async Task<ReadOnlyCollection<ServiceBusReceivedMessage>> ReceiveMessages(int batchSize, CancellationToken cancellationToken)
         {
@@ -31,7 +37,7 @@ namespace SFA.DAS.Payments.ServiceFabric.Core
             for (var i = 0; i < 10 && messages.Count <= batchSize; i++)
             {
                 cancellationToken.ThrowIfCancellationRequested();
-                var receivedMessages = await messageReceiver.ReceiveAsync(batchSize, TimeSpan.FromMilliseconds(500))
+                var receivedMessages = await messageReceiver.ReceiveMessagesAsync(batchSize, TimeSpan.FromMilliseconds(500),cancellationToken)
                     .ConfigureAwait(false);
                 if (receivedMessages == null || !receivedMessages.Any())
                     break;
@@ -40,39 +46,52 @@ namespace SFA.DAS.Payments.ServiceFabric.Core
             return messages.AsReadOnly();
         }
 
-        public async Task Complete(IEnumerable<string> lockTokens)
+        //public async Task Complete(IEnumerable<string> lockTokens)
+        //{
+        //    //await messageReceiver.CompleteAsync(lockTokens).ConfigureAwait(false);
+        //}
+
+        //public async Task Complete(string lockToken)
+        //{
+        //    //await messageReceiver.CompleteAsync(lockToken);
+        //}
+
+        public async Task Complete(ServiceBusReceivedMessage message, CancellationToken cancellationToken)
         {
-            await messageReceiver.CompleteAsync(lockTokens).ConfigureAwait(false);
+            await messageReceiver.CompleteMessageAsync(message, cancellationToken).ConfigureAwait(false);
+        }
+        
+        public async Task Abandon(ServiceBusReceivedMessage message, CancellationToken cancellationToken)
+        {
+            await messageReceiver.AbandonMessageAsync(message, null, cancellationToken).ConfigureAwait(false);
         }
 
-        public async Task Complete(string lockToken)
+        //public async Task Abandon(IList<string> lockTokens)
+        //{
+        //    //await Task.WhenAll(lockTokens.Select(token => messageReceiver.AbandonAsync(token)))
+        //    //    .ConfigureAwait(false);
+        //}
+
+        //public async Task Abandon(string lockToken)
+        //{
+        //    //await messageReceiver.AbandonAsync(lockToken);
+        //}
+
+        public async Task DeadLetter(string lockToken, CancellationToken cancellationToken)
         {
-            await messageReceiver.CompleteAsync(lockToken);
+            var failedMessage = messages.FirstOrDefault(msg => msg.LockToken == lockToken)
+                                ?? throw new InvalidOperationException($"Cannot move the message to the dead letter queue, Message not found in list of received messages");
+            await DeadLetter(failedMessage, cancellationToken).ConfigureAwait(false);
         }
 
-        public async Task Abandon(IList<string> lockTokens)
+        public async Task DeadLetter(ServiceBusReceivedMessage failedMessage, CancellationToken cancellationToken)
         {
-            await Task.WhenAll(lockTokens.Select(token => messageReceiver.AbandonAsync(token)))
-                .ConfigureAwait(false);
-        }
-
-        public async Task Abandon(string lockToken)
-        {
-            await messageReceiver.AbandonAsync(lockToken);
-        }
-
-        public async Task DeadLetter(ServiceBusReceivedMessage message)
-        {
-            var failedMessage =
-                messages.FirstOrDefault(msg => msg.LockToken == message.LockToken);
-            if (failedMessage == null)
-                throw new InvalidOperationException($"Cannot move the message to the dead letter queue Message not found in list of received messages");
-            await messageReceiver.DeadLetterAsync(failedMessage.LockToken).ConfigureAwait(false);
+            await messageReceiver.DeadLetterMessageAsync(failedMessage, null, cancellationToken).ConfigureAwait(false);
         }
 
         public async Task Close()
         {
-            if (!messageReceiver.IsClosedOrClosing)
+            if (!messageReceiver.IsClosed)
                 await messageReceiver.CloseAsync().ConfigureAwait(false);
         }
     }
--- src/SFA.DAS.Payments.ServiceFabric.Core/MessageDeserializer.cs
+++ src/SFA.DAS.Payments.ServiceFabric.Core/MessageDeserializer.cs
@@ -4,8 +4,7 @@ using System.Reflection;
 using System.Text;
 using System.Xml;
 using Autofac;
-using Microsoft.Azure.ServiceBus;
-using Microsoft.Azure.ServiceBus.InteropExtensions;
+using Azure.Messaging.ServiceBus;
 using Newtonsoft.Json;
 
 namespace SFA.DAS.Payments.ServiceFabric.Core
@@ -18,44 +17,51 @@ namespace SFA.DAS.Payments.ServiceFabric.Core
         }
 
 
-        public object DeserializeMessage(Message message)
+        public object DeserializeMessage(ServiceBusReceivedMessage message)
         {
-            if (!message.UserProperties.ContainsKey(NServiceBus.Headers.EnclosedMessageTypes))
-                throw new InvalidOperationException($"Cannot deserialise the message, no 'enclosed message types' header was found. Message id: {message.MessageId}, label: {message.Label}");
-            var enclosedTypes = (string)message.UserProperties[NServiceBus.Headers.EnclosedMessageTypes];
+            if (!message.ApplicationProperties.ContainsKey(NServiceBus.Headers.EnclosedMessageTypes))
+                throw new InvalidOperationException($"Cannot deserialise the message, no 'enclosed message types' header was found. Message id: {message.MessageId}");
+            var enclosedTypes = (string)message.ApplicationProperties[NServiceBus.Headers.EnclosedMessageTypes];
             var typeName = enclosedTypes.Split(';').FirstOrDefault();
             if (string.IsNullOrEmpty(typeName))
-                throw new InvalidOperationException($"Message type not found when trying to deserialise the message.  Message id: {message.MessageId}, label: {message.Label}");
+                throw new InvalidOperationException($"Message type not found when trying to deserialise the message.  Message id: {message.MessageId}");
             var messageType = Type.GetType(typeName, assemblyName => { assemblyName.Version = null; return Assembly.Load(assemblyName); }, null);
             var sanitisedMessageJson = GetMessagePayload(message);
             var deserialisedMessage = JsonConvert.DeserializeObject(sanitisedMessageJson, messageType);
             return deserialisedMessage;
         }
 
-        private string GetMessagePayload(Message receivedMessage)
+        private string GetMessagePayload(ServiceBusReceivedMessage receivedMessage)
         {
             const string transportEncodingHeaderKey = "NServiceBus.Transport.Encoding";
-            var transportEncoding = receivedMessage.UserProperties.ContainsKey(transportEncodingHeaderKey)
-                ? (string)receivedMessage.UserProperties[transportEncodingHeaderKey]
+            var transportEncoding = receivedMessage.ApplicationProperties.ContainsKey(transportEncodingHeaderKey)
+                ? (string)receivedMessage.ApplicationProperties[transportEncodingHeaderKey]
                 : "application/octet-stream";
-            byte[] messageBody;
-            if (transportEncoding.Equals("wcf/byte-array", StringComparison.OrdinalIgnoreCase))
-            {
-                var doc = receivedMessage.GetBody<XmlElement>();
-                messageBody = Convert.FromBase64String(doc.InnerText);
-            }
-            else
-                messageBody = receivedMessage.Body;
 
-            var monitoringMessageJson = Encoding.UTF8.GetString(messageBody);
-            var sanitisedMessageJson = monitoringMessageJson
+            if (transportEncoding.Equals("wcf/byte-array", StringComparison.OrdinalIgnoreCase))
+                throw new InvalidOperationException("Messages serialized using Xml are not supported for POC");
+
+            var sanitisedMessageJson = receivedMessage.ToString()
                 .Trim(Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble())
                     .ToCharArray());
             return sanitisedMessageJson;
+
+            //byte[] messageBody;
+            //if (transportEncoding.Equals("wcf/byte-array", StringComparison.OrdinalIgnoreCase))
+            //{
+            //    throw new InvalidOperationException("Legacy Xml messages not supported for ")
+                 
+            //    //var doc = receivedMessage.GetBody<XmlElement>();
+            //    //messageBody = Convert.FromBase64String(doc.InnerText);
+            //}
+            //else
+            //    messageBody = receivedMessage.Body;
+
+            //var monitoringMessageJson = Encoding.UTF8.GetString(messageBody);
+            //var sanitisedMessageJson = monitoringMessageJson
+            //    .Trim(Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble())
+            //        .ToCharArray());
+            //return sanitisedMessageJson;
         }
     }
-
-    
-
-    //public class Default
 }
