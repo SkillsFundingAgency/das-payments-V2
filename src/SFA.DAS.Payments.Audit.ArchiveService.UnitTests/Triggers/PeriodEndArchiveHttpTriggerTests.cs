013726b8d7081e9a19b261af5287ad93508c9ea3
Liam Wright <mail@liam-wright.co.uk>
2023-09-08 11:26:51 +0100

    Additional tests added

--- src/SFA.DAS.Payments.Audit.ArchiveService.UnitTests/Triggers/PeriodEndArchiveHttpTriggerTests.cs
+++ src/SFA.DAS.Payments.Audit.ArchiveService.UnitTests/Triggers/PeriodEndArchiveHttpTriggerTests.cs
@@ -5,8 +5,6 @@ using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
-using SFA.DAS.Payments.Audit.ArchiveService.Helpers;
-using SFA.DAS.Payments.Audit.ArchiveService.Orchestrators;
 using SFA.DAS.Payments.Audit.ArchiveService.Triggers;
 
 namespace SFA.DAS.Payments.Audit.ArchiveService.UnitTests.Triggers;
@@ -21,29 +19,21 @@ public class PeriodEndArchiveHttpTriggerTests
         mockOrchestrationClient = mocker.Mock<IDurableOrchestrationClient>();
         mockEntityClient = mocker.Mock<IDurableEntityClient>();
         logger = mocker.Mock<IPaymentLogger>();
-        triggerHelper = mocker.Mock<ITriggerHelper>();
     }
 
     private Mock<IPaymentLogger> logger;
     private Mock<IDurableEntityClient> mockEntityClient;
     private AutoMock mocker;
     private Mock<IDurableOrchestrationClient> mockOrchestrationClient;
-    private Mock<ITriggerHelper> triggerHelper;
 
     [Test]
     public async Task WhenHttpTrigger_ReceivesPostRequest_ThenOrchestratorIsStarted()
     {
         var req = new HttpRequestMessage { Method = HttpMethod.Post };
 
-        triggerHelper.Setup(x => x.GetRunningInstances(nameof(PeriodEndArchiveHttpTrigger),
-                nameof(PeriodEndArchiveOrchestrator), mockOrchestrationClient.Object, logger.Object))
-            .ReturnsAsync(new OrchestrationStatusQueryResult());
+        SetupRunningInstances(null);
+        SetupStartOrchestration("1234", new HttpResponseMessage(HttpStatusCode.Accepted));
 
-        mockOrchestrationClient.Setup(x => x.StartNewAsync(It.IsAny<string>(), It.IsAny<string>()))
-            .ReturnsAsync("1234");
-        mockOrchestrationClient
-            .Setup(x => x.CreateCheckStatusResponse(It.IsAny<HttpRequestMessage>(), It.IsAny<string>(), false))
-            .Returns(new HttpResponseMessage(HttpStatusCode.Accepted));
         var response = await PeriodEndArchiveHttpTrigger.HttpStart(req, mockOrchestrationClient.Object,
             mockEntityClient.Object, logger.Object);
         var content = await response.Content.ReadAsStringAsync();
@@ -51,5 +41,45 @@ public class PeriodEndArchiveHttpTriggerTests
         response.Should().NotBeNull();
         response.StatusCode.Should().Be(HttpStatusCode.Accepted);
         content.Should().Be("Started orchestrator [PeriodEndArchiveOrchestrator] with ID [1234]\n\n\n\n");
+    }
+
+    [Test]
+    public async Task WhenHttpTrigger_ReceivesPostRequest_AndInstancesAlreadyExist_ThenOrchestratorIsNotStarted()
+    {
+        var req = new HttpRequestMessage { Method = HttpMethod.Post };
+
+        var orchestrationResult = new OrchestrationStatusQueryResult
+        {
+            DurableOrchestrationState = new List<DurableOrchestrationStatus>
+                { new() { CreatedTime = DateTime.Now, Name = "Instance01" } }
+        };
+
+        SetupStartOrchestration("1234", new HttpResponseMessage(HttpStatusCode.Accepted));
+        SetupRunningInstances(orchestrationResult);
+
+        var response = await PeriodEndArchiveHttpTrigger.HttpStart(req, mockOrchestrationClient.Object,
+            mockEntityClient.Object, logger.Object);
+        var content = await response.Content.ReadAsStringAsync();
+
+        response.Should().NotBeNull();
+        response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+        content.Should().Be("An instance of PeriodEndArchiveOrchestrator is already running.");
+    }
+
+    public void SetupStartOrchestration(string runId, HttpResponseMessage message)
+    {
+        mockOrchestrationClient.Setup(x => x.StartNewAsync(It.IsAny<string>(), It.IsAny<string>()))
+            .ReturnsAsync(runId);
+        mockOrchestrationClient
+            .Setup(x => x.CreateCheckStatusResponse(It.IsAny<HttpRequestMessage>(), It.IsAny<string>(), false))
+            .Returns(message);
+    }
+
+    public void SetupRunningInstances(OrchestrationStatusQueryResult? queryResult)
+    {
+        mockOrchestrationClient
+            .Setup(x => x.ListInstancesAsync(It.IsAny<OrchestrationStatusQueryCondition>(), CancellationToken.None))
+            .ReturnsAsync(queryResult
+            );
     }
 }
