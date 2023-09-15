6e3e5814e09d9e7928881c2c842e72eb550aa435
Liam Wright <mail@liam-wright.co.uk>
2023-09-15 09:29:01 +0100

    Fixed GET call to ArchivingFunction

--- src/SFA.DAS.Payments.Audit.ArchiveService/Triggers/PeriodEndArchiveHttpTrigger.cs
+++ src/SFA.DAS.Payments.Audit.ArchiveService/Triggers/PeriodEndArchiveHttpTrigger.cs
@@ -1,10 +1,12 @@
 ﻿using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using AzureFunctions.Autofac;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.Audit.ArchiveService.Extensions;
@@ -29,12 +31,18 @@ public static class PeriodEndArchiveHttpTrigger
         if (req.Method == HttpMethod.Post)
         {
             ITriggerHelper triggerHelper = new TriggerHelper();
-            return await triggerHelper.StartOrchestrator(req, starter, log, nameof(PeriodEndArchiveOrchestrator), nameof(PeriodEndArchiveHttpTrigger));
+            return await triggerHelper.StartOrchestrator(req, starter, log, nameof(PeriodEndArchiveOrchestrator),
+                nameof(PeriodEndArchiveHttpTrigger));
         }
 
-        var entityId = new EntityId(nameof(HandleCurrentJobId.PeriodEndArchiveEntityName),
-            HandleCurrentJobId.PeriodEndArchiveEntityName);
+        var entityId = new EntityId(nameof(HandleCurrentJobId.Handle), HandleCurrentJobId.PeriodEndArchiveEntityName);
         var stateResponse = await client.ReadEntityStateAsync<JObject>(entityId);
+        return new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent(JsonConvert.SerializeObject(stateResponse), Encoding.UTF8, "application/json")
+        };
+        var response = req.CreateResponse(HttpStatusCode.OK, "{}");
+        return response;
         return req.CreateResponse(HttpStatusCode.OK, stateResponse.EntityState);
     }
 }
