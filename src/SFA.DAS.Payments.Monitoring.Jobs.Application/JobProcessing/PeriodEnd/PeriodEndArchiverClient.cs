f80ac6abb368474a74134db654097edc87926e46
Owen Campbell <ocampbell.personal@gmail.com>
2023-09-19 12:25:06 +0100

    Archive Start call

--- src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/PeriodEnd/PeriodEndArchiverClient.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/PeriodEnd/PeriodEndArchiverClient.cs
@@ -2,12 +2,14 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
 
 namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.PeriodEnd
 {
     public interface IPeriodEndArchiverClient
     {
-        Task<bool> ArchiveStatus();
+        Task StartArchive();
+        Task<string> ArchiveStatus();
     }
 
     public class EntityState
@@ -22,32 +24,42 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.PeriodEnd
         public EntityState EntityState { get; set; }
     }
 
+
+
     public class PeriodEndArchiverClient : IPeriodEndArchiverClient
     {
         private readonly string authCode;
+        private readonly ITelemetry telemetry;
         private readonly Uri functionAddressUri;
 
         public PeriodEndArchiverClient(string authCode, string functionAddress)
         {
             this.authCode = authCode;
+            this.telemetry = telemetry;
             functionAddressUri = new Uri(functionAddress);
         }
 
-        public async Task<bool> ArchiveStatus()
+        public async Task StartArchive()
+        {
+            await new HttpClient { Timeout = TimeSpan.FromSeconds(270) }.PostAsync(BuildUriFromParameters(), null);
+        }
+
+        public async Task<string> ArchiveStatus()
         {
             var result = await new HttpClient { Timeout = TimeSpan.FromSeconds(270) }.GetAsync(BuildUriFromParameters());
 
-            if (!result.IsSuccessStatusCode) return false;
+            if (!result.IsSuccessStatusCode) return "Failed";
 
             var content = await result.Content.ReadAsStringAsync();
 
             var periodEndArchiverStatusSummary = JsonConvert.DeserializeObject<PeriodEndArchiverStatusSummary>(content);
-            return periodEndArchiverStatusSummary.EntityState.Status == "Succeeded";
+            return periodEndArchiverStatusSummary.EntityState.Status;
         }
 
         private string BuildUriFromParameters()
         {
             return $"{new Uri(functionAddressUri, "/orchestrators/PeriodEndArchiveOrchestrator")}";
         }
+
     }
 }
--- src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Handlers/PeriodEnd/RecordPeriodEndFCSHandOverCompleteJobHandler.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Handlers/PeriodEnd/RecordPeriodEndFCSHandOverCompleteJobHandler.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
 using SFA.DAS.Payments.Application.Messaging;
 using SFA.DAS.Payments.Core;
+using SFA.DAS.Payments.Monitoring.Jobs.Application;
 using SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing;
 using SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.PeriodEnd;
 using SFA.DAS.Payments.Monitoring.Jobs.Messages.Commands;
@@ -19,18 +22,27 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.Handlers.PeriodEnd
         private readonly IPaymentLogger logger;
         private readonly IPeriodEndJobService periodEndJobService;
         private readonly IPeriodEndArchiverClient periodEndArchiverClient;
+        private readonly IJobStorageService jobStorageService;
+        private readonly ITelemetry telemetry;
 
-        public RecordPeriodEndFcsHandOverCompleteJobHandler(IPaymentLogger logger,
+
+        public RecordPeriodEndFcsHandOverCompleteJobHandler(
+            IPaymentLogger logger,
             IPeriodEndJobService periodEndJobService,
             IGenericPeriodEndJobStatusManager jobStatusManager,
-            IPeriodEndArchiverClient periodEndArchiverClient)
+            IPeriodEndArchiverClient periodEndArchiverClient,
+            IJobStorageService jobStorageService,
+            ITelemetry telemetry)
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this.periodEndJobService =
                 periodEndJobService ?? throw new ArgumentNullException(nameof(periodEndJobService));
             this.jobStatusManager = jobStatusManager ?? throw new ArgumentNullException(nameof(jobStatusManager));
+            this.jobStorageService = jobStorageService ?? throw new ArgumentNullException(nameof(jobStorageService));
             this.periodEndArchiverClient = periodEndArchiverClient;
             this.periodEndArchiverClient = periodEndArchiverClient;
+            this.jobStorageService = jobStorageService;
+            this.telemetry = telemetry ?? throw new ArgumentNullException(nameof(telemetry));
         }
 
         public async Task Handle(IList<RecordPeriodEndFcsHandOverCompleteJob> messages,
@@ -38,19 +50,60 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.Handlers.PeriodEnd
         {
             foreach (var message in messages)
             {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+
                 logger.LogInfo($"Handling period end Fcs handover job: {message.ToJson()}");
                 await periodEndJobService.RecordPeriodEndJob(message, cancellationToken);
                 jobStatusManager.StartMonitoringJob(message.JobId, JobType.PeriodEndFcsHandOverCompleteJob);
 
-                // Poll Archive Status 
-                var archiveFinished = false;
-                while (!archiveFinished)
-                {
-                    archiveFinished = await periodEndArchiverClient.ArchiveStatus();
-                }
+                await periodEndArchiverClient.StartArchive();
 
-                logger.LogInfo($"Handled period end Fcs handover job: {message.JobId}");
+                // Poll Archive Status 
+                var jobStatus = JobStatus.InProgress;
+
+                while (jobStatus == JobStatus.InProgress)
+                {
+                    var response = await periodEndArchiverClient.ArchiveStatus();
+
+                    jobStatus = response switch
+                    {
+                        "Started" => JobStatus.InProgress,
+                        "InProgress" => JobStatus.InProgress,
+                        "Queued" => JobStatus.InProgress,
+                        "Failed" => JobStatus.DcTasksFailed,
+                        "Succeeded" => JobStatus.Completed,
+                        _ => jobStatus
+                    };
+
+                    await jobStorageService.SaveJobStatus(message.JobId, jobStatus, DateTimeOffset.Now, cancellationToken);
+                    Thread.Sleep(TimeSpan.FromMinutes(10));
+                }
+                stopwatch.Stop();
+
+                logger.LogInfo($"Handling period end Fcs handover job: {message.JobId}");
+                SendTelemetry(message, jobStatus, stopwatch.Elapsed);
+
             }
+        }
+
+        private void SendTelemetry(RecordPeriodEndFcsHandOverCompleteJob job, JobStatus jobStatus, TimeSpan elapsedTime)
+        {
+            var properties = new Dictionary<string, string>
+            {
+                { TelemetryKeys.JobId, job.JobId.ToString()},
+                { TelemetryKeys.JobType, nameof(RecordPeriodEndRequestReportsJob)},
+                { TelemetryKeys.CollectionPeriod, job.CollectionPeriod.ToString()},
+                { TelemetryKeys.AcademicYear, job.CollectionYear.ToString()},
+                { TelemetryKeys.Status, jobStatus.ToString("G")}
+            };
+
+            var metrics = new Dictionary<string, double>
+            {
+                { TelemetryKeys.Duration, elapsedTime.TotalMilliseconds},
+            };
+
+            telemetry.TrackEvent("Finished Job", properties, metrics);
         }
     }
 }
