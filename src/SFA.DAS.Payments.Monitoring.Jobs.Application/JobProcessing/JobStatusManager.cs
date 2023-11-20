e7ab92825e504fe1d10acef0a79004ab2273b7d3
Liam Wright <mail@liam-wright.co.uk>
2023-11-20 14:54:40 +0000

    PV2-3070 - Added TryCatch to JobStatusManager loop to reveal ILR errors

--- src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/JobStatusManager.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Application/JobProcessing/JobStatusManager.cs
@@ -19,12 +19,14 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing
 
     public abstract class JobStatusManager : IJobStatusManager
     {
+        private readonly ConcurrentDictionary<long, bool> currentJobs;
+        private readonly TimeSpan interval;
         private readonly IPaymentLogger logger;
         private readonly IUnitOfWorkScopeFactory scopeFactory;
-        private readonly ConcurrentDictionary<long, bool> currentJobs;
         protected CancellationToken cancellationToken;
-        private readonly TimeSpan interval;
-        protected JobStatusManager(IPaymentLogger logger, IUnitOfWorkScopeFactory scopeFactory, IJobServiceConfiguration configuration)
+
+        protected JobStatusManager(IPaymentLogger logger, IUnitOfWorkScopeFactory scopeFactory,
+            IJobServiceConfiguration configuration)
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this.scopeFactory = scopeFactory ?? throw new ArgumentNullException(nameof(scopeFactory));
@@ -34,26 +36,40 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing
 
         public Task Start(string partitionEndpointName, CancellationToken suppliedCancellationToken)
         {
-            this.cancellationToken = suppliedCancellationToken;
+            cancellationToken = suppliedCancellationToken;
             return Run(partitionEndpointName);
+        }
+
+
+        public void StartMonitoringJob(long jobId, JobType jobType)
+        {
+            currentJobs.AddOrUpdate(jobId, false, (key, status) => status);
         }
 
         private async Task Run(string partitionEndpointName)
         {
             await LoadExistingJobs();
             while (!cancellationToken.IsCancellationRequested)
-            {
-                var tasks = currentJobs.Select(job => CheckJobStatus(partitionEndpointName, job.Key)).ToList();
-                await Task.WhenAll(tasks);
-                var completedJobs = currentJobs.Where(item => item.Value).ToList();
-                foreach (var completedJob in completedJobs)
+                try
                 {
-                    logger.LogInfo($"Found completed job.  Will now stop monitoring job: {completedJob.Key}, ThreadId {Thread.CurrentThread.ManagedThreadId}, PartitionId {partitionEndpointName}");
-                    if (!currentJobs.TryRemove(completedJob.Key, out _))
-                        logger.LogWarning($"Couldn't remove completed job from jobs list. ThreadId {Thread.CurrentThread.ManagedThreadId}, PartitionId {partitionEndpointName},  Job: {completedJob.Key}, status: {completedJob.Value}");
+                    var tasks = currentJobs.Select(job => CheckJobStatus(partitionEndpointName, job.Key)).ToList();
+                    await Task.WhenAll(tasks);
+                    var completedJobs = currentJobs.Where(item => item.Value).ToList();
+                    foreach (var completedJob in completedJobs)
+                    {
+                        logger.LogInfo(
+                            $"Found completed job.  Will now stop monitoring job: {completedJob.Key}, ThreadId {Thread.CurrentThread.ManagedThreadId}, PartitionId {partitionEndpointName}");
+                        if (!currentJobs.TryRemove(completedJob.Key, out _))
+                            logger.LogWarning(
+                                $"Couldn't remove completed job from jobs list. ThreadId {Thread.CurrentThread.ManagedThreadId}, PartitionId {partitionEndpointName},  Job: {completedJob.Key}, status: {completedJob.Value}");
+                    }
+
+                    await Task.Delay(interval, cancellationToken);
                 }
-                await Task.Delay(interval, cancellationToken);
-            }
+                catch (Exception ex)
+                {
+                    logger.LogError($"Failed to check job status. Error: {ex.Message}", ex);
+                }
         }
 
         private async Task LoadExistingJobs()
@@ -64,10 +80,7 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing
                 {
                     var jobStorage = scope.Resolve<IJobStorageService>();
                     var jobs = await GetCurrentJobs(jobStorage);
-                    foreach (var job in jobs)
-                    {
-                        StartMonitoringJob(job, JobType.EarningsJob);
-                    }
+                    foreach (var job in jobs) StartMonitoringJob(job, JobType.EarningsJob);
                 }
             }
             catch (Exception e)
@@ -76,13 +89,14 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing
             }
         }
 
-      
 
         private async Task CheckJobStatus(string partitionEndpointName, long jobId)
         {
             try
             {
-                using (var scope = scopeFactory.Create($"CheckJobStatus:{jobId}, ThreadId {Thread.CurrentThread.ManagedThreadId}, PartitionId {partitionEndpointName}"))
+                using (var scope =
+                       scopeFactory.Create(
+                           $"CheckJobStatus:{jobId}, ThreadId {Thread.CurrentThread.ManagedThreadId}, PartitionId {partitionEndpointName}"))
                 {
                     try
                     {
@@ -95,13 +109,16 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing
                     catch (Exception ex)
                     {
                         scope.Abort();
-                        logger.LogWarning($"Failed to update job status for job: {jobId}, ThreadId {Thread.CurrentThread.ManagedThreadId}, PartitionId {partitionEndpointName} Error: {ex.Message}. {ex}");
+                        logger.LogWarning(
+                            $"Failed to update job status for job: {jobId}, ThreadId {Thread.CurrentThread.ManagedThreadId}, PartitionId {partitionEndpointName} Error: {ex.Message}. {ex}");
                     }
                 }
             }
             catch (Exception e)
             {
-                logger.LogError($"Failed to create or abort the scope of the state manager transaction for: {jobId}, ThreadId {Thread.CurrentThread.ManagedThreadId}, PartitionId {partitionEndpointName} Error: {e.Message}", e);
+                logger.LogError(
+                    $"Failed to create or abort the scope of the state manager transaction for: {jobId}, ThreadId {Thread.CurrentThread.ManagedThreadId}, PartitionId {partitionEndpointName} Error: {e.Message}",
+                    e);
                 throw;
             }
         }
@@ -109,12 +126,5 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing
         public abstract IJobStatusService GetJobStatusService(IUnitOfWorkScope scope);
 
         public abstract Task<List<long>> GetCurrentJobs(IJobStorageService jobStorage);
-        
-       
-
-        public void StartMonitoringJob(long jobId, JobType jobType)
-        {
-            currentJobs.AddOrUpdate(jobId, false, (key, status) => status);
-        }
     }
 }
