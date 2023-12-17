441569999462f0248827f705631f182efc34c2b1
Owen <ocampbell.personal@gmail.com>
2023-12-17 13:47:33 +0000

    Payments.Application

--- src/SFA.DAS.Payments.Application/Infrastructure/Logging/VersionInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SFA.DAS.Payments.Application.Infrastructure.Logging
-{
-    public class VersionInfo : IVersionInfo
-    {
-        public string ServiceReleaseVersion { get; set; } = "1.000";
-    }
-
-}
