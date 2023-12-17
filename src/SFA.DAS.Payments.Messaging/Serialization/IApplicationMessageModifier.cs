810364be6819a4de79fc46be88e237e0972f9208
Owen <ocampbell.personal@gmail.com>
2023-12-17 12:58:52 +0000

    Messaging

--- src/SFA.DAS.Payments.Messaging/Serialization/IApplicationMessageModifier.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SFA.DAS.Payments.Messaging.Serialization
-{
-    public interface IApplicationMessageModifier
-    {
-        object Modify(object applicationMessage);
-    }
-}
--- src/SFA.DAS.Payments.Messaging/Serialization/IMessageDeserializer.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Azure.ServiceBus;
-
-namespace SFA.DAS.Payments.Messaging.Serialization
-{
-    public interface IMessageDeserializer
-    {
-        object DeserializeMessage(Message message);
-    }
-}
