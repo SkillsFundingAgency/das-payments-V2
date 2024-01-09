c77baaa355948d14ab5bd36b80698a64ee9a7262
abroodie <abroodie@gmail.com>
2024-01-09 10:19:46 +0000

    added unit tests for job status manager

--- src/SFA.DAS.Payments.Monitoring.Jobs.Application.UnitTests/JobStatusManagerTests.cs
+++ src/SFA.DAS.Payments.Monitoring.Jobs.Application.UnitTests/JobStatusManagerTests.cs
@@ -7,7 +7,6 @@ using NUnit.Framework;
 using SFA.DAS.Payments.Application.Infrastructure.UnitOfWork;
 using SFA.DAS.Payments.Monitoring.Jobs.Application.Infrastructure.Configuration;
 using SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing;
-using SFA.DAS.Payments.Monitoring.Jobs.Application.JobProcessing.Earnings;
 using SFA.DAS.Payments.Monitoring.Jobs.Model;
 
 namespace SFA.DAS.Payments.Monitoring.Jobs.Application.UnitTests
@@ -16,41 +15,65 @@ namespace SFA.DAS.Payments.Monitoring.Jobs.Application.UnitTests
     public class JobStatusManagerTests
     {
         private AutoMock mocker;
+        private Mock<IJobStatusService> mockStatusService;
 
         [SetUp]
         public void SetUp()
         {
             mocker = AutoMock.GetLoose();
-            var mockStatusService = mocker.Mock<IEarningsJobStatusService>();
+            mockStatusService = mocker.Mock<IJobStatusService>();
             mockStatusService.Setup(x => x.ManageStatus(It.IsAny<long>(), It.IsAny<CancellationToken>()))
                 .ReturnsAsync(false);
             var mockScope = mocker.Mock<IUnitOfWorkScope>();
-            mockScope.Setup(x => x.Resolve<IEarningsJobStatusService>())
-                .Returns(mockStatusService.Object);
             mocker.Mock<IUnitOfWorkScopeFactory>()
                 .Setup(x => x.Create(It.IsAny<string>()))
                 .Returns(mockScope.Object);
             mocker.Mock<IJobServiceConfiguration>()
                 .Setup(x => x.JobStatusInterval)
                 .Returns(TimeSpan.FromMilliseconds(100));
+            mocker.Mock<IJobStatusServiceFactory>()
+                .Setup(x => x.Create(It.IsAny<IUnitOfWorkScope>(), It.IsAny<JobType>()))
+                .Returns(mockStatusService.Object);
         }
 
-        //[Test]
-        //public async Task Starts_Monitoring_Job()
-        //{
-        //    var manager = mocker.Create<EarningsJobStatusManager>();
-        //    var cancellationTokenSource = new CancellationTokenSource();
-        //    cancellationTokenSource.CancelAfter(10000);
-        //    try
-        //    {
-        //        _ = manager.Start("Test", cancellationTokenSource.Token).ConfigureAwait(false);
-        //        manager.StartMonitoringJob(99, JobType.EarningsJob);
-        //        await Task.Delay(1000, cancellationTokenSource.Token).ConfigureAwait(false);
-        //    }
-        //    catch (TaskCanceledException) { }
-        //    catch (OperationCanceledException) { }
-        //    mocker.Mock<IEarningsJobStatusService>()
-        //        .Verify(svc => svc.ManageStatus(It.Is<long>(id => id == 99), It.IsAny<CancellationToken>()));
-        //}
+        [Test]
+        public async Task Checks_Status_Of_Job()
+        {
+            var manager = mocker.Create<JobStatusManager>();
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.CancelAfter(10000);
+            try
+            {
+                _ = manager.Start("Test", cancellationTokenSource.Token).ConfigureAwait(false);
+                await Task.Delay(200, cancellationTokenSource.Token).ConfigureAwait(false);
+                manager.StartMonitoringJob(99, JobType.EarningsJob);
+                await Task.Delay(1000, cancellationTokenSource.Token).ConfigureAwait(false);
+            }
+            catch (TaskCanceledException) { }
+            catch (OperationCanceledException) { }
+            mockStatusService
+                .Verify(svc => svc.ManageStatus(It.Is<long>(id => id == 99), It.IsAny<CancellationToken>()));
+        }
+
+        [Test]
+        public async Task Does_Not_Check_Job_After_It_Has_Completed()
+        {
+            mockStatusService.Setup(x => x.ManageStatus(It.IsAny<long>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+            var manager = mocker.Create<JobStatusManager>();
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.CancelAfter(10000);
+            try
+            {
+                _ = manager.Start("Test", cancellationTokenSource.Token).ConfigureAwait(false);
+                await Task.Delay(200, cancellationTokenSource.Token).ConfigureAwait(false);
+                manager.StartMonitoringJob(99, JobType.EarningsJob);
+                await Task.Delay(1000, cancellationTokenSource.Token).ConfigureAwait(false);
+            }
+            catch (TaskCanceledException) { }
+            catch (OperationCanceledException) { }
+            mockStatusService
+                .Verify(svc => svc.ManageStatus(It.Is<long>(id => id == 99), It.IsAny<CancellationToken>()),Times.Once);
+        }
     }
 }
