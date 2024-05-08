1351ab1cda0ac382c54b73d7ac4f50670ae20e5e
abroodie <abroodie@gmail.com>
2024-05-08 15:48:12 +0100

    added failing test for Price_Episode_Not_Found_In_Current_Year_Returns_New

--- src/SFA.DAS.Payments.DataLocks.Domain.UnitTests/Services/PriceEpisodeStatusCalculatorTests.cs
+++ src/SFA.DAS.Payments.DataLocks.Domain.UnitTests/Services/PriceEpisodeStatusCalculatorTests.cs
@@ -15,7 +15,7 @@ namespace SFA.DAS.Payments.DataLocks.Domain.UnitTests.Services
     public class PriceEpisodeStatusCalculatorTests
     {
         private AutoMock mocker;
-
+         
         [SetUp]
         public void SetUp()
         {
@@ -51,7 +51,86 @@ namespace SFA.DAS.Payments.DataLocks.Domain.UnitTests.Services
 
             };
 
-            calc.DetermineStatus(priceEpisode, earnings, new List<PriceEpisodeStatusChange>()).Should().Be(PriceEpisodeStatus.New);
+            calc.DetermineStatus(2324, priceEpisode, earnings, new List<PriceEpisodeStatusChange>()).Should().Be(PriceEpisodeStatus.New);
+        }
+
+        [Test]
+        public void Price_Episode_Not_Found_In_Previous_Price_Episodes_Returns_New()
+        {
+            var calc = mocker.Create<PriceEpisodeStatusCalculator>();
+
+            var priceEpisode = new PriceEpisode
+            {
+                Identifier = "1234",
+                AgreedPrice = 1000, 
+            };
+
+            var earnings = new List<OnProgrammeEarning>
+            {
+                new OnProgrammeEarning
+                {
+                    
+                    Type = OnProgrammeEarningType.Learning,
+                    Periods = new ReadOnlyCollection<EarningPeriod>(new List<EarningPeriod>
+                    {
+                        new EarningPeriod
+                        {
+                            AccountId = 12, Amount = 100, ApprenticeshipId = 1, Period = 1,
+                            DataLockFailures = new List<DataLockFailure>()
+                        }
+                    })
+                }
+            };
+
+            var previousPriceEpisodeStatuses = new List<PriceEpisodeStatusChange>
+            {
+                new PriceEpisodeStatusChange
+                {
+                    DataLock = new LegacyDataLockEvent{ PriceEpisodeIdentifier = "4321" , AcademicYear = "2324" }
+                }
+            };
+
+            calc.DetermineStatus(2324,priceEpisode, earnings, previousPriceEpisodeStatuses).Should().Be(PriceEpisodeStatus.New);
+        }
+
+        [Test]
+        public void Price_Episode_Not_Found_In_Current_Year_Returns_New()
+        {
+            var calc = mocker.Create<PriceEpisodeStatusCalculator>();
+
+            var priceEpisode = new PriceEpisode
+            {
+                Identifier = "1234",
+                AgreedPrice = 1000,
+            };
+
+            var earnings = new List<OnProgrammeEarning>
+            {
+                new OnProgrammeEarning
+                {
+
+                    Type = OnProgrammeEarningType.Learning,
+                    Periods = new ReadOnlyCollection<EarningPeriod>(new List<EarningPeriod>
+                    {
+                        new EarningPeriod
+                        {
+                            AccountId = 12, Amount = 100, ApprenticeshipId = 1, Period = 1,
+                            DataLockFailures = new List<DataLockFailure>()
+                        }
+                    })
+                }
+            };
+
+            var previousPriceEpisodeStatuses = new List<PriceEpisodeStatusChange>
+            {
+                new PriceEpisodeStatusChange
+                {
+                    DataLock = new LegacyDataLockEvent{ PriceEpisodeIdentifier = "1234" , AcademicYear = "2223" }, 
+                    AgreedPrice = 1000
+                }
+            };
+
+            calc.DetermineStatus(2324, priceEpisode, earnings, previousPriceEpisodeStatuses).Should().Be(PriceEpisodeStatus.New);
         }
     }
 }
