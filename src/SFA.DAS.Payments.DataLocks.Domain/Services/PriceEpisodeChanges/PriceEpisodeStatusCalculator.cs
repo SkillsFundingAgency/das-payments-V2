2461e1d6c4e35b0907caa24910530f04e7031802
abroodie <abroodie@gmail.com>
2024-05-08 13:45:28 +0100

    Implemented PriceEpisodeStatusCalc behaviour for new PriceEpisodes

--- src/SFA.DAS.Payments.DataLocks.Domain/Services/PriceEpisodeChanges/PriceEpisodeStatusCalculator.cs
+++ src/SFA.DAS.Payments.DataLocks.Domain/Services/PriceEpisodeChanges/PriceEpisodeStatusCalculator.cs
@@ -13,41 +13,46 @@ namespace SFA.DAS.Payments.DataLocks.Domain.Services.PriceEpisodeChanges
     public class PriceEpisodeStatusCalculator
     {
         public List<(string identifier, PriceEpisodeStatus status)> Calculate(
-            IEnumerable<CurrentPriceEpisode> currentPriceEpisodes, 
+            IEnumerable<CurrentPriceEpisode> currentPriceEpisodes,
             IEnumerable<PriceEpisode> receivedPriceEpisodes)
         {
             var matched = receivedPriceEpisodes
                 .Select(x => (x.Identifier, Match(currentPriceEpisodes, x)));
 
-            var receivedIdentifiers = 
+            var receivedIdentifiers =
                 receivedPriceEpisodes.Select(x => x.Identifier);
 
             var removed = currentPriceEpisodes
                 .Select(x => x.PriceEpisodeIdentifier)
                 .Except(receivedIdentifiers)
                 .Select(id => (id, PriceEpisodeStatus.Removed));
-            
+
             return matched.Union(removed).ToList();
         }
 
         public PriceEpisodeStatus DetermineStatus(PriceEpisode priceEpisode, List<OnProgrammeEarning> earnings, List<PriceEpisodeStatusChange> previousPriceEpisodeStatuses)
         {
+            var previousPriceEpisode = previousPriceEpisodeStatuses.FirstOrDefault(previous =>
+                previous.DataLock.PriceEpisodeIdentifier.Equals(priceEpisode.Identifier));
+            if (previousPriceEpisode == null)
+                return PriceEpisodeStatus.New;
+
             throw new NotImplementedException();
         }
-       
+
 
         public PriceEpisodeStatus Match(
-            IEnumerable<CurrentPriceEpisode> recordedPriceEpisodes, 
+            IEnumerable<CurrentPriceEpisode> recordedPriceEpisodes,
             PriceEpisode newPriceEpisode)
         {
             var previous = recordedPriceEpisodes.FirstOrDefault(MatchById(newPriceEpisode));
 
             if (previous == null)
                 return PriceEpisodeStatus.New;
-            
+
             if (previous.AgreedPrice == newPriceEpisode.AgreedPrice)
                 return PriceEpisodeStatus.New;
-            
+
             return PriceEpisodeStatus.Updated;
         }
 
