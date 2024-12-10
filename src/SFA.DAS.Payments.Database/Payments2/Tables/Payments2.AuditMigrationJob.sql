55f079dcc706f06d47115023b1b1f50cf421f17e
abroodie <abroodie@gmail.com>
2024-12-10 14:25:12 +0000

    added new table to record history of adf pipeline

---
+++ src/SFA.DAS.Payments.Database/Payments2/Tables/Payments2.AuditMigrationJob.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [Payments2].[AuditMigrationJob]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_AuditArchiveJob PRIMARY KEY CLUSTERED,
+	[AcademicYear] SMALLINT NOT NULL,
+	[Period] TINYINT NOT NULL,
+	[StartDate] DATETIME2 NULL,
+	[CompletionDate] DATETIME2 NULL,
+	[CreationDate] DATETIME2 NOT NULL DEFAULT sysutcdatetime()
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_AuditMigrationJob__AcademicYear_Period] on [Payments2].[AuditMigrationJob] 
+(
+	[AcademicYear],
+	[Period]
+)
+WITH (ONLINE = ON)
+GO
